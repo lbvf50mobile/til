4c176edeb1c8aa2fe4c688679c35df510fe070d6
Dmitry <lbvf50.mobile@gmail.com>
2018-03-30 23:59:45 +0700

    005. Using ruby -W0, -W1, -W2 keys. -W keys have infulens only on warn but not on .puts

---
+++ 20180330_Friday/20180330_05.rb
@@ -0,0 +1,4 @@
+# using -W0, -W1, -W2 keys in ruby
+puts "this is puts"
+warn "this is warn"
+$stderr.puts 'this is sterr.puts'
---
+++ 20180330_Friday/20180330_05.sh
@@ -0,0 +1,8 @@
+echo "ruby"
+ruby 20180330_05.rb
+echo "ruby -W0"
+ruby -W0 20180330_05.rb
+echo "ruby -W1"
+ruby -W0 20180330_05.rb
+echo "ruby -W2"
+ruby -W0 20180330_05.rb
