49d11e5e76163c1f43e0726d8255a5541f3b8b32
Dmitry <lbvf50.mobile@gmail.com>
2018-04-19 23:28:46 +0700

    Now touch.rb is able to create dir itself.

--- touch.rb
+++ touch.rb
@@ -1,3 +1,7 @@
+# Create directory for today TIL
+dir_name = Time.new.strftime("%Y%m%d_%A")
+puts "Create dir: %s" % dir_name
+system('mkdir ' + dir_name)
 # Create file in current day's dirrecotry, in YYYYDDMM.rb format
 t = Time.new
 file_path = t.strftime("%Y%m%d_%A") + "/" + t.strftime("%Y%m%d.rb")
