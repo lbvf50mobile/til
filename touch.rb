b2b17d0eff7045b7f8d6059d30e3c778176e5542
Dmitry <lbvf50.mobile@gmail.com>
2018-05-16 19:11:29 +0700

    Add possibility to set extention of creting file 'ruby touch.rb sh'

--- touch.rb
+++ touch.rb
@@ -1,9 +1,10 @@
 # Create directory for today TIL
+ext =  ARGV[0] ? ARGV[0] : "rb"
 dir_name = Time.new.strftime("%Y%m%d_%A")
 puts "Create dir: %s" % dir_name
 system('mkdir ' + dir_name)
 # Create file in current day's dirrecotry, in YYYYDDMM.rb format
 t = Time.new
-file_path = t.strftime("%Y%m%d_%A") + "/" + t.strftime("%Y%m%d.rb")
+file_path = t.strftime("%Y%m%d_%A") + "/" + t.strftime("%Y%m%d.#{ext}")
 puts "Create file: %s" % file_path
 system("touch " + file_path)
