0af646cd16b854d0f1fd6c9d8a1861eb91fc2896
Dmitry <lbvf50.mobile@gmail.com>
2018-05-23 13:28:44 +0700

    Add -n key for ruby touch.rb

--- touch.rb
+++ touch.rb
@@ -1,10 +1,11 @@
 # Create directory for today TIL
 ext =  ARGV[0] ? ARGV[0] : "rb"
+ext = ('-n' ===  ext) ? "" : ".#{ext}"
 dir_name = Time.new.strftime("%Y%m%d_%A")
 puts "Create dir: %s" % dir_name
 system('mkdir ' + dir_name)
 # Create file in current day's dirrecotry, in YYYYDDMM.rb format
 t = Time.new
-file_path = t.strftime("%Y%m%d_%A") + "/" + t.strftime("%Y%m%d.#{ext}")
+file_path = t.strftime("%Y%m%d_%A") + "/" + t.strftime("%Y%m%d#{ext}")
 puts "Create file: %s" % file_path
 system("touch " + file_path)
