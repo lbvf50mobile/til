27903e5b810b03f6090dc9397e261757b87642f6
Dmitry <lbvf50.mobile@gmail.com>
2018-05-31 23:56:48 +0700

    073. if/if bash

---
+++ 20180531_Thursday/20180531.sh
@@ -0,0 +1,13 @@
+# if/fi bash
+
+if [ -d 20180531_Thursday ]; then
+    echo "20180531_Thursday is dir"
+    else
+    echo "20180531_Thursday is not dir"
+fi
+
+if [ -f 20180531_Thursday ]; then
+    echo "20180531_Thursday is file"
+    else
+    echo "20180531_Thursday is not file"
+fi
