52ff881daed492b2c6b0f85d44276ed4a684691a
Dmitry <lbvf50.mobile@gmail.com>
2018-06-05 22:52:46 +0700

    075. Echo the arguments (Positional Parameters).

---
+++ 20180605_Tuesday/20180605.sh
@@ -0,0 +1,8 @@
+# Echo the arguments (Positional Parameters).
+# https://stackoverflow.com/a/29258644
+
+# $ alias x='20180605_Tuesday/20180605.sh the test'
+echo "this is 0: "$0
+echo "this is 1: "$1
+echo "this is 2: "$2
+echo "this is ?: "$?
