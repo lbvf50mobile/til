b7b37e9fbf8a99efee0285ea229ba74b975acea4
Dmitry <lbvf50.mobile@gmail.com>
2018-06-09 23:33:38 +0700

    Add line into the touch when shell script

--- touch.rb
+++ touch.rb
@@ -9,3 +9,9 @@ t = Time.new
 file_path = t.strftime("%Y%m%d_%A") + "/" + t.strftime("%Y%m%d#{ext}")
 puts "Create file: %s" % file_path
 system("touch " + file_path)
+if '.sh' == ext
+    # Why? quotes
+    #system("echo \"echo #{file_path} 2\" > #{file_path}")
+    File.open(file_path, 'w'){|f| f.write("echo #{file_path}") }
+    system("chmod u+x #{file_path}")
+end
