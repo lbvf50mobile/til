2c6c23a71b36c23908a59d6a7cb505fa14ea70a0
Dmitry <lbvf50.mobile@gmail.com>
2018-06-20 22:16:47 +0700

    086.5 Bash extrakt keys. (fix)

--- 20180620_Wednesday/20180620.sh
+++ 20180620_Wednesday/20180620.sh
@@ -3,6 +3,6 @@
 
 echo "alias x='bash 20180620_Wednesday/20180620.sh'"
 
-20180620_Wednesday/run.sh  -o one -t two position
+20180620_Wednesday/run.sh  -o one -t two position1 position2 position3
 
 
--- 20180620_Wednesday/run.sh
+++ 20180620_Wednesday/run.sh
@@ -3,9 +3,9 @@ red=`tput setaf 1`
 green=`tput setaf 2`
 reset=`tput sgr0`
 
-echo "${green}Parameters:${reset}"
+echo "${green}Parameters \$@:${reset}"
 echo $@
-echo "${green}Dollar Hash:${reset}"
+echo "${green}\$#:${reset}"
 echo $#
 
 
@@ -17,15 +17,31 @@ two=
 arg=
 
 while (("$#")); do
-case "$!" in
+case "$1" in
     -o)
     echo "${green}-o key:${reset} $1"
-    shift 
+    one=$2
+    shift 2
     ;;
-    *) 
-    echo "${red}star:${reset} $1"
+    -t)
+    echo "${green}-t key:${reset} $1"
+    two=$2
+    shift 2
+    ;;
+    *) # Preser positional arumetns
+    P1="$P1 $1"
     shift 
     ;;
     esac
 done
+# set positional arguments in their proper place
+echo "${red}eval set -- \"\$P1\"${reset}"
+
+eval set -- "$P1"
+
+echo "${green}One:${reset} $one"
+echo "${green}Two:${reset} $two"
+echo "${green}\$1:${reset} $1"
+echo "${green}\$2:${reset} $2"
+echo "${green}\$3:${reset} $3"
 
