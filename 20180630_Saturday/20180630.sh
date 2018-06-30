9754db06cd911432e5f97e995d699eeaa512972e
Dmitry <lbvf50.mobile@gmail.com>
2018-07-01 00:54:53 +0700

    Now it start working

--- 20180630_Saturday/20180630.sh
+++ 20180630_Saturday/20180630.sh
@@ -1,16 +1,16 @@
 echo "alias x='bash 20180630_Saturday/20180630.sh'" 
-export TIMESTAMP_LAST=`date +"%s%N"`
-export TIMESTAMP_NOW=`date +"%s%N"`
+export TIMESTAMP_LAST=`date +"%s"`
+export TIMESTAMP_NOW=`date +"%s"`
 
 
 time for i in {1..10}; do
     (echo "Some test"; sleep 1) |while read -r line ; do
       echo "I: $i"
-      TIMESTAMP_NOW=`date +"%s%N"`
+      TIMESTAMP_NOW=`date +"%s"`
       echo "NOW : $TIMESTAMP_NOW"
       echo "LAST: $TIMESTAMP_LAST"
       echo "DIF : $((${TIMESTAMP_NOW}-${TIMESTAMP_LAST}))"
-      TIMESTAMP_LAST=${TIMESTAMP_NOW}
+      TIMESTAMP_LAST=$TIMESTAMP_NOW
       echo "LAST: $TIMESTAMP_LAST"
     done
 done
