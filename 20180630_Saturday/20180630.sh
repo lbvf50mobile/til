eac042546cf87d1ec0ba6061905a72c783a0df1c
Dmitry <lbvf50.mobile@gmail.com>
2018-07-01 00:21:04 +0700

    Working with interesting script

---
+++ 20180630_Saturday/20180630.sh
@@ -0,0 +1,15 @@
+echo "alias x='bash 20180630_Saturday/20180630.sh'" 
+export TIMESTAMP_LAST=`date +"%s%N"`
+export TIMESTAMP_NOW=`date +"%s%N"`
+
+
+time for i in {1..10}; do
+    (echo "Some test"; sleep 1) |while read -r line ; do
+      echo "I: $i"
+      TIMESTAMP_NOW=`date +"%s%N"`
+      echo "NOW : $TIMESTAMP_NOW"
+      echo "LAST: $TIMESTAMP_LAST"
+      echo "DIF : $((${TIMESTAMP_NOW}-${TIMESTAMP_LAST}))"
+      TIMESTAMP_LAST=${TIMESTAMP_NOW}
+    done
+done
