1264b454fa8b3bcfd6628eacf3608276cb1eb2a5
Dmitry <lbvf50.mobile@gmail.com>
2018-07-04 01:06:43 +0700

    '$ cat /proc/self/stat' is the analog of '$ echo 12676'

---
+++ 20180704_Wednesday/20180704.sh
@@ -0,0 +1,11 @@
+# '$ cat /proc/self/stat' is analog of '$ echo $$'
+# https://www.tldp.org/LDP/Linux-Filesystem-Hierarchy/html/proc.html
+echo "alias x='bash 20180704_Wednesday/20180704.sh'"
+
+echo "1) echo"
+echo $$
+echo "2) cat"
+cat /proc/self/stat
+echo ""
+echo "3) cat from ruby -e"
+ruby -e '`cat /proc/self/stat`'
