6b0e7ecec21f8dd7f6bd12a43a24adcb39d2a325
Dmitry <lbvf50.mobile@gmail.com>
2018-07-10 21:45:26 +0700

    104. Ruby String#%

---
+++ 20180710_Tuesday/20180710.rb
@@ -0,0 +1,9 @@
+# Ruby String#%
+
+# https://apidock.com/ruby/Kernel/sprintf
+# https://ruby-doc.org/core-2.2.0/String.html#method-i-25
+# https://stackoverflow.com/a/4175455/8574922
+
+p "alias x='ruby 20180710_Tuesday/20180710.rb'" 
+
+puts "%02d" % 5
