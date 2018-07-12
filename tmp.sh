5cd4279b6c9264e824a31b8a361f66bb19fc36c7
Dmitry <lbvf50.mobile@gmail.com>
2018-07-12 23:09:33 +0700

    use scp

--- tmp.sh
+++ tmp.sh
@@ -1,4 +1,5 @@
-COOLVAR=77
-set | grep VAR
-ruby -e 'p `set | grep VAR`'
-ruby -e 'p %x{set | grep VAR}'
+
+echo "the content" > cpfile1
+scp -i ~/.ssh/keyfile cpfile1 username@host:cpfile2
+scp -i ~/.ssh/keyfile username@host:cpfile2 cpfile3
+cat cpfile3
