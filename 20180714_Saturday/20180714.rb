b46ecd1bcc38260ea584dff2e02aaaea66ea0f48
Dmitry <lbvf50.mobile@gmail.com>
2018-07-14 02:30:05 +0700

    108.5 String#gsub with a block. And skip a block with do/end.

--- 20180714_Saturday/20180714.rb
+++ 20180714_Saturday/20180714.rb
@@ -1,12 +1,12 @@
-# String#gsub with a block
+# String#gsub with a block.
 p "alias x='ruby 20180714_Saturday/20180714.rb'" 
 
 # "Ruby is a COOL language!" (WIN)
 p "rubyisacoollanguage".gsub(/(ruby)(is)(a)(cool)(language)/) {
-   $1.capitalize + " %s %s %s %s!" % [$2,$3,$4.upcase,$5]
+   p "one " + $1.capitalize + " %s %s %s %s!" % [$2,$3,$4.upcase,$5]
 }
 
 # #<Enumerator: "rubyisacoollanguage":gsub(/(ruby)(is)(a)(cool)(language)/)> (WIN)
 p "rubyisacoollanguage".gsub(/(ruby)(is)(a)(cool)(language)/) do
-    $1.capitalize + " %s %s %s %s!" % [$2,$3,$4.upcase,$5]
+    p "two " + $1.capitalize + " %s %s %s %s!" % [$2,$3,$4.upcase,$5]
 end
