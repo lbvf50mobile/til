6d445024ff0c8fb2aa6e8d23f4306ae861669645
Dmitry <lbvf50.mobile@gmail.com>
2018-07-16 23:43:41 +0700

    109. Start sed.

---
+++ 20180716_Monday/20180716.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Start sed.
+
+# https://quickleft.com/blog/command-line-tutorials-sed-awk/ 
+
+echo "alias x='bash 20180716_Monday/20180716.sh'" 
+echo "Ballo world" | sed s/Ba/He/
