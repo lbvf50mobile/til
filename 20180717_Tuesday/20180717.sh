25f532901d35b182776fe6eef429c31137a1c167
Dmitry <lbvf50.mobile@gmail.com>
2018-07-17 14:08:10 +0700

    Bash Substring Replacement.

---
+++ 20180717_Tuesday/20180717.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Bash Substring Replacement.
+
+# https://www.tldp.org/LDP/abs/html/string-manipulation.html
+
+echo "alias x='bash 20180717_Tuesday/20180717.sh'" 
+
+z="ballo" 
+echo ${z/ba/he}
