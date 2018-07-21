65b7df945a71e9915717851de96a414c42466f01
Dmitry <lbvf50.mobile@gmail.com>
2018-07-21 23:59:30 +0700

    Test cat and file read

--- tmp.rb
+++ tmp.rb
@@ -1,22 +1,11 @@
-# Count how many workds in GREP documentation
+#  what is better cat or File.read
 # https://www.gnu.org/software/grep/manual/grep.html
 
 require 'benchmark'
-require 'colorize'
+repeat = 1000
+Benchmark.bm { |x|
 
-puts "Read lines.".green
+    x.report("cat"){ repeat.times{`cat g.html`}}
+    x.report("File.read"){ repeat.times{File.read('g.html')}}
 
-lines = ""
-time = Benchmark.measure {
-    lines =  File.readlines('g.html')
 }
-puts lines.size
-puts  ("Read all lines: %.2f" % time.real).magenta
-
-words = lines.reduce(:+).split.select{|x| /^[a-zA-Z]+$/ === x}.map{|x| x.downcase}.uniq.sort
-puts "Total words:".green
-p words.size
-puts "First 10 words:".green
-p words[1..10]
-puts "Last 10 words:".green
-p words[-10..-1]
