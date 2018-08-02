f23b6d8bb4643cd64cd1a6983da957b3d8fe2c1a
Dmitry <lbvf50.mobile@gmail.com>
2018-08-02 22:37:01 +0700

    BASH -r and -f (improve for 'test')

--- tmp.sh
+++ tmp.sh
@@ -5,7 +5,8 @@
 # 1) Create -r and -f file
 touch -- -r
 echo 2 > -f
+ls | grep "-"
 [ -f -r ] && echo "File exists"
-# [-f -f ] && echo "And F exists" # how?
+[ -f '-f' ] && echo "And F exists" || echo "No file" # how?
 # 2) delete this fis
 rm -- -r -f
