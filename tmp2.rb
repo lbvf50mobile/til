56ef73cf5c8659b11e9fb7d170f8e1f4f22b3fe8
Dmitry <lbvf50.mobile@gmail.com>
2018-08-03 14:31:55 +0700

    Match left general goin

--- tmp2.rb
+++ tmp2.rb
@@ -1,8 +1,26 @@
-# https://www.codewars.com/kata/form-the-largest/train/ruby
+# Left match general for all strings.
 
-x = [2,1,3]
+def find_left_match (strings_array)
+    answer = nil
+    (strings_array.map{|string| string.size}.min).times do |index|
+        cat_strings = strings_array.map{|string| string[0..index]}
+        if cat_strings[1..-1].all? { |str| str == cat_strings[0]}
+            answer = cat_strings[0]
+        else
+            break
+        end
+    end
+    answer
+end
 
-p x.sort_by(&:-@)
-p x.sort_by{ |a| -a} 
-puts(5.send(:-@))
-puts(-5)
+
+require "minitest/autorun"
+
+describe "Left match general for all strings." do
+    it "find abc" do
+        assert_equal "abc",find_left_match(%w{abc1 abc2 abc3 abc4})
+        assert_equal "a",find_left_match(%w{a asdsdfsdf a234234 a23423432})
+        assert_equal "a",find_left_match(%w{a})
+        refute_equal "a",find_left_match(%w{b})
+    end
+end
