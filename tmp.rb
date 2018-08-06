0a561d1d2c897934ebf0766319362e100d14fecb
Dmitry <lbvf50.mobile@gmail.com>
2018-08-06 14:31:13 +0700

    reopen the reduce s=index0 x=index1

--- tmp.rb
+++ tmp.rb
@@ -2,17 +2,4 @@ require 'pp'
 require 'colorize'
 
 # Read file
-lines = File.readlines('tmp.html')
-puts "tmp.html lines:".green
-pp lines
-
-# Clear HTML
-clear_html = lines.map{|x| x.gsub(/<[^<>]+>/,"")}
-puts "tmp.html lines without html tags:".green
-pp clear_html
-
-# Save output in file
-# https://stackoverflow.com/a/18900669/8574922
-File.open('tmp_out.html','w') do |f|
-    clear_html.each{ |l| f.write(l)}
-end
+%w{1 2 3}.reduce{|s,x| hash = {s:s,x:x}; p hash }
