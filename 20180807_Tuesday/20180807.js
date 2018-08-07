577458479dd8c40f11be125a3f6135ba95177e0a
Dmitry <lbvf50.mobile@gmail.com>
2018-08-08 03:11:23 +0700

    128. JS Analogs of Math.floor: ~~x, x^0, x|0.

--- 20180807_Tuesday/20180807.js
+++ 20180807_Tuesday/20180807.js
@@ -6,6 +6,7 @@ console.log("alias x='node 20180807_Tuesday/20180807.js'")
 var colors = require('colors');
 
 console.log("Analogs of floor ~~x, x^0, x|0".green)
+console.log(Math.floor(11.2))
 console.log(~~(11.2))
 console.log((11.2)^0)
 console.log((11.2)|0)
