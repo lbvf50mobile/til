fd88951afe28d86087daf59d43374721927ef437
Dmitry <lbvf50.mobile@gmail.com>
2018-08-16 23:00:00 +0700

    rearangeLastN

--- tmp.rb
+++ tmp.rb
@@ -1,8 +1,8 @@
 require 'pp'
 require 'colorize'
-# https://app.codesignal.com/interview-practice/task/XP2Wn9pwZW6hvqH67
+# https://app.codesignal.com/interview-practice/task/5vcioHMkhGqkaQQYt
 
-puts "reverseNodesInKGroups".cyan
+puts "rearangeLastN".green
 
 require "minitest/autorun"
 
@@ -73,104 +73,52 @@ def list_reverse list
 end
 
 
-def reverse_chunk! hash
-    hash[:t].next = nil
-    list_reverse(hash[:h])
-    hash[:h],hash[:t] =  hash[:t],hash[:h]
-    hash
-end
 
-def reverseNodesInKGroups(l,k)
-    return l if 1 == k
+ def rearrangeLastN(l, n)
+    return l if l.nil?
     size = list_size(l)
-    # first rotate
-    head = {h: l, t: pointer_to_index(l,k-1)}
-    tail = {h: head[:t].next}
-    reverse_chunk!(head)
-    head[:t].next = tail[:h]
-    size -= k
-    while ( size >= k)
-        center = {h: head[:t].next, t: pointer_to_index(head[:t].next, k-1)}
-        tail = {h: center[:t].next}
-        reverse_chunk!(center)
-        center[:t].next = tail[:h]
-        head[:t].next = center[:h]
-        head[:t] = center[:t]
-        size -= k
-    end
-    head[:h]
-end
- 
+    return l if size <= n || 0 == n 
+    head = {h: l, t: pointer_to_index(l, size - n - 1)}
+    tail = {h: head[:t].next, t: pointer_to_index(head[:t].next, n - 1)}
+    tail[:t].next = head[:h]
+    head[:t].next = nil
+    tail[:h]
+ end
 
 describe "List" do
-  it "should do noting when k = 1" do
-    l = list_from_array((1..5).to_a)
-    k = 1
-    a = reverseNodesInKGroups(l, k)
-    assert_equal (1..5).to_a, array_from_list(a)
-  end
-
-  it "Need just rotate all array" do
-    l = list_from_array((1..5).to_a)
-    k = 5
-    a = reverseNodesInKGroups(l, k)
-    assert_equal (1..5).to_a.reverse, array_from_list(a)
-  end
-
-  it "Need just rotate all array size 2" do
-    l = list_from_array([1,2])
-    k = 2
-    a = reverseNodesInKGroups(l, k)
-    assert_equal [2,1], array_from_list(a)
-  end
-
-  it "Need to rotate one chunk" do
-    l = list_from_array([1,2,3])
-    k = 2
-    a = reverseNodesInKGroups(l, k)
-    assert_equal [2,1,3], array_from_list(a)
-  end
-
-  it "Need to rotate two chunks" do
-    l = list_from_array([1,2,3,4])
-    k = 2
-    a = reverseNodesInKGroups(l, k)
-    assert_equal [2,1,4,3], array_from_list(a)
-  end
-
-  it "Need to rotate two chunks and it has tail" do
-    l = list_from_array([1,2,3,4,5])
-    k = 2
-    a = reverseNodesInKGroups(l, k)
-    assert_equal [2,1,4,3,5], array_from_list(a)
-  end
-
-  it "Need to rotate 3 chunks " do
-    l = list_from_array([1,2,3,4,5,6])
-    k = 2
-    a = reverseNodesInKGroups(l, k)
-    assert_equal [2,1,4,3,6,5], array_from_list(a)
-  end
-  it "Need to rotate 4 chunks " do
-    l = list_from_array([1,2,3,4,5,6,7,8])
-    k = 2
-    a = reverseNodesInKGroups(l, k)
-    assert_equal [2,1,4,3,6,5,8,7], array_from_list(a)
-  end
-  it "Need to rotate 4 chunks and tail" do
-    l = list_from_array([1,2,3,4,5,6,7,8,9])
-    k = 2
-    a = reverseNodesInKGroups(l, k)
-    assert_equal [2,1,4,3,6,5,8,7,9], array_from_list(a)
-  end
-
-
-  it "solve frims example" do
-    l = list_from_array((1..5).to_a)
-    k = 2
-    a = reverseNodesInKGroups(l, k)
-    assert_equal [2, 1, 4, 3, 5], array_from_list(a)
-  end
-  
-    
+    it "should implement it's tast with s=5 n=1" do
+        l = list_from_array([1,2,3,4,5])
+        n = 1
+        assert_equal [ 5, 1, 2, 3, 4], array_from_list(rearrangeLastN(l, n))
+    end
+    it "should implement it's tast with s=5 n=2" do
+        l = list_from_array([1,2,3,4,5])
+        n = 2
+        assert_equal [4, 5, 1, 2, 3], array_from_list(rearrangeLastN(l, n))
+    end
+    it "should implement it's tast with s=5 n=3" do
+        l = list_from_array([1,2,3,4,5])
+        n = 3
+        assert_equal [3, 4, 5, 1, 2], array_from_list(rearrangeLastN(l, n))
+    end
+    it "should implement it's tast with s=5 n=4" do
+        l = list_from_array([1,2,3,4,5])
+        n = 4
+        assert_equal (2..5).to_a + [1], array_from_list(rearrangeLastN(l, n))
+    end
+    it "should implement it's tast with s=5 n=4" do
+        l = list_from_array([1,2,3,4,5])
+        n = 5
+        assert_equal (1..5).to_a , array_from_list(rearrangeLastN(l, n))
+    end
+    it "should implement it's tast with s=5 n=0" do
+        l = list_from_array([1,2,3,4,5])
+        n = 0
+        assert_equal (1..5).to_a , array_from_list(rearrangeLastN(l, n))
+    end
+    it "should implement it's tast with nil n=0" do
+        l = nil
+        n = 0
+        assert_nil rearrangeLastN(l, n)
+    end
 end
