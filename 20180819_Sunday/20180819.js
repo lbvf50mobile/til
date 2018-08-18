4d2c2c0020d8752635c7797a7b3bdaaadb636e0f
Dmitry <lbvf50.mobile@gmail.com>
2018-08-19 01:17:53 +0700

    137.7 (fix) Rest parameter. Extended parameter handling.

--- 20180819_Sunday/20180819.js
+++ 20180819_Sunday/20180819.js
@@ -1,7 +1,7 @@
 console.log("alias x='node 20180819_Sunday/20180819.js'")
 
 // ES6 Rest parameter. Extended parameter handling.
-// http://es6-features.org/#SpreadOperator
+// http://es6-features.org/#RestParameter
 
 var colors = require('colors');
 const f = (...z) => console.log(z.toString().green)
