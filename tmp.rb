46317c31d0699297f4064b2478625e1b5841faff
Dmitry <lbvf50.mobile@gmail.com>
2018-08-19 17:06:38 +0700

    containCloseNums (optimized)

--- tmp.rb
+++ tmp.rb
@@ -5,23 +5,15 @@ puts "containCloseNums".blue
 
 require "minitest/autorun"
 
-def delta_less_than_or_equal(array:nil,element:nil, delta: nil)
-    array.each do |x|
-        return true if delta >= (x-element).abs
-    end
-    false
-end
 
 def containsCloseNums(nums, k)
     hash = {}
     nums.each_with_index do |value,index|
         if hash[value]
-            return true if delta_less_than_or_equal(array: hash[value], 
-                element: index,
-                delta: k)
-            hash[value].push(index)
+            return true if k >= (hash[value] - index).abs
+            hash[value] = index
         else
-            hash[value] = [index]
+            hash[value] = index
         end
     end
     false
@@ -31,10 +23,7 @@ describe "Hash" do
     it "true" do
         assert true
     end
-    it "delta  less than or equal x" do
-        assert delta_less_than_or_equal(array:[1,2,3],element:5, delta: 2)
-        refute delta_less_than_or_equal(array:[1,2,3],element:55,delta: 7)
-    end
+  
     it "should work with first exaple" do
         nums = [0, 1, 2, 3, 5, 2]
         k = 3
