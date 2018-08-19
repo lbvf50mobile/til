bc3e595737bd19f2713bbaf0a4fa84327acc7ff7
Dmitry <lbvf50.mobile@gmail.com>
2018-08-19 17:02:36 +0700

    conainCloseNums first atempt

--- tmp.rb
+++ tmp.rb
@@ -1,48 +1,48 @@
 require 'pp'
 require 'colorize'
-# https://app.codesignal.com/interview-practice/task/3PcnSKuRkqzp8F6BN/solutions
-puts "areFollowingPatterns".red
+# https://app.codesignal.com/interview-practice/task/njfXsvjRthFKmMwLC
+puts "containCloseNums".blue
 
 require "minitest/autorun"
 
-require 'set'
-def areFollowingPatterns(strings, patterns)
-    return false if strings.size != patterns.size
+def delta_less_than_or_equal(array:nil,element:nil, delta: nil)
+    array.each do |x|
+        return true if delta >= (x-element).abs
+    end
+    false
+end
+
+def containsCloseNums(nums, k)
     hash = {}
-    values = Set.new
-    patterns.zip(strings).each do |pattern,string|
-        if(hash[pattern])
-            return false if hash[pattern] != string
+    nums.each_with_index do |value,index|
+        if hash[value]
+            return true if delta_less_than_or_equal(array: hash[value], 
+                element: index,
+                delta: k)
+            hash[value].push(index)
         else
-            return false if values.include?(string)
-            hash[pattern] = string
-            values.add(string)
+            hash[value] = [index]
         end
     end
-    true
+    false
 end
 
 describe "Hash" do
     it "true" do
         assert true
     end
-    it "should work with first example" do
-        strings = ["cat", "dog", "dog"]
-        patterns = ["a", "b", "b"]
-        assert areFollowingPatterns(strings, patterns) 
+    it "delta  less than or equal x" do
+        assert delta_less_than_or_equal(array:[1,2,3],element:5, delta: 2)
+        refute delta_less_than_or_equal(array:[1,2,3],element:55,delta: 7)
     end
-    it "should not work with second example" do
-        strings = ["cat", "dogggy", "dog"]
-        patterns = ["a", "b", "b"]
-        refute areFollowingPatterns(strings, patterns) 
+    it "should work with first exaple" do
+        nums = [0, 1, 2, 3, 5, 2]
+        k = 3
+        assert containsCloseNums(nums, k)
     end
-    it "should work with first example" do
-        strings = ["aaa", 
-            "aaa", 
-            "aaa"]
-        patterns = ["aaa", 
-            "bbb", 
-            "aaa"]
-        refute areFollowingPatterns(strings, patterns) 
+    it "should not work with second exaple" do
+        nums = [0, 1, 2, 3, 5, 2]
+        k = 2
+        refute containsCloseNums(nums, k)
     end
 end
