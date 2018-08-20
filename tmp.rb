5dd563070500eab2238985a47368aa6cd68c5494
Dmitry <lbvf50.mobile@gmail.com>
2018-08-20 23:03:53 +0700

    possibleSums first approach very long

--- tmp.rb
+++ tmp.rb
@@ -1,37 +1,62 @@
 require 'pp'
 require 'colorize'
-# https://app.codesignal.com/interview-practice/task/njfXsvjRthFKmMwLC
-puts "containCloseNums".blue
+# https://app.codesignal.com/interview-practice/task/rMe9ypPJkXgk3MHhZ
+puts "possibleSums".green
 
 require "minitest/autorun"
 
-
-def containsCloseNums(nums, k)
-    hash = {}
-    nums.each_with_index do |value,index|
-        if hash[value]
-            return true if k >= (hash[value] - index).abs
-            hash[value] = index
-        else
-            hash[value] = index
-        end
-    end
-    false
+# [5,7],[1,2] ==> [5,7,7]
+def plain_array(coins: [], quantity: [])
+    coins.zip(quantity).map{|value,amount| Array.new(amount,value) }.reduce(:+)
 end
 
+def possibleSums(coins, quantity)
+    sums_hash = {}
+    plain_coins = plain_array(coins: coins, quantity: quantity)
+    (1..plain_coins.size).each do |length|
+        plain_coins.combination(length).each {|x| sums_hash[x.reduce(:+)] = 1 }
+    end
+    sums_hash.size
+end
+
+
+def test_function (coins, quantity, answer)
+    assert_equal answer, possibleSums(coins, quantity)
+end
+
+Tests = [
+    [10, 50, 100],
+    [1, 2, 1],
+    9,
+    [10, 50, 100, 500],
+    [5, 3, 2, 2],
+    122,
+    [1],
+    [5],
+    5,
+    [1, 1],
+    [2, 3],
+    5,
+    #[1, 2],
+    # [50000, 2],
+    # 50004,
+]
+
 describe "Hash" do
-    it "true" do
-        assert true
-    end
-  
-    it "should work with first exaple" do
-        nums = [0, 1, 2, 3, 5, 2]
-        k = 3
-        assert containsCloseNums(nums, k)
-    end
-    it "should not work with second exaple" do
-        nums = [0, 1, 2, 3, 5, 2]
-        k = 2
-        refute containsCloseNums(nums, k)
-    end
+ it "true" do
+    assert true
+ end
+ it "exted array do" do
+    coins = [5,2]
+    quantity = [1,2]
+    assert [5,2,2], plain_array(coins: coins, quantity: quantity)
+ end
+ it "should solve #1 example" do
+    coins = [10, 50, 100] 
+    quantity = [1, 2, 1]
+    assert_equal 9, possibleSums(coins, quantity)
+ end
+ it "should solve tasks automatically" do
+    Tests.each_slice(3){ |coins, quantity, answer| test_function(coins, quantity, answer)}
+ end
 end
