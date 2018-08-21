82ef32168a3bd0abaaa0f03cb12a79452f5d6fd3
Dmitry <lbvf50.mobile@gmail.com>
2018-08-22 00:01:55 +0700

    swapLexOrder working with

--- tmp.rb
+++ tmp.rb
@@ -1,101 +1,36 @@
 require 'pp'
 require 'colorize'
-# https://app.codesignal.com/interview-practice/task/rMe9ypPJkXgk3MHhZ
-puts "possibleSums".green
+# https://app.codesignal.com/interview-practice/task/5vXzdE9yzjsoMZ9sk
+puts "swapLexOrder".red
 
 require "minitest/autorun"
 
 
-
-def possibleSums(coins, quantity)
-    sum_hash = {}
-    h = {}
-    coins.zip(quantity).each{|c,q| h[c] = h[c] ? h[c] + q : q }
-    coins = h.keys
-    quantity = h.values
-    (0..coins.size-1).each{ |n| 
-        rec_solver(coins.drop(n),quantity.drop(n),0,sum_hash)
-    }
-    sum_hash.size
+def swap(arr,how)
+    arr = arr.clone
+    arr[how[0]],arr[how[1]] = arr[how[1]],arr[how[0]]
 end
-def rec_solver(c,q,sum = 0,sum_hash)
-    return false if c.empty?
-    coin,amount = c[0],q[0]
-    (1..amount).each do |x|
-        s = coin*x + sum
-        sum_hash[s] = 1
-        (1..c.size-1).each{ |n| 
-            rec_solver(c.drop(n),q.drop(n),s,sum_hash)
-        }
-    end
-end
-def possibleSums_copy(coins, quantity)
-    set = Hash.new(false)
-    set[0] = true
-    coins.each_with_index do |var, idx|
-        set.keys.each do |key|
-            quantity[idx].times do |i|
-                set[key + var*(i+1)] = true
-            end
-        end
-    end
-    set.size - 1
+def action_hash(str_arr: nil, swap_pair: nil, swap_array: nil)
+    "#{str_arr.inspect},#{swap_pair.inspect},#{swap_array.inspect}"
 end
 
-def possibleSums_iterate(coins, quantity)
-    h = Hash.new(false)
-    h[0] = 1
-    coins.each_with_index do |value, index|
-        h.keys.each do |key|
-            quantity[index].times do |amount|
-                h[key + value*(amount+1)] = 1
-            end 
-        end
-    end
-    h.size - 1
-end
-
-def test_function (coins, quantity, answer)
-    assert_equal answer, possibleSums(coins, quantity)
-end
 
 Tests = [
-    [10, 50, 100],
-    [1, 2, 1],
-    9,
-    [10, 50, 100, 500],
-    [5, 3, 2, 2],
-    122,
-    [1],
-    [5],
-    5,
-    [1, 1],
-    [2, 3],
-    5,
-    [1, 2],
-    [50000, 2],
-    50004,
+
 ]
 
 describe "Hash" do
- it "should work copypast edition" do
-    coins = [10, 50, 100] 
-    quantity = [1, 2, 1]
-    assert_equal possibleSums(coins, quantity), possibleSums_copy(coins, quantity)
-    assert_equal possibleSums_copy(coins, quantity), possibleSums_iterate(coins, quantity)
+ it "works" do
+    assert true
  end
- it "should use iterate solution" do
-    coins = [10, 50, 100] 
-    quantity = [1, 2, 1]
-    assert_equal possibleSums(coins, quantity), possibleSums_iterate(coins, quantity)
+ it "should swap to elements" do
+    assert_equal [2,1], swap([1,2],[0,1])
  end
-
- it "should solve #1 example" do
-    coins = [10, 50, 100] 
-    quantity = [1, 2, 1]
-    assert_equal 9, possibleSums(coins, quantity)
- end
- it "should solve tasks automatically" do
-    Tests.each_slice(3){ |coins, quantity, answer| test_function(coins, quantity, answer)}
+ it "should create action hash" do
+    str_arr = [?a,?b]
+    swap_pair = [0,1]
+    swap_array =[[0,1],[0,2]]
+    assert_equal "#{str_arr.inspect},#{swap_pair.inspect},#{swap_array.inspect}",
+    action_hash(str_arr: str_arr, swap_pair: swap_pair, swap_array: swap_array)
  end
 end
