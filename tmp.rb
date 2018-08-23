d8bb74bf1b9eda434fcf9e8cd0f02578b4348d4c
Dmitry <lbvf50.mobile@gmail.com>
2018-08-23 12:51:48 +0700

    Generaged dfs

--- tmp.rb
+++ tmp.rb
@@ -9,29 +9,48 @@ require "minitest/autorun"
 require 'benchmark'
 
 
-
-def find(x,nice_hash)
-    nice_hash[x]
-end
-def make(x,nice_hash)
-    nice_hash[x] = x
-end
-def union(x,y,nice_hash)
-    a = find(x,nice_hash)
-    b = find(y,nice_hash)
-    return if a == b
-    tmp = (a+b).sort.uniq
-    nice_hash[x] = tmp
-    nice_hash[y] = tmp
-end
-
-def dsu(u)
-    nice_hash = {}
-    u.flatten.each{|x| make([x],nice_hash)}
+def adjacency_list(u)
+    aj = {}
     u.each do |x,y|
-        union([x],[y],nice_hash)
+        aj[x] = aj[x] ? aj[x].push(y) : [y]
+        aj[y] = aj[y] ? aj[y].push(x) : [x]
     end
-    nice_hash.values.uniq.map{|x| x.sort}
+    aj
+end
+def dfs(v,used,comp,vertex,aj)
+    used[v] = 1
+    comp.push(v)
+    vertex.each do |to|
+        dfs(to,used,comp,vertex,aj) if used[to].nil?
+    end
+end
+def dsu(u)
+    aj = adjacency_list(u)
+    vertex = aj.keys
+    used = {}
+    connected = []
+    puts "aj: #{aj}".cyan
+    vertex.each do |v|
+        puts "v: #{v} used: #{used}"
+        if used[v].nil?
+            comp = []
+            dfs(v,used,comp,vertex,aj)
+            connected.push(comp.sort)
+            puts "comp: #{comp}".cyan
+        end
+    end
+    connected
+end
+
+def sort_index(str,pairs)
+    # Save array of [indexes,char_by_these_indexes]
+    pairs = pairs.map do |ids|
+        chars = ids.reduce([]){|s,i| s.push(str[i-1]); s}.sort.reverse
+        ids.each_with_index do |pos,i|
+            str[pos-1] = chars[i]
+        end
+    end
+    str.join
 end
 def dsu_my(u)
     (0..u.size-1).each do |i|
@@ -46,16 +65,6 @@ def dsu_my(u)
         end
     end
     u.compact.map{|x| x.sort}
-end
-def sort_index(str,pairs)
-    # Save array of [indexes,char_by_these_indexes]
-    pairs = pairs.map do |ids|
-        chars = ids.reduce([]){|s,i| s.push(str[i-1]); s}.sort.reverse
-        ids.each_with_index do |pos,i|
-            str[pos-1] = chars[i]
-        end
-    end
-    str.join
 end
 def swapLexOrder(str, pairs)
     pairs1_2 = dsu(pairs.clone)
@@ -87,68 +96,11 @@ Tests = [
     "abdc",
     [[1, 4], [3, 4]],
     "dbca",
-    "abdc",
-    [[1,4], 
-    [3,4]],
-    "dbca",
-    "abcdefgh",
-    [[1,4], [7,8]],
-    "dbcaefhg",
-    #----------------
-    "acxrabdz",
-    [[1,3], [6,8], [3,8], [2,7]],
-    "zdxrabca",
-    #---------------
-    "z",
-    [],
-    "z",
-    #------------
-    "dznsxamwoj",
-    [[1,2], [3,4], [6,5], [8,10]],
-    "zdsnxamwoj",
-    #---
-    "fixmfbhyutghwbyezkveyameoamqoi",
-    [[8,5], 
- [10,8], 
- [4,18], 
- [20,12], 
- [5,2], 
- [17,2], 
- [13,25], 
- [29,12], 
- [22,2], 
- [17,11]],
- "fzxmybhtuigowbyefkvhyameoamqei",
- "lvvyfrbhgiyexoirhunnuejzhesylojwbyatfkrv",
- [[13,23], 
- [13,28], 
- [15,20], 
- [24,29], 
- [6,7], 
- [3,4], 
- [21,30], 
- [2,13], 
- [12,15], 
- [19,23], 
- [10,19], 
- [13,14], 
- [6,16], 
- [17,25], 
- [6,21], 
- [17,26], 
- [5,6], 
- [12,24]],
- "lyyvurrhgxyzvonohunlfejihesiebjwbyatfkrv"
-
 ]
 
 describe "Hash" do
  it "auto test" do
     Tests.each_slice(3){|str,pairs,ans| assert_equal ans, swapLexOrder(str, pairs)}
- end
- it "auto test" do
-    skip
-    Tests.each_slice(3){|str,pairs,ans| assert_equal ans, swapLexOrder_time(str, pairs)}
  end
 end
 
