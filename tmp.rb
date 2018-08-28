21064decb68adffa314473a200866325fc482703
Dmitry <lbvf50.mobile@gmail.com>
2018-08-28 23:11:53 +0700

    isSubtree fails

--- tmp.rb
+++ tmp.rb
@@ -1,8 +1,8 @@
 require 'pp'
 require 'colorize'
-# https://app.codesignal.com/interview-practice/task/jAKLMWLu8ynBhYsv6
+# https://app.codesignal.com/interview-practice/task/mDpAJnDQkJqaYYsCg
 3.times do puts "" end
-puts "kthSmallestInBST".cyan
+puts "isSubtree".yellow
 puts ""
 
 
@@ -11,15 +11,36 @@ require "minitest/autorun"
 require 'benchmark'
 require 'json'
 
-def dfs(t,a)
-    return a if t.nil?
-    dfs(t.left,a)
-    a.push t.value
-    dfs(t.right,a)
-    return a
+
+def bfs_equal(t1, t2)
+    q1 = []
+    q2 = []
+    q1.push t1
+    q2.push t2
+    while (!q1.empty?) && (!q2.empty?)
+        c1 = q1.pop
+        c2 = q2.pop
+        return false if c1.value != c2.value
+        q1.push c1.left if ! c1.left.nil?
+        q1.push c1.right if ! c1.right.nil?
+        q2.push c2.left if ! c2.left.nil?
+        q2.push c2.right if ! c2.right.nil?
+    end
+    return false if (!q1.empty?) || (!q2.empty?)
+    true
 end
-def kthSmallestInBST(t, k)
-    dfs(t,[])[k-1]
+
+
+def isSubtree(t1, t2)
+    q = []
+    q.push t1
+    while !q.empty?
+        current = q.pop
+        return true if (current.value == t2.value) && bfs_equal(current, t2) 
+        q.push current.left if ! current.left.nil?
+        q.push current.right if ! current.right.nil?
+    end
+    false
 end
 
 
@@ -56,27 +77,64 @@ end
 
 Tests = [
     '{
-        "value": 3,
+        "value": 5,
         "left": {
-            "value": 1,
-            "left": null,
-            "right": null
-        },
-        "right": {
-            "value": 5,
+            "value": 10,
             "left": {
                 "value": 4,
+                "left": {
+                    "value": 1,
+                    "left": null,
+                    "right": null
+                },
+                "right": {
+                    "value": 2,
+                    "left": null,
+                    "right": null
+                }
+            },
+            "right": {
+                "value": 6,
+                "left": null,
+                "right": {
+                    "value": -1,
+                    "left": null,
+                    "right": null
+                }
+            }
+        },
+        "right": {
+            "value": 7,
+            "left": null,
+            "right": null
+        }
+    }',
+    '{
+        "value": 10,
+        "left": {
+            "value": 4,
+            "left": {
+                "value": 1,
                 "left": null,
                 "right": null
             },
             "right": {
-                "value": 6,
+                "value": 2,
+                "left": null,
+                "right": null
+            }
+        },
+        "right": {
+            "value": 6,
+            "left": null,
+            "right": {
+                "value": -1,
                 "left": null,
                 "right": null
             }
         }
     }',
-    4,5,
+    true
 
 ]
 
@@ -86,9 +144,10 @@ Tests = [
 describe "Trees" do
   
     it "auto test" do
-       Tests.each_slice(3)do |json, k, ans|
-            t = hsh2tree(JSON.parse(json)) 
-            assert_equal ans,kthSmallestInBST(t, k)
+       Tests.each_slice(3)do |json1, json2, ans|
+            t1 = hsh2tree(JSON.parse(json1))
+            t2 = hsh2tree(JSON.parse(json1)) 
+            assert_equal ans,isSubtree(t1, t2)
         end
     end
 end
