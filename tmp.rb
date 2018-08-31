e7bf3b9b2f91964aaafdd8ee5647cba49eef027f
Dmitry <lbvf50.mobile@gmail.com>
2018-08-31 22:07:53 +0700

    finsSubstrings Provision for solving

--- tmp.rb
+++ tmp.rb
@@ -1,10 +1,11 @@
 require 'pp'
 require 'colorize'
-# https://app.codesignal.com/interview-practice/task/AaWaYxi8gjtbqgp2M
-# https://www.youtube.com/watch?v=sx8i54fkyk8&t=425s
+# https://app.codesignal.com/interview-practice/task/Ki9zRh5bfRhH6oBau/description
+# https://youtu.be/dUBkaqrcYT8
 3.times do puts "" end
-puts "restoreBinaryTree".green
+puts "findSubstrings".red
 puts ""
+
 
 
 
@@ -15,117 +16,33 @@ require 'json'
 
 require 'ostruct'
 
-def restoreBinaryTree(i, pr)
-    return nil if pr.empty?
-    tree = Tree.new(pr[0])
-    idx = i.index(pr[0])
-    tree.left = restoreBinaryTree(i[0...idx], pr[1..idx])
-    tree.right = restoreBinaryTree(i[idx+1..-1], pr[idx+1..-1])
-    tree
-end
 
 
-def dump()
-    j = '{
-        "value": 1,
-        "left": {
-            "value": 2,
-            "left": {
-                "value": 4,
-                "left": null,
-                "right": null
-            },
-            "right": null
-        },
-        "right": {
-            "value": 3,
-            "left": {
-                "value": 5,
-                "left": null,
-                "right": null
-            },
-            "right": {
-                "value": 6,
-                "left": null,
-                "right": null
-            }
-        }
-    }'
-    h = JSON.parse(j)
-    hsh2tree(h)
+def findSubstrings(words,parts)
+    ["Apple", 
+        "Me[lon]", 
+        "Or[a]nge", 
+        "Water[mel]on"]
 end
 
-class Tree
-   attr_accessor :value, :left, :right
-  def initialize(val)
-    @value = val
-    @left = nil
-    @right = nil
-  end
-  
-end
-
-def hsh2tree(hsh)
-    return nil if hsh.nil?
-    obj = Tree.new(hsh["value"])
-    obj.left = hsh2tree(hsh["left"])
-    obj.right = hsh2tree(hsh["right"])
-    obj
-end
-
-def tree2hsh(tree)
-    return nil if tree.nil?
-    hsh = {"value" => tree.value}
-    hsh["left"] = tree2hsh(tree.left)
-    hsh["right"] = tree2hsh(tree.right)
-    hsh
-end
 
 
 Tests = [
-    [4, 2, 1, 5, 3, 6],
-    [1, 2, 4, 3, 5, 6],
-    ' {
-        "value": 1,
-        "left": {
-            "value": 2,
-            "left": {
-                "value": 4,
-                "left": null,
-                "right": null
-            },
-            "right": null
-        },
-        "right": {
-            "value": 3,
-            "left": {
-                "value": 5,
-                "left": null,
-                "right": null
-            },
-            "right": {
-                "value": 6,
-                "left": null,
-                "right": null
-            }
-        }
-    }',
-    [100000, 99999, 99998],
-    [99999, 100000, 99998],
-    '{
-        "value": 99999,
-        "left": {
-            "value": 100000,
-            "left": null,
-            "right": null
-        },
-        "right": {
-            "value": 99998,
-            "left": null,
-            "right": null
-        }
-    }',
+    ["Apple", 
+    "Melon", 
+    "Orange", 
+    "Watermelon"],
 
+    ["a", 
+    "mel", 
+    "lon", 
+    "el", 
+    "An"],
+
+    ["Apple", 
+    "Me[lon]", 
+    "Or[a]nge", 
+    "Water[mel]on"],
 ]
 
 
@@ -134,10 +51,41 @@ Tests = [
 describe "Trees" do
   
     it "auto test" do
-       Tests.each_slice(3)do |inorder, preorder, ans|
-            tree = restoreBinaryTree(inorder, preorder)
-            r_hash = tree2hsh(tree)
-            assert_equal JSON.parse(ans),r_hash
+       Tests.each_slice(3)do |words, parts, ans|
+            assert_equal ans, findSubstrings(words,parts)
         end
     end
 end
+
+# https://youtu.be/dUBkaqrcYT8
+# Trie is an efficient information retrieval dat structure using which search coplexites can be brought to optimal limit.
+# Storing keys in binary search tree will need time proporitonal to M*logN, where M is maximum string lenght and N is no.
+# keys in tree.
+# Using trie, we can search the key in O(m) time. However the penalty is on trie storage requirements.
+# Insertion in a Trie
+# Every character of input key is inserted as an indiviual trie node.
+# The children is an array of pointers to next level trie nodes
+# Keys refest to the work thaty you are inserting or searchin the trie
+# insert nd search costs O(K) where K is length of key.
+# The memory requirements of trie is O(ALPHABET_SIZE*k*N) where N is numer of keys in trie.
+# ALPHABET_SIZe is the numbr of alphabets i.e. 26.
+# For exmple lets insert the key 'there' in the Trie structure. Initialize all the nodes of the root with NULL.
+# Now, we simply replace the chracter `t` triw node with   a children node with corepsonds to caharcter 't' 
+# (e.g. 3rd node for chracter 'c')
+# Similary keep inserting the charactrs at each level until the end of the keys is reached.
+# Mark the last character's tire node as a leaf node.
+# Rememeber, ll other child nodes for each level will stay NILL.
+# Here every character of the keys is stored in the form of a trie node in the trie structure.
+# the leaf node determines the end of the key.
+# Please note that for this case there is only one child for each node.
+# if the input key is new or an extension of exsiting key, we need to construct node of the key, and makr leaf node.
+# lets extend this trie sturcuture by inserting even more keys.
+# Now, the given trie consists of multiple keys nd we can observer tht with the lelp of leaf nodes.
+# Searching in a Trie
+# While serching we only compare the characrters and move down.
+# The search can terminate doe to end of string, if the value field of last node is non-zeor then
+# the key exist in trie.
+# The search can also temrinate due to lack of keys nodes in trie withoug examing all the characters.
+# now, lets try to search the key which we inserted in this trie, 'there'
+# we start for the root comparing the fist charackter of our key with it's children nodes.
+# on finding the required node we repeat the same process for next character in our key and step down each level.
