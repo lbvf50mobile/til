027bf64665637ed6fd9a034ed4943e1c7e7dc47a
Dmitry <lbvf50.mobile@gmail.com>
2018-09-07 23:21:40 +0700

    140. Ruby each_index

---
+++ 20180907_Friday/20180907.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+
+# Ruby each_index
+
+p "alias x='ruby 20180907_Friday/20180907.rb'"
+
+[10,9,7,5].each_index do |x|
+    p x
+end
--- til_alias.sh
+++ til_alias.sh
@@ -1,1 +1,1 @@
-alias x='ruby 20180821_Tuesday/20180821.rb'
+alias x='ruby 20180907_Friday/20180907.rb'
