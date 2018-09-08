2d510f38372051c17799af7960f742cee5c8a99d
Dmitry <lbvf50.mobile@gmail.com>
2018-09-08 22:29:23 +0700

    find kthLargestElement

--- tmp.rb
+++ tmp.rb
@@ -1,8 +1,8 @@
 require 'pp'
 require 'colorize'
-# https://app.codesignal.com/interview-practice/task/oZXs4td52fsdWC9kR
+# https://app.codesignal.com/interview-practice/task/BG94ZFECSNo6Kv7XW/comments
 3.times do puts "" end
-puts "deleteFromBST".green
+puts "kthLargestElement".cyan
 puts ""
 
 
@@ -17,72 +17,6 @@ require 'ostruct'
 
 
 
-# https://www.geeksforgeeks.org/binary-search-tree-set-2-delete/
-# https://gist.github.com/lbvf50mobile/911c879df1e5e2fef595076ac8efc0a0
-def delete(root: nil, data: nil)
-    if root.nil?
-        return nil
-    elsif data < root.value
-        root.left = delete(root: root.left, data: data)
-    elsif data > root.value
-        root.right = delete(root: root.right, data: data)
-    elsif root.value == data
-        if (!root.left) && (!root.right)
-            root = nil
-        elsif root.right && root.left.nil?
-            root = root.right
-        elsif root.left
-            parent = root 
-            tmp = root.left
-            left = true
-            while tmp.right
-                left = false
-                parent = tmp
-                tmp = tmp.right
-            end
-            root.value = tmp.value
-            if left
-                parent.left = tmp.left
-            else
-                parent.right = tmp.left
-            end
-        end
-    end
-    root
-end
-def deleteFromBST(t,queries)
-    return nil if t.nil?
-    queries.each do |x|
-        t = delete(root:t,data:x)
-    end
-    t
-end
-
-class Tree
-    attr_accessor :value, :left, :right
-   def initialize(val)
-     @value = val
-     @left = nil
-     @right = nil
-   end
-   
-end
- 
- def hsh2tree(hsh)
-     return nil if hsh.nil?
-     obj = Tree.new(hsh["value"])
-     obj.left = hsh2tree(hsh["left"])
-     obj.right = hsh2tree(hsh["right"])
-     obj
- end
- 
- def tree2hsh(tree)
-     return nil if tree.nil?
-     hsh = {"value" => tree.value}
-     hsh["left"] = tree2hsh(tree.left)
-     hsh["right"] = tree2hsh(tree.right)
-     hsh
- end
 
 require_relative 'tests/codesignal_tests.rb'
 Tests = CodeSignalTests.tests
@@ -109,12 +43,7 @@ end
 describe "Trees" do
     it "auto test" do
        Tests.each_slice(3)do |t_json, queries, ans_json|
-            t = hsh2tree(Oj.load(t_json))
-            ans = Oj.load(ans_json)
-            p t_json.scan(/[[:digit:]]+/).size
-            p count_dfs(t)
-            p count_bfs([t])
-            assert_equal ans, tree2hsh(deleteFromBST(t,queries))
+            skip
         end
     end
   
