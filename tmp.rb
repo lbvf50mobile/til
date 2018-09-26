96da93fc7fc09dfd120c10ca3443586157906e92
Dmitry <lbvf50.mobile@gmail.com>
2018-09-27 00:07:51 +0700

    decodeString split by digit and number

--- tmp.rb
+++ tmp.rb
@@ -13,13 +13,51 @@ require 'benchmark'
 require 'oj'
 require 'ostruct'
 
+
+
 def decodeString(s)
     "abababab"
+end
+def convert(x)
+    ans = []
+    while !x.empty? do
+        if /[[:digit:]]/ === x[0]
+            counter = 0 
+            x.chars.each do |z|
+                if /[[:digit:]]/ === z
+                    counter += 1
+                else
+                    break
+                end
+            end
+            tmp = x[0...counter]
+            ans.push tmp.to_i
+            x = x[counter..-1]
+        else
+            counter = 0 
+            x.chars.each do |z|
+                if /[^[[:digit:]]]/ === z
+                    counter += 1
+                else
+                    break
+                end
+            end
+            tmp = x[0...counter]
+            ans.push tmp
+            x = x[counter..-1]
+
+        end
+    end
+    ans
 end
 
 
 describe "decodeString" do
     it "shold works" do
         assert_equal "abababab", decodeString("4[ab]")
+    end
+    it "work with splitter" do
+        assert_equal [44,"zzz",88], convert('44zzz88')
+        # assert_equal [[4,"zz"],"8dd5aa",[7,"[]"]], convert("4[zz]8dd5aa7[[]]")
     end
 end
