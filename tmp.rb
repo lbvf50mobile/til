9ab385caeeee4c2687d2ee7bd82093ae7430d354
Dmitry <lbvf50.mobile@gmail.com>
2018-10-01 23:40:01 +0700

    nextLarger add stack solution for compare

--- tmp.rb
+++ tmp.rb
@@ -26,11 +26,37 @@ def nextLarger(s)
     end
     ans
 end
+# https://app.codesignal.com/interview-practice/task/MdHZFgZFERPPagfdD/solutions/9e3ENgdQQnB7kxTLN
+def nextLargerStack(a)
+    stack = [] 
+    result = []
+    i = 0
+    while i < a.length 
+        while !stack.empty? && a[i] > stack[-1][0]
+            result[stack.pop[1]] = a[i]
+        end 
+        stack.push([a[i],i])
+        i += 1
+    end 
+
+    while !stack.empty? 
+        val = stack.pop
+        result[val[1]] = -1
+    end 
+    result
+end
+
 
 
 
 describe "nextLarger" do
     it "shold works" do
         assert_equal [7, 8, 8, -1], nextLarger([6, 7, 3, 8])
+    end
+    it "should check stack status" do
+        100.times do 
+            arr = (1...(rand(100)+10)).to_a.shuffle
+            assert_equal nextLarger(arr),nextLarger(arr)
+        end
     end
 end
