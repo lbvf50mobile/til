53ac3c47a3fe992e86e62ee3ccc5ccfe823116e1
Dmitry <lbvf50.mobile@gmail.com>
2018-10-06 23:39:52 +0700

    countClouds count coulds using connected component search

--- tmp.rb
+++ tmp.rb
@@ -14,9 +14,52 @@ require 'oj'
 require 'ostruct'
 
 
+# https://e-maxx.ru/algo/connected_components
+def dfs(v,used,adjacency_list)
+    used[v] = true
+    adjacency_list[v].each do |vertex|
+        if used[vertex].nil?
+            dfs(vertex,used,adjacency_list,)
+        end
+    end
+end
+
 
 def countClouds(s)
-    2
+    check_map = [
+        [-1,-1],[0,-1],[+1,-1],
+        [-1,0],[+1,0],
+        [-1,+1],[0,+1],[+1,+1]
+    ]
+    adjacency_list = {}
+    s.each_with_index do |ar,i|
+        ar.each_with_index do |val, j|
+            if "1" == val
+                puts "#{[i,j]}".green
+                adjacency_list[[i,j]] = check_map.reduce([]){|sum, pos|
+                    x = i+pos[0]
+                    y = j+pos[1]
+                    if x>=0 && y >=0 && s[x] && s[x][y] && "1" == s[x][y]
+                        sum.push [x,y]
+                    end
+                    sum
+                 }
+            end
+        end
+    end
+
+    pp adjacency_list
+    n = 0
+    used = {}
+    adjacency_list.each_key do |v|
+        if used[v].nil?
+            n+=1
+            dfs(v,used,adjacency_list)
+        end
+    end
+
+    
+    n
 end
 
 
@@ -30,7 +73,7 @@ describe "countClouds" do
         ['0', '1', '1', '1', '1'],
         ['0', '0', '0', '0', '1'],
         ['1', '0', '0', '1', '1']]
-        assert_equal 2, countClouds(2)
+        assert_equal 2, countClouds(a)
     end
   
 end
