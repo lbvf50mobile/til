2f3fa0f9af9194019002d42cd840ef0baabb837b
Dmitry <lbvf50.mobile@gmail.com>
2018-10-11 23:31:37 +0700

    hasdeadLock undetsted implementation

--- tmp.rb
+++ tmp.rb
@@ -14,8 +14,29 @@ require 'oj'
 require 'ostruct'
 
 # https://www.geeksforgeeks.org/detect-cycle-in-a-graph/
+def is_cycled(i,visited,recStack,s)
+    visited[i] = true
+    recStack[i] = true
+    s[i].each do |neighbour|
+        if visited[neighbour].nil?
+            return true if is_cycled(neighbour,visited,recStack,s)
+        else
+            recStack[neighbour] = true
+            return true
+        end
+    end
+    recStack[i] = nil
+    false 
+end
 def hasDeadlock(s)
-    true
+    visited = []
+    recStack = []
+    s.each_index do |node|
+        if visited[node].nil?
+            return true if is_cycled(i,visited,recStack,s)
+        end
+    end
+    false
 end
 
 describe "countClouds" do
