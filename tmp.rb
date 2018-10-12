a4a9cab32a6b2ba28ddc1d864e52af47307d312d
Dmitry <lbvf50.mobile@gmail.com>
2018-10-12 20:02:05 +0700

    hasDeadlock

--- tmp.rb
+++ tmp.rb
@@ -15,16 +15,12 @@ require 'ostruct'
 
 # https://www.geeksforgeeks.org/detect-cycle-in-a-graph/
 def is_cycled(i,visited, visited_global,s)
-    puts "#{i} - i #{visited} - v"
     visited[i] = true
     visited_global[i] = true
     s[i].each do |neighbour|
-        puts "#{i} hi I'm 4 ".red if i == 4
-        puts "#{neighbour} and i'm pointed to #{visited[neighbour].nil?.inspect}".green if 4 == i
         if visited[neighbour].nil?
             return true if is_cycled(neighbour,visited.clone, visited_global,s)
         else
-            puts "And i going to retun true #{i} #{neighbour}"
             return true
         end
     end
