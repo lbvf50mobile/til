89588803bb07f75faa77063c2f1a1274fbcf1ca3
Dmitry <lbvf50.mobile@gmail.com>
2018-10-12 19:25:02 +0700

    had Deadlock add greedy

--- tmp.rb
+++ tmp.rb
@@ -14,14 +14,15 @@ require 'oj'
 require 'ostruct'
 
 # https://www.geeksforgeeks.org/detect-cycle-in-a-graph/
-def is_cycled(i,visited,s)
+def is_cycled(i,visited, visited_global,s)
     puts "#{i} - i #{visited} - v"
     visited[i] = true
+    visited_global[i] = true
     s[i].each do |neighbour|
         puts "#{i} hi I'm 4 ".red if i == 4
         puts "#{neighbour} and i'm pointed to #{visited[neighbour].nil?.inspect}".green if 4 == i
         if visited[neighbour].nil?
-            return true if is_cycled(neighbour,visited.clone,s)
+            return true if is_cycled(neighbour,visited.clone, visited_global,s)
         else
             puts "And i going to retun true #{i} #{neighbour}"
             return true
@@ -30,10 +31,12 @@ def is_cycled(i,visited,s)
     false 
 end
 def hasDeadlock(s)
-    visited = {}
+    visited_global = {}
     s.each_index do |node|
         visited = {}
-        return true if is_cycled(node,visited,s)
+        if visited_global[node].nil?
+            return true if is_cycled(node,visited, visited_global,s)
+        end
     end
     false
 end
