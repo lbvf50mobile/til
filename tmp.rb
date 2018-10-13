2f5201f2c402ce83414fbfc1f2ff18c632c8966b
Dmitry <lbvf50.mobile@gmail.com>
2018-10-13 23:01:32 +0700

    singlePointOfFailure preparations

--- tmp.rb
+++ tmp.rb
@@ -1,8 +1,8 @@
 require 'pp'
 require 'colorize'
-# https://app.codesignal.com/interview-practice/task/ZTqKqNwK6ZL6GDpJ5/description
+# https://app.codesignal.com/interview-practice/task/qmKLsQcqeEBckLx2q
 3.times do puts "" end
-puts "hasDeadlock".green
+puts "singlePointOfFailure".cyan
 puts ""
 
 
@@ -14,42 +14,14 @@ require 'oj'
 require 'ostruct'
 
 # https://www.geeksforgeeks.org/detect-cycle-in-a-graph/
-def is_cycled(i,visited, visited_global,s)
-    visited[i] = true
-    visited_global[i] = true
-    s[i].each do |neighbour|
-        if visited[neighbour].nil?
-            return true if is_cycled(neighbour,visited.clone, visited_global,s)
-        else
-            return true
-        end
-    end
-    false 
-end
-def hasDeadlock(s)
-    visited_global = {}
-    s.each_index do |node|
-        visited = {}
-        if visited_global[node].nil?
-            return true if is_cycled(node,visited, visited_global,s)
-        end
-    end
-    false
+
+def singlePointOfFailure(connections)
+ 1
 end
 
 describe "countClouds" do
     it "shold works" do
-        a = [[1], 
-        [2], 
-        [3,4], 
-        [4], 
-        [0]]
-        assert hasDeadlock(a)
-        b = [[1,2,3], 
-        [2,3], 
-        [3], 
-        []]
-        refute hasDeadlock(b)
+        a = [[0, 1], [1, 0]]
+        assert_equal 1,singlePointOfFailure(a)
     end
-  
 end
