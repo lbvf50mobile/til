8311becc3c25045540571b6cd2cb2544ca1f724a
Dmitry <lbvf50.mobile@gmail.com>
2018-10-15 21:40:24 +0700

    148. JS module.exports, require and Ruby require_relative. (add comment)

--- 20181015_Monday/20181015.rb
+++ 20181015_Monday/20181015.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-# JS module.exporst, require and Ruby require_relative
+# JS module.exports, require and Ruby require_relative
 
 p "alias x='ruby 20181015_Monday/20181015.rb'" 
 require_relative "one.rb"
