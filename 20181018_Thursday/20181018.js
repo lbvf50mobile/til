3386e670cc4783e6df81c10cfcf5f63b0008c800
Dmitry <lbvf50.mobile@gmail.com>
2018-10-18 23:58:44 +0700

    151. JS using Expect lib

---
+++ 20181018_Thursday/20181018.js
@@ -0,0 +1,7 @@
+// JS using Expect lib.
+console.log("alias x='node 20181018_Thursday/20181018.js'")
+
+// https://github.com/mjackson/expect
+// https://docs.npmjs.com/getting-started/installing-npm-packages-locally
+// using CommonJS modules
+var expect = require('expect')
