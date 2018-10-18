f2e77e6310f12ca88af1c553ecda3139514eabc3
Dmitry <lbvf50.mobile@gmail.com>
2018-10-19 00:28:26 +0700

    151. Js Using Expect lib

--- 20181018_Thursday/20181018.js
+++ 20181018_Thursday/20181018.js
@@ -5,3 +5,5 @@ console.log("alias x='node 20181018_Thursday/20181018.js'")
 // https://docs.npmjs.com/getting-started/installing-npm-packages-locally
 // using CommonJS modules
 var expect = require('expect')
+expect(1).toEqual(1,"this is works")
+//expect(1).toEqual(2,"this is not works")
