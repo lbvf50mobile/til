9a6d27bb8f0bf067aac01eddb52132099ea31163
Dmitry <lbvf50.mobile@gmail.com>
2018-11-04 00:06:16 +0700

    165. JS Array.of()

--- 20181103_Saturday/20181103.js
+++ 20181103_Saturday/20181103.js
@@ -1,5 +1,6 @@
 // JS Array.of()
 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/of
 console.log("alias x='node 20181103_Saturday/20181103.js'")
 
 console.log(Array.of(7))
