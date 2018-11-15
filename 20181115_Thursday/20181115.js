8ffaf756f1a06597c9e44b5f2dfde9e19250767d
Dmitry <lbvf50.mobile@gmail.com>
2018-11-15 23:59:34 +0700

    175. JS Nested object and array dstructuring.

--- 20181115_Thursday/20181115.js
+++ 20181115_Thursday/20181115.js
@@ -1,4 +1,8 @@
+// JS Nested object and array desturcturing.
+
 console.log("alias x='node 20181115_Thursday/20181115.js'");
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment#Object_destructuring
 
 console.log("Nested object and array dstructuring");
 let methadata = {
