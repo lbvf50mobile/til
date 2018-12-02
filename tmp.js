86ad1df2e7b8ac95a3b981db34706b8dbcd9129a
Dmitry <lbvf50.mobile@gmail.com>
2018-12-02 15:44:14 +0700

    arrow function always points to window

--- tmp.js
+++ tmp.js
@@ -1,9 +1,19 @@
-let x = {
+var x = {
     p: 1,
-    m: ()=> console.log(`ARROW: The "p" in x is ${this.p}`)
+    m: ()=> console.log(`ARROW substitute: The "p" in a is ${this.p}`)
 };
+x.m(); // ARROW substitute: The "p" in a is undefined
 
-x.m(); // 1? undefined
+var a = {
+    p: 1,
+    m: ()=> { console.log("A object",p); console.log(this); console.log(`The "p" in a is ${this.p}`);}
+};
+a.m(); // ARROW substitute: The "p" in a is undefined
+var global_one = {
+    p: "--I am a global_one 'p' value--",
+    m: function(){ console.log("global_one object "); console.log(this.p); a.m();}
+}
+global_one.m(); // // ARROW substitute: The "p" in a is undefined.
 
 let z = {
     p: 101,
