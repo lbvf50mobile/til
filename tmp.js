8b9bea04cc2b228d332656dce1d5c201518d1b38
Dmitry <lbvf50.mobile@gmail.com>
2018-12-04 23:17:49 +0700

    JS 'then' quest

--- tmp.js
+++ tmp.js
@@ -1,22 +1,4 @@
-var x = {
-    p: 1,
-    m: ()=> console.log(`ARROW substitute: The "p" in a is ${this.p}`)
-};
-x.m(); // ARROW substitute: The "p" in a is undefined
-
-var a = {
-    p: 1,
-    m: ()=> { console.log("A object",p); console.log(this); console.log(`The "p" in a is ${this.p}`);}
-};
-a.m(); // ARROW substitute: The "p" in a is undefined
-var global_one = {
-    p: "--I am a global_one 'p' value--",
-    m: function(){ console.log("global_one object "); console.log(this.p); a.m();}
-}
-global_one.m(); // // ARROW substitute: The "p" in a is undefined.
-
-let z = {
-    p: 101,
-    m: function(){console.log(`NORMAL: The "p" in z is ${this.p}`)}
-};
-z.m(); // 101
+let music = new Promise((ok,error)=>ok("op!"));
+music.then(n=>{console.log(n + 11); return n + " ca!"}).then(n=>console.log(n+4));
+music.then(n=>console.log(n+ 21));
+music.then(n=>console.log(n+31));
