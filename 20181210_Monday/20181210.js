e055ba6440f29a130d683d4609946259acc193e1
Dmitry <lbvf50.mobile@gmail.com>
2018-12-11 22:03:02 +0700

    196. JS Promise: If onFulfilled return a prommise, the return value of 'then' will be resolved/rejected by the promise.

--- 20181210_Monday/20181210.js
+++ 20181210_Monday/20181210.js
@@ -3,3 +3,34 @@
 console.log("alias x='node 20181210_Monday/20181210.js'");
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/then#Chaining
+
+resolveLater = (resolve,reject) =>{
+    setTimeout(()=>resolve(10),1000);
+};
+
+rejectLater = (resolve,reject) => {
+    setTimeout(()=>reject(new Error('Error')), 1000);
+};
+
+var p1 = Promise.resolve('foo');
+var p2 = p1.then(()=>{
+    // Return promise here, that will be resolved to 10 after 1 second
+    return new Promise(resolveLater);
+});
+p2.then((v)=>{
+    console.log('resolved',v); // "resolved", 10
+},(e)=>{
+    //not called
+    console.log('rejected',e);
+});
+
+var p3 = p1.then(()=>{
+    // Return promise here, that will be rejected with 'Error' after 1 second
+    return new Promise(rejectLater);
+});
+p3.then(v=>{
+    // not called
+    console.log("resolved",v);
+},e =>{
+    console.log("rejected", e); // "rejected", 'Error'
+});
