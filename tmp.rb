9eb9953867d1066023069907c0e70f2e6a4e7f44
Dmitry <lbvf50.mobile@gmail.com>
2018-12-18 03:33:01 +0700

    Ruby ampersand

--- tmp.rb
+++ tmp.rb
@@ -1,5 +1,7 @@
-x_array = [1,4,3,4,5]
-p  x_array.each_cons(2).map{|x0,x1| (x0-x1).abs}.to_a
-p x_array.each_slice(2).map{|x0,x1| (x0-x1).abs}.to_a
-p x_array.combination(2).map{|x0,x1| (x0-x1).abs}.to_a
-p x_array.permutation(2).map{|x0,x1| (x0-x1).abs}.to_a
+# https://gist.github.com/lbvf50mobile/61d322dc9f1a764523d80090c8fa791b
+# https://stackoverflow.com/a/1217114/8574922
+p [[?a,?b],[?c,?d]].map(&:join)
+# &:join is a shortcut for :join.to_proc 
+p [[?a,?b],[?c,?d]].map(&:join.to_proc)
+# &:join.to_proc  equal to Proc.new{|o| o.send :join}
+p [[?a,?b],[?c,?d]].map(&Proc.new{|o| o.send :join})
