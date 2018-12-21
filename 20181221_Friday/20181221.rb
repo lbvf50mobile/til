8c9e53c0a9cdd731883734282b4b0c0beb0aa4fe
Dmitry <lbvf50.mobile@gmail.com>
2018-12-21 21:38:15 +0700

    205. Ruby Class.new{block}, fill array, recursion, String#.next! return same object;

--- 20181221_Friday/20181221.rb
+++ 20181221_Friday/20181221.rb
@@ -1,4 +1,4 @@
-# Ruby Class.new {block}, fill array, recursion;
+# Ruby Class.new {block}, fill array, recursion, String#.next! return same object;
 p "alias x='ruby 20181221_Friday/20181221.rb'" 
 
 # @vladz
@@ -27,3 +27,51 @@ export_class = Class.new do
 
   p 'answer'
   p fun 7
+
+def fun2 n
+  k = 0
+  s = 0
+  while n != 0 do
+    k = k + 1
+    if( 0 == n%2) 
+        s = s + n%10
+    end
+    n = (n / 10.0).to_i
+  end
+    [k,s]
+end
+
+p fun2(300)
+
+# @callhose
+arr = []
+int = '0'
+puts "The: int"
+p int.object_id
+2.times{|_|
+    puts "The int.next!"
+    p int.next!.object_id 
+    arr << int.next!
+}
+p int.object_id == int.next!.object_id # true
+p int.object_id != int.next.object_id # true
+
+# @RomanPa
+s = "12"
+a = []
+a << s
+p a
+s.reverse!
+p a 
+s = "12"
+a = []
+a.push s
+p a
+s.reverse!
+p a 
+s = "12"
+a = []
+a.push s.clone
+p a
+s.reverse!
+p a 
