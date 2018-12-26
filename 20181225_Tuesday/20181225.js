13ae02975b8d0799e8da93120a14af49a6955849
Dmitry <lbvf50.mobile@gmail.com>
2018-12-26 23:43:55 +0700

    208. JS Using promises >> Error Propagation

--- 20181225_Tuesday/20181225.js
+++ 20181225_Tuesday/20181225.js
@@ -26,3 +26,26 @@ doSomething(0)
 .then(r => doThirdThing(r))
 .then(f => console.log(`Final result: ${f}`))
 .catch(failureCallback)
+
+// Basically, a promise chain stopsif there's an exceptions,
+// Looking down the chain for catch handlers instead, this is
+// very much modeletd after how synchronuse code works:
+let syncDoSomething = x =>{
+    if(0.1 > Math.random()){
+        throw new Error("Boom" + (x+1))
+    }
+    return x + 1
+};
+let syncDoSomethingElse = syncDoSomething;
+let syncDoThirdThing = syncDoSomething;
+let syncFailureCallback = e => console.log(e.message);
+
+try {
+    let r = syncDoSomething(0);
+    let r1 = syncDoSomethingElse(r);
+    let r2 = syncDoThirdThing(r1);
+    console.log(`Got the final result: ${r2}`);
+    
+}catch(e){
+    syncFailureCallback(e)
+}
