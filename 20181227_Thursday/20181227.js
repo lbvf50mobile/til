4bcee033f0ca54bad3a57e9fb14d0bf44715d4bd
Dmitry <lbvf50.mobile@gmail.com>
2018-12-29 23:39:24 +0700

    210. Using Promises >> Composition.

--- 20181227_Thursday/20181227.js
+++ 20181227_Thursday/20181227.js
@@ -1,3 +1,17 @@
 // JS Using Promises >> Composition.
 console.log("alias x='node 20181227_Thursday/20181227.js'");
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Using_promises#Composition
+
+// Compositions
+
+// Promise.resolve() and Promise.reject() are shotcuts to manually
+// creat an already resolved or rejected promise respectively. This can be useful at
+// times.
+
+// We can start operations in parallel and wait for them all to fhinish like this:
+let func1 = () => new Promise((resolve) => setTimeout(()=>resolve("10 ms"),10));
+let func2 = () => new Promise((resolve) => setTimeout(()=>resolve("20 ms"),20));
+let func3 = () => new Promise((resolve) => setTimeout(()=>resolve("30 ms"),30));
+
+Promise.all([func1(),func2(),func3()])
+.then(([r1, r2, r3]) => console.log(r1,r2,r3));
