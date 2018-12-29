cbc8732eabf772827481395167e3c604a754c9e9
Dmitry <lbvf50.mobile@gmail.com>
2018-12-29 23:40:52 +0700

    212. JS Using Promises >> Nesting;

---
+++ 20181229_Saturday/20181229.js
@@ -0,0 +1,3 @@
+// JS Using promises >> Nesting;
+console.log("alias x='node 20181229_Saturday/20181229.js'");
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Using_promises#Nesting
--- til_alias.sh
+++ til_alias.sh
@@ -1,1 +1,1 @@
-alias x='node 20181228_Friday/20181228.js'
+alias x='node 20181229_Saturday/20181229.js'
