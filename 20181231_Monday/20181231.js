5cc770b7113c8bbb9988ad3d74835ea3d3ba7fcf
Dmitry <lbvf50.mobile@gmail.com>
2018-12-31 18:32:42 +0700

    213. JS Return Statmente with multy comma separated values

---
+++ 20181231_Monday/20181231.js
@@ -0,0 +1,7 @@
+// JS Return statement with multy comma separated values.
+
+console.log("alias x='node 20181231_Monday/20181231.js'");
+// https://stackoverflow.com/a/10284572
+
+j = () => {return 1,2,3,4,5};
+console.log(j()); // 5
--- til_alias.sh
+++ til_alias.sh
@@ -1,1 +1,1 @@
-alias x='node 20181229_Saturday/20181229.js'
+alias x='node 20181231_Monday/20181231.js'
