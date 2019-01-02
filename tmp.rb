827b6b2c9939eb1f8993ff1a3479cbe248e4df41
Dmitry <lbvf50.mobile@gmail.com>
2019-01-02 21:58:58 +0700

    help for El Anonimo in telegram

--- tmp.rb
+++ tmp.rb
@@ -1,7 +1,6 @@
-# help answering on @vladz quesiton
-def year_per_hour(array_salary_pey_year_in_dollars)
-    work_hours_in_year = 8*5*4*12;
-    p array_salary_pey_year_in_dollars.map{|x| x/work_hours_in_year}
+# help answering on El Anonimo reqeut
+def boom
+    "hi"
 end
 
-year_per_hour([100_000, 160_000])
+p boom == boom() # true
