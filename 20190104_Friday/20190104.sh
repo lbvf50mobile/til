cb5122085fe3c2b27b5e48b453d13950ef62017b
Dmitry <lbvf50.mobile@gmail.com>
2019-01-04 22:14:29 +0700

    214. BASH cp -t; and sorking git

--- 20190104_Friday/20190104.sh
+++ 20190104_Friday/20190104.sh
@@ -5,3 +5,9 @@ echo "alias x='bash 20190104_Friday/20190104.sh'"
 
 # https://www.shellscript.sh/tips/cp-t/
 # https://www.computerhope.com/unix/ucp.htm 
+
+git log -10 --oneline --grep="^2"
+echo "-- SORT --"
+git log -10 --oneline --grep="^2" | ruby -e '
+puts STDIN.read.split("\n").sort{|x,y| y[8..11].to_i <=> x[8..11].to_i}.join("\n")
+'
