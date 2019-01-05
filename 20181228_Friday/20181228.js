b28e3210355cccde446be50dfc3b5c1cdbebb0b8
Dmitry <lbvf50.mobile@gmail.com>
2019-01-05 21:44:20 +0700

    211. JS Using promises >> Timing;

--- 20181228_Friday/20181228.js
+++ 20181228_Friday/20181228.js
@@ -2,3 +2,11 @@
 console.log("alias x='node 20181228_Friday/20181228.js'");
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Using_promises#Timing
+
+// Timing
+
+// To avoid surpirses, function passed to then() will never be called synchronously,
+// even with an already resolved promise:
+
+Promise.resolve().then(()=>console.log(2));
+console.log(1); // 1, 2
