c22d16b50582c363e9f536b180b346d542e8cada
Dmitry <lbvf50.mobile@gmail.com>
2019-01-08 02:16:49 +0700

    215. Ruby Enumerable#gropu_by

--- 20190107_Monday/20190107.rb
+++ 20190107_Monday/20190107.rb
@@ -14,3 +14,13 @@ p [1,1,1,1,1,2,2,2,15,15,20,25,25,40,4,4].group_by{|o| o.send :itself}.values
 p [1,1,1,1,1,2,2,2,15,15,20,25,25,40,4,4].group_by{|o| o.itself}.values  
 p [1,1,1,1,1,2,2,2,15,15,20,25,25,40,4,4].group_by{|o| o }.values
 # https://goo.gl/yyYoHD
+
+# Explanation of Proc and Block for @morozzzko https://goo.gl/sxfb5d
+p :itself.to_proc  # <Proc:0x00000001bf44c8(&:itself)>
+p :itself.to_proc
+p :itself.to_proc.object_id == :itself.to_proc.object_id
+def zet
+    yield
+end
+zet{ p "this is block"}
+zet(&Proc.new{ p "and this is Proc"})
