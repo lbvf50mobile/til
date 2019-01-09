0aa542888453e9c8d56e6292a7cc0a04c04b661f
Dmitry <lbvf50.mobile@gmail.com>
2019-01-09 18:22:33 +0700

    211. JS Using promises >> Timing;

--- 20181228_Friday/20181228.js
+++ 20181228_Friday/20181228.js
@@ -8,5 +8,16 @@ console.log("alias x='node 20181228_Friday/20181228.js'");
 // To avoid surpirses, function passed to then() will never be called synchronously,
 // even with an already resolved promise:
 
-Promise.resolve().then(()=>console.log(2));
-console.log(1); // 1, 2
+Promise.resolve().then(()=>console.log("2 first"));
+console.log("1 first"); // 1, 2
+
+// Instead or runnin immediately, the passed-in function is put on
+// a microtask queue, which means it runs later when the queue
+// is emptied at the end of the current run of the JavaScript
+// event loop, i.e. pretty soon:
+
+const wait = ms => new Promise(resolve => setTimeout(resolve,ms));
+wait().then(()=>console.log(4));
+
+Promise.resolve().then(()=>console.log("2 second")).then(()=>console.log(3));
+console.log("1 second"); // 1, 2, 3, 4
