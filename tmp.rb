ce0c87f51c32add28620c75b7452eee8a6f93bf5
Dmitry <lbvf50.mobile@gmail.com>
2019-01-12 19:32:25 +0700

    Lambda-proc-hash pine tree

--- tmp.rb
+++ tmp.rb
@@ -1,17 +1,15 @@
-func = -> (z:, **a) { p a; z + 's' }
-user = { name: 'X', type: 'hash', z: "hey" }
-p func.(user) # heys
+require "colorize"
+# https://goo.gl/Gfqs6o
 
-def x(z:,**a)
-    p a;
-    p z;
-end
+lambda = ->(raz:, dva:, tri:, **chetire){
+    puts "This is raz %s" % [raz.green]
+    puts "this is dva %s" %  [dva.cyan]
+    puts "This is try %s" % [tri.blue]
+    puts "this is chetire %s" % [chetire.to_s.green]
+}
 
-x(user)
+p lambda.class
+lambda.(%i{raz dva tri chetire pyat shyest ruby classno sho etst}
+    .map{|x| [x,x.to_s]}.to_h)
 
-def z(a,*b)
-    p a
-    p b
-end
-
-z([1,2,3])
+# https://goo.gl/Gfqs6o
