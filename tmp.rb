4d93599dc6222024551f7028bd6a69c3aec7a899
Dmitry <lbvf50.mobile@gmail.com>
2019-01-22 21:52:56 +0700

    Ruby with_index(1)

--- tmp.rb
+++ tmp.rb
@@ -1,2 +1,4 @@
 p %w{a b c}.each_with_index.map{|x,i| "#{x}:#{i+1}"}
 p %w{a b c}.map.with_index(1){|x,i| "#{x}:#{i}"}
+p %w{a b c}.each_with_index.map{|x,i| "#{x}:#{i+1}"}
+p %w{a b c}.map.with_index(1){|x,i| "#{x}:#{i}"}
