789046bd646a0a7e51ca8902b04f6170e13cbe9d
Dmitry <lbvf50.mobile@gmail.com>
2019-01-22 22:10:25 +0700

    Ruby with_index(1)

--- tmp.rb
+++ tmp.rb
@@ -1,4 +1,16 @@
 p %w{a b c}.each_with_index.map{|x,i| "#{x}:#{i+1}"}
 p %w{a b c}.map.with_index(1){|x,i| "#{x}:#{i}"}
-p %w{a b c}.each_with_index.map{|x,i| "#{x}:#{i+1}"}
-p %w{a b c}.map.with_index(1){|x,i| "#{x}:#{i}"}
+require "benchmark"
+n = 500_000
+Benchmark.bm do |z|
+ z.report { 
+     n.times do 
+        %w{a b c}.each_with_index.map{|x,i| "#{x}:#{i+1}"}
+    end
+}
+z.report{
+    n.times do
+        %w{a b c}.map.with_index(1){|x,i| "#{x}:#{i}"}
+    end
+}
+end
