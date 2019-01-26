f4c6013eed8cdb8cd4a84679941ee18e718505e7
Dmitry <lbvf50.mobile@gmail.com>
2019-01-26 23:24:05 +0700

    table.unpack(arg) lua

--- tmp.lua
+++ tmp.lua
@@ -1,6 +1,10 @@
 function z(...)
     print(arg) -- https://github.com/krunkathos/quasimofo/blob/master/lib/strict.lua#L37
+    -- https://www.tutorialspoint.com/lua/lua_functions.htm
+    -- http://lua-users.org/wiki/FunctionCallTutorial
+    print(table.unpack(arg))
     print(...)
 end
 
-z(1,2)
+z(1,2,3)
+-- (function() print("cool") end)()
