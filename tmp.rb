005b3d8ae3dbeb18e281f874372445af1f95443e
Dmitry <lbvf50.mobile@gmail.com>
2019-01-26 17:36:33 +0700

    Help to @vkazancev

--- tmp.rb
+++ tmp.rb
@@ -1,37 +1,23 @@
-p %w{a b c}.each_with_index.map{|x,i| "#{x}:#{i+1}"}
-p %w{a b c}.map.with_index(1){|x,i| "#{x}:#{i}"}
-require "benchmark"
-n = 5
-Benchmark.bm do |z|
- z.report { 
-     n.times do 
-        %w{a b c}.each_with_index.map{|x,i| "#{x}:#{i+1}"}
-    end
-}
-z.report{
-    n.times do
-        %w{a b c}.map.with_index(1){|x,i| "#{x}:#{i}"}
-    end
-}
-end
-
-# TODO consize explanation of Enumerable
-
-def troll_filter message
-    message.split(" ")
-    .map{ |x| 
-        x.chars.zip(%w{c r o a k}.cycle).map{|x,y| y}.join 
-    }.join(" ")
-end
-
-def troll_filter1(message)
-    message.gsub(/\w+/) { |x| "croak".each_char.cycle.take(x.size).join }
-end
-
-p troll_filter "sucks. sucks" # "croak croak"
-p troll_filter1 "sucks. sucks" # "croak croak"
-
-p "croak".each_char.cycle.take("azaza torlolo".size).join
-
+require "colorize"
+#help to @vkazancev
+=begin
+name  = gets
+puts " %s name aftter %s " % ['p'.green, 'gets'.cyan]
+p name
+puts " %s name aftter %s " % ['puts'.red, 'gets'.cyan]
+puts name
+name = name.chomp
+puts " %s name aftter %s " % ['p'.green, 'chomp'.cyan]
+p name
+puts " %s name aftter %s " % ['puts'.red, 'chomp'.cyan]
+puts name
+up = gets.chomp
+p up
+=end
+puts "one \n"
+puts "tow"
+p "Hi\n".chomp == "Hi\n".strip # true
+p "Hi         \n".chomp != "Hi  \n".strip # true
+p "Hi     ".strip == "Hi\n\n\n\n\n\n".strip # true
 
 
