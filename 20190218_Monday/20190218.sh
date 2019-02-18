10b6499892da6fe2cde649992f737ac7f101343b
Dmitry <lbvf50.mobile@gmail.com>
2019-02-18 21:15:32 +0700

    246. BASH Arithmetic evaluation of octa / hexadecimal constants by @kitten256

--- 20190218_Monday/20190218.sh
+++ 20190218_Monday/20190218.sh
@@ -2,7 +2,7 @@
 
 echo "alias x='bash 20190218_Monday/20190218.sh'" 
 
-# BASH Arithmetic evaluation of octal / hexadecimal constants by @kitten256
+# BASH Arithmetic evaluation of octal/hexadecimal constants by @kitten256
 
 # https://www.tldp.org/LDP/abs/html/testconstructs.html#DBLBRACKETS
 
@@ -10,7 +10,7 @@ red=`tput setaf 1`
 green=`tput setaf 2`
 reset=`tput sgr0`
 
-echo "${red}Octal${reset} and ${green}hexadecimal ${reset} values"
+echo "${red}Octal${reset} and ${green}hexadecimal ${reset}values"
 
 [[ 0x10 -eq 16 ]]  && echo "${green}yes${reset}"  || echo "${red}no${reset}"
 [[ 020 -eq 16 ]] && echo "${green}yes${reset}" || echo "${red}no${reset}"
