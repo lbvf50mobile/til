cf27b9064085ef8a6a2d38b748418c31a09446e9
Dmitry <lbvf50.mobile@gmail.com>
2019-02-26 22:34:53 +0700

    249. Ruby Bridge in a graph.

--- 20190225_Monday/20190225.rb
+++ 20190225_Monday/20190225.rb
@@ -1,3 +1,28 @@
 p "alias x='ruby 20190225_Monday/20190225.rb'" 
 
 # https://www.geeksforgeeks.org/bridge-in-a-graph/
+
+# Bridges in a graph
+
+# An edgein an andirected conncted graph is a bridge if removing it disconnects the graph.
+# For a disconnecte undirected graph, definition is similar, a bridge is an edge removing which
+# increases number of disconnected components.
+
+# Like Articluation Points, bridges represents vulnerabilities in a connected network and are
+# userful for desigining reliable networks.
+# For example, in a wired computed network, an articulation point indicated the critical computers
+# and a bridge indicates the critical wires or connections.
+
+# How to find all bridges in a givern graph?
+
+#  A simple approach is to one by one remove all edges and see if remova of a enge causes
+# disconnected graph. Following are steps of simple approach for connected graph.
+# Folowwing are steps of simplw approach fo connected graph.
+
+# Fo every edge (u,v), do following
+# - a) remove (u,v) from graph
+# - b) See if the gpagh remains conncted (we can either use BFS of DFS)
+# - c) Add (u,v) bacl to the graph.
+
+# Tim complexity of abmove methos is O(E*(V+E)) for a graph represented using adjacency list.
+# Can we do better?
