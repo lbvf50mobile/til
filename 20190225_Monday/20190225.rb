a0f3e0962bb190359fbe330e92ec9d8990e7e223
Dmitry <lbvf50.mobile@gmail.com>
2019-02-27 01:58:59 +0700

    249. Ruby Bridge in a graph. (program)

--- 20190225_Monday/20190225.rb
+++ 20190225_Monday/20190225.rb
@@ -35,3 +35,61 @@ p "alias x='ruby 20190225_Monday/20190225.rb'"
 # from the subtree rooted with v.  As discussed in the previewos post 
 # (https://www.geeksforgeeks.org/articulation-points-or-cut-vertices-in-a-graph/)
 # The condition of an endge (u,b) to be a bridge is, "low[v] > disc[u]"
+
+class Graph
+  def initialize(size)
+    @V = size
+    @graph = {}
+    @time = 0
+  end  
+  def _add(u,v)
+    @graph[u].nil? ? @graph[u] = [v] : @graph[u].push[v]
+  end
+  def addEdge(u,v)
+    _add(u,v)
+    _add(v,u)
+  end
+  def inspect
+    @graph.to_s
+  end
+
+  def bridgeUtil(u,visited, parent, low, disc)
+    visited[u] = true
+    disc[u] = @time
+    low[u] = @time
+    @time += 1
+    @graph[u].each do |v|
+      if(visited[v].nil?)
+          parent[v] = u
+          bridgeUtil(v, visited, parent, low, disc)
+          low[u] = [low[u], low[v]].min
+          p [u,v] if low[v] > disc[u]
+      elsif v != parent[u]
+        low[u] = [low[u], disc[v]].min
+      end
+    end
+  end
+
+  # DFS based function to find all bridges.
+  # It uses recusive function bridgeUntil()
+  def bridge
+      visited = {}
+      disc = [Float::INFINITY] * @V
+      low = [Float::INFINITY] * @V
+      parent = [-1] * @V
+      (0...@V).each{|u|
+        bridgeUtil(u, visited, parent, low, disc)
+      }
+
+  end
+
+end
+
+g1 = Graph.new(5)
+g1.addEdge(1, 0) 
+g1.addEdge(0, 2) 
+g1.addEdge(2, 1) 
+g1.addEdge(0, 3) 
+g1.addEdge(3, 4) 
+
+g1.bridge()
