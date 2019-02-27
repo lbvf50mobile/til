7fc1643ca5c3cace0102f4d6500a700dec385236
Dmitry <lbvf50.mobile@gmail.com>
2019-02-28 00:34:34 +0700

    ByState Kata compare by words

--- tmp.rb
+++ tmp.rb
@@ -1,82 +1,23 @@
-require 'pp'
 require 'colorize'
-# https://app.codesignal.com/interview-practice/task/qmKLsQcqeEBckLx2q
-3.times do puts "" end
-puts "singlePointOfFailure".cyan
-puts ""
+# https://gist.github.com/lbvf50mobile/1b99ed56192b6cf0acbec76f8344478d
+# https://www.codewars.com/kata/59d0ee709f0cbcf65400003b
+a = File.read('1')
+b =  File.read('2')
+flag = true
+norm = ""
+f = ['','']
+p 0..[a.size,b.size].min
 
 
-
-
-require "minitest/autorun"
-require 'benchmark'
-require 'oj'
-require 'ostruct'
-
-# https://www.geeksforgeeks.org/detect-cycle-in-a-graph/
-# https://www.geeksforgeeks.org/bridge-in-a-graph/
-
-
-    def _add(u,v, graph)
-      graph[u].nil? ? graph[u] = [v] : graph[u].push(v)
+(0..[a.size,b.size].min).each{ |x|
+    flag = false if a[x] != b[x]
+    if flag
+        norm << a[x]
+    else
+        f[0] << a[x] if a[x]
+        f[1] << b[x] if b[x]
     end
-  
-    def bridgeUtil(u,visited, parent, low, disc, hash)
-      visited[u] = true
-      disc[u] = hash[:time]
-      low[u] = hash[:time]
-      hash[:time] += 1
-      hash[:graph][u].each do |v|
-        if(visited[v].nil?)
-            parent[v] = u
-            bridgeUtil(v, visited, parent, low, disc, hash)
-            low[u] = [low[u], low[v]].min
-              if low[v] > disc[u]
-                hash[:answer].push([u,v])
-              end
-        elsif v != parent[u]
-          low[u] = [low[u], disc[v]].min
-        end
-      end
-    end
-  
-    # DFS based function to find all bridges.
-    # It uses recusive function bridgeUntil()
-    def bridge(hash)
-        visited = {}
-        graph_size = hash[:graph].size
-        disc = [Float::INFINITY] * graph_size
-        low = [Float::INFINITY] * graph_size
-        parent = [-1] * graph_size
-        (0...graph_size).each{|u|
-          bridgeUtil(u, visited, parent, low, disc, hash)
-        }
-        hash[:answer]
-  
-    end
-  
-
-
-def singlePointOfFailure(connections)
-    graph = {}
-    hash = {answer:[], time:0, graph: graph}
-    connections.each_with_index do |row, u|
-        row.each_with_index do |cell, v|
-            _add(u,v,graph) if 1 == cell 
-        end
-    end
-    bridge(hash).size
-end
-
-describe "countClouds" do
-    it "shold works" do
-        a = [[0, 1], [1, 0]]
-        assert_equal 1,singlePointOfFailure(a)
-    end
-    it "shold works" do
-        a = [[0,1,1], 
-        [1,0,1], 
-        [1,1,0]]
-        assert_equal 0,singlePointOfFailure(a)
-    end
-end
+}
+puts norm.green
+puts f[0].red
+puts f[1].yellow
