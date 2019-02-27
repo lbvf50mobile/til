93dd1e63bd20690c055178376962b270afd1fa10
Dmitry <lbvf50.mobile@gmail.com>
2019-02-27 22:32:39 +0700

    Single poing of failure

--- tmp.rb
+++ tmp.rb
@@ -16,8 +16,67 @@ require 'ostruct'
 # https://www.geeksforgeeks.org/detect-cycle-in-a-graph/
 # https://www.geeksforgeeks.org/bridge-in-a-graph/
 
+class Graph
+    def initialize(size)
+      @V = size
+      @graph = {}
+      @time = 0
+      @answer = []
+    end  
+    def _add(u,v)
+      @graph[u].nil? ? @graph[u] = [v] : @graph[u].push(v)
+    end
+    def addEdge(u,v)
+      _add(u,v)
+      _add(v,u)
+    end
+    def inspect
+      @graph.to_s
+    end
+  
+    def bridgeUtil(u,visited, parent, low, disc)
+      visited[u] = true
+      disc[u] = @time
+      low[u] = @time
+      @time += 1
+      @graph[u].each do |v|
+        if(visited[v].nil?)
+            parent[v] = u
+            bridgeUtil(v, visited, parent, low, disc)
+            low[u] = [low[u], low[v]].min
+              if low[v] > disc[u]
+                @answer.push([u,v])
+              end
+        elsif v != parent[u]
+          low[u] = [low[u], disc[v]].min
+        end
+      end
+    end
+  
+    # DFS based function to find all bridges.
+    # It uses recusive function bridgeUntil()
+    def bridge
+        visited = {}
+        disc = [Float::INFINITY] * @V
+        low = [Float::INFINITY] * @V
+        parent = [-1] * @V
+        (0...@V).each{|u|
+          bridgeUtil(u, visited, parent, low, disc)
+        }
+        @answer
+  
+    end
+  
+end
+
 def singlePointOfFailure(connections)
- 1
+    g1 = Graph.new(connections.size)
+    connections.each_with_index do |row, u|
+        row.each_with_index do |cell, v|
+            g1._add(u,v) if 1 == cell 
+        end
+    end
+    g1.bridge().size
 end
 
 describe "countClouds" do
