989bd0eed6ca082e887728a9cac3faba27283e00
Dmitry <lbvf50.mobile@gmail.com>
2019-03-02 23:02:10 +0700

    250. Ruby Articulation Points (or Cut Vertices) in a Graph. (text)

--- 20190228_Thursday/20190228.rb
+++ 20190228_Thursday/20190228.rb
@@ -8,3 +8,11 @@ p "alias x='ruby 20190228_Thursday/20190228.rb'"
 
 # A vertes in an undirected connceted graph is an atriculation point
 # (or cut vertex) if revoming it (and gedges thought it)  disconnects the graph.
+# Articulation points represent vulnerabilities in a connected network - single points
+# whose failure would split the network into 2 or more disconnected components. They
+# are useful for desiging reliable networks.
+
+# For a disconnected undirected graph, an articulation point is a vertex remoting
+# which increases number of connected components.
+
+# Following are some example graphs with articulation points encircled with red color.
