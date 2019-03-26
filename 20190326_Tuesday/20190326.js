389800aa7bfd7ebc70595adf81860f0cb049cad8
Dmitry <lbvf50.mobile@gmail.com>
2019-03-26 23:57:08 +0700

    255. Js Array.prototype.from;

--- 20190326_Tuesday/20190326.js
+++ 20190326_Tuesday/20190326.js
@@ -12,3 +12,15 @@ console.log(x);
 let set = new Set(['one','one', 'one', 'two', 'three', 'four']);
 x = Array.from(set);
 console.log(x);
+
+// Array from Map
+let map = new Map([[1,2], [2,4], [4,8]]);
+x = Array.from(map);
+console.log(x);
+
+let mapper = new Map([['1','a'],['2','b']]);
+x = Array.from(mapper.values());
+console.log(x);
+
+x = Array.from(mapper.keys());
+console.log(x);
