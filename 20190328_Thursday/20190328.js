d3c6a7a4bcea92a9bc3d7cf385337986d6ecdae5
Dmitry <lbvf50.mobile@gmail.com>
2019-03-28 23:56:00 +0700

    256. JS Array.prototype.copyWithin();

--- 20190328_Thursday/20190328.js
+++ 20190328_Thursday/20190328.js
@@ -1,6 +1,6 @@
 console.log("alias x='node 20190328_Thursday/20190328.js'");
 
-// JS Array.porotoype.copywithin()
+// JS Array.porotoype.copyWithin();
 
 // The copyWithin methos shallow copies part of an aray to another
 // location in the same array and returns it, withou modifing its size
@@ -8,3 +8,6 @@ console.log("alias x='node 20190328_Thursday/20190328.js'");
 let array1 = ['a', 'b', 'c', 'd', 'e'];
 // copy to index 0 the element a the index 3
 console.log(array1.copyWithin(0,3,4));
+
+// copy to index 1 all elements from index 3 to the end
+console.log(array1.copyWithin(1,3));
