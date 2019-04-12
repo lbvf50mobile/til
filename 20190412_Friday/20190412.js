2bd85295c72f2baa0a2e49a4d3022e04474ceb1e
Dmitry <lbvf50.mobile@gmail.com>
2019-04-13 00:05:56 +0700

    267. JS ordered count of characters with a Map object.(compare with obj)

--- 20190412_Friday/20190412.js
+++ 20190412_Friday/20190412.js
@@ -1,10 +1,11 @@
 console.log("alias x='node 20190412_Friday/20190412.js'");
 
-// JS orderded count of characters with a Map object.
+// JS orderded count of characters with a Map object. (compare with obj)
 
 let data = ['aabbbbccccc', 'bbbbaaccccc', 'cccccbbbbaa'];
 
 let count = x => {
+    console.log("count_map");
    let map = [...x].reduce((acum,value)=>{
         if(undefined == acum.get(value)){
             acum.set(value, 1)
@@ -17,7 +18,26 @@ let count = x => {
      return  [...map];
 }
 
+let count_object = x =>{
+    console.log("count_object");
+    let obj = [...x].reduce((acum,value)=>{
+        if(undefined == acum[value]){
+            acum[value] = 1;
+        }else{
+            acum[value] += 1;
+        }
+        return acum;
+    },{})
+    ans = []
+    for ( let k in obj){
+        ans.push([k,obj[k]])
+    }
+    return ans;
+
+}
+
 data.forEach(str => {
     console.log(str)
     console.log(count(str))
+    console.log(count_object(str))
 })
