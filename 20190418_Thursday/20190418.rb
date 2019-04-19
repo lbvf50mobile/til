59c74a5c93eb861cff80cc4200344a377c00bf96
Dmitry <lbvf50.mobile@gmail.com>
2019-04-19 23:03:19 +0700

    272. Ruby solution of Combs problem with binary shift.

--- 20190418_Thursday/20190418.rb
+++ 20190418_Thursday/20190418.rb
@@ -20,15 +20,38 @@ def combs(c1, c2)
         memo
     end.min
 end
+def combs_by_kov(c1,c2)
+    a = c1.gsub(/./){|x| x == "*" ? 1 : 0}.to_i(2)
+    b = c2.gsub(/./){|x| x == "*" ? 1 : 0}.to_i(2)
+    i = 0
+    j = 0
+    p [c1,a.to_s(2)]
+    (0..[c1.size,c2.size].max).each do |ii|
+        tmp = a << ii
+        p [ii,tmp.to_s(2), b.to_s(2), 0 != tmp & b]
+    end
+    while 0 != a << i & b do
+        p i
+        i += 1
+    end
+    while 0 != b << j & a do
+        j += 1
+    end
+    l1 = [c1.size+i,c2.size].max
+    l2 = [c2.size+j,c1.size].max
+    [l1,l2].min
+end
 
 tests = [
-    {c1: '*..*', c2: '*..*', answer: 5}
+    {c1: '*..*', c2: '*.*', answer: 5},
+    {c1: "*...*", c2: "*.*", answer: 5}
 ]
 
 describe "RunnersMeetings" do
     it "shold works" do
         tests.each do |test|
             assert_equal test[:answer], combs(test[:c1],test[:c2])
+            assert_equal test[:answer], combs_by_kov(test[:c1],test[:c2])
         end
     end
 end
