dc73d5f0b10b8512e53ad00a8320c52bdaba044f
Dmitry <lbvf50.mobile@gmail.com>
2019-04-24 19:17:53 +0700

    275. Ruby Christmas tree task solution. (generete level one)

--- 20190423_Tuesday/20190423.rb
+++ 20190423_Tuesday/20190423.rb
@@ -4,13 +4,18 @@ p "alias x='ruby 20190423_Tuesday/20190423.rb'"
 
 require "minitest/autorun"
 def christmasTree(levelNum, levelHeight)
+    max_width = 5 + (levelHeight - 1) * 2
+    line_size = ->(line_number){max_width - (levelHeight  - line_number)}
+    blank_amount = ->(line_number){ levelHeight - line_number }
+    asterisks_amount = ->(line_number){line_size.(line_number) - blank_amount.(line_number)}
     crown = ["    *", 
         "    *", 
         "   ***"]
-    level1 = [ 
-        "  *****", 
-        " *******", 
-        "*********"]
+    level1 = (1..levelHeight).reduce([]) do |memo,line_number| 
+       value =  [" "] * blank_amount.(line_number) + [?*]*asterisks_amount.(line_number)
+       memo.push( value.join) 
+       memo
+    end
     foot = ["   ***"] 
     crown + level1 + foot
 
