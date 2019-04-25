ab4c5dd7dc2db8170932a89e3f7f416abe3414b9
Dmitry <lbvf50.mobile@gmail.com>
2019-04-25 18:34:42 +0700

    275. Ruby Christmas tree sast solution. (fix change same objects in memory)

--- 20190423_Tuesday/20190423.rb
+++ 20190423_Tuesday/20190423.rb
@@ -15,11 +15,13 @@ def christmasTree(levelNum, levelHeight)
     line_size = ->(line_number){max_width - (levelHeight  - line_number)}
     blank_amount = ->(line_number){ levelHeight - line_number }
     asterisks_amount = ->(line_number){line_size.(line_number) - blank_amount.(line_number)}
-    level1_hash = (1..levelHeight).reduce([]) do |memo,line_number| 
+    level1_hash_proc = ->(){(1..levelHeight).reduce([]) do |memo,line_number| 
         value =  {empty: blank_amount.(line_number), asterisks: asterisks_amount.(line_number)}
         memo.push(value) 
         memo
-     end
+    end
+    }
+    level1_hash = level1_hash_proc.()
 
     # Crown
     crown_top = ->(){ {empty:max_width-levelHeight-2, asterisks:1}}
@@ -40,7 +42,7 @@ def christmasTree(levelNum, levelHeight)
     extend_by_2_asterisks = ->(arr){ arr.map{|x|  x[:asterisks] += 2 ; x}}
     (2..levelNum).each do |level|
         empty_extend_array = extend_by_1_empty.(empty_extend_array)
-        new_level =  extend_by_2_asterisks.(level1_hash.clone)
+        new_level =  extend_by_2_asterisks.(level1_hash_proc.())
         levels += new_level
     end
     result_array = empty_extend_array[0] + empty_extend_array[1]  + levels + empty_extend_array[2]
