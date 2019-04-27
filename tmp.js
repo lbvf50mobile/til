5712eadaf4f62eef92cded7d41cff788eddeda3b
Dmitry <lbvf50.mobile@gmail.com>
2019-04-27 22:59:48 +0700

    fileNaming solution by @jouko_v

--- tmp.js
+++ tmp.js
@@ -1,12 +1,31 @@
-let test = "hi";
-let z = [...test];
-console.log(z);
-let west = ["west"];
-let y = [...west];
-console.log(y)
-let test2 = 22;
-try{
-let x = [...test2];
-}catch(e){
-    console.log(e.message)
+let a ={}
+console.log(a[4], undefined == a[4])
+console.log(new Set([1,1,1,1,2]))
+let z = new Set([1,1,1,1,2])
+z.forEach(x => console.log(x))
+
+function fileNaming(names) {
+    h = {}
+    for(let i in names){
+        n = names[i]
+        if( undefined == h[n]){
+            h[n] = 1;
+        }else{
+            for( ; h[names[i]] != undefined ;){
+                let value = n + `(${h[n]})`
+                names[i] = value
+                h[n] += 1
+            }
+            h[names[i]] = 1
+        }
+    }
+    return names;
 }
+
+input = ["doc", 
+"doc", 
+"image", 
+"doc(1)", 
+"doc"]
+
+console.log(fileNaming(input))
