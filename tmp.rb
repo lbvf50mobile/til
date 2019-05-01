ac4156aade55abcdc0e57f7f5320055b4b4d85d3
Dmitry <lbvf50.mobile@gmail.com>
2019-05-01 20:26:25 +0700

    Fibonacci number

--- tmp.rb
+++ tmp.rb
@@ -2,3 +2,18 @@
 
 p a=3
 p a
+
+def feb n
+    return 0 if 0 == n
+    return 1 if 1 == n
+    fn1 = 0
+    fn2 = 1
+    f = fn1+fn2
+    (2..n).each do |n|
+        f = fn1+fn2
+        fn2,fn1 = f,fn2
+    end
+    f
+end
+
+p (0..10).map{|x| feb x}
