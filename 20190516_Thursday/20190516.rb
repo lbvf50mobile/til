4d81452fda2b46065ee494ed6b1372a87f329cb4
Dmitry <lbvf50.mobile@gmail.com>
2019-05-16 22:54:07 +0700

    292. Ruby Enumerable#reverse_each; (optimize number of stirngs in the output)

--- 20190516_Thursday/20190516.rb
+++ 20190516_Thursday/20190516.rb
@@ -1,10 +1,19 @@
+#!/usr/bin/env ruby
 p "alias x='ruby 20190516_Thursday/20190516.rb'" 
 
 # Ruby Enumerable#reverse_each;
 
 # https://ruby-doc.org/core-2.6.3/Enumerable.html#method-i-reverse_each
-
-[*1..4].reverse_each{|x| p x}
-(1..4).reverse_each{|x| p x}
-(1..4).reverse_each.each{|x| p x}
-(1..4).each.reverse_each{|x| p x}
+require "colorize"
+s = ""
+[*1..4].reverse_each{|x| s << x.to_s}
+puts s.green
+s = ""
+(1..4).reverse_each{|x| s << x.to_s}
+puts s.red
+s = ""
+(1..4).reverse_each.each{|x| s << x.to_s}
+puts s.green
+s =""
+(1..4).each.reverse_each{|x| s << x.to_s}
+puts s.red
