59c3aa81457b8d6cc5a7fb7b3767e887fee1212e
Dmitry <lbvf50.mobile@gmail.com>
2019-05-28 17:53:32 +0700

    JS. Footbal-yellow-and-red-cards prototype solution.

--- tmp.js
+++ tmp.js
@@ -1,6 +1,25 @@
-// Spread operator split string in functions.
-// Spread operator have low priority than +;
-x1 = x => Math.max(...[...x.toString()]);
-x2 = x => Math.max(...x.toString());
-x3 = x => Math.max(...x+'');
-console.log(x1(10),x2(10),x3(10));
+// https://www.codewars.com/kata/football-yellow-and-red-cards/javascript
+// https://stackoverflow.com/questions/3261587/subclassing-javascript-arrays-typeerror-array-prototype-tostring-is-not-generi
+// http://perfectionkills.com/how-ecmascript-5-still-does-not-allow-to-subclass-an-array/
+Array.prototype.create_team = function(){ 
+    Array(12).fill(1).map( x => new Object({R: 0, Y:0, in: true})).forEach( x => this.push(x)); 
+    this[0].in = false // One player on the bench 
+    return this;
+}
+Array.prototype.count = function(){ return this.filter( x => x.in).length;}
+Array.prototype.remove = function(){ this.forEach( y => {if(y.R >= 1 || y.Y >=2) y.in = false})}
+function menStillStanding(cards) {
+    let [a,b] = [(new Array).create_team(), (new Array).create_team()]
+    let teams = {A: a, B: b}
+    cards.some(x =>{
+       let [_,team, player, card] = x.match(/([A-Z])(\d+)([A-Z])/);
+       let [tm, plr] = [teams[team],teams[team][player]];
+       plr[card] += 1;
+       tm.remove();
+       return 7 > tm.count();
+    });
+    return [a.count(), b.count()];
+  }
+
+  console.log(menStillStanding(["A4Y", "A4Y"]).toString() == [10,11].toString());
+  console.log( (new Array).create_team());
