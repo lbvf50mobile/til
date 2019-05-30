ebdfd605112f004630a79c1d8dd5e714788d4ff1
Dmitry <lbvf50.mobile@gmail.com>
2019-05-30 14:44:53 +0700

    306. JS Spread opertaor in Objects. (docs Spread in Object Literals)

--- 20190530_Thursday/20190530.js
+++ 20190530_Thursday/20190530.js
@@ -1,5 +1,6 @@
 console.log("alias x='node 20190530_Thursday/20190530.js'");
-// JS Spread operator in Objects.
+// JS Spread operator in Objects. (docs Spread in Object Literals)
 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax#Spread_in_object_literals
 z = {first: 1, ...(true ? {second:2} : {})};
 console.log(z,"JS Spread operator in Objects.")
