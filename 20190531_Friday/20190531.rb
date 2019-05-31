a7aa372a30674f81aad4c50e9a83a2eb68b4f0da
Dmitry <lbvf50.mobile@gmail.com>
2019-05-31 22:11:32 +0700

    307. Ruby str[start,length], str[regex,capture]; (docs url)

--- 20190531_Friday/20190531.rb
+++ 20190531_Friday/20190531.rb
@@ -1,8 +1,11 @@
 p "alias x='ruby 20190531_Friday/20190531.rb'" 
 
 # Ruby str[start, length], str[regexp, capture];
+# https://ruby-doc.org/core-2.4.0/String.html#method-i-5B-5D
 
- p '12345ab'[5,6]
+ p '12345abc'[5,1]
+ p '12345abc'[5,2]
+ p '12345abc'[5,3]
  p '12345ab'[/a|b/,0]
  p '12345ab'[/a|b/,1]
  p '12345ab'[/a|b/,2]
