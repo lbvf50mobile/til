ce8dbdeec41f57f7ed281c371d8f9a64206d6a92
Dmitry <lbvf50.mobile@gmail.com>
2019-06-03 01:04:20 +0700

    Whose Turn task have an error, passed code with no check on valid position (valid_cell(cell))

--- tmp.js
+++ tmp.js
@@ -1,3 +1,28 @@
-let max_by_second = x => x.sort(([_,a],[__,b]) => b - a)[0]; 
-let a = [[1,2],[5,101],[8,0],[7,15]]
-console.log(max_by_second(a)); // [5,101]
+delta = [[1,2],[2,1],[2,-1],[1,-2],[-1,-2],[-2,-1],[-2,1],[-1,2]];
+possible_turns = ([x,y]) => delta.map( ([x1,y1]) => [x+x1, y+y1]).filter( ([x,y]) => 0 <= x && x <= 7 && 0 <= y && y <= 7);
+parser = x => [x[0].charCodeAt(0) - 'a'.charCodeAt(0), parseInt(x[1])]
+bfs = (s,f) => {
+        var level = 0;
+        var hash = {};
+        hash[s] = level;
+        var burn = [s]
+        while (burn.length > 0){
+                let tmp = burn.shift();
+                level = hash[tmp];
+                console.log(tmp,level)
+                
+                if(tmp.toString() == f.toString()) return level;
+                //console.log(possible_turns(tmp))
+                possible_turns(tmp).forEach( x=>{
+                        if(undefined == hash[x]){
+                                burn.push(x);
+                                hash[x] = level + 1;
+                        }
+                });
+                
+        }
+        return level;
+}
+//console.log(parser('f3'))
+//console.log(parser('b8'))
+bfs([ 5, 3 ],[ 1, 8 ])
--- tmp.rb
+++ tmp.rb
@@ -1,6 +1,25 @@
-# @jouko_v Ruby return array
-def test
-    return 1,2
+def moves; [[1,2],[2,1],[2,-1],[1,-2],[-1,-2],[-2,-1],[-2,1],[-1,2]]; end
+def str2arr str; str.split(';').map{ |cell| [cell[0].ord - ?a.ord ,cell[1].to_i - 1]} end
+def valid_cell cell; cell[0].between?(0,7) && cell[1].between?(0,7) end
+def possible_turns(cell); moves.reduce([]){ |mem,(x,y)| a = [cell[0]+x,cell[1]+y]; valid_cell(cell) && mem.push(a); mem}; end
+require 'colorize'
+def bfs(burn, final)
+    used = {}
+    level = 0
+    used[burn.first] = level
+    while !burn.empty?
+        x = burn.shift
+        level = used[x]
+        puts "#{x} #{level}"
+        break if x == final
+        puts "#{possible_turns(x)}"
+        possible_turns(x).each do |x|
+            if used[x].nil?
+                used[x] = level+1;
+                burn.push(x)
+            end
+        end
+    end
+    level
 end
-
-p [1,2] == test
+bfs([[ 5, 3 ]],[ 1, 8 ])
