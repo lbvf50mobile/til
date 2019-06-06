f521fb725ba568a28794d3542e53c9db9de5f31e
Dmitry <lbvf50.mobile@gmail.com>
2019-06-06 18:30:19 +0700

    311. Ruby Chess Triangle. (Simplify, represent as 3 arrays)

--- 20190605_Wednesday/20190605.rb
+++ 20190605_Wednesday/20190605.rb
@@ -75,6 +75,7 @@ def chessTriangle(n,m)
     end
     variants.uniq!
     valid = variants.all?{|x| check_validnes(x)}
+    variants = variants.reduce([]){|memo,x| memo.push( [[*x[:p1],x[:t][0]],[*x[:p2],x[:t][1]],[*x[:p3],x[:t][2]]]); memo}
     throw 'Invalind variants' unless valid
     puts "%s. Triangles based on full brootforse %s #{variants.first}" % ["#{n}x#{m}".red, variants.size.to_s.yellow]
 end
