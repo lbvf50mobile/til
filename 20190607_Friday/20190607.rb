dac4dea08174e294b93d89116885612c20c0366b
Dmitry <lbvf50.mobile@gmail.com>
2019-06-07 15:04:17 +0700

    313. Ruby int32_to_ip; a.pack(?N).unpack('C*') * ?.

--- 20190607_Friday/20190607.rb
+++ 20190607_Friday/20190607.rb
@@ -14,8 +14,8 @@ require "minitest/autorun"
 # ary * int -> new_ary
 # ary * str -> new_string
 
-def int32_to_ip(int32)
-    [int32].pack(?N).unpack('C*') * ?.
+def int32_to_ip(*a)
+    a.pack(?N).unpack('C*') * ?.
 end
 
 def int32_to_ip2(*arr)
