0202b1366f30feab7d443e03828b3915867cfa2d
Dmitry <lbvf50.mobile@gmail.com>
2019-06-07 15:11:19 +0700

    313. Ruby int32_to_ip; a.pack(?N).unpack('C*') * ?. ; (add docs about literals)

--- 20190607_Friday/20190607.rb
+++ 20190607_Friday/20190607.rb
@@ -5,14 +5,21 @@ p "alias x='ruby 20190607_Friday/20190607.rb'"
 require "minitest/autorun"
 
 # https://www.codewars.com/kata/int32-to-ipv4/ruby
+
 # https://www.rubydoc.info/stdlib/core/Array:pack
 # N         | Integer | 32-bit unsigned, network (big-endian) byte order
+
 # https://apidock.com/ruby/String/unpack
 # C             | Integer | 8-bit unsigned (unsigned char)
 #  An asterisk (“*”) will use up all remaining elements
+
 # https://docs.ruby-lang.org/en/2.0.0/Array.html#method-i-2A
 # ary * int -> new_ary
 # ary * str -> new_string
+
+# https://ruby-doc.org/core-2.4.0/doc/syntax/literals_rdoc.html
+# There is also a character literal notation to represent single character strings, 
+# which syntax is a question mark (?) 
 
 def int32_to_ip(*a)
     a.pack(?N).unpack('C*') * ?.
