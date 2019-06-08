78dccd8018442e9d171958e01287cde360b269db
Dmitry <lbvf50.mobile@gmail.com>
2019-06-08 07:10:45 +0700

    JS/Ruby [^], and 'g' Regex flag in JS String.prototype.match. (kata: link + solution)

--- 20190608_Saturday/20190608.js
+++ 20190608_Saturday/20190608.js
@@ -3,6 +3,11 @@ console.log("alias x='node 20190608_Saturday/20190608.js'");
 // JS/Ruby [^], and 'g' Regex flag in JS String.prototype.match.
 
 // https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1/solutions/javascript
+// https://www.codewars.com/users/jacobb
+function duplicateCount(text){
+    return (text.toLowerCase().split('').sort().join('').match(/([^])\1+/g) || []).length;
+  }
+
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp#Parameters
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/match#Return_value
 
