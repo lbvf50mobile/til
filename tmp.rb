61c71300d4962bc09468bfad00d21177e3c5d130
Dmitry <lbvf50.mobile@gmail.com>
2019-06-13 22:58:15 +0700

    Splat operator works with Enumerable

--- tmp.rb
+++ tmp.rb
@@ -1,25 +1,12 @@
-def moves; [[1,2],[2,1],[2,-1],[1,-2],[-1,-2],[-2,-1],[-2,1],[-1,2]]; end
-def str2arr str; str.split(';').map{ |cell| [cell[0].ord - ?a.ord ,cell[1].to_i - 1]} end
-def valid_cell cell; cell[0].between?(0,7) && cell[1].between?(0,7) end
-def possible_turns(cell); moves.reduce([]){ |mem,(x,y)| a = [cell[0]+x,cell[1]+y]; valid_cell(cell) && mem.push(a); mem}; end
-require 'colorize'
-def bfs(burn, final)
-    used = {}
-    level = 0
-    used[burn.first] = level
-    while !burn.empty?
-        x = burn.shift
-        level = used[x]
-        puts "#{x} #{level}"
-        break if x == final
-        puts "#{possible_turns(x)}"
-        possible_turns(x).each do |x|
-            if used[x].nil?
-                used[x] = level+1;
-                burn.push(x)
-            end
-        end
-    end
-    level
+# Splat Operator works with Enumerable.
+def cool_splat x
+    [*x]
 end
-bfs([[ 5, 3 ]],[ 1, 8 ])
+p cool_splat(1..2) # [1,2]
+p cool_splat([1,2]) # [1,2]
+p cool_splat([1,2].each_with_index) # [[1,0],[2,1]]
+p [1,2].each_with_index # #<Enumerator: [1, 2]:each_with_index>
+p [1,2].each_with_index.class.ancestors
+p [1,2].class.ancestors
+p (1..2).class.ancestors
+p [[1,2],(1..2),[1,2].each].map{|x| x.is_a? Enumerable}
