9ff86915b6f03aa650942f5598850f2f5ffe0110
Dmitry <lbvf50.mobile@gmail.com>
2019-06-15 20:28:35 +0700

    320. JS \B(?=(\d{3})+(?!\d)); (docs positive/negative lookahead)

--- 20190615_Saturday/20190615.js
+++ 20190615_Saturday/20190615.js
@@ -3,7 +3,8 @@ console.log("alias x='node 20190615_Saturday/20190615.js'");
 // JS \B(?=(\d{3})+(?!\d))
 
 // https://www.codewars.com/users/wizgeeky
-// https://regex101.com/r/fqoV22/1
+// https://regex101.com/r/fqoV22
+// https://www.stefanjudis.com/today-i-learned/the-complicated-syntax-of-lookaheads-in-javascript-regular-expressions/
 function groupByCommas(n) {
     return n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   }
