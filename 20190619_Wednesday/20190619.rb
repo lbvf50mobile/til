4f7d9b5d38e8a4c3fa566bf53971b65fe561c848
Dmitry <lbvf50.mobile@gmail.com>
2019-06-19 20:11:51 +0700

    322. Ruby Enumerable#grep (add docs)

--- 20190619_Wednesday/20190619.rb
+++ 20190619_Wednesday/20190619.rb
@@ -2,6 +2,9 @@ p "alias x='ruby 20190619_Wednesday/20190619.rb'"
 
 # Ruby Enumerable#grep
 
+# https://www.rubyguides.com/2015/10/ruby-case/
+# https://ruby-doc.org/core-2.6.3/Enumerable.html#method-i-grep
+
 p [?a,1,2,?c,?e].grep(Fixnum)
 p [?a,1,2,?c,?e,3,4].grep(1..2)
 p [?a,1,2,?c,?e,3,4,'aaa', 'will not be selected too'].grep(/^[a-c]/)
