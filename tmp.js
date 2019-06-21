5f730649d64ca9dd894d1d84ddd7535df6817b57
Dmitry <lbvf50.mobile@gmail.com>
2019-06-21 18:30:27 +0700

    one more time about JS ASI

--- tmp.js
+++ tmp.js
@@ -1,12 +1,3 @@
-// JS ASI example.
-
-
- find2ndLargest = x =>  [...new Set(x)]
-.filter(x => 'number' == typeof x).sort((a,b) => a-b).reverse()[1] || null;
-
-console.log(find2ndLargest([-5,-7,0,-8,1]))
-
-find2ndLargest = x =>  ( a = [...new Set(x)]
-.filter(x => 'number' == typeof x).sort((a,b) => a-b).reverse()[1], a == undefined ? null : a) ;
-
-console.log(find2ndLargest([-5,-7,0,-8,1]))
+sum  = 0
+a  = [1,2,3]
+[1,2,3].map(x => sum+=x);
