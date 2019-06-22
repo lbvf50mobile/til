b6c0d2a857299401d9a8fef68fe8d95b9a3a7be3
Dmitry <lbvf50.mobile@gmail.com>
2019-06-22 22:22:46 +0700

    325. JS Negative lookahaed assertion /x(?!y)/;

--- 20190622_Saturday/20190622.js
+++ 20190622_Saturday/20190622.js
@@ -2,4 +2,6 @@ console.log("alias x='node 20190622_Saturday/20190622.js'");
 
 // JS Negative lookahead assertion /x(?!y)/;
 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions/Assertions
+
 console.log(/\d+(?!\.)/g.exec('3.141'));
