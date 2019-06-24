540dca4c641253f5fb14a4288f86c42f73aa6025
Dmitry <lbvf50.mobile@gmail.com>
2019-06-24 12:44:23 +0700

    325. JS Negative lookagead assertion /x(?!y)/ add examples.

--- 20190622_Saturday/20190622.js
+++ 20190622_Saturday/20190622.js
@@ -4,4 +4,15 @@ console.log("alias x='node 20190622_Saturday/20190622.js'");
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions/Assertions
 
-console.log(/\d+(?!\.)/g.exec('3.141'));
+
+// Basic example Negative lookahead assertion /x(?!y)/;
+console.log(/\d+(?!\.)/g.exec('3.141')); // [ '141', index: 2, input: '3.141' ]
+
+let orangeNotLemon = "Do you want to have an orange? Yes, I do not want to have a lemon!";
+
+// Different meaning of '?!' combination usage in Assertions /x(?!y)/ and  Ranges /[^?!]/
+let selectNotLemonRegex = /[^?!]+have(?! a lemon)[^?!]+[?!]/gi
+console.log(orangeNotLemon.match(selectNotLemonRegex)); // [ 'Do you want to have an orange?' ]
+
+let selectNotOrangeRegex = /[^?!]+have(?! an orange)[^?!]+[?!]/gi
+console.log(orangeNotLemon.match(selectNotOrangeRegex)); // [ ' Yes, I do not want to have a lemon!' ]
