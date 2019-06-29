247d06b85d22ba1b1a4786ac343eb1ab3d6fe274
Dmitry <lbvf50.mobile@gmail.com>
2019-06-29 22:31:39 +0700

    330. BASH Indirection (thnx: @ob_hs). (fix BASH comments)

--- 20190629_Saturday/20190629.sh
+++ 20190629_Saturday/20190629.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 echo "alias x='bash 20190629_Saturday/20190629.sh'" 
 
-// BASH Indirection (thnx: @ob_hs).
+# BASH Indirection (thnx: @ob_hs).
 
-// https://wiki.bash-hackers.org/syntax/pe#indirection
-// thnx: @ob_hs
+# https://wiki.bash-hackers.org/syntax/pe#indirection
+# thnx: @ob_hs
 
 red=`tput setaf 1`
 green=`tput setaf 2`
