591237a4f93019c3fc63f52bca2bec1cca5d4fcb
Dmitry <lbvf50.mobile@gmail.com>
2019-07-06 12:29:25 +0700

    Amazon: Add JS and script to positions visualizator

--- 20190706_Saturday/20190706.rb
+++ 20190706_Saturday/20190706.rb
@@ -1,6 +1,6 @@
 p "alias x='ruby 20190706_Saturday/20190706.rb'" 
 require 'sinatra'
-
+set :public_folder, File.dirname(__FILE__) + '/static'
 get '/' do
     File.read('20190706_Saturday/index.html')
 end
