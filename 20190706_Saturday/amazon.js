30aae217aaa0f74eca82666b4077dd9399030965
Dmitry <lbvf50.mobile@gmail.com>
2019-07-10 17:03:34 +0700

    Amazon add colors on the board: Be not afraid of growing slowly, be afraid of standing still.

--- 20190706_Saturday/amazon.js
+++ 20190706_Saturday/amazon.js
@@ -4,14 +4,15 @@ const board_table_id = 'board'
 const board_array = () => [...Array(8).keys()].reverse()
 .map(row => [...Array(8).keys()].map( column => [column,row]) );
 const crd2str = a => `${String.fromCharCode(a[0]+'a'.charCodeAt(0))}${a[1]+1}`;
-const str2crd = a => [a[0].charCodeAt(0)-'a'.charCodeAt(0),parseInt(a[1])-1]
+const str2crd = a => [a[0].charCodeAt(0)-'a'.charCodeAt(0),parseInt(a[1])-1];
+const color = a => 0 == (a[0]+a[1])%2 ? 'black' : 'white';
 
 const append_board_table = () =>{
     let arr = board_array();
     arr.forEach(row =>{
         let html_row = $('<tr></tr>');
         row.forEach(cell =>{
-            let html_cell = $(`<td>${cell[0]};${cell[1]};${crd2str(cell)}; ${str2crd(crd2str(cell))}</td>`);
+            let html_cell = $(`<td class="cell ${color(cell)}" >${cell[0]};${cell[1]};${crd2str(cell)}; ${str2crd(crd2str(cell))}</td>`);
             html_row.append(html_cell);
         });
         $(`#${board_table_id}`).append(html_row);
