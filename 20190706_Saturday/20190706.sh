1a319eab21db0f76ce755c3b1c5cabd3159f7bff
Dmitry <lbvf50.mobile@gmail.com>
2019-07-10 16:24:14 +0700

    Amazon add static server by browser-sync: be not afraind of growing slowly, be afraind of standing still.

--- 20190706_Saturday/20190706.sh
+++ 20190706_Saturday/20190706.sh
@@ -4,8 +4,11 @@ red=`tput setaf 1`
 green=`tput setaf 2`
 reset=`tput sgr0`
 
-echo "${green}localhost:4567${reset}"
+echo "${red}Change working directory: ${reset}${green}(but it dose not harms parent process)${rest}"
+echo "${green}browser-sync start --file 20190706_Saturday/index.html --server${reset}"
 # does not works :(
 # https://scotch.io/tutorials/how-to-use-browsersync-for-faster-development
 # https://github.com/jwald200/browser_sync_proxy
 # browser-sync start --proxy "localhost:4567" # --files "20190706_Saturday/static/*.*"
+cd 20190706_Saturday/
+browser-sync start --file index.html --server 
