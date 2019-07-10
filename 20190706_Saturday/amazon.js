08c5bf41c447f41bfeb4c4b45db163e4a2ce6308
Dmitry <lbvf50.mobile@gmail.com>
2019-07-10 16:54:12 +0700

    Amazon browser-sync now can crd2str/str2crd: Be no afrain of growing slowly, be afrain of standing still.

--- 20190706_Saturday/amazon.js
+++ 20190706_Saturday/amazon.js
@@ -3,13 +3,15 @@ const board_table_id = 'board'
 
 const board_array = () => [...Array(8).keys()].reverse()
 .map(row => [...Array(8).keys()].map( column => [column,row]) );
+const crd2str = a => `${String.fromCharCode(a[0]+'a'.charCodeAt(0))}${a[1]+1}`;
+const str2crd = a => [a[0].charCodeAt(0)-'a'.charCodeAt(0),parseInt(a[1])-1]
 
 const append_board_table = () =>{
     let arr = board_array();
     arr.forEach(row =>{
         let html_row = $('<tr></tr>');
         row.forEach(cell =>{
-            let html_cell = $(`<td>${cell[0]};${cell[1]}</td>`);
+            let html_cell = $(`<td>${cell[0]};${cell[1]};${crd2str(cell)}; ${str2crd(crd2str(cell))}</td>`);
             html_row.append(html_cell);
         });
         $(`#${board_table_id}`).append(html_row);
