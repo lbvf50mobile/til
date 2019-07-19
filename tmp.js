575bc083e5cd59dce4bf62a4791092e10e4159c7
Dmitry <lbvf50.mobile@gmail.com>
2019-07-19 22:07:24 +0700

    simple assembler interpreter: one

--- tmp.js
+++ tmp.js
@@ -1,3 +1,10 @@
-sum  = 0
-a  = [1,2,3]
-[1,2].map(x => sum+=x);
+// https://www.codewars.com/kata/simple-assembler-interpreter/train/javascript
+function simple_assembler(program) {
+    let cmd = program.map( x => new Object({c:x.split(' ')[0], a:x.split(' ')[1], b:x.split(' ')[2]}));
+    let act = {
+      mov: (({a,b}) => console.log('mov', a,b)),
+      inc: (({a,b}) => console.log('inc', a,b)),
+      dec: (({a,b}) => console.log('mov', a,b)),
+      jnz: (({a,b}) => console.log('mov', a,b)),
+    }
+  }
