a747895a9f51f0b6077c8b76d550ff51c5f63ae8
Dmitry <lbvf50.mobile@gmail.com>
2019-07-27 20:23:26 +0700

    Look behind

--- tmp.rb
+++ tmp.rb
@@ -1,6 +1,6 @@
 def one(solution, args)
     argumentVariants = args.join('|')
-    pattern = /([^\w$])(#{argumentVariants})(\W)/
+    p pattern = /([^\w$])(#{argumentVariants})(\W)/
     repl = '\1$\2\3'
     p solution.scan(pattern)
     return solution.gsub(pattern, repl)
@@ -8,12 +8,15 @@ end
 
 def two(solution, args)
     argumentVariants = args.join('|')
-    pattern = /(#{argumentVariants})(?=[^\w$])(\W)/
+    p pattern = /(?<=[^\w$])(#{argumentVariants})(\W)/
     repl = '$\1\2'
     p solution.scan(pattern)
     return solution.gsub(pattern, repl)
 end
 
 require 'colorize'
-puts "%s" % one("function returnSecond(fu_,_re5,NOO) {\t  return _re5;\t}",["fu_", "_re5", "NOO"]).red
+# puts "%s" % one("function returnSecond(fu_,_re5,NOO) {\t  return _re5;\t}",["fu_", "_re5", "NOO"]).red
 puts "%s" % two("function returnSecond(fu_,_re5,NOO) {\t  return _re5;\t}",["fu_", "_re5", "NOO"]).green
+
+# puts "%s" % one("function add($n, m) {\t  return n + $m;\t}",[?n,?m]).red
+puts "%s" % two("function add($n, m) {\t  return n + $m;\t}",[?n,?m]).green
