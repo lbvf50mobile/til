04689e616921fa2bc37a4ef0a618347c3218a1ab
Dmitry <lbvf50.mobile@gmail.com>
2019-07-27 19:41:31 +0700

    one work second break

--- tmp.rb
+++ tmp.rb
@@ -1,12 +1,19 @@
-def programTranslation(solution, args)
+def one(solution, args)
     argumentVariants = args.join('|')
-    p argumentVariants
     pattern = /([^\w$])(#{argumentVariants})(\W)/
-    p pattern
     repl = '\1$\2\3'
     p solution.scan(pattern)
     return solution.gsub(pattern, repl)
 end
 
+def two(solution, args)
+    argumentVariants = args.join('|')
+    pattern = /(#{argumentVariants})(?=[^\w$])(\W)/
+    repl = '$\1\2'
+    p solution.scan(pattern)
+    return solution.gsub(pattern, repl)
+end
 
-p programTranslation("function add($n, m) {\t  return n + $m;\t}",[?n,?m]);
+require 'colorize'
+puts "%s" % one("function returnSecond(fu_,_re5,NOO) {\t  return _re5;\t}",["fu_", "_re5", "NOO"]).red
+puts "%s" % two("function returnSecond(fu_,_re5,NOO) {\t  return _re5;\t}",["fu_", "_re5", "NOO"]).green
