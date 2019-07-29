f7dd48524bec1631f94707ea62aa0c852f30205c
Dmitry <lbvf50.mobile@gmail.com>
2019-07-29 20:11:29 +0700

    repetitonEncription

--- 20190729_Monday/20190729_repetition_encription.rb
+++ 20190729_Monday/20190729_repetition_encription.rb
@@ -8,7 +8,7 @@ require "minitest/autorun"
 require 'ostruct'
 
 def repetitionEncryption(letter)
-    pattern = /([a-z]+)[\W]+\1/i
+    pattern = /(?:[^a-z]|^)([a-z]+)[\W]+\1(?:[^a-z]|$)/i
     return letter.scan(pattern).size
 end
 
@@ -19,6 +19,18 @@ describe "repetitionEncryption" do
             OpenStruct.new({
                 letter: "Hi, hi Jane! I'm so. So glad to to finally be able to write - WRITE!! - to you!",
                 answer: 4
+            }),
+            OpenStruct.new({
+                letter: "Everything is fine, fine perfectly here. Here I have fun (fun all the day!) days. Although I miss you, so please please, Jane, write, write me whenever you can! Can you do that? That is the only (!!ONLY!!) thing I ask from you, you sunshine.",
+                answer: 9
+            }),
+            OpenStruct.new({
+                letter: "ho-ho--he-he",
+                answer: 2
+            }),
+            OpenStruct.new({
+                letter: "WeLl wElL",
+                answer: 1
             }),
             
         ].each{|x| assert_equal x.answer, repetitionEncryption(x.letter)}
