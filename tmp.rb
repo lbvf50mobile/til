6c5b74c2c6fbef95f0d88fc8a92875c5bc8023af
Dmitry <lbvf50.mobile@gmail.com>
2019-07-30 20:32:03 +0700

    lrc2subRip empty text

--- tmp.rb
+++ tmp.rb
@@ -1,22 +1,2 @@
-def one(solution, args)
-    argumentVariants = args.join('|')
-    p pattern = /(?<=[^\w$])(#{argumentVariants})(\W)/
-    repl = "$\1\2"
-    p solution.scan(pattern)
-    return solution.gsub(pattern, repl)
-end
-
-def two(solution, args)
-    argumentVariants = args.join('|')
-    p pattern = /(?<=[^\w$])(#{argumentVariants})(\W)/
-    repl = '$\1\2'
-    p solution.scan(pattern)
-    return solution.gsub(pattern, repl)
-end
-
-require 'colorize'
-puts "%s" % one("function returnSecond(fu_,_re5,NOO) {\t  return _re5;\t}",["fu_", "_re5", "NOO"]).red
-puts "%s" % two("function returnSecond(fu_,_re5,NOO) {\t  return _re5;\t}",["fu_", "_re5", "NOO"]).green
-
-puts "%s" % one("function add($n, m) {\t  return n + $m;\t}",[?n,?m]).red
-puts "%s" % two("function add($n, m) {\t  return n + $m;\t}",[?n,?m]).green
+p "[00:12.00] Happy birthday dear coder,".scan(/^\[(\d+):(\d+)\.(\d+)\]( (.*)){0,1}$/)
+p "[00:12.00]".scan(/^\[(\d+):(\d+)\.(\d+)\]( (.*)){0,1}$/)
