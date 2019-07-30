82edc3d8d7fa19e5a28b06375a33d31e2f28156c
Dmitry <lbvf50.mobile@gmail.com>
2019-07-30 19:33:30 +0700

    hour impromevents

--- 20190730_Tuesday/lrc2subrip.rb
+++ 20190730_Tuesday/lrc2subrip.rb
@@ -1,7 +1,7 @@
 class Task
 def length2secs len; arr = len.split(?:).map(&:to_i); arr[0]*60*60 + arr[1] * 60 + arr[2] end
 def lrc2hash str; arr = str.scan(/^\[(\d+):(\d+)\.(\d+)\] (.*)$/)[0]; {sec: arr[0].to_i*60 + arr[1].to_i, xx: arr[2].to_i, text: arr[3]} end
-def sec2hh_mm_ss sec; [sec/(60*60), (sec-sec/(60*60))/60, sec%60] end
+def sec2hh_mm_ss sec; [sec/(60*60), (sec-(sec/(60*60))*60*60)/60, sec%60] end
 def subrip_time sec_xx_hash
     arr =  sec2hh_mm_ss(sec_xx_hash[:sec])
     "%02d:%02d:%02d,%02d0" % [*arr,sec_xx_hash[:xx]]
