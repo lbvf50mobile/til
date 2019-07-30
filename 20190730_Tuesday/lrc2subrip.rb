054a8ceee576be7b42d4e6e41b7221092e78bf2f
Dmitry <lbvf50.mobile@gmail.com>
2019-07-30 18:57:08 +0700

    lrc2subRip new regex

--- 20190730_Tuesday/lrc2subrip.rb
+++ 20190730_Tuesday/lrc2subrip.rb
@@ -1,6 +1,6 @@
 class Task
 def length2secs len; arr = len.split(?:).map(&:to_i); arr[0]*60*60 + arr[1] * 60 + arr[2] end
-def lrc2secs str; arr = str.scan(/^\[(d+):(d+)\.(d+)\]/) end
+def lrc2secs str; arr = str.scan(/^\[(\d+):(\d+)\.(\d+)\]/)[0]; {sec: arr[0].to_i*60 + arr[1].to_i, xx: arr[2].to_i} end
 def lrc2subRip(lrcLyrics, songLength)
     p stop = length2secs(songLength)
     p lrcLyrics.map{|x| p x; lrc2secs(x)}
