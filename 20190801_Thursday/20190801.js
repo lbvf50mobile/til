e7b3cac7e629e38322cfa8b98b78ae5a26ad6dd6
Dmitry <lbvf50.mobile@gmail.com>
2019-08-02 00:51:39 +0700

    351. JS use regex in String.prototype.replace. And the way creae new RegExp, with options as arguments form string. +1

--- 20190801_Thursday/20190801.js
+++ 20190801_Thursday/20190801.js
@@ -1,6 +1,7 @@
 console.log("alias x='node 20190801_Thursday/20190801.js'");
 
-// JS use regex in String.prototype.replace.
+// JS use regex in String.prototype.replace. And the way creae new RegExp, with options as arguments form string.
 
 console.log("using string as replace",  "11111".replace(1,"-replace-"))
+
 console.log('usiong regex', "11111".replace(new RegExp(1,'g'),"-replace-"))
