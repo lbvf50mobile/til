67ecadd84ec29b2937b5eb8830e6194b9d0334d8
Dmitry <lbvf50.mobile@gmail.com>
2019-08-03 23:37:06 +0700

    cellsJoing start

---
+++ 20190803_Saturday/20190803cellsJoining.rb
@@ -0,0 +1,34 @@
+p "alias x='ruby 20190803_Saturday/20190803cellsJoining.rb'" 
+
+# Ruby cellsJoining
+
+require "minitest/autorun"
+require_relative "cellsJoining.rb"
+require 'ostruct'
+
+describe "cellsJoining" do
+    it "Pass the tests" do
+        [
+            OpenStruct.new({
+                table: ["+----+--+-----+----+", 
+                    "|abcd|56|!@#$%|qwer|", 
+                    "|1234|78|^&=()|tyui|", 
+                    "+----+--+-----+----+", 
+                    "|zxcv|90|77777|stop|", 
+                    "+----+--+-----+----+", 
+                    "|asdf|~~|ghjkl|100$|", 
+                    "+----+--+-----+----+"],
+                    coords: [[2,0], 
+                    [1,1]],
+                answer: ["+----+--+-----+----+", 
+                    "|abcd|56|!@#$%|qwer|", 
+                    "|1234|78|^&=()|tyui|", 
+                    "+----+--+-----+----+", 
+                    "|zxcv 90|77777|stop|", 
+                    "|       +-----+----+", 
+                    "|asdf ~~|ghjkl|100$|", 
+                    "+-------+-----+----+"]
+            }),
+        ].each{|x| assert_equal x.answer, Task.new.cellsJoining(x.input)}
+    end
+end
