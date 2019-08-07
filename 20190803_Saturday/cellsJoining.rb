3e4a345673f0991859c25ed4ad22370d60ea3515
Dmitry <lbvf50.mobile@gmail.com>
2019-08-07 20:55:43 +0700

    cells Joins Display

--- 20190803_Saturday/cellsJoining.rb
+++ 20190803_Saturday/cellsJoining.rb
@@ -1,12 +1,28 @@
+require 'colorize'
 class Task
     def replace(str,width)
         str = str.gsub(/^(\+-+){#{width}}/,' ')
         str[0] = ?|
         str
     end
-    def cellsJoining(input)
+    def cellsJoining(input,coords)
+        
         get_columns_size = ->arr{arr[0].count(?+)-1}
         p columns_size = get_columns_size[input]
+        bottom, left = coords[0]
+        top, right = coords[1]
+
+        start, finish = top+1, bottom
+        
+        hash_positions = {bottom: bottom, top: top, left: left, right: right}
+        shows_bounds hash_positions
+        shows_start_finish(start: start, finish: finish)
+
+        input.each do |x|
+            is_border = (?+ == x[0])
+            puts "%s %s" % [x, is_border.inspect]
+        end
+
         ["+----+--+-----+----+", 
             "|abcd|56|!@#$%|qwer|", 
             "|1234|78|^&=()|tyui|", 
@@ -16,4 +32,14 @@ class Task
             "|asdf ~~|ghjkl|100$|", 
             "+-------+-----+----+"]
     end
+    # ---------------------------------
+    def shows_bounds h
+        arr = [h[:top],h[:bottom],h[:left], h[:right]].map(&:to_s).map(&:green)
+        puts "top: %s, bottom: %s, left: %s, right: %s" % arr
+    end
+    def shows_start_finish h
+        arr = [h[:start],h[:finish]].map(&:to_s).map(&:cyan)
+        puts "start: %s, finish: %s" % arr
+    end
+    
 end
