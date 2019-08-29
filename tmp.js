3ef1e45bfd973e4f189b1a4f8b596f0ad0ee207b
Dmitry <lbvf50.mobile@gmail.com>
2019-08-29 23:24:17 +0700

    First tast from @kotsabiukmv

--- tmp.js
+++ tmp.js
@@ -1,41 +1,29 @@
-// https://www.codewars.com/users/Steffan153 about [...Array(1000).keys()]
+// @kotsabiukmv
 
-var timer = function(name) {
-  var start = new Date();
-  return {
-      stop: function() {
-          var end  = new Date();
-          var time = end.getTime() - start.getTime();
-          console.log('Timer:', name, 'finished in', time, 'ms');
-      }
+// Array generator
+let generate = x => Array.from({length: x}, x => Math.random() > 0.5);
+
+
+// 1) able to jump only over 1 postion
+let check1 = arr => {
+  if(!arr[0]) return false; // need to be able to start.
+  for(var i = 0; i < arr.length ; i++){
+    if(!arr[i] && i+1 < arr.length && !arr[i+1]) return false;
   }
-};
-
-first = timer('[...Array(1000).keys()]')
-for(i = 0; i < 100000; i++){
-   [...Array(1000).keys()]
+  return true;
 }
-first.stop(); // Timer: [...Array(1000).keys()] finished in 9291 ms
 
-second = timer('Array.from({length: 1000}, (i,_) => i)')
-for(i = 0; i < 100000; i++){
-  Array.from({lenght: 1000}, (i,_) => i)
-}
-second.stop(); // Timer: Array.from({length: 1000}, (i,_) => i) finished in 57 ms
+test = [[true,false,true],true]
+console.log(`Array [${String(test[0])}] must be  ${test[1]}: it is ${check1(test[0])}`)
 
+test = [[true,true,true],true]
+console.log(`Array [${String(test[0])}] must be  ${test[1]}: it is ${check1(test[0])}`)
 
-first = timer('[...Array(1000).keys()]: only once');
-   [...Array(1000).keys()]
-first.stop(); // Timer: [...Array(1000).keys()] finished in 9291 ms
+test = [[true,true,false],true]
+console.log(`Array [${String(test[0])}] must be  ${test[1]}: it is ${check1(test[0])}`)
 
-second = timer('Array.from({length: 1000}, (i,_) => i): only once')
-  Array.from({lenght: 1000}, (i,_) => i)
-second.stop(); // Timer: Array.from({length: 1000}, (i,_) => i) finished in 57 ms
+test = [[false,true,false],false]
+console.log(`Array [${String(test[0])}] must be  ${test[1]}: it is ${check1(test[0])}`)
 
-first = timer('[...Array(100).keys()]: 1000 * 100');
-for(i = 0; i < 1000; i++) [...Array(100).keys()]
-first.stop(); // Timer: [...Array(1000).keys()] finished in 9291 ms
-
-second = timer('Array.from({length: 100}, (i,_) => i): 1000 * 100')
-for(i = 0; i < 1000; i++) Array.from({lenght: 100}, (i,_) => i)
-second.stop(); // Timer: Array.from({length: 1000}, (i,_) => i) finished in 57 ms
+test = [[true,false,false],false]
+console.log(`Array [${String(test[0])}] must be  ${test[1]}: it is ${check1(test[0])}`)
