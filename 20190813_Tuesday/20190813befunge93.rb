0129ff5986372a0d25b195cb743735b334b9f6d6
Dmitry <lbvf50.mobile@gmail.com>
2019-09-04 21:20:16 +0700

    Befunge93; moving right

--- 20190813_Tuesday/20190813befunge93.rb
+++ 20190813_Tuesday/20190813befunge93.rb
@@ -21,66 +21,75 @@ describe "befunge93" do
     end
 
     it  "If the stack is empty and it is necessary to pop a value, no exception is raised; instead, 0 is produced." do
-        bomba = {stack: [1]}
-        assert_equal 1, Task.new.mega_pop(bomba)
-        assert_equal [], bomba[:stack]
-        assert_equal 0, Task.new.mega_pop(bomba)
-        assert_equal [], bomba[:stack]
+        state = {stack: [1]}
+        assert_equal 1, Task.new.mega_pop(state)
+        assert_equal [], state[:stack]
+        assert_equal 0, Task.new.mega_pop(state)
+        assert_equal [], state[:stack]
     end
     it "should move left/rigth/top/bottom" do
-        bomba = {x_position: 0, y_position: 0, width: 3, height: 4, direction: 'left'}
+        state = {x_position: 0, y_position: 0, width: 3, height: 4, direction: 'left'}
         
         # moving left
-        bomba[:direction] = 'left'
-        Task.new.move_pointer(bomba)
-        assert_equal 1, bomba[:x_position]
-        Task.new.move_pointer(bomba)
-        assert_equal 2, bomba[:x_position]
-        Task.new.move_pointer(bomba)
-        assert_equal 0, bomba[:x_position]
+        state[:direction] = 'left'
+        Task.new.move_pointer(state)
+        assert_equal 1, state[:x_position]
+        Task.new.move_pointer(state)
+        assert_equal 2, state[:x_position]
+        Task.new.move_pointer(state)
+        assert_equal 0, state[:x_position]
         
         # moving right
-        bomba[:direction] = 'right'
-        Task.new.move_pointer(bomba)
-        assert_equal 2, bomba[:x_position]
-        Task.new.move_pointer(bomba)
-        assert_equal 1, bomba[:x_position]
-        Task.new.move_pointer(bomba)
-        assert_equal 0, bomba[:x_position]
-        Task.new.move_pointer(bomba)
-        assert_equal 2, bomba[:x_position]
+        state[:direction] = 'right'
+        Task.new.move_pointer(state)
+        assert_equal 2, state[:x_position]
+        Task.new.move_pointer(state)
+        assert_equal 1, state[:x_position]
+        Task.new.move_pointer(state)
+        assert_equal 0, state[:x_position]
+        Task.new.move_pointer(state)
+        assert_equal 2, state[:x_position]
 
         # moving down
-        bomba[:direction] = 'down'
-        Task.new.move_pointer(bomba)
-        assert_equal 1, bomba[:y_position]
-        Task.new.move_pointer(bomba)
-        assert_equal 2, bomba[:y_position]
-        Task.new.move_pointer(bomba)
-        assert_equal 3, bomba[:y_position]
-        Task.new.move_pointer(bomba)
-        assert_equal 0, bomba[:y_position]
-        assert_equal 2, bomba[:x_position]
+        state[:direction] = 'down'
+        Task.new.move_pointer(state)
+        assert_equal 1, state[:y_position]
+        Task.new.move_pointer(state)
+        assert_equal 2, state[:y_position]
+        Task.new.move_pointer(state)
+        assert_equal 3, state[:y_position]
+        Task.new.move_pointer(state)
+        assert_equal 0, state[:y_position]
+        assert_equal 2, state[:x_position]
 
          # moving up
-         bomba[:direction] = 'up'
-         Task.new.move_pointer(bomba)
-         assert_equal 3, bomba[:y_position]
-         Task.new.move_pointer(bomba)
-         assert_equal 2, bomba[:y_position]
-         Task.new.move_pointer(bomba)
-         assert_equal 1, bomba[:y_position]
-         Task.new.move_pointer(bomba)
-         assert_equal 0, bomba[:y_position]
-         assert_equal 2, bomba[:x_position]
+         state[:direction] = 'up'
+         Task.new.move_pointer(state)
+         assert_equal 3, state[:y_position]
+         Task.new.move_pointer(state)
+         assert_equal 2, state[:y_position]
+         Task.new.move_pointer(state)
+         assert_equal 1, state[:y_position]
+         Task.new.move_pointer(state)
+         assert_equal 0, state[:y_position]
+         assert_equal 2, state[:x_position]
     end
     it "should use bridge left bridge; skip next cell" do
-        bomba = {x_position: 0, y_position: 0, width: 3, height: 4, direction: 'left'}
-        Task.new.move_bridge(bomba)
-        assert_equal 0, bomba[:y_position]
-        assert_equal 2, bomba[:x_position]
-        Task.new.move_bridge(bomba)
-        assert_equal 0, bomba[:y_position]
-        assert_equal 1, bomba[:x_position]
+        state = {x_position: 0, y_position: 0, width: 3, height: 4, direction: 'left'}
+        Task.new.move_bridge(state)
+        assert_equal 0, state[:y_position]
+        assert_equal 2, state[:x_position]
+        Task.new.move_bridge(state)
+        assert_equal 0, state[:y_position]
+        assert_equal 1, state[:x_position]
+    end
+    it "should use bridge right bridge; skip next cell" do
+        state = {x_position: 2, y_position: 1, width: 3, height: 4, direction: 'right'}
+        Task.new.move_bridge(state)
+        assert_equal 1, state[:y_position]
+        assert_equal 0, state[:x_position]
+        Task.new.move_bridge(state)
+        assert_equal 1, state[:y_position]
+        assert_equal 1, state[:x_position]
     end
 end
