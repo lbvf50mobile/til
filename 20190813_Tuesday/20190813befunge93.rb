302c891b413f1f810bb96c4bd899fdfa52f73ba4
Dmitry <lbvf50.mobile@gmail.com>
2019-09-05 17:57:09 +0700

    Beunge93 put direction instructions into the separate file

--- 20190813_Tuesday/20190813befunge93.rb
+++ 20190813_Tuesday/20190813befunge93.rb
@@ -6,6 +6,8 @@ require "minitest/autorun"
 require_relative "befunge93.rb"
 require 'ostruct'
 
+require_relative "tests/direction_instructions.rb"
+
 describe "befunge93" do
     it "Pass the tests" do
         [
@@ -18,96 +20,5 @@ describe "befunge93" do
                 answer: "Hello World!\n"
             }),
         ].each{|x| assert_equal x.answer, Task.new.befunge93(x.program)}
-    end
-
-    it  "If the stack is empty and it is necessary to pop a value, no exception is raised; instead, 0 is produced." do
-        state = {stack: [1]}
-        assert_equal 1, Task.new.mega_pop(state)
-        assert_equal [], state[:stack]
-        assert_equal 0, Task.new.mega_pop(state)
-        assert_equal [], state[:stack]
-    end
-    it "should move left/rigth/top/bottom" do
-        state = {x_position: 0, y_position: 0, width: 3, height: 4, direction: 'left'}
-        
-        # moving left
-        state[:direction] = 'left'
-        Task.new.move_pointer(state)
-        assert_equal 1, state[:x_position]
-        Task.new.move_pointer(state)
-        assert_equal 2, state[:x_position]
-        Task.new.move_pointer(state)
-        assert_equal 0, state[:x_position]
-        
-        # moving right
-        state[:direction] = 'right'
-        Task.new.move_pointer(state)
-        assert_equal 2, state[:x_position]
-        Task.new.move_pointer(state)
-        assert_equal 1, state[:x_position]
-        Task.new.move_pointer(state)
-        assert_equal 0, state[:x_position]
-        Task.new.move_pointer(state)
-        assert_equal 2, state[:x_position]
-
-        # moving down
-        state[:direction] = 'down'
-        Task.new.move_pointer(state)
-        assert_equal 1, state[:y_position]
-        Task.new.move_pointer(state)
-        assert_equal 2, state[:y_position]
-        Task.new.move_pointer(state)
-        assert_equal 3, state[:y_position]
-        Task.new.move_pointer(state)
-        assert_equal 0, state[:y_position]
-        assert_equal 2, state[:x_position]
-
-         # moving up
-         state[:direction] = 'up'
-         Task.new.move_pointer(state)
-         assert_equal 3, state[:y_position]
-         Task.new.move_pointer(state)
-         assert_equal 2, state[:y_position]
-         Task.new.move_pointer(state)
-         assert_equal 1, state[:y_position]
-         Task.new.move_pointer(state)
-         assert_equal 0, state[:y_position]
-         assert_equal 2, state[:x_position]
-    end
-    it "should use bridge left bridge; skip next cell" do
-        state = {x_position: 0, y_position: 0, width: 3, height: 4, direction: 'left'}
-        Task.new.move_bridge(state)
-        assert_equal 0, state[:y_position]
-        assert_equal 1, state[:x_position]
-        Task.new.move_bridge(state)
-        assert_equal 0, state[:y_position]
-        assert_equal 2, state[:x_position]
-    end
-    it "should use bridge right bridge; skip next cell" do
-        state = {x_position: 2, y_position: 1, width: 3, height: 4, direction: 'right'}
-        Task.new.move_bridge(state)
-        assert_equal 1, state[:y_position]
-        assert_equal 1, state[:x_position]
-        Task.new.move_bridge(state)
-        assert_equal 1, state[:y_position]
-        assert_equal 0, state[:x_position]
-    end
-    it "should use bridge DOWN bridge; skip next cell" do
-        state = {x_position: 2, y_position: 3, width: 3, height: 4, direction: 'down'}
-        Task.new.move_bridge(state)
-        assert_equal 0, state[:y_position]
-        assert_equal 2, state[:x_position]
-        Task.new.move_bridge(state)
-        assert_equal 1, state[:y_position]
-        assert_equal 2, state[:x_position]
-    end
-    it "should use bridge UP bridge; skip next cell" do
-        state = {x_position: 2, y_position: 2, width: 3, height: 4, direction: 'up'}
-        Task.new.move_bridge(state)
-        assert_equal 1, state[:y_position]
-        assert_equal 2, state[:x_position]
-        Task.new.move_bridge(state)
-        assert_equal 0, state[:y_position]
-        assert_equal 2, state[:x_position]
     end
 end
---
+++ 20190813_Tuesday/tests/direction_instructions.rb
@@ -0,0 +1,92 @@
+describe "Befunge-93 Direction instructions" do
+    it  "If the stack is empty and it is necessary to pop a value, no exception is raised; instead, 0 is produced." do
+        state = {stack: [1]}
+        assert_equal 1, Task.new.mega_pop(state)
+        assert_equal [], state[:stack]
+        assert_equal 0, Task.new.mega_pop(state)
+        assert_equal [], state[:stack]
+    end
+    it "should move left/rigth/top/bottom" do
+        state = {x_position: 0, y_position: 0, width: 3, height: 4, direction: 'left'}
+        
+        # moving left
+        state[:direction] = 'left'
+        Task.new.move_pointer(state)
+        assert_equal 1, state[:x_position]
+        Task.new.move_pointer(state)
+        assert_equal 2, state[:x_position]
+        Task.new.move_pointer(state)
+        assert_equal 0, state[:x_position]
+        
+        # moving right
+        state[:direction] = 'right'
+        Task.new.move_pointer(state)
+        assert_equal 2, state[:x_position]
+        Task.new.move_pointer(state)
+        assert_equal 1, state[:x_position]
+        Task.new.move_pointer(state)
+        assert_equal 0, state[:x_position]
+        Task.new.move_pointer(state)
+        assert_equal 2, state[:x_position]
+
+        # moving down
+        state[:direction] = 'down'
+        Task.new.move_pointer(state)
+        assert_equal 1, state[:y_position]
+        Task.new.move_pointer(state)
+        assert_equal 2, state[:y_position]
+        Task.new.move_pointer(state)
+        assert_equal 3, state[:y_position]
+        Task.new.move_pointer(state)
+        assert_equal 0, state[:y_position]
+        assert_equal 2, state[:x_position]
+
+         # moving up
+         state[:direction] = 'up'
+         Task.new.move_pointer(state)
+         assert_equal 3, state[:y_position]
+         Task.new.move_pointer(state)
+         assert_equal 2, state[:y_position]
+         Task.new.move_pointer(state)
+         assert_equal 1, state[:y_position]
+         Task.new.move_pointer(state)
+         assert_equal 0, state[:y_position]
+         assert_equal 2, state[:x_position]
+    end
+    it "should use bridge left bridge; skip next cell" do
+        state = {x_position: 0, y_position: 0, width: 3, height: 4, direction: 'left'}
+        Task.new.move_bridge(state)
+        assert_equal 0, state[:y_position]
+        assert_equal 1, state[:x_position]
+        Task.new.move_bridge(state)
+        assert_equal 0, state[:y_position]
+        assert_equal 2, state[:x_position]
+    end
+    it "should use bridge right bridge; skip next cell" do
+        state = {x_position: 2, y_position: 1, width: 3, height: 4, direction: 'right'}
+        Task.new.move_bridge(state)
+        assert_equal 1, state[:y_position]
+        assert_equal 1, state[:x_position]
+        Task.new.move_bridge(state)
+        assert_equal 1, state[:y_position]
+        assert_equal 0, state[:x_position]
+    end
+    it "should use bridge DOWN bridge; skip next cell" do
+        state = {x_position: 2, y_position: 3, width: 3, height: 4, direction: 'down'}
+        Task.new.move_bridge(state)
+        assert_equal 0, state[:y_position]
+        assert_equal 2, state[:x_position]
+        Task.new.move_bridge(state)
+        assert_equal 1, state[:y_position]
+        assert_equal 2, state[:x_position]
+    end
+    it "should use bridge UP bridge; skip next cell" do
+        state = {x_position: 2, y_position: 2, width: 3, height: 4, direction: 'up'}
+        Task.new.move_bridge(state)
+        assert_equal 1, state[:y_position]
+        assert_equal 2, state[:x_position]
+        Task.new.move_bridge(state)
+        assert_equal 0, state[:y_position]
+        assert_equal 2, state[:x_position]
+    end
+end
