ea92cb58e534fced4bd417add150087a4c503702
Dmitry <lbvf50.mobile@gmail.com>
2019-09-10 19:58:23 +0700

    371. Go: Concurency in Go. Jake Wright. (improved)

--- 20190909_Monday/20190909.go
+++ 20190909_Monday/20190909.go
@@ -10,18 +10,41 @@ import (
 )
 
 func main() {
+	fmt.Println("alias x='go run 20190909_Monday/20190909.go'")
 	fmt.Println("Hello, playground")
+	steps := 40
+	jobs := make(chan int, steps)
+	results := make(chan int, steps)
+
+	go worker(jobs, results)
+	go worker(jobs, results)
+
+	for i:=0;  i<steps; i++{
+		jobs <- i
+	}
+	close(jobs)
+
+	for j := 0; j < steps; j++{
+		fmt.Println(<-results)
+	}
 } //test
+
+func one_worker(*jobs, *results, steps){
+
+}
+func two_worker(*jobs, *results, steps){
+	
+}
 
 func worker(jobs <-chan int, results chan<- int){
 	for n := range jobs{
-		results <-fib(n)
+		results <- fib(n)
 	}
 }
 
 func fib(n int) int{
 	if n <= 1 {
-		return 0
+		return n
 	} 
 	
 	return fib(n-1) + fib(n-2)
