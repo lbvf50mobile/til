b50fd469ce1405b41fd668d8654a3cd2ab836125
Dmitry <lbvf50.mobile@gmail.com>
2019-09-21 22:13:10 +0700

    381. Go: x := make([]int,4); fills array with zeors. append([]int{1},x...) will create 5 elements array. (add urls)

--- 20190921_Saturday/20190921.go
+++ 20190921_Saturday/20190921.go
@@ -1,5 +1,5 @@
     package main
-    // Go: x := make([]int, 4); fills array with zeros. append([]int{1}, x...) will create 5 elements array.""
+    // Go: x := make([]int, 4); fills array with zeros. append([]int{1}, x...) will create 5 elements array.
     import (
         "fmt"
     )
@@ -8,6 +8,7 @@
         // https://tour.golang.org/moretypes/11
         // Make fill slices with zeros
         // https://gist.github.com/lbvf50mobile/185fddd2b73bfe9f25a5c18227afab1b#file-pendulum-go-L4
+        // https://stackoverflow.com/a/16248257/8574922
         one := make([]int, 5)
         two := make([]int, 0)
         fmt.Println("alias x='go run 20190921_Saturday/20190921.go'")
