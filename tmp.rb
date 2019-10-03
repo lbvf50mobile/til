897e7944359241498bd4283562ad3117d2cfb40a
Dmitry <lbvf50.mobile@gmail.com>
2019-10-03 22:13:58 +0700

    Question about return value in ensure and resque

--- tmp.rb
+++ tmp.rb
@@ -1,15 +1,20 @@
-# https://www.codewars.com/kata/the-millionth-fibonacci-kata/train/javascript
-# https://www.geeksforgeeks.org/even-fibonacci-numbers-sum/
-prev1,prev2,cur = 1,0,0
-open('tmp.txt',?a) {|f|
-        f.puts "0n,"
-        f.puts '1n,' 
-        (2..2_000).each do |x|
-            cur = prev1 + prev2
-            f.puts "#{cur}n,"
-            prev2 = prev1
-            prev1 = cur
-        end
-}
+def a
+    1.some_wierd_method
+    "Boom"
+rescue NoMethodError
+    p "An Error: " + $!.message
+ensure
+    p "This  value mustbe returned"
+end
 
+p a
 
+def boom
+    if false
+        "a"
+    else
+        "double a"
+    end
+end
+
+p boom
