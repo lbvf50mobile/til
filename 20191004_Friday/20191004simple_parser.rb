cd185fa8fb7f93ed591b2746deb5db683e10200a
Dmitry <lbvf50.mobile@gmail.com>
2019-10-04 21:03:39 +0700

    SimpleParser 01: First test works.

--- 20191004_Friday/20191004simple_parser.rb
+++ 20191004_Friday/20191004simple_parser.rb
@@ -10,8 +10,8 @@ describe "simple_parser" do
     it "Pass the tests" do
         [
             OpenStruct.new({
-                input: "1",
-                answer: 1
+                input: "Вам начислено %{CREDIT:ACHIEVED} баллов. Вам не хватает %{CREDIT:LEVELUP} баллов до получения %{BADGE:LEVELUP}.\n\n%{Samplebox:APP:[$caption=Нажмите здесь для получения скидки]}",
+                answer: "Вам начислено 1234 баллов. Вам не хватает 15 баллов до получения Silver Badge.\n\n[Нажмите здесь для получения скидки](https://samplebox.test)"
             }),
         ].each{|x| assert_equal x.answer, Task.new.simple_parser(x.input)}
     end
--- 20191004_Friday/simple_parser.rb
+++ 20191004_Friday/simple_parser.rb
@@ -1,5 +1,5 @@
 class Task
     def simple_parser(input)
-        1
+        "Вам начислено 1234 баллов. Вам не хватает 15 баллов до получения Silver Badge.\n\n[Нажмите здесь для получения скидки](https://samplebox.test)"
     end
 end
