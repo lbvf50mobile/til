fbcb21ca311d544bfa598627b9f5e992ce9fed5c
Dmitry <lbvf50.mobile@gmail.com>
2019-10-26 23:05:54 +0700

    Befunge93 output instuctions tests.

--- 20190813_Tuesday/tests/output_instructions.rb
+++ 20190813_Tuesday/tests/output_instructions.rb
@@ -2,13 +2,43 @@ describe "Befunge-93 Output instructions" do
     
     # https://coolefriend.com/know-names-of-symbols-in-your-computer-keyboard/
     it ".: pop value and output it as an integer followed by a space" do
-        assert true
-        # Task.new.do_dot(state)
+        state = {
+            stack: [8,4,"x"], 
+            x_position: 2, 
+            y_position: 1, 
+            width: 3, 
+            height: 4, 
+            direction: 'right', 
+            program: [""],
+            output: ""}
+        Task.new.do_dot(state)
+        assert_equal "#{?x.ord} ", state[:output]
+        Task.new.do_dot(state)
+        assert_equal "#{?x.ord} 4 ", state[:output]
+        Task.new.do_dot(state)
+        assert_equal "#{?x.ord} 4  8 ", state[:output]
+        Task.new.do_dot(state)
+        assert_equal "#{?x.ord} 4  8 0 ", state[:output]
        
     end
     it ",: pop value and output it as ASCII character" do
-        assert true
-        # Task.new.do_comma(state)
+        # 
+        state = {
+            stack: [?B.ord,?A.ord,"x"], 
+            x_position: 2, 
+            y_position: 1, 
+            width: 3, 
+            height: 4, 
+            direction: 'right', 
+            program: [""],
+            output: ""}
+            Task.new.do_comma(state)
+        assert_equal "x", state[:output]
+        Task.new.do_comma(state)
+        assert_equal "xA", state[:output]
+        Task.new.do_comma(state)
+        assert_equal "xAB", state[:output]
+        
     end
     
 end
