da2344285fc53c7a07c480a248e4c483082e6647
Dmitry <lbvf50.mobile@gmail.com>
2019-10-26 21:48:23 +0700

    Add Output intsturctions to framework for Output instuctions

--- 20190813_Tuesday/tests/output_instructions.rb
+++ 20190813_Tuesday/tests/output_instructions.rb
@@ -1,5 +1,14 @@
 describe "Befunge-93 Output instructions" do
-    it "should works" do
-        assert true;
+    
+    # https://coolefriend.com/know-names-of-symbols-in-your-computer-keyboard/
+    it ".: pop value and output it as an integer followed by a space" do
+        assert true
+        # Task.new.do_dot(state)
+       
     end
+    it ",: pop value and output it as ASCII character" do
+        assert true
+        # Task.new.do_comma(state)
+    end
+    
 end
