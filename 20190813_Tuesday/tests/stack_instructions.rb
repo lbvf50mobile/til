0b2912c7e86cac83b2cb9f4f73c45c3828f369c2
Dmitry <lbvf50.mobile@gmail.com>
2019-10-26 20:43:54 +0700

    Befunge93 Stack instructions first tests. Be not afraid of growing slowly be afraind of standing still.

--- 20190813_Tuesday/tests/stack_instructions.rb
+++ 20190813_Tuesday/tests/stack_instructions.rb
@@ -1,6 +1,19 @@
 describe "Befunge-93 Logical stack instructions" do
     # https://coolefriend.com/know-names-of-symbols-in-your-computer-keyboard/
-    it "should work" do
-        assert true
+    it ":: duplicate value on top of the stack" do
+        state = {stack: [8,4], direction: 'left', x_position: 0, width: 3}
+        Task.new.do_colon(state)
+        assert_equal [8,4,4], state[:stack]
+       
+    end
+    it "\\: swap the top stack value with the second to the top" do
+        state = {stack: [8,4], direction: 'left', x_position: 0, width: 3}
+        Task.new.do_back_slash(state)
+        assert_equal [4,8], state[:stack]
+    end
+    it "$: pop value from the stack and discard it" do
+        state = {stack: [8,4], direction: 'left', x_position: 0, width: 3}
+        Task.new.do_dollar(state)
+        assert_equal [8], state[:stack]
     end
 end
