62f2497ab8c4743a0c8ff612b19a62f37c6752c1
Dmitry <lbvf50.mobile@gmail.com>
2019-10-26 23:54:06 +0700

    408. Bash: Using grep regex parathesis. Leetcode: valid-phone-number. (add more links)

--- 20191026_Saturday/20191026.sh
+++ 20191026_Saturday/20191026.sh
@@ -4,9 +4,14 @@
 
 echo "alias x='bash 20191026_Saturday/20191026.sh'" 
 
-# https://gist.github.com/lbvf50mobile/c534cd0a64ab40a6ad3bc84eda246b41
+# Leetcode: task and solution.
 # https://leetcode.com/problems/valid-phone-numbers/submissions/
+# https://gist.github.com/lbvf50mobile/c534cd0a64ab40a6ad3bc84eda246b41
+
+# Regex docs:
+# https://www.gnu.org/software/grep/manual/html_node/index.html
 # https://www.digitalocean.com/community/tutorials/using-grep-regular-expressions-to-search-for-text-patterns-in-linux
 # http://www.robelle.com/smugbook/regexpr.html
+# https://stackoverflow.com/questions/4761242/escaping-a-parenthesis-in-grep-ack
 
 grep '^\(([0-9]\{3\}) [0-9]\{3\}-[0-9]\{4\}\|[0-9]\{3\}-[0-9]\{3\}-[0-9]\{4\}\)$' file.txt
