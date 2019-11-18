cbb5be78af870c21f333ae2434fb8d5cebcfdf48
Dmitry <lbvf50.mobile@gmail.com>
2019-11-19 00:00:39 +0700

    427. Leetcode: is power of three (four)? (github + links)

--- 20191118_Monday/20191118.rb
+++ 20191118_Monday/20191118.rb
@@ -1,9 +1,14 @@
-# Leetcode: is power of three and four?
+# Leetcode: is power of three (four)?
 
 p "alias x='ruby 20191118_Monday/20191118.rb'" 
 
-# https://gist.github.com/lbvf50mobile/3113a45aefccaa11b6637658d5ae4bba https://leetcode.com/problems/power-of-three/submissions/
-# https://gist.github.com/lbvf50mobile/c35a44987e6246848b51b2434113d43b https://leetcode.com/problems/power-of-four/submissions/
+# Three
+# https://gist.github.com/lbvf50mobile/3113a45aefccaa11b6637658d5ae4bba 
+# https://leetcode.com/problems/power-of-three/submissions/
+
+# Four
+# https://gist.github.com/lbvf50mobile/c35a44987e6246848b51b2434113d43b 
+# https://leetcode.com/problems/power-of-four/submissions/
 
 x = 1
 
@@ -26,4 +31,13 @@ def is_power_of_three(n)
     return false if 0 != n%3
     return false if n <= 0
     return is_power_of_three(n/3)
+end
+
+# https://leetcode.com/problems/power-of-four/submissions/
+# Runtime: 40 ms, faster than 37.50% of Ruby online submissions for Power of Four.
+# Memory Usage: 9.5 MB, less than 100.00% of Ruby online submissions for Power of Four.
+def is_power_of_four(num)
+    return false if num < 1
+    return true if num == 1
+    0 == Math.log2(num)%2
 end
