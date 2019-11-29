452d29b8eb14b684fdca9be5c537e45cebe52da1
Dmitry <lbvf50.mobile@gmail.com>
2019-11-29 23:29:22 +0700

    Test ith leetcode.com nim-game

--- tmp.rb
+++ tmp.rb
@@ -1,57 +1,24 @@
-# @LazyNick
-=begin
-You have an array of ints. Create function which groups numbers by sum of pairs
-(sum = 5). Each element of array has to be used only once.
----
-test data:
-input: [22, 3, 5, 0, 2, 2]
-resutls: [[3, 2], [5, 0]]
-
-input: [-5, 33, 2, 2, 3, 5, 0, 10, 3]
-resutls: [[-5, 10], [2, 3], [2, 3], [5, 0]]
-
-input: [5, 5, 5, 0, 0, 0, 5]
-resutls: [[5, 0], [5, 0], [5, 0]]
-=end
+# https://leetcode.com/problems/nim-game/
 
 
-require "minitest/autorun"
-sum = 5
-check = [
-    [[22, 3, 5, 0, 2, 2],[[3, 2], [5, 0]]],
-    [[-5, 33, 2, 2, 3, 5, 0, 10, 3],[[-5, 10], [2, 3], [2, 3], [5, 0]]],
-    [[5, 5, 5, 0, 0, 0, 5],[[5, 0], [5, 0], [5, 0]]],
-] 
-
-
-def solution(x,sum)
-    hash = {}
-    answer = []
-
-
-    # Geneerate hash when it has keys ans values and values as array of keys
-    x.each_with_index do |value,index|
-        hash[value] ? hash[value].push(index) : hash[value] = [index]
-    end
-
-
-    # Generate answer by extracting appropriate values from the hash
-    x.each_with_index do |value,index|
-        tmp = hash[sum-value]
-        if tmp && ! tmp.empty?
-            i = tmp.shift 
-            answer.push([value,sum-value])  if i > index
-        end
-    end
-    
-    answer
+def can_win_nim(n)
+    @win = false;
+    dfs(n,true)
+    @win
 end
 
-
-describe "Find sum" do
-    it "return right answer" do
-        check.each do |input,output|
-            assert_equal output, solution(input,sum)
-        end
+def dfs(n,myturn)
+    return if @win
+    if n <= 3
+        @win = true if myturn
+        return
+    end
+    (1..3).each do |x|
+        dfs(n-x,!myturn)
     end
 end
+
+(1..10).each do |x|
+    p [x,can_win_nim(x)]
+end
+
