72e4e6842946deada2cb672f64ffef7de174407f
Dmitry <lbvf50.mobile@gmail.com>
2019-12-21 04:12:03 +0700

    Leetcode: 611. Valid Triangle Number.

--- tmp.rb
+++ tmp.rb
@@ -11,21 +11,43 @@ end
 
 def trr2 array
     ans = []
-    (0...array.size-1).each do |ai|
-        (ai+1...array.size).each do |bi|
-            (0...array.size).each do |ci|
-                if( ai != bi && bi != ci && ci != ai)
-                    a,b,c = array[ai], array[bi], array[ci]
-                    ans.push [a,b,c] if a+b < c && a+c < b && c+b < a
-                end
+    (0...array.size-2).each do |ai|
+        (ai+1...array.size-1).each do |bi|
+            (bi+1...array.size).each do |ci|
+                a,b,c = [array[ai],array[bi],array[ci]]
+                ans.push [a,b,c] if a+b > c && a+c > b && b+c > a
+                
             end
         end
     end
     ans
 end
 
-p triangles([1,2,4]) # [1,2,4]
-p triangles([1, 2, 3, 5, 6, 6, 10]).size # 20
 
-p trr2([1,2,4]) # [1,2,4]
 p trr2([1, 2, 3, 5, 6, 6, 10]).size # 20
+
+# https://leetcode.com/problems/valid-triangle-number/submissions/
+# @param {Integer[]} nums
+# @return {Integer}
+# Runtime: 108 ms, faster than 100.00% of Ruby online submissions for Valid Triangle Number.
+# Memory Usage: 9.8 MB, less than 100.00% of Ruby online submissions for Valid Triangle Number.
+
+def triangle_number(a)
+    a.sort!
+    r = 0
+    (2...a.size).reverse_each do |i|
+        s = a[i]
+        lo = 0
+        hi = i - 1
+        while lo < hi
+            if a[lo] + a[hi] <= s
+                lo += 1
+                next
+            end
+            r += hi - lo
+            hi -= 1
+        end
+    end
+    r
+    
+end
