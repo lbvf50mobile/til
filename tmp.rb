b31abb698ca274e78a0b4d24041b4cce40e7589a
Dmitry <lbvf50.mobile@gmail.com>
2019-12-21 03:38:18 +0700

    all tirangles.

--- tmp.rb
+++ tmp.rb
@@ -1,28 +1,31 @@
-# https://leetcode.com/problems/nim-game/
-# https://gist.github.com/lbvf50mobile/f8b22caacea83b61234f99e025e1ddee
+# Return all possible triangles.
+# https://leetcode.com/problems/valid-triangle-number/
 
-def can_win_nim(n)
-    n % 4 != 0
+def triangles array
+    ans = []
+    array.combination(2).each{|a,b|
+      array.each{|c| ans.push [a,b,c] if c > a+b}
+    }
+ ans
 end
 
-def can_win_nim1(n)
-    @win = false;
-    dfs(n,true)
-    @win
-end
-
-def dfs(n,myturn)
-    return if @win
-    if n <= 3
-        @win = true if myturn
-        return
+def trr2 array
+    ans = []
+    (0...array.size-1).each do |ai|
+        (ai+1...array.size).each do |bi|
+            (0...array.size).each do |ci|
+                if( ai != bi && bi != ci && ci != ai)
+                    a,b,c = array[ai], array[bi], array[ci]
+                    ans.push [a,b,c] if a+b < c && a+c < b && c+b < a
+                end
+            end
+        end
     end
-    (1..3).each do |x|
-        dfs(n-x,!myturn)
-    end
+    ans
 end
 
-(1..10).each do |x|
-    p [x,can_win_nim(x)]
-end
+p triangles([1,2,4]) # [1,2,4]
+p triangles([1, 2, 3, 5, 6, 6, 10]).size # 20
 
+p trr2([1,2,4]) # [1,2,4]
+p trr2([1, 2, 3, 5, 6, 6, 10]).size # 20
