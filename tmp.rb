05ea33184dcc042bb032be10bbbf7ce9ed63abb2
Dmitry <lbvf50.mobile@gmail.com>
2019-12-26 00:18:14 +0700

    Help for @lazylead.

--- tmp.rb
+++ tmp.rb
@@ -1,54 +1,26 @@
-# Return all possible triangles.
-# https://leetcode.com/problems/valid-triangle-number/
+# Help for @lazylead 
 
-def triangles array
-    ans = []
-    array.combination(2).each{|a,b|
-      array.each{|c| ans.push [a,b,c] if c > a+b}
+licence_file_path = "test_licence.txt"
+file_name_teplate = /.+\.txt$/
+dir_path = 'one'
+puts "Licence file: #{licence_file_path}\nFile name template: #{file_name_teplate.inspect}\nDirpath: #{dir_path}\nLicence text:\n"
+p licence_text = File.read(licence_file_path)
+
+stack = [dir_path]
+
+while !stack.empty?
+    dir = stack.pop
+    Dir.entries(dir).each{|name|
+        next if /^\.+$/ === name
+        new_name = "#{dir}/#{name}"
+        if File.directory?(new_name)
+            stack.unshift(new_name)
+        elsif file_name_teplate === new_name
+            data = File.read(new_name)
+            File.open(new_name, 'w') { |file| file.write(licence_text+data) }
+            puts "ADD: #{new_name}"
+        else
+            puts "SKIP: #{new_name}"
+        end
     }
- ans
-end
-
-def trr2 array
-    ans = []
-    (0...array.size-2).each do |ai|
-        (ai+1...array.size-1).each do |bi|
-            (bi+1...array.size).each do |ci|
-                a,b,c = [array[ai],array[bi],array[ci]]
-                ans.push [a,b,c] if a+b > c && a+c > b && b+c > a
-                
-            end
-        end
-    end
-    ans
-end
-
-
-p trr2([1, 2, 3, 5, 6, 6, 10]).size # 20
-
-# https://leetcode.com/problems/valid-triangle-number/submissions/
-# 611. Valid Triangle Number
-# @param {Integer[]} nums
-# @return {Integer}
-# Runtime: 108 ms, faster than 100.00% of Ruby online submissions for Valid Triangle Number.
-# Memory Usage: 9.8 MB, less than 100.00% of Ruby online submissions for Valid Triangle Number.
-
-def triangle_number(a)
-    a.sort!
-    r = 0
-    (2...a.size).reverse_each do |i|
-        s = a[i]
-        lo = 0
-        hi = i - 1
-        while lo < hi
-            if a[lo] + a[hi] <= s
-                lo += 1
-                next
-            end
-            r += hi - lo
-            hi -= 1
-        end
-    end
-    r
-    
 end
