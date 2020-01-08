664852227c66fea11d38d248583cc124f06fd4b1
Dmitry <lbvf50.mobile@gmail.com>
2020-01-08 21:08:19 +0700

    Coding Interview: Dynamic Programming.

--- tmp.rb
+++ tmp.rb
@@ -1,26 +1,51 @@
-# Help for @lazylead 
-
-licence_file_path = "test_licence.txt"
-file_name_teplate = /.+\.txt$/
-dir_path = 'one'
-puts "Licence file: #{licence_file_path}\nFile name template: #{file_name_teplate.inspect}\nDirpath: #{dir_path}\nLicence text:\n"
-p licence_text = File.read(licence_file_path)
-
-stack = [dir_path]
-
-while !stack.empty?
-    dir = stack.pop
-    Dir.entries(dir).each{|name|
-        next if /^\.+$/ === name
-        new_name = "#{dir}/#{name}"
-        if File.directory?(new_name)
-            stack.unshift(new_name)
-        elsif file_name_teplate === new_name
-            data = File.read(new_name)
-            File.open(new_name, 'w') { |file| file.write(licence_text+data) }
-            puts "ADD: #{new_name}"
-        else
-            puts "SKIP: #{new_name}"
-        end
-    }
+# 1049. Last Stone Weight II
+# https://leetcode.com/problems/last-stone-weight-ii/
+# Fails on: [1,1,2,3,5,8,13,21,34,55,89,14,23,37,61,98]
+# Time Limit Exceeded
+# @param {Integer[]} stones
+# @return {Integer}
+class Array
+    def sum
+        self.reduce(:+)
+    end
 end
+def last_stone_weight_ii(stones)
+    @min_weight_from_size = Array.new(stones.size+1)
+    @min_weight_from_size[stones.size] = stones.sum
+    @visited_sets = {}
+    stones.sort!
+    @visited_sets[stones.inspect] = true
+    recursion(stones)
+    return 0 if @min_weight_from_size[0]
+    @min_weight_from_size[1]
+end
+
+def recursion(stones)
+    # p stones
+    sum = stones.sum
+    val = @min_weight_from_size[stones.size]
+    @min_weight_from_size[stones.size] = sum if val.nil? || val > sum
+    # p  @min_weight_from_size
+    stones.each_with_index do |stone, index|
+        # remove index
+        new_removed= stones
+            .each_with_index
+            .each_with_object([]){|(x,i),o| 
+                o.push(x) if i != index
+                }
+        # smash each stone by selected stone
+        (0...new_removed.size).each do |i|
+            new_stones = new_removed.clone
+            new_stones[i] = (new_stones[i] - stone).abs
+            new_stones = new_stones.reject{|x| x.zero?}.sort
+            label = new_stones.inspect
+            if @visited_sets[label].nil?
+                @visited_sets[label] = true
+                recursion(new_stones)
+            end
+        end
+        
+    end
+end
+puts "last_stone_weight_ii([1,1,2,3,5,8,13,21,34,55,89,14,23,37,61,98])"
+p last_stone_weight_ii([1,1,2,3,5,8,13,21,34,55,89,14,23,37,61,98])
