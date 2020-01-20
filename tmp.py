94939eadffb2f2d66cd3c8c00c8f96452a28d713
Dmitry <lbvf50.mobile@gmail.com>
2020-01-20 22:39:39 +0700

    Working with sets.

--- tmp.py
+++ tmp.py
@@ -1,3 +1,8 @@
-a = [[1,2],[3,4]]
-b = [sub_x for x in a for sub_x in x]
-print(b)
+one = set(['yellow','red','blue','grey'])
+two = set(['red','green','grey'])
+three = set(['black','braun','blue','green','grey'])
+all = one | two |three
+common = one & two & three
+print(all)
+print(common)
+print(all-common)
