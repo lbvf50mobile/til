2b81a6ee7187628a361b4accd0593754c0fcc8c1
Dmitry <lbvf50.mobile@gmail.com>
2020-01-21 22:02:58 +0700

    Python items

--- tmp.py
+++ tmp.py
@@ -1,19 +1,9 @@
-a = {1,2,7,6}
-b = {2,3,4,7}
-c = {4,5,6,7}
-aib = a^b # {1,3,4,6}
-print(aib)
-bic = b^c # {2,3,5,6}
-print(bic)
-cia = c^a # {1,2,4,5}
-print(cia)
-aib_i_c = (a^b)^c # {1,3,5,7}
-print(aib_i_c)
-a_i_bic = a ^ (b^c) # {1,3,5,7}
-print(a_i_bic)
-aibic = a^b^c
-print(aibic)
-answer = (a|b|c) - (a^b^c)
-print(answer) # {2,4,6}
+a = {'a': 1, 'b':2}
+print(a)
+for k in a:
+    print(k)
 
+print(a.items())
+
+print([[a[k],k] for k in a])
 
