950192f7d087e95c5701fa6c55631e585a60fde1
Dmitry <lbvf50.mobile@gmail.com>
2020-01-24 01:28:39 +0700

    483. 416. Partition Equal Subset Sum. (add gist)

--- tmp.py
+++ tmp.py
@@ -1,8 +1,11 @@
-from itertools import permutations, islice, combinations
+from itertools import combinations_with_replacement
 
-a = [ str(i) for i in range(11)]
-print(sorted(a))
-print(list(islice("abc",0,1))[0])
+digits = [1, 5, 2]
+k = 2
+d = 3
 
-print(list(combinations("abc",2)))
-print(sum((1,2,3)))
+def createNumber(digs):
+    return "".join(map(str, digs))
+x = sorted(map(lambda x: createNumber(list(x)),filter(lambda x: 0 == sum(x)%d, set(combinations_with_replacement(digits+digits,k)))))
+
+print(list(x))
