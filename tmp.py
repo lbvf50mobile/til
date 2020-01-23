4f3557da29402ad6d5aa2dcbf7cbc39eb0f7b60b
Dmitry <lbvf50.mobile@gmail.com>
2020-01-23 23:52:42 +0700

    Pythong work

--- tmp.py
+++ tmp.py
@@ -1,5 +1,8 @@
-from itertools import permutations, islice
+from itertools import permutations, islice, combinations
 
 a = [ str(i) for i in range(11)]
 print(sorted(a))
 print(list(islice("abc",0,1))[0])
+
+print(list(combinations("abc",2)))
+print(sum((1,2,3)))
