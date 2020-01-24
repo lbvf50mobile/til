55ffb09c40edee006d4a54c1c95c726c376c383e
Dmitry <lbvf50.mobile@gmail.com>
2020-01-24 22:40:24 +0700

    Python prodcut.

--- tmp.py
+++ tmp.py
@@ -1,4 +1,4 @@
-from itertools import combinations_with_replacement
+from itertools import product, combinations_with_replacement
 
 digits = [1, 5, 2]
 k = 2
@@ -8,4 +8,7 @@ def createNumber(digs):
     return "".join(map(str, digs))
 x = sorted(map(lambda x: createNumber(list(x)),filter(lambda x: 0 == sum(x)%d, set(combinations_with_replacement(digits+digits,k)))))
 
-print(list(x))
+# print(list(x))
+
+z = list(product(digits, repeat = 2))
+print(z)
