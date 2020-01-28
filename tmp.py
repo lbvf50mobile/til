c9515cd3a6eace794dc3987083fdcb70c73d9ab4
Dmitry <lbvf50.mobile@gmail.com>
2020-01-28 21:42:14 +0700

    Python group dating.

--- tmp.py
+++ tmp.py
@@ -1,8 +1,8 @@
+male = [5,28,14,99,17]
+female = [5,14,29,99,16]
 
-athletes = [1,2,3,4]
+ans = list(zip(*filter(lambda x: x[0] != x[1],zip(male,female))))
+ans1 = [[x for i,x in enumerate(male) if male[i] != female[i]],[y for i,y in enumerate(female) if male[i] != female[i]]]
 
-arr = [j for i in zip(athletes[1::2],athletes[0::2]) for j in i]
-arr1 = [athletes[i^1] for i in range(len(athletes))]
-
-print(arr)
-print(arr1)
+print(ans)
+print(ans1)
