336913cbaed5b839bb5b8e1486357dd4d43dd7e2
Dmitry <lbvf50.mobile@gmail.com>
2020-01-29 23:25:21 +0700

    Python append retuns None.

--- tmp.py
+++ tmp.py
@@ -1,8 +1,2 @@
-male = [5,28,14,99,17]
-female = [5,14,29,99,16]
-
-ans = list(zip(*filter(lambda x: x[0] != x[1],zip(male,female))))
-ans1 = [[x for i,x in enumerate(male) if male[i] != female[i]],[y for i,y in enumerate(female) if male[i] != female[i]]]
-
-print(ans)
-print(ans1)
+x = [0]
+print(x.append(2))
