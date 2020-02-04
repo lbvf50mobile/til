1965c5bfdd0d29f102dd2910e3823baeb9b2982b
Dmitry <lbvf50.mobile@gmail.com>
2020-02-04 22:46:44 +0700

    Python: sorting reverse.

--- tmp.py
+++ tmp.py
@@ -1,17 +1,10 @@
-# CalcBonuses
-# https://stackoverflow.com/questions/32139885/yield-in-list-comprehensions-and-generator-expressions
-# https://stackoverflow.com/questions/231767/what-does-the-yield-keyword-do/231855#231855
-def calcBonuses(bonuses, n):
-    it = [(yield x) for x in bonuses]
-    res = 0
+# Calc Final Score
+print("Calc Final Score")
 
-    try:
-        for _ in range(n):
-            res += next(it)
-    except StopIteration:
-        res = 0
+scores = [4, 2, 4, 5]
+n = 3
 
-    return res
+srtd = sorted(scores,reverse = True)
 
-x = calcBonuses([4,2,4,5],3)
-print(x)
+print(scores)
+print(srtd)
