1667b5490ef3259c840be97cc509f7f094fb85e2
Dmitry <lbvf50.mobile@gmail.com>
2020-02-14 00:34:04 +0700

    Python enumerate with start=1.

--- tmp.py
+++ tmp.py
@@ -1,10 +1,3 @@
-def fractions():
-        while True:
-            yield [1,1]
-
-gen = fractions()
-
-print(next(gen))
-
-print(next(gen))
-print(next(gen)!= [1,1])
+a = [10,20,30,40,50]
+for value,index in enumerate(a,start=1):
+    print(value,index)
