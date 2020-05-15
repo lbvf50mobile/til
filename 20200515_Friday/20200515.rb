40c3e3419220961ae929e1c4f547594464ef1323
Dmitry <lbvf50.mobile@gmail.com>
2020-05-16 00:51:30 +0700

    586. Codesignal: fillingBlocks. (improve 1,2,3,4.). Leetcode: 766. Toeplitz Matrix.

--- 20200515_Friday/20200515.rb
+++ 20200515_Friday/20200515.rb
@@ -5,25 +5,17 @@ p "alias x='ruby 20200515_Friday/20200515.rb'"
 
 def fillingBlocks(n)
     @n = n
-    @c = 0
     array = Array.new(2).map{ Array.new(5,0)}
     rec(array,0)
 end
 
 def rec(array, index)
-    
     filled = [1,1,1,1,0]
-    p "Start."
-    p index
-    p array[0]
-    p array[1]
-    p "------------"
     if @n - 1 == index && array[0] == filled && array[1].all?{|x| 0 == x}
         return 1 
     end
     return 0 if @n == index
     if 1 == array[0][4]
-        p "Here must be return 0."
         return 0
     end
     return 0 if array[0].any?{|x| x > 1}
@@ -32,22 +24,17 @@ def rec(array, index)
         return rec(new_array,index+1)
     end
     first_zero = array[0].index(0)
-    p "first zero is #{first_zero}"
     horizontal = [array[0].clone, array[1].clone]
     vertical = [array[0].clone, array[1].clone]
     horizontal[0][first_zero] += 1
     horizontal[0][first_zero+1] += 1
     vertical[0][first_zero] += 1
     vertical[1][first_zero] += 1
-    p "call #{[horizontal, index]} "
-    p "call #{[vertical, index]} "
-    a = rec(horizontal, index) 
-    b = rec(vertical, index)
-    p "the a is #{a}"
-    p "the b is #{b}"
-    p "and I'm retun"
-    return a + b
+    rec(horizontal, index) + rec(vertical, index)
 end
-
+p fillingBlocks(1)
+p fillingBlocks(2)
 p fillingBlocks(3) # 11
+p fillingBlocks(4) 
+p [fillingBlocks(1) == 1,fillingBlocks(2) == 5,fillingBlocks(3) == 11,fillingBlocks(4) == 36 ].all?
 
