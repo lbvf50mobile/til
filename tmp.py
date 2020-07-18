5d429b580ee2fa6f7cbb91743be5dc71a9338cb7
Dmitry <lbvf50.mobile@gmail.com>
2020-07-18 23:41:00 +0700

    638. Leetcode: 989. Add to Array-Form of Integer. (solution and article)

--- tmp.py
+++ tmp.py
@@ -1,2 +1,6 @@
-a = [10,20,30,40,50]
-print(a[::-1])
+from re import match
+s = "075335440555"
+x = match('(\+44|0)7\d{9}',s.replace("-",''))
+print(x)
+print(match('2','2111'))
+print(match('1','2111'))
