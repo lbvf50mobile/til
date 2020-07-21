4ab1dea9909449ffc73145dbfe6e6317a31cfa16
Dmitry <lbvf50.mobile@gmail.com>
2020-07-22 00:52:53 +0700

    Benchmarks for, each, index.

--- tmp.rb
+++ tmp.rb
@@ -1,2 +1,33 @@
-p "bcaccbbacbcbcab".size
-p "b*.c*..*.b*b.c*".size
+require 'benchmark'
+
+
+p n = 5000
+
+arr = Array.new(n).map{
+    subarr = Array.new(n,false)
+    subarr[rand(n)] = true
+    subarr
+}
+
+Benchmark.bm do |x|
+  
+ 
+  x.report {
+      arr.each do |a|
+        (0...a.size).each{|i| break if a[i]}
+      end
+  }
+  x.report {
+    arr.each do |a|
+        for i  in 0...a.size
+            break if a[i]
+        end
+    end
+}
+
+  x.report {
+    arr.each do |a|
+        a.index{|elem| elem}
+    end
+  }
+end
