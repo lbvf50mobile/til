9d31535ad3e656dcd7cd9c3300132dd1077e41d0
Dmitry <lbvf50.mobile@gmail.com>
2020-08-18 03:27:05 +0700

    660. Leetcode: 1154. Day of the Year. Some tests. (Add Python.)

--- tmp.py
+++ tmp.py
@@ -1,6 +1,10 @@
-from re import match
-s = "075335440555"
-x = match('(\+44|0)7\d{9}',s.replace("-",''))
-print(x)
-print(match('2','2111'))
-print(match('1','2111'))
+
+def day_of_year(date):
+    year,month,day = [int(x) for x in date.split("-")]
+    arr = [0,31,28,31,30,31,30,31,31,30,31,30,31]
+    if (0 == year%4 and 0 != year%100) or (0 == year%100 and 0 == year%400):
+        arr[2] = 29
+    return sum(arr[0:month]) + day
+
+
+print(day_of_year('2016-2-29'))
