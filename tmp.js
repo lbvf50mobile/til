2a527317ed15376ab358c9461dbbfe1f26f1013d
Dmitry <lbvf50.mobile@gmail.com>
2020-11-07 01:01:52 +0700

    Program for sorting.

--- tmp.js
+++ tmp.js
@@ -1,19 +1,42 @@
-// https://www.geeksforgeeks.org/even-fibonacci-numbers-sum/
-// This dose not works because Even Fibonacci Numbers are not indexes of numbers but there values
+// Implementation of sorting array contained two types of entities.
+let arr = [
+  "Name 1",
+"Third",
+"First",
+"Name 5",
+"Fourth",
+"Second",
+"Name 3",
+];
 
-const evenFib = n =>{
-   if(n < 2) return 0;
-   if(2 == n) return 2;
-   ef1 = 0n
-   ef2 = 2n
-   next = 0n
-   counter = 1
-   while( counter < n){
-     next = 4 * ef1 + ef2
-     ef2 = ef1
-     ef1 = next
-     n +=2
-   }
+console.log("Input array:",arr);
 
-   return next
+const super_sort = x =>{
+  let names = x.filter(x => /[0-9]$/.test(x));
+  let numbers = x.filter(x => /^[a-z]+$/i.test(x));
+  console.log("names", names);
+  console.log("numbers", numbers);
+  let sorted_names = sort_names(names);
+  let sorted_numbers = sort_numbers(numbers);
+  return [...sorted_names, ...sorted_numbers];
 }
+const sort_names = x =>{
+  x = x.map(x => [Number(x.match(/[0-9]+$/gi)[0]),x]);
+  x.sort((a,b) => a[0] - b[0]);
+  return x.map(x => x[1]);
+}
+const sort_numbers = x =>{
+  let order = {
+    'first': 1,
+    'second': 2,
+    'third': 3,
+    "fourth": 4,
+  };
+  x = x.slice();
+  x.sort((a,b) => order[a.toLowerCase()] - order[b.toLowerCase()])
+  return x;
+}
+
+
+let sorted_arr = super_sort(arr);
+console.log("Sorted array:", sorted_arr);
