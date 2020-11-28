48ec7646efbbbaf6d41b0b718bd5341183f630fc
Dmitry <lbvf50.mobile@gmail.com>
2020-11-29 02:07:15 +0700

    747. Leetcode: 1314. Matrix Block Sum. (article and soluton)

--- 20201129_Sunday/20201129.rb
+++ 20201129_Sunday/20201129.rb
@@ -1,5 +1,10 @@
 p "alias x='ruby 20201129_Sunday/20201129.rb'" 
 
+=begin
+- https://leetcode.com/problems/matrix-block-sum
+- https://gist.github.com/lbvf50mobile/5181bbe654aa1c9c4bfed162817ef7e3
+- https://leetcode.com/problems/matrix-block-sum/discuss/952381/Ruby%3A-2d-Prefix-sum-array.-Big-top-left-%2B-upper-left.
+=end
 
 
 def matrix_block_sum(mat, k)
@@ -8,6 +13,7 @@ def matrix_block_sum(mat, k)
     dp = Array.new(h).map{ Array.new(w,0)}
     
     answer = Array.new(h).map{ Array.new(w,0)}
+   
     
     dp[0][0] = mat[0][0]
     # Fill row.
@@ -18,7 +24,6 @@ def matrix_block_sum(mat, k)
     (1...h).each do |i|
         row_sum = mat[i][0]
         (1...w).each do |j|
-            puts "row #{i} column #{j}: #{mat[i][j]} row_sum = #{row_sum}"
             dp[i][j] = row_sum + dp[i-1][j] + mat[i][j]
             row_sum += mat[i][j]
         end
@@ -29,34 +34,35 @@ def matrix_block_sum(mat, k)
         return 0 if j < 0
         dp[i][j]
     }
-    p "Sum:"
-    p dp
-    (0...w).each do |i|
-        (0...h).each do |j|
+
+    (0...h).each do |i|
+        (0...w).each do |j|
             min_row, max_row = [0,i - k].max, [i + k,max_i].min
             min_col, max_col = [0,j - k].max, [j + k, max_j].min
-            puts "cell:(#{i}:#{j}) min:(#{min_row}:#{min_col}) max(#{max_row}:#{max_col})"
             max_value = get_sum[max_row,max_col]
-            puts "max(#{max_row}:#{max_col}) = #{max_value}"
             bottom_value = get_sum[min_row-1, max_col]
-            puts "bottom(#{min_row-1},#{max_col}) = #{bottom_value}"
             left_value = get_sum[max_row,min_col - 1]
-            puts "left(#{max_row},#{min_col - 1}) = #{left_value}"
             add_value = get_sum[min_row-1,min_col-1]
-            puts "add(#{min_row-1},#{min_col-1}) = #{add_value}"
             ans = max_value - bottom_value - left_value + add_value
-            puts "cell:(#{i}:#{j}) = #{max_value} - #{bottom_value} - #{left_value} + #{add_value}"
-            
             answer[i][j] = ans
-            puts "-------------------------------------"
         end
     end
     answer    
 end
+def sz(x)
+    p [x.size,x[0].size]
+end
 
-
+=begin
 out = matrix_block_sum([[1,2,3],[4,5,6],[7,8,9]], 1)
 p "answer"
 p out
 p "correct"
 p [[12,21,16],[27,45,33],[24,39,28]]
+=end
+
+out = matrix_block_sum([[67,64,78],[99,98,38],[82,46,46],[6,52,55],[55,99,45]], 3)
+p "answer"
+p out
+p "correct"
+p [[731,731,731],[930,930,930],[930,930,930],[930,930,930],[721,721,721]]
