d14eb3907d6240f1ca34fe06f13214852ffaef7b
Dmitry <lbvf50.mobile@gmail.com>
2021-06-07 23:32:07 +0700

    907. Leetcode: 1689. Partitioning Into Minimum Number Of Deci-Binary Numbers. (solution and testcase generator)

---
+++ 20210607_Monday/20210607.rb
@@ -0,0 +1,7 @@
+p "alias x='ruby 20210607_Monday/20210607.rb'" 
+
+p "Generate tests for https://leetcode.com/problems/partitioning-into-minimum-number-of-deci-binary-numbers/ Leetcode. 1689."
+
+p Array.new(10).map{ (?0..?9).to_a.sample}.join
+
+
