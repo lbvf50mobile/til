56e4f1bdf14d5eb7c6e55c6d3fbdbbaefcac5c5e
Dmitry <lbvf50.mobile@gmail.com>
2021-07-29 22:52:25 +0700

    952. Leetcode: 542. 01 Matrix. (begining)

---
+++ 20210729_Thursday/20210729.rb
@@ -0,0 +1,8 @@
+# Leetcode: 542. 01 Matrix.
+# https://leetcode.com/explore/challenge/card/july-leetcoding-challenge-2021/612/week-5-july-29th-july-31st/3831/
+# https://leetcode.com/problems/01-matrix/
+# @param {Integer[][]} mat
+# @return {Integer[][]}
+def update_matrix(mat)
+    
+end
