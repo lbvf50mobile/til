b0250b211e02679d6a8459f5c03d633ea553ede7
Dmitry <lbvf50.mobile@gmail.com>
2021-07-31 23:24:58 +0700

    954. Leetcode: 42. Trapping Rain Water. (begining)

---
+++ 20210731_Saturday/20210731.rb
@@ -0,0 +1,7 @@
+# Leetcode: 42. Trapping Rain Water.
+# https://leetcode.com/explore/challenge/card/july-leetcoding-challenge-2021/612/week-5-july-29th-july-31st/3833/
+# @param {Integer[]} height
+# @return {Integer}
+def trap(height)
+    
+end
