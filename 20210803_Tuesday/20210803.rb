fc19c43842f821098222fab36dd72674175a232b
Dmitry <lbvf50.mobile@gmail.com>
2021-08-03 17:25:36 +0700

    956. Leetcode: 90. Subsets II. (solution)

--- 20210803_Tuesday/20210803.rb
+++ 20210803_Tuesday/20210803.rb
@@ -1,13 +1,14 @@
 # Leetcode: 90. Subsets II.
 # https://leetcode.com/explore/challenge/card/august-leetcoding-challenge-2021/613/week-1-august-1st-august-7th/3837/
-# Wrong answer.
+# Accepted.
+# Thanks God!
 # @param {Integer[]} nums
 # @return {Integer[][]}
 def subsets_with_dup(nums)
-  require 'set'
-  ans = Set.new
+  ans = []
+  nums.sort!
   (0..nums.size).each do |size|
-    nums.combination(size).each{|comb| ans.add(comb)}
+    nums.combination(size).each{|comb| ans.push(comb)}
   end
-  return ans.to_a
+  return ans.uniq
 end
