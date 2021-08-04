9eccdd2a88621f47942625d062b0804bc3409a5a
Dmitry <lbvf50.mobile@gmail.com>
2021-08-04 23:26:07 +0700

    957. Leetcode: 113. Path Sum II. (begining)

---
+++ 20210804_Wednesday/20210804.rb
@@ -0,0 +1,17 @@
+# Leetcode: 113. Path Sum II.
+# https://leetcode.com/explore/challenge/card/august-leetcoding-challenge-2021/613/week-1-august-1st-august-7th/3838/
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @param {Integer} target_sum
+# @return {Integer[][]}
+def path_sum(root, target_sum)
+    
+end
