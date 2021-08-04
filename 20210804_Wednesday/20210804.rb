b1cff7e27ca9fc525a2dba2ad2607406bf434db3
Dmitry <lbvf50.mobile@gmail.com>
2021-08-04 23:43:40 +0700

    957. Leetcode: 113. Path Sum II. (article and solution)

--- 20210804_Wednesday/20210804.rb
+++ 20210804_Wednesday/20210804.rb
@@ -1,5 +1,9 @@
 # Leetcode: 113. Path Sum II.
 # https://leetcode.com/explore/challenge/card/august-leetcoding-challenge-2021/613/week-1-august-1st-august-7th/3838/
+# = = = = = = =
+# Accepted.
+# Thanks God!
+# = = = = = = =
 # Definition for a binary tree node.
 # class TreeNode
 #     attr_accessor :val, :left, :right
@@ -13,5 +17,22 @@
 # @param {Integer} target_sum
 # @return {Integer[][]}
 def path_sum(root, target_sum)
-    
+  return [] if root.nil?
+  @ans = []
+  @ts = target_sum
+  bt(root,[],0)
+  @ans
 end
+
+def bt(node, path, sum)
+  sum += node.val
+  path.push(node.val)
+  if node.left.nil? && node.right.nil?
+    @ans.push(path.clone) if sum == @ts
+  else
+    bt(node.left, path, sum) if node.left
+    bt(node.right, path, sum) if node.right
+  end
+  path.pop() # need to free from current node every time.
+end
+
