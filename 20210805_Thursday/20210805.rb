6fe747030e7419495af484c5fb1252c2cdd2602d
Dmitry <lbvf50.mobile@gmail.com>
2021-08-05 23:13:23 +0700

    958. Leetcode: 877. Stone Game. (begining)

---
+++ 20210805_Thursday/20210805.rb
@@ -0,0 +1,7 @@
+# Leetcode: 877. Stone Game.
+# https://leetcode.com/explore/challenge/card/august-leetcoding-challenge-2021/613/week-1-august-1st-august-7th/3870/
+# @param {Integer[]} piles
+# @return {Boolean}
+def stone_game(piles)
+    
+end
