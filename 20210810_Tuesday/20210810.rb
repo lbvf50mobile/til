fcbbceecd4ddd16309f4ad696bf5b6ff20bd8ff9
Dmitry <lbvf50.mobile@gmail.com>
2021-08-10 23:49:42 +0700

    962. Leetcode: 926. Flip String to Monotone Increasing. (solution)

--- 20210810_Tuesday/20210810.rb
+++ 20210810_Tuesday/20210810.rb
@@ -2,16 +2,19 @@
 # https://leetcode.com/explore/challenge/card/august-leetcoding-challenge-2021/614/week-2-august-8th-august-14th/3876/
 # https://leetcode.com/problems/flip-string-to-monotone-increasing/
 # https://leetcode.com/problems/flip-string-to-monotone-increasing/solution/
+# = = = = = = =
+# Accepted.
+# Thanks God!
+# = = = = = = =
 # @param {String} s
 # @return {Integer}
 def min_flips_mono_incr(s)
   prfx = [0]
-  s.chars.map(&:to_i).each{|x| prfx.push(prfx.last + x)}
+  s.chars.map(&:to_i).each{|x| prfx.push(prfx[-1] + x)}
   len = s.size
-  ans = prfx[0] + len - 0 - (prfx.last - prfx[0])
-  (1...len).each{|j|
-    value = prfx[j] + len - j - (prfx.last - prfx[j])
-    ans = value if value < ans
+  ans = []
+  (0..len).each{|j|
+    ans.push(prfx[j] + len - j - (prfx[-1] - prfx[j]))
   }
-  ans
+  ans.min
 end
