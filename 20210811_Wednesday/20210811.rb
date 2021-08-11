3c73f7481dfe5d082fc36c99c8f0cf750a4ced49
Dmitry <lbvf50.mobile@gmail.com>
2021-08-11 22:00:04 +0700

    963. Leetcode: 954. Array of Doubled Pairs. (begining)

---
+++ 20210811_Wednesday/20210811.rb
@@ -0,0 +1,8 @@
+# Leetcode: 954. Array of Doubled Pairs.
+# https://leetcode.com/explore/challenge/card/august-leetcoding-challenge-2021/614/week-2-august-8th-august-14th/3877/
+# https://leetcode.com/problems/array-of-doubled-pairs/
+# @param {Integer[]} arr
+# @return {Boolean}
+def can_reorder_doubled(arr)
+    
+end
