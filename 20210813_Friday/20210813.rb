228ceb424a25badf2b2587915a5aea3ed14767f6
Dmitry <lbvf50.mobile@gmail.com>
2021-08-13 17:57:15 +0700

    965. Leetcode: 73. Set Matrix Zeroes. (begining)

---
+++ 20210813_Friday/20210813.rb
@@ -0,0 +1,8 @@
+# Leetcode: 73. Set Matrix Zeroes.
+# https://leetcode.com/explore/challenge/card/august-leetcoding-challenge-2021/614/week-2-august-8th-august-14th/3888/
+# https://leetcode.com/problems/set-matrix-zeroes/
+# @param {Integer[][]} matrix
+# @return {Void} Do not return anything, modify matrix in-place instead.
+def set_zeroes(matrix)
+    
+end
