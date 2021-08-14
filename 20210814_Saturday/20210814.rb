b90485c876ddea350b02e6787f9bab5ffc5d3982
Dmitry <lbvf50.mobile@gmail.com>
2021-08-14 23:14:39 +0700

    966. Leetcode: 546. Remove Boxes. (begining)

---
+++ 20210814_Saturday/20210814.rb
@@ -0,0 +1,8 @@
+# Leetcode: 546. Remove Boxes.
+# https://leetcode.com/explore/challenge/card/august-leetcoding-challenge-2021/614/week-2-august-8th-august-14th/3889/
+# https://leetcode.com/problems/remove-boxes/
+# @param {Integer[]} boxes
+# @return {Integer}
+def remove_boxes(boxes)
+    
+end
