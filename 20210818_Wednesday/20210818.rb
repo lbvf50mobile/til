32dea1bd971f5fde54b5cbe20d80450b536120a2
Dmitry <lbvf50.mobile@gmail.com>
2021-08-18 22:57:35 +0700

    969. Leetcode: 1448. Count Good Nodes in Binary Tree. (begining)

---
+++ 20210818_Wednesday/20210818.rb
@@ -0,0 +1,17 @@
+# Leetcode: 1448. Count Good Nodes in Binary Tree.
+# https://leetcode.com/explore/challenge/card/august-leetcoding-challenge-2021/615/week-3-august-15th-august-21st/3899/
+# https://leetcode.com/problems/count-good-nodes-in-binary-tree/
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {Integer}
+def good_nodes(root)
+    
+end
