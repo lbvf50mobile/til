0058a559a5e4f33ab2469a8085f9ce473e22b827
Dmitry <lbvf50.mobile@gmail.com>
2021-08-19 22:56:00 +0700

    970. Leetcode: 91. Decode Ways. (begnining)

---
+++ 20210819_Thursday/20210819.rb
@@ -0,0 +1,7 @@
+# Leetcode: 91. Decode Ways.
+# https://leetcode.com/explore/challenge/card/august-leetcoding-challenge-2021/615/week-3-august-15th-august-21st/3902/
+# @param {String} s
+# @return {Integer}
+def num_decodings(s)
+    
+end
