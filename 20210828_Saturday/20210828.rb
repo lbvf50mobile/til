196eef26879329a7cce2dc4760a8bedcb2d55b8d
Dmitry <lbvf50.mobile@gmail.com>
2021-08-28 23:21:46 +0700

    978. Leetcode: 1235. Maximum Profit in Job Scheduling. (begining)

---
+++ 20210828_Saturday/20210828.rb
@@ -0,0 +1,10 @@
+# Leetcode: 1235. Maximum Profit in Job Scheduling.
+# https://leetcode.com/problems/maximum-profit-in-job-scheduling/
+# https://leetcode.com/explore/challenge/card/august-leetcoding-challenge-2021/616/week-4-august-22nd-august-28th/3950/
+# @param {Integer[]} start_time
+# @param {Integer[]} end_time
+# @param {Integer[]} profit
+# @return {Integer}
+def job_scheduling(start_time, end_time, profit)
+    
+end
