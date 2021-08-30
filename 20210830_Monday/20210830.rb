df609a98054e1612f64faeaed24fe3dfeaab3543
Dmitry <lbvf50.mobile@gmail.com>
2021-08-30 22:35:30 +0700

    979. Leetcode: 598. Range Addition II. (begining)

---
+++ 20210830_Monday/20210830.rb
@@ -0,0 +1,10 @@
+# Leetcode: 598. Range Addition II.
+# https://leetcode.com/problems/range-addition-ii/
+# https://leetcode.com/explore/challenge/card/august-leetcoding-challenge-2021/617/week-5-august-29th-august-31st/3957/
+# @param {Integer} m
+# @param {Integer} n
+# @param {Integer[][]} ops
+# @return {Integer}
+def max_count(m, n, ops)
+    
+end
