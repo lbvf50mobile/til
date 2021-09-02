08ef5f6ed02037631b118d90db8856c673ee2c3c
Dmitry <lbvf50.mobile@gmail.com>
2021-09-02 23:32:51 +0700

    981. Leetcode: 565. Array Nesting. (solution and Questions)

--- 20210901_Wednesday/20210901.rb
+++ 20210901_Wednesday/20210901.rb
@@ -1,17 +1,23 @@
 # Leetcode: 565. Array Nesting.
 # https://leetcode.com/problems/array-nesting/
+# = = = = = = =
+# Accepted.
+# Thanks God!
+# = = = = = = =
 # https://leetcode.com/explore/challenge/card/september-leetcoding-challenge-2021/636/week-1-september-1st-september-7th/3960/
 # @param {Integer[]} nums
 # @return {Integer}
 def array_nesting(nums)
   # https://leetcode.com/problems/array-nesting/solution/
-  # TLE.
   ans = 0
+  visited = {}
   (0...nums.size).each do |i|
+    next if visited[i]
     count = 0
     j = nums[i]
     while true
       j = nums[j]
+      visited[j] = true
       count +=1
       break if j == nums[i]
     end
