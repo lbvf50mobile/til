dec068aa1043efdc620ec18aaf2798e65fc4e844
Dmitry <lbvf50.mobile@gmail.com>
2021-09-03 23:42:03 +0700

    983. Leetcode: 587. Erect the Fence. (begining)

---
+++ 20210903_Friday/20210903.rb
@@ -0,0 +1,8 @@
+# Leetcode: 587. Erect the Fence.
+# https://leetcode.com/problems/erect-the-fence/
+# https://leetcode.com/explore/challenge/card/september-leetcoding-challenge-2021/636/week-1-september-1st-september-7th/3962/
+# @param {Integer[][]} trees
+# @return {Integer[][]}
+def outer_trees(trees)
+    
+end
