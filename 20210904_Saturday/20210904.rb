4b86cac6a24eb0270384aba92c606456a3437468
Dmitry <lbvf50.mobile@gmail.com>
2021-09-04 23:46:22 +0700

    984. Leetcode: 834. Sum of Distances in Tree. (begining)

---
+++ 20210904_Saturday/20210904.rb
@@ -0,0 +1,9 @@
+# Leetcode: 834. Sum of Distances in Tree.
+# https://leetcode.com/problems/sum-of-distances-in-tree/
+# https://leetcode.com/explore/challenge/card/september-leetcoding-challenge-2021/636/week-1-september-1st-september-7th/3963/
+# @param {Integer} n
+# @param {Integer[][]} edges
+# @return {Integer[]}
+def sum_of_distances_in_tree(n, edges)
+    
+end
