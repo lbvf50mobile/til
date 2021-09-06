4caf6d837a02b0d8a3a1bb995eadf4712cf97243
Dmitry <lbvf50.mobile@gmail.com>
2021-09-06 23:17:05 +0700

    985. Leetcode: 1629. Slowest Key. (begining)

---
+++ 20210906_Monday/20210906.rb
@@ -0,0 +1,9 @@
+# Leetcode: 1629. Slowest Key.
+# https://leetcode.com/problems/slowest-key/
+# https://leetcode.com/explore/challenge/card/september-leetcoding-challenge-2021/636/week-1-september-1st-september-7th/3965/
+# @param {Integer[]} release_times
+# @param {String} keys_pressed
+# @return {Character}
+def slowest_key(release_times, keys_pressed)
+    
+end
