510240874a75f9ba6a13400bfbeab5eb9fce6c95
Dmitry <lbvf50.mobile@gmail.com>
2021-09-06 23:35:47 +0700

    985. Leetcode: 1629. Slowest Key. (article and solution)

--- 20210906_Monday/20210906.rb
+++ 20210906_Monday/20210906.rb
@@ -1,9 +1,20 @@
 # Leetcode: 1629. Slowest Key.
 # https://leetcode.com/problems/slowest-key/
+# = = = = = = =
+# Accepted.
+# Thanks God!
+# = = = = = = =
 # https://leetcode.com/explore/challenge/card/september-leetcoding-challenge-2021/636/week-1-september-1st-september-7th/3965/
 # @param {Integer[]} release_times
 # @param {String} keys_pressed
 # @return {Character}
 def slowest_key(release_times, keys_pressed)
-    
+  n = release_times.size
+  durations = Array.new(n)
+  durations[0] = release_times[0]
+  (1...n).each do |i|
+    durations[i] = release_times[i] - release_times[i-1]
+  end
+  max = durations.max
+  keys_pressed.chars.zip(durations).select{|(a,b)| max == b}.sort_by{|a,b| a}.last.first
 end
