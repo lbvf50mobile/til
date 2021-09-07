8bda09eaeaebb7e92f951c5e6ee1edcfc7c5ce3e
Dmitry <lbvf50.mobile@gmail.com>
2021-09-07 22:24:14 +0700

    986. Leetcode: 206. Reverse Linked List. (begining)

---
+++ 20210907_Tuesday/20210907.rb
@@ -0,0 +1,16 @@
+# Leetcode: 206. Reverse Linked List.
+# https://leetcode.com/problems/reverse-linked-list/
+# https://leetcode.com/explore/challenge/card/september-leetcoding-challenge-2021/636/week-1-september-1st-september-7th/3966/
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val = 0, _next = nil)
+#         @val = val
+#         @next = _next
+#     end
+# end
+# @param {ListNode} head
+# @return {ListNode}
+def reverse_list(head)
+    
+end
