f9906b27390a3e87ab6e9cb3c2eccd360c9c670e
Dmitry <lbvf50.mobile@gmail.com>
2021-09-07 22:45:12 +0700

    986. Leetcode: 206. Reverse Linked List. (article and solution)

--- 20210907_Tuesday/20210907.rb
+++ 20210907_Tuesday/20210907.rb
@@ -1,5 +1,9 @@
 # Leetcode: 206. Reverse Linked List.
 # https://leetcode.com/problems/reverse-linked-list/
+# = = = = = = =
+# Accepted.
+# Thanks God!
+# = = = = = = =
 # https://leetcode.com/explore/challenge/card/september-leetcoding-challenge-2021/636/week-1-september-1st-september-7th/3966/
 # Definition for singly-linked list.
 # class ListNode
@@ -12,5 +16,18 @@
 # @param {ListNode} head
 # @return {ListNode}
 def reverse_list(head)
-    
+  return head if head.nil?
+  prev, curr = nil, head
+  while curr
+    if curr.next.nil? # no way to move forward.
+      curr.next = prev
+      return curr # time to return.
+    else
+      tmp = curr.next # it is possible to move forward.
+      curr.next = prev
+      prev = curr
+      curr = tmp # make step foward, next become current.
+    end
+  end
+  raise "This line could not be reached, error in the algorithm or in the input."
 end
