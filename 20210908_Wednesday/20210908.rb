092a25357f1ce6a230f3e1f40b4f7540227a6f09
Dmitry <lbvf50.mobile@gmail.com>
2021-09-08 23:45:38 +0700

    987. Leetcode: 848. Shifting Letters. (article and solution)

--- 20210908_Wednesday/20210908.rb
+++ 20210908_Wednesday/20210908.rb
@@ -1,9 +1,18 @@
 # Leetcode: 848. Shifting Letters.
 # https://leetcode.com/problems/shifting-letters/
+# = = = = = = =
+# Accepted.
+# Thanks God!
+# = = = = = = =
 # https://leetcode.com/explore/challenge/card/september-leetcoding-challenge-2021/637/week-2-september-8th-september-14th/3968/
 # @param {String} s
 # @param {Integer[]} shifts
 # @return {String}
 def shifting_letters(s, shifts)
-    
+  (0...shifts.size-1).reverse_each do |i|
+    shifts[i] = (shifts[i]+shifts[i+1])%26
+  end
+  s.chars.map.with_index{|char, i|
+    ((((char.ord - ?a.ord) + shifts[i])%26) + ?a.ord).chr
+  }.join
 end
