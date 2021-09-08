335c010b47199cd9cb300f5325ee8a94da7c57cc
Dmitry <lbvf50.mobile@gmail.com>
2021-09-08 23:09:34 +0700

    987. Leetcode: 848. Shifting Letters. (begining)

---
+++ 20210908_Wednesday/20210908.rb
@@ -0,0 +1,9 @@
+# Leetcode: 848. Shifting Letters.
+# https://leetcode.com/problems/shifting-letters/
+# https://leetcode.com/explore/challenge/card/september-leetcoding-challenge-2021/637/week-2-september-8th-september-14th/3968/
+# @param {String} s
+# @param {Integer[]} shifts
+# @return {String}
+def shifting_letters(s, shifts)
+    
+end
