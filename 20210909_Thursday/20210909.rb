7391b8de55577d801584802a873dbb0df5e618a7
Dmitry <lbvf50.mobile@gmail.com>
2021-09-09 22:55:47 +0700

    988. Leetcode: 764. Largest Plus Sign. (begining)

---
+++ 20210909_Thursday/20210909.rb
@@ -0,0 +1,9 @@
+# Leetcode: 764. Largest Plus Sign.
+# https://leetcode.com/problems/largest-plus-sign/
+# https://leetcode.com/explore/challenge/card/september-leetcoding-challenge-2021/637/week-2-september-8th-september-14th/3969/
+# @param {Integer} n
+# @param {Integer[][]} mines
+# @return {Integer}
+def order_of_largest_plus_sign(n, mines)
+    
+end
