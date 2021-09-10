1950d0a9c8ed7187f039ea2b35cfa90b44c0f638
Dmitry <lbvf50.mobile@gmail.com>
2021-09-10 23:09:33 +0700

    989. Leetcode: 446. Arithmetic Slices II - Subsequence. (begining)

---
+++ 20210910_Friday/20210910.rb
@@ -0,0 +1,8 @@
+# Leetcode: 446. Arithmetic Slices II - Subsequence.
+# https://leetcode.com/problems/arithmetic-slices-ii-subsequence/
+# https://leetcode.com/explore/challenge/card/september-leetcoding-challenge-2021/637/week-2-september-8th-september-14th/3970/
+# @param {Integer[]} nums
+# @return {Integer}
+def number_of_arithmetic_slices(nums)
+    
+end
