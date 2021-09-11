090151338ce03354bfc9b1ba7764986d22b6476b
Dmitry <lbvf50.mobile@gmail.com>
2021-09-12 02:12:04 +0700

    990. Leetcode: 224. Basic Calculator. (without brackets)

--- 20210911_Saturday/20210911.rb
+++ 20210911_Saturday/20210911.rb
@@ -4,5 +4,35 @@
 # @param {String} s
 # @return {Integer}
 def calculate(s)
-    
+  last = s.size - 1
+  total = 0
+  sign = 1 # it means +;
+  current = 0
+  is_char = ->index{ s[index].ord.between?(?0.ord,?9.ord)}
+  (0...s.size).each do |i|
+    char = s[i]
+    if ?+ == char
+      sign = 1
+    elsif ?- == char
+      sign = -1
+    elsif " " == char
+      ; # do nothin on spacebar;
+    elsif is_char[i]
+      # First digit in a number. (I think this might be improved, if current is zero first and not first could be skipped)
+      if 0 == i || ! is_char[i-1]
+        current = char.to_i
+      # Not last digit in a number.
+      else
+        current = current*10 + char.to_i
+      end
+      # And time to increase change the total.
+      if last == i || ! is_char[i+1]
+        raise "At i = #{i} and char = #{char} no sign" if 0 == sign
+        total += sign * current
+        current = 0
+        sign = 0
+      end
+    end
+  end
+  return total 
 end
