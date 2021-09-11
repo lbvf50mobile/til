afc9d0ad4ac7f1ecf6ce25c9ddc87b472a180232
Dmitry <lbvf50.mobile@gmail.com>
2021-09-12 02:50:00 +0700

    990. Leetcode: 224. Basic Calculator. (article and solution)

--- 20210911_Saturday/20210911.rb
+++ 20210911_Saturday/20210911.rb
@@ -1,5 +1,9 @@
 # Leetcode: 224. Basic Calculator.
 # https://leetcode.com/problems/basic-calculator/description/
+# = = = = = = =
+# Accepted.
+# Thanks God!
+# = = = = = = =
 # https://leetcode.com/explore/challenge/card/september-leetcoding-challenge-2021/637/week-2-september-8th-september-14th/3971/
 # @param {String} s
 # @return {Integer}
@@ -9,6 +13,7 @@ def calculate(s)
   sign = 1 # it means +;
   current = 0
   is_char = ->index{ s[index].ord.between?(?0.ord,?9.ord)}
+  stack = []
   (0...s.size).each do |i|
     char = s[i]
     if ?+ == char
@@ -17,6 +22,16 @@ def calculate(s)
       sign = -1
     elsif " " == char
       ; # do nothin on spacebar;
+    elsif ?( == char
+      stack.push([total,sign])
+      total = 0
+      sign = 1
+      current = 0
+    elsif ?) == char
+      prev_total, prev_sign = stack.pop()
+      current = 0
+      total = prev_total + prev_sign * total
+      sign = 0
     elsif is_char[i]
       current = current*10 + char.to_i
       # And time to increase change the total.
