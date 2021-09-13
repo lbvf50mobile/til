899e6fdffb3c4361748ff568918c1274af27073f
Dmitry <lbvf50.mobile@gmail.com>
2021-09-13 23:22:54 +0700

    991. Leetcode: 1189. Maximum Number of Balloons. (begining)

---
+++ 20210913_Monday/20210913.rb
@@ -0,0 +1,8 @@
+# Leetcode: 1189. Maximum Number of Balloons.
+# https://leetcode.com/problems/maximum-number-of-balloons/
+# https://leetcode.com/explore/challenge/card/september-leetcoding-challenge-2021/637/week-2-september-8th-september-14th/3973/
+# @param {String} text
+# @return {Integer}
+def max_number_of_balloons(text)
+    
+end
