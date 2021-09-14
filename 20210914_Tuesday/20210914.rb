7d70e1bcac9951ad357b7a9c7002da7a6e32a82d
Dmitry <lbvf50.mobile@gmail.com>
2021-09-15 00:28:03 +0700

    992. Leetcode: 917. Reverse Only Letters. (article and solution)

--- 20210914_Tuesday/20210914.rb
+++ 20210914_Tuesday/20210914.rb
@@ -1,8 +1,26 @@
 # Leetcode: 917. Reverse Only Letters.
 # https://leetcode.com/problems/reverse-only-letters/
+# = = = = = = =
+# Accepted.
+# Thanks God!
+# = = = = = = =
 # https://leetcode.com/explore/challenge/card/september-leetcoding-challenge-2021/637/week-2-september-8th-september-14th/3974/
 # @param {String} s
 # @return {String}
 def reverse_only_letters(s)
-    
+  # Create the indices array.
+  indices = []
+  s.chars.each_with_index do |el,i|
+    if /[a-z]/i === el
+      indices.push(i)
+    end
+  end
+  # Create two pointers to swap characters.
+  i,j = 0,indices.size - 1
+  while i < j
+    s[indices[i]],s[indices[j]] = s[indices[j]],s[indices[i]]
+    i += 1
+    j -= 1
+  end
+  return s
 end
