5a58aa864236b798d7f0f488885d318edf25bbb1
Dmitry <lbvf50.mobile@gmail.com>
2021-09-22 23:17:18 +0700

    999.  Leetcode: 1239. Maximum Length of a Concatenated String with Unique Characters. (begining)

---
+++ 20210922_Wednesday/20210922.rb
@@ -0,0 +1,8 @@
+# Leetcode: 1239. Maximum Length of a Concatenated String with Unique Characters.
+# https://leetcode.com/problems/maximum-length-of-a-concatenated-string-with-unique-characters/
+# https://leetcode.com/explore/challenge/card/september-leetcoding-challenge-2021/639/week-4-september-22nd-september-28th/3984/
+# @param {String[]} arr
+# @return {Integer}
+def max_length(arr)
+    
+end
