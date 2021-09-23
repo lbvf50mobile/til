015132b6dd4fd01d8eed2e2262e365bfa355ef2d
Dmitry <lbvf50.mobile@gmail.com>
2021-09-23 22:05:19 +0700

    1000. Leetcode: 1328. Break a Palindrome. (article and solution)

--- 20210923_Thursday/20210923.rb
+++ 20210923_Thursday/20210923.rb
@@ -1,8 +1,21 @@
 # Leetcode: 1328. Break a Palindrome.
 # https://leetcode.com/problems/break-a-palindrome/
+# = = = = = = =
+# Accepted.
+# Thanks God!
+# = = = = = = =
 # https://leetcode.com/explore/challenge/card/september-leetcoding-challenge-2021/639/week-4-september-22nd-september-28th/3985/
 # @param {String} palindrome
 # @return {String}
 def break_palindrome(palindrome)
-    
+  return "" if 1 == palindrome.size
+  (0...palindrome.size/2).each do |i|
+    if ?a != palindrome[i]
+      palindrome[i] = ?a
+      return palindrome
+    end
+  end
+  # All mirrored characters in the str are 'a'.
+  palindrome[palindrome.size-1] = ?b
+  return palindrome
 end
