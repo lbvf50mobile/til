18d4298c203286da5d8ac47d3162e813b95a507e
Dmitry <lbvf50.mobile@gmail.com>
2021-09-23 21:37:19 +0700

    1000. Leetcode: 1328. Break a Palindrome. (begining)

---
+++ 20210923_Thursday/20210923.rb
@@ -0,0 +1,8 @@
+# Leetcode: 1328. Break a Palindrome.
+# https://leetcode.com/problems/break-a-palindrome/
+# https://leetcode.com/explore/challenge/card/september-leetcoding-challenge-2021/639/week-4-september-22nd-september-28th/3985/
+# @param {String} palindrome
+# @return {String}
+def break_palindrome(palindrome)
+    
+end
