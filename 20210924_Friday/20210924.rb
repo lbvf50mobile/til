54f8add781129f8000a64398c07cb6f9605c4f57
Dmitry <lbvf50.mobile@gmail.com>
2021-09-24 23:24:59 +0700

    1001. Leetcode: 1137. N-th Tribonacci Number. (begining)

---
+++ 20210924_Friday/20210924.rb
@@ -0,0 +1,8 @@
+# Leetcode: 1137. N-th Tribonacci Number.
+# https://leetcode.com/problems/n-th-tribonacci-number/
+# https://leetcode.com/explore/challenge/card/september-leetcoding-challenge-2021/639/week-4-september-22nd-september-28th/3986/
+# @param {Integer} n
+# @return {Integer}
+def tribonacci(n)
+    
+end
