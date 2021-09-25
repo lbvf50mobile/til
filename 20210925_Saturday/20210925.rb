5865c180139f59c758275b9eba241f46ad54cf01
Dmitry <lbvf50.mobile@gmail.com>
2021-09-25 23:20:50 +0700

    1002. Leetcode: 1293. Shortest Path in a Grid with Obstacles Elimination. (begining)

---
+++ 20210925_Saturday/20210925.rb
@@ -0,0 +1,9 @@
+# Leetcode: 1293. Shortest Path in a Grid with Obstacles Elimination.
+# https://leetcode.com/problems/shortest-path-in-a-grid-with-obstacles-elimination/
+# https://leetcode.com/explore/challenge/card/september-leetcoding-challenge-2021/639/week-4-september-22nd-september-28th/3987/
+# @param {Integer[][]} grid
+# @param {Integer} k
+# @return {Integer}
+def shortest_path(grid, k)
+    
+end
