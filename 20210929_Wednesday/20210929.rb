b8dbc456672dc4218f9409405641eb2a31b5ce6c
Dmitry <lbvf50.mobile@gmail.com>
2021-09-29 23:24:57 +0700

    1005. Leetcode: 725. Split Linked List in Parts. (article and solution)

---
+++ 20210929_Wednesday/20210929.rb
@@ -0,0 +1,17 @@
+# Leetcode: 725. Split Linked List in Parts.
+# https://leetcode.com/problems/split-linked-list-in-parts/
+# https://leetcode.com/explore/challenge/card/september-leetcoding-challenge-2021/640/week-5-september-29th-september-30th/3992/
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val = 0, _next = nil)
+#         @val = val
+#         @next = _next
+#     end
+# end
+# @param {ListNode} head
+# @param {Integer} k
+# @return {ListNode[]}
+def split_list_to_parts(head, k)
+    
+end
