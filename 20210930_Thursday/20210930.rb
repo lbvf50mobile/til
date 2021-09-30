b24701940db7b366b889347c4d9bdaedc5f4420d
Dmitry <lbvf50.mobile@gmail.com>
2021-10-01 01:24:28 +0700

    1006. Leetcode: 698. Partition to K Equal Sum Subsets. (in action)

--- 20210930_Thursday/20210930.rb
+++ 20210930_Thursday/20210930.rb
@@ -5,5 +5,49 @@
 # @param {Integer} k
 # @return {Boolean}
 def can_partition_k_subsets(nums, k)
-    
+  sum = nums.sum
+  return true if 1 == k
+  return false if 0 != sum%k
+  @k,@chunk = k, sum/k
+  @n = nums
+  return false if @n.any?{|x| x > @chunk} 
+
+  p "Greetings chunk is #{@chunk}"
+  @used = Array.new(nums.size,false)
+  @used[0] = true
+  @reached = false
+
+  rec(@n,0)
+
+  @reached
 end
+
+
+def rec(sum,step)
+  # Calculate current sum, check state.
+  return if @reached
+  # Check do reach a new step.
+  new_step = @chunk * (step+1)
+  return if sum > new_step # Jump over a step.
+  if new_step == sum # Reach a step.
+    step += 1
+    # Now need to check do the aim reached.
+    if @used.all? && @k = step
+      @reached = true
+      return
+    end
+  end
+
+  
+  # Call next.
+  (0...@n.size).each do |i|
+    if ! @used[i]
+      @used[i] = true
+      sum += @n[i]
+      rec(sum, step)
+      sum -= @n[i]
+      @uesd[i] = false
+    end
+  end
+end
+
