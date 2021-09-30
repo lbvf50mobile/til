5fbc0155707d42f186e48a6d0093a5b0dd8ce8e6
Dmitry <lbvf50.mobile@gmail.com>
2021-09-30 23:21:23 +0700

    1006. Leetcode: 698. Partition to K Equal Sum Subsets. (begining)

---
+++ 20210930_Thursday/20210930.rb
@@ -0,0 +1,9 @@
+# Leetcode: 698. Partition to K Equal Sum Subsets.
+# https://leetcode.com/problems/partition-to-k-equal-sum-subsets/
+# https://leetcode.com/explore/challenge/card/september-leetcoding-challenge-2021/640/week-5-september-29th-september-30th/3993/
+# @param {Integer[]} nums
+# @param {Integer} k
+# @return {Boolean}
+def can_partition_k_subsets(nums, k)
+    
+end
