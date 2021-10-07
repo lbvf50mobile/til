179c4ee2e9d3e306d5285a2050e77d711b8ff788
Dmitry <lbvf50.mobile@gmail.com>
2021-10-08 00:27:27 +0700

    1012. Leetcode: 79. Word Search. (TLE)

--- 20211007_Thursday/20211007.rb
+++ 20211007_Thursday/20211007.rb
@@ -3,6 +3,9 @@
 # @param {Character[][]} board
 # @param {String} word
 # @return {Boolean}
+# TLE:
+# [["A","A","A","A","A","A"],["A","A","A","A","A","A"],["A","A","A","A","A","A"],["A","A","A","A","A","A"],["A","A","A","A","A","A"],["A","A","A","A","A","A"]]
+# "AAAAAAAAAAAAAAB"
 def exist(board, word)
   @b,@w = board, word
   @ans = false
@@ -17,17 +20,21 @@ end
 
 def rec(i,j,k)
   return if @ans
-  return if ! i.between?(0,@b.size-1)
-  return if ! j.between?(0,@b[0].size-1)
+  return if ! valid(i,j)
   return if ! @unused[i][j]
   return if  @b[i][j] != @w[k]
   if k == @w.size - 1
     @ans = true
     return
   end
+  moves = [[i,j+1],[i,j-1],[i-1,j],[i+1,j]]
   @unused[i][j] = false
-  [[i,j+1],[j,j-1],[i-1,j],[i+1,j]].each do |(ni,nj)|
+  moves.each do |(ni,nj)|
     rec(ni,nj,k+1)
   end
   @unused[i][j] = true
+end
+
+def valid(i,j)
+  i.between?(0,@b.size-1) && j.between?(0,@b[0].size-1)
 end
