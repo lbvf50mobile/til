1454b0d1c18453c5406dec9983abb1aa5a066094
Dmitry <lbvf50.mobile@gmail.com>
2021-10-07 19:25:18 +0700

    1011. Leetcode: 442. Find All Duplicates in an Array. (article and solution)

--- 20211006_Wednesday/20211006.rb
+++ 20211006_Wednesday/20211006.rb
@@ -1,5 +1,11 @@
 # Leetcode: 442. Find All Duplicates in an Array.
 # https://leetcode.com/problems/find-all-duplicates-in-an-array/
+# = = = = = = =
+# Accepted.
+# Thanks God!
+# = = = = = = =
+# Runtime: 116 ms, faster than 94.44% of Ruby online submissions for Find All Duplicates in an Array.
+# Memory Usage: 223.1 MB, less than 5.56% of Ruby online submissions for Find All Duplicates in an Array.
 # @param {Integer[]} nums
 # @return {Integer[]}
 def find_duplicates(nums)
