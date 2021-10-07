ef77a79a94d93f2f96f19f44615934f18356b0e4
Dmitry <lbvf50.mobile@gmail.com>
2021-10-08 04:09:54 +0700

    1012. Leetcode: 79. Word Search. (still TLE)

--- 20211007_Thursday/20211007.rb
+++ 20211007_Thursday/20211007.rb
@@ -3,36 +3,36 @@
 # @param {Character[][]} board
 # @param {String} word
 # @return {Boolean}
-# TLE:
-# [["A","A","A","A","A","A"],["A","A","A","A","A","A"],["A","A","A","A","A","A"],["A","A","A","A","A","A"],["A","A","A","A","A","A"],["A","A","A","A","A","A"]]
-# "AAAAAAAAAAAAAAB"
 def exist(board, word)
   @b,@w = board, word
-  @ans = false
-  @unused = Array.new(@b.size).map{ Array.new(@b[0].size,true)}
+
   (0...@b.size).each do |i|
     (0...@b[0].size).each do |j|
-      rec(i,j,0) if @b[i][j] == @w[0]
+      next if @b[i][j] != @w[0]
+      return true if rec(i,j,0) 
     end
   end
-  @ans
+  return false
 end
 
 def rec(i,j,k)
-  return if @ans
-  return if ! valid(i,j)
-  return if ! @unused[i][j]
-  return if  @b[i][j] != @w[k]
-  if k == @w.size - 1
-    @ans = true
-    return
+  return true if k == @w.size - 1
+  tmp = @b[i][j]
+  @b[i][j] = ?#
+  if  valid(i+1,j)  && @b[i+1][j] == @w[k+1]
+    return true if rec(i+1,j,k+1)
   end
-  moves = [[i,j+1],[i,j-1],[i-1,j],[i+1,j]]
-  @unused[i][j] = false
-  moves.each do |(ni,nj)|
-    rec(ni,nj,k+1)
+  if  valid(i-1,j)  && @b[i-1][j] == @w[k+1]
+    return true if rec(i-1,j,k+1)
   end
-  @unused[i][j] = true
+  if  valid(i,j+1)  && @b[i][j+1] == @w[k+1]
+    return true if rec(i,j+1,k+1)
+  end
+  if  valid(i,j-1)  && @b[i][j-1] == @w[k+1]
+    return true if rec(i,j-1,k+1)
+  end
+  @b[i][j] = tmp
+  return false
 end
 
 def valid(i,j)
