10d698bebb7db9d88faf2317b8e824519a3427e8
Dmitry <lbvf50.mobile@gmail.com>
2021-10-08 23:48:17 +0700

    1013. Leetcode: 208. Implement Trie (Prefix Tree). (begining)

---
+++ 20211008_Friday/20211008.rb
@@ -0,0 +1,42 @@
+# Leetcode: 208. Implement Trie (Prefix Tree).
+# https://leetcode.com/problems/implement-trie-prefix-tree/
+class Trie
+      def initialize()
+                
+            end
+
+
+=begin
+    :type word: String
+        :rtype: Void
+=end
+    def insert(word)
+              
+          end
+
+
+=begin
+    :type word: String
+        :rtype: Boolean
+=end
+    def search(word)
+              
+          end
+
+
+=begin
+    :type prefix: String
+        :rtype: Boolean
+=end
+    def starts_with(prefix)
+              
+          end
+
+
+end
+
+# Your Trie object will be instantiated and called as such:
+# # obj = Trie.new()
+# # obj.insert(word)
+# # param_2 = obj.search(word)
+# # param_3 = obj.starts_with(prefix)
