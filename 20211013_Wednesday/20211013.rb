15fc6384213ee0de483640eaaa0ee7a752a8346d
Dmitry <lbvf50.mobile@gmail.com>
2021-10-13 23:16:22 +0700

    1017. Leetcode: 1008. Construct Binary Search Tree from Preorder Traversal. (begining)

---
+++ 20211013_Wednesday/20211013.rb
@@ -0,0 +1,16 @@
+# Leetcode: 1008. Construct Binary Search Tree from Preorder Traversal.
+# https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal/
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {Integer[]} preorder
+# @return {TreeNode}
+def bst_from_preorder(preorder)
+    
+end
