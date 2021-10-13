34defd0c46eb44d21177e727401a49ceb38df2fb
Dmitry <lbvf50.mobile@gmail.com>
2021-10-13 23:49:03 +0700

    1017. Leetcode: 1008. Construct Binary Search Tree from Preorder Traversal. (article and solution)

--- 20211013_Wednesday/20211013.rb
+++ 20211013_Wednesday/20211013.rb
@@ -1,5 +1,11 @@
 # Leetcode: 1008. Construct Binary Search Tree from Preorder Traversal.
 # https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal/
+# = = = = = = =
+# Accepted.
+# Thanks God!
+# = = = = = = =
+# Runtime: 105 ms, faster than 7.14% of Ruby online submissions for Construct Binary Search Tree from Preorder Traversal.
+# Memory Usage: 210.1 MB, less than 71.43% of Ruby online submissions for Construct Binary Search Tree from Preorder Traversal.
 # Definition for a binary tree node.
 # class TreeNode
 #     attr_accessor :val, :left, :right
@@ -12,5 +18,25 @@
 # @param {Integer[]} preorder
 # @return {TreeNode}
 def bst_from_preorder(preorder)
-    
+  @head = TreeNode.new(preorder[0])
+  (1...preorder.size).each do |i|
+    insert(preorder[i])
+  end
+  @head
+end
+
+def insert(value)
+  node = @head
+  while true
+    step = value < node.val ? node.left : node.right
+    if step
+      node = step
+    elsif value < node.val
+      node.left = TreeNode.new(value)
+      return
+    else
+      node.right = TreeNode.new(value)
+      return
+    end
+  end
 end
