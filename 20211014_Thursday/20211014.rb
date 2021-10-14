b7775651d401dca8d52f7054207b70933c1cd0b6
Dmitry <lbvf50.mobile@gmail.com>
2021-10-14 23:42:30 +0700

    1018. Leetcode: 279. Perfect Squares. (begining)

---
+++ 20211014_Thursday/20211014.rb
@@ -0,0 +1,9 @@
+# Leetcode: 279. Perfect Squares.
+# https://leetcode.com/problems/perfect-squares/
+# based on:
+# https://leetcode.com/problems/perfect-squares/discuss/71495/An-easy-understanding-DP-solution-in-Java
+# @param {Integer} n
+# @return {Integer}
+def num_squares(n)
+    
+end
