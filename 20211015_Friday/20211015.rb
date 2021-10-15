9bb6878e6a0c78835c153717f901176c2cc847de
Dmitry <lbvf50.mobile@gmail.com>
2021-10-15 23:19:52 +0700

    1019. Leetcode: 309. Best Time to Buy and Sell Stock with Cooldown. (begining)

---
+++ 20211015_Friday/20211015.rb
@@ -0,0 +1,7 @@
+# Leetcode: 309. Best Time to Buy and Sell Stock with Cooldown.
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/
+# @param {Integer[]} prices
+# @return {Integer}
+def max_profit(prices)
+    
+end
