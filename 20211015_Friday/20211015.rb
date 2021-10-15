20eb18f327c4e96aa98662c968eb3c5b16a388dd
Dmitry <lbvf50.mobile@gmail.com>
2021-10-15 23:43:26 +0700

    1019. Leetcode: 309. Best Time to Buy and Sell Stock with Cooldown. (solution)

--- 20211015_Friday/20211015.rb
+++ 20211015_Friday/20211015.rb
@@ -1,7 +1,23 @@
 # Leetcode: 309. Best Time to Buy and Sell Stock with Cooldown.
 # https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/
+# = = = = = = =
+# Accepted.
+# Thanks God!
+# = = = = = = =
+# Runtime: 91 ms, faster than 25.00% of Ruby online submissions for Best Time to Buy and Sell Stock with Cooldown.
+# Solution based on:
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/discuss/75927/Share-my-thinking-process
 # @param {Integer[]} prices
 # @return {Integer}
 def max_profit(prices)
-    
+  # https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/discuss/75927/Share-my-thinking-process
+  return 0  if prices.size < 2
+  sell, buy, prev_sell, prev_buy = 0, -prices[0],0,0
+  prices.each do |price|
+    prev_buy = buy
+    buy = [prev_sell - price, prev_buy].max
+    prev_sell = sell
+    sell = [prev_buy + price, prev_sell].max
+  end
+  sell
 end
