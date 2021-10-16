b5acdc4f9cd1624de50585631b1b1ccece9932cd
Dmitry <lbvf50.mobile@gmail.com>
2021-10-16 23:10:10 +0700

    1020. Leetcode: 123. Best Time to Buy and Sell Stock III. (begining)

---
+++ 20211016_Saturday/20211016.rb
@@ -0,0 +1,7 @@
+# Leetcode: 123. Best Time to Buy and Sell Stock III.
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/
+# @param {Integer[]} prices
+# @return {Integer}
+def max_profit(prices)
+    
+end
