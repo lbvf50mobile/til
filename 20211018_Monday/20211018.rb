2c52cb8158544f70ceb3bfa7e5997b8ed5d5d86b
Dmitry <lbvf50.mobile@gmail.com>
2021-10-18 19:13:51 +0700

    1021. Leetcode: 993. Cousins in Binary Tree. (begining)

---
+++ 20211018_Monday/20211018.rb
@@ -0,0 +1,18 @@
+# Leetcode: 993. Cousins in Binary Tree.
+# https://leetcode.com/problems/cousins-in-binary-tree/
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @param {Integer} x
+# @param {Integer} y
+# @return {Boolean}
+def is_cousins(root, x, y)
+    
+end
