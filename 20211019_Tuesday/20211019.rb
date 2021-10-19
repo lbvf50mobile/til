c17c5b0ca0f62443bd5c186b01fbe5307c7f68ad
Dmitry <lbvf50.mobile@gmail.com>
2021-10-19 23:46:41 +0700

    1022. Leetcode: 496. Next Greater Element I. (begining)

---
+++ 20211019_Tuesday/20211019.rb
@@ -0,0 +1,8 @@
+# Leetcode: 496. Next Greater Element I.
+# https://leetcode.com/problems/next-greater-element-i/
+# @param {Integer[]} nums1
+# @param {Integer[]} nums2
+# @return {Integer[]}
+def next_greater_element(nums1, nums2)
+    
+end
