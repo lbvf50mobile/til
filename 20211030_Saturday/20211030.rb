7ed7fcf58ca436a232567ba7a357b1a7b5b2ab32
Dmitry <lbvf50.mobile@gmail.com>
2021-10-30 23:32:16 +0700

    1033. Leetcode: 1044. Longest Duplicate Substring. (begining)

---
+++ 20211030_Saturday/20211030.rb
@@ -0,0 +1,7 @@
+# Leetcode: 1044. Longest Duplicate Substring.
+# https://leetcode.com/problems/longest-duplicate-substring/
+# @param {String} s
+# @return {String}
+def longest_dup_substring(s)
+    
+end
