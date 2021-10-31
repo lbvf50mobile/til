801ed5407373ec4538b7d95eea3b88a267dc8dd2
Dmitry <lbvf50.mobile@gmail.com>
2021-10-31 23:42:01 +0700

    1034. Leetcode: 430. Flatten a Multilevel Doubly Linked List. (article and solution)

--- 20211031_Sunday/20211031.rb
+++ 20211031_Sunday/20211031.rb
@@ -1,5 +1,11 @@
 # Leetcode: 430. Flatten a Multilevel Doubly Linked List.
 # https://leetcode.com/problems/flatten-a-multilevel-doubly-linked-list/
+# = = = = = = =
+# Accepted.
+# Thanks God!
+# = = = = = = =
+# Runtime: 56 ms, faster than 95.45% of Ruby online submissions for Flatten a Multilevel Doubly Linked List.
+# Memory Usage: 210.4 MB, less than 40.91% of Ruby online submissions for Flatten a Multilevel Doubly Linked List.
 # Definition for a Node.
 # class Node
 #     attr_accessor :val, :prev, :next, :child
@@ -16,13 +22,21 @@
 def flatten(root)
   return root if ! root
   head = root
-  que = []
+  stack = []
   node = root
   while node
-    que.unshift(node.child) if node.child
-    if node.next.nil? && (! que.empty?)
-      tmp = que.pop()
+    # Set child as next.
+    if node.child
+      stack.push(node.next) if node.next
+      node.next = node.child
+      node.child.prev = node
+      node.child = nil
+    end
+    # Next is nil, need to get next from stack.
+    if node.next.nil? && (! stack.empty?)
+      tmp = stack.pop()
       node.next = tmp
+      tmp.prev = node
     end
     node = node.next
   end
