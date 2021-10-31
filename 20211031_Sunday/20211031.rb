ee4140deab4042b288d5aa9ed4e2fb53a4dfebc1
Dmitry <lbvf50.mobile@gmail.com>
2021-10-31 22:28:01 +0700

    1034. Leetcode: 430. Flatten a Multilevel Doubly Linked List. (begining)

---
+++ 20211031_Sunday/20211031.rb
@@ -0,0 +1,18 @@
+# Leetcode: 430. Flatten a Multilevel Doubly Linked List.
+# https://leetcode.com/problems/flatten-a-multilevel-doubly-linked-list/
+# Definition for a Node.
+# class Node
+#     attr_accessor :val, :prev, :next, :child
+#     def initialize(val=nil, prev=nil, next_=nil, child=nil)
+#         @val = val
+#         @prev = prev
+#         @next = next_
+#         @child = child
+#     end
+# end
+
+# @param {Node} root
+# @return {Node}
+def flatten(root)
+    
+end
