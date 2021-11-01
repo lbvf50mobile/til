8504db3460e97bfc34f318d6ec90cff5f452400c
Dmitry <lbvf50.mobile@gmail.com>
2021-11-01 23:33:14 +0700

    1035. Leetcode: 130. Surrounded Regions. (begining)

---
+++ 20211101_Monday/20211101.rb
@@ -0,0 +1,7 @@
+# Leetcode: 130. Surrounded Regions.
+# https://leetcode.com/problems/surrounded-regions/
+# @param {Character[][]} board
+# @return {Void} Do not return anything, modify board in-place instead.
+def solve(board)
+    
+end
