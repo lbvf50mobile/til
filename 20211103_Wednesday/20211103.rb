71db09c5ffc5bd7dd213ab09a69671dcf9fdccc8
Dmitry <lbvf50.mobile@gmail.com>
2021-11-03 23:32:48 +0700

    1037. Leetcode: 129. Sum Root to Leaf Numbers. (begining)

---
+++ 20211103_Wednesday/20211103.rb
@@ -0,0 +1,16 @@
+# Leetcode: 129. Sum Root to Leaf Numbers.
+# https://leetcode.com/problems/sum-root-to-leaf-numbers/
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {Integer}
+def sum_numbers(root)
+    
+end
