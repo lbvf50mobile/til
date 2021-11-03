568d4500833101499fa29965bd9a13c0716b1103
Dmitry <lbvf50.mobile@gmail.com>
2021-11-03 23:47:18 +0700

    1037. Leetcode: 129. Sum Root to Leaf Numbers. (article and solution)

--- 20211103_Wednesday/20211103.rb
+++ 20211103_Wednesday/20211103.rb
@@ -1,5 +1,11 @@
 # Leetcode: 129. Sum Root to Leaf Numbers.
 # https://leetcode.com/problems/sum-root-to-leaf-numbers/
+# = = = = = = =
+# Accepted.
+# Thanks God!
+# = = = = = = =
+# Runtime: 48 ms, faster than 88.24% of Ruby online submissions for Sum Root to Leaf Numbers.
+# Memory Usage: 209.9 MB, less than 35.29% of Ruby online submissions for Sum Root to Leaf Numbers.
 # Definition for a binary tree node.
 # class TreeNode
 #     attr_accessor :val, :left, :right
@@ -12,5 +18,17 @@
 # @param {TreeNode} root
 # @return {Integer}
 def sum_numbers(root)
-    
+  @sum = 0
+  dfs(root,0)
+  @sum
+end
+
+def dfs(node, num)
+  return if node.nil?
+  num = num*10 + node.val
+  if node.left.nil? && node.right.nil?
+    @sum += num
+  end
+  dfs(node.left, num)
+  dfs(node.right, num)
 end
