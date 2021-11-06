b83a2d95e5c75ab439f374f93d6db883b7a37a85
Dmitry <lbvf50.mobile@gmail.com>
2021-11-07 00:11:45 +0700

    1040. Leetcode: 260. Single Number III. (article and solution)

--- 20211106_Saturday/20211106.rb
+++ 20211106_Saturday/20211106.rb
@@ -1,7 +1,25 @@
 # Leetcode: 260. Single Number III.
 # https://leetcode.com/problems/single-number-iii/
+# = = = = = = =
+# Accepted.
+# Thanks God!
+# = = = = = = =
+# Runtime: 105 ms, faster than 11.11% of Ruby online submissions for Single Number III.
+# Memory Usage: 211.5 MB, less than 22.22% of Ruby online submissions for Single Number III.
 # @param {Integer[]} nums
 # @return {Integer[]}
 def single_number(nums)
-    
+  # Based on:
+  # https://leetcode.com/problems/single-number-iii/discuss/68907/Naive-Python
+  require "set"
+  a = Set.new
+  b = Set.new
+  nums.each do |el|
+    if ! a.include?(el)
+      a.add(el)
+    else
+      b.add(el)
+    end
+  end
+  (a-b).to_a
 end
