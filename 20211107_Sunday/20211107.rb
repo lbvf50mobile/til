d1aee76b054612029575d69518d07a3d90a15905
Dmitry <lbvf50.mobile@gmail.com>
2021-11-07 19:57:59 +0700

    1041. Leetcode: 43. Multiply Strings. (begining)

---
+++ 20211107_Sunday/20211107.rb
@@ -0,0 +1,8 @@
+# Leetcode: 43. Multiply Strings.
+# https://leetcode.com/problems/multiply-strings/ 
+# @param {String} num1
+# @param {String} num2
+# @return {String}
+def multiply(num1, num2)
+    
+end
