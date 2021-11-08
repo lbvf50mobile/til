d41d2c468049339c715c821df1edf88e7486440e
Dmitry <lbvf50.mobile@gmail.com>
2021-11-08 23:49:51 +0700

    1042. Leetcode: 96. Unique Binary Search Trees. (solution)

--- 20211108_Monday/20211108.rb
+++ 20211108_Monday/20211108.rb
@@ -1,7 +1,24 @@
 # Leetcode: 96. Unique Binary Search Trees.
 # https://leetcode.com/problems/unique-binary-search-trees/
+# = = = = = = =
+# Accepted.
+# Thanks God!
+# = = = = = = =
+# Runtime: 52 ms, faster than 96.30% of Ruby online submissions for Unique Binary Search Trees.
+# Memory Usage: 209.7 MB, less than 92.59% of Ruby online submissions for Unique Binary Search Trees.
 # @param {Integer} n
 # @return {Integer}
 def num_trees(n)
-    
+  @dp = Array.new(20).map{ Array.new(20)}
+  track(1,n)
+end
+
+def track(i,j)
+  return 1 if i > j
+  return @dp[i][j] if @dp[i][j]
+  total = 0
+  (i..j).each do |k|
+    total += track(i,k-1) *  track(k+1,j) 
+  end
+  @dp[i][j] = total 
 end
