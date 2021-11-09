e95a0d61c18f5000bda17dc295f12d06aa2e7e7a
Dmitry <lbvf50.mobile@gmail.com>
2021-11-09 23:12:53 +0700

    1043. Leetcode: 1178. Number of Valid Words for Each Puzzle. (begining)

---
+++ 20211109_Tuesday/20211109.rb
@@ -0,0 +1,8 @@
+# Leetcode: 1178. Number of Valid Words for Each Puzzle.
+# https://leetcode.com/problems/number-of-valid-words-for-each-puzzle/
+# @param {String[]} words
+# @param {String[]} puzzles
+# @return {Integer[]}
+def find_num_of_valid_words(words, puzzles)
+    
+end
