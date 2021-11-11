4678cee1ed1df87ccebd1de3aa064e92351ce176
Dmitry <lbvf50.mobile@gmail.com>
2021-11-11 23:28:04 +0700

    1045. Leetcode: 1413. Minimum Value to Get Positive Step by Step Sum. (begining)

---
+++ 20211111_Thursday/20211111.rb
@@ -0,0 +1,7 @@
+# Leetcode: 1413. Minimum Value to Get Positive Step by Step Sum.
+# https://leetcode.com/problems/minimum-value-to-get-positive-step-by-step-sum/
+# @param {Integer[]} nums
+# @return {Integer}
+def min_start_value(nums)
+    
+end
