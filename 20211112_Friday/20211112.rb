3d6af67384fd66dfbf12eb15111bc5ad7fe4de00
Dmitry <lbvf50.mobile@gmail.com>
2021-11-12 19:53:46 +0700

    1046. Leetcode: 203. Remove Linked List Elements. (begining)

---
+++ 20211112_Friday/20211112.rb
@@ -0,0 +1,16 @@
+# Leetcode: 203. Remove Linked List Elements.
+# https://leetcode.com/problems/remove-linked-list-elements/
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val = 0, _next = nil)
+#         @val = val
+#         @next = _next
+#     end
+# end
+# @param {ListNode} head
+# @param {Integer} val
+# @return {ListNode}
+def remove_elements(head, val)
+    
+end
