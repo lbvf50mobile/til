538061393f743a3bb79da7b565b7736664f3bf54
Dmitry <lbvf50.mobile@gmail.com>
2021-11-12 20:28:42 +0700

    1046. Leetcode: 203. Remove Linked List Elements. (solution)

--- 20211112_Friday/20211112.rb
+++ 20211112_Friday/20211112.rb
@@ -1,5 +1,11 @@
 # Leetcode: 203. Remove Linked List Elements.
 # https://leetcode.com/problems/remove-linked-list-elements/
+# = = = = = = =
+# Accepted.
+# Thanks God!
+# = = = = = = =
+# Runtime: 76 ms, faster than 53.19% of Ruby online submissions for Remove Linked List Elements.
+# Memory Usage: 211.3 MB, less than 61.70% of Ruby online submissions for Remove Linked List Elements.
 # Definition for singly-linked list.
 # class ListNode
 #     attr_accessor :val, :next
@@ -12,5 +18,26 @@
 # @param {Integer} val
 # @return {ListNode}
 def remove_elements(head, val)
-    
+  # First loop. Remove from the begining.
+  while head && head.val == val
+    head = head.next
+  end
+  prev,tmp = nil, head
+  while tmp
+    # What if there no any val?
+    if tmp.val != val
+      prev = tmp
+      tmp = tmp.next
+    # But the val appears.
+    else
+      while tmp && tmp.val == val
+        tmp = tmp.next
+      end
+      # Now tmp does not have val.
+      # If prev need to switch pointers.
+      prev.next = tmp if prev
+    end
+  end
+  head
 end
+  
