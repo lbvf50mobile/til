c785237b9c609aba505549247670ccbb6109208e
Dmitry <lbvf50.mobile@gmail.com>
2021-11-13 15:38:53 +0700

    1047. Leetcode: 739. Daily Temperatures. (begining)

---
+++ 20211113_Saturday/20211113.rb
@@ -0,0 +1,7 @@
+# Leetcode: 739. Daily Temperatures.
+# https://leetcode.com/problems/daily-temperatures/
+# @param {Integer[]} temperatures
+# @return {Integer[]}
+def daily_temperatures(temperatures)
+    
+end
