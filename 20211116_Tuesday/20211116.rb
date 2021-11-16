a80d420875044264d60c51848088a88252197b0d
Dmitry <lbvf50.mobile@gmail.com>
2021-11-17 01:45:29 +0700

    1049. Leetcode: 668. Kth Smallest Number in Multiplication Table. (solution)

--- 20211116_Tuesday/20211116.rb
+++ 20211116_Tuesday/20211116.rb
@@ -1,15 +1,38 @@
 # Leetcode: 668. Kth Smallest Number in Multiplication Table.
 # https://leetcode.com/problems/kth-smallest-number-in-multiplication-table/
+# = = = = = = =
+# Accepted.
+# Thanks God!
+# = = = = = = =
+# Runtime: 364 ms, faster than 100.00% of Ruby online submissions for Kth Smallest Number in Multiplication Table.
+# Memory Usage: 209.8 MB, less than 50.00% of Ruby online submissions for Kth Smallest Number in Multiplication Table.
 # @param {Integer} m
 # @param {Integer} n
 # @param {Integer} k
 # @return {Integer}
 def find_kth_number(m, n, k)
-  arr = []
-  (1..m).each do |i|
-    (1..n).each do |j|
-      arr.push(i*j)
+  # Based on:
+  # https://leetcode.com/problems/kth-smallest-number-in-multiplication-table/solution/
+  @m,@n,@k = m,n,k
+  lo, hi = 1, @m*@n
+  while lo < hi
+    mi = (lo+hi)/2
+    if ! enough(mi)
+      lo = mi + 1
+    else
+      hi = mi
     end
   end
-  arr.sort[k-1]
+  lo
+end
+
+
+def enough(x)
+  count = 0
+  (1..@m).each do |i|
+    a = x/i
+    a = @n if @n < a
+    count += a
+  end
+  return count >= @k
 end
