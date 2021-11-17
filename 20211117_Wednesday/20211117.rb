55d4deea18443a237868557f9d5ff20a5c2c508c
Dmitry <lbvf50.mobile@gmail.com>
2021-11-17 23:42:48 +0700

    1050. Leetcode: 62. Unique Paths. (article and solution)

--- 20211117_Wednesday/20211117.rb
+++ 20211117_Wednesday/20211117.rb
@@ -1,12 +1,19 @@
 # Leetcode: 62. Unique Paths.
 # https://leetcode.com/problems/unique-paths/
+# = = = = = = =
+# Accepted.
+# Thanks God!
+# = = = = = = =
+# Runtime: 68 ms, faster than 37.65% of Ruby online submissions for Unique Paths.
+# Memory Usage: 209.9 MB, less than 34.12% of Ruby online submissions for Unique Paths.
 # @param {Integer} m
 # @param {Integer} n
 # @return {Integer}
 def unique_paths(m, n)
   dp = Array.new(m).map{ Array.new(n,0)}
-  (0...n).each{|j| dp[0][j] = 1}
-  (1...m).each do |i|
+  dp[0][0] = 1 # Before start I forgot to made pushes from first row.
+  # Fixing.
+  (0...m).each do |i|
     (0...n).each do |j|
       value = dp[i][j]
       [[i+1,j],[i,j+1]].each do |(ii,jj)|
