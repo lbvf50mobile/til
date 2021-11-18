07ee5c7a8d8ce31999f384f4f3d8a595aa7757be
Dmitry <lbvf50.mobile@gmail.com>
2021-11-18 23:48:22 +0700

    1051. Leetcode: 448. Find All Numbers Disappeared in an Array. (article and solution)

--- 20211118_Thursday/20211118.rb
+++ 20211118_Thursday/20211118.rb
@@ -1,7 +1,19 @@
 # Leetcode: 448. Find All Numbers Disappeared in an Array. 
 # https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+# = = = = = = =
+# Accepted.
+# Thanks God!
+# = = = = = = =
+# Runtime: 108 ms, faster than 97.50% of Ruby online submissions for Find All Numbers Disappeared in an Array.
+# Memory Usage: 216.8 MB, less than 61.25% of Ruby online submissions for Find All Numbers Disappeared in an Array.
 # @param {Integer[]} nums
 # @return {Integer[]}
 def find_disappeared_numbers(nums)
-    
+  arr = Array.new(nums.size + 1)
+  nums.each{|i| arr[i] = true}
+  ans = []
+  (1..nums.size).each do |j|
+    ans.push(j) if arr[j].nil?
+  end
+  ans
 end
