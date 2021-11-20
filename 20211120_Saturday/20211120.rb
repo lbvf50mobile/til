3e84a7d9f1fa3d5ff11a6e213243598cf78eca8e
Dmitry <lbvf50.mobile@gmail.com>
2021-11-20 22:46:17 +0700

    1053. Leetcode: 540. Single Element in a Sorted Array. (begining)

---
+++ 20211120_Saturday/20211120.rb
@@ -0,0 +1,7 @@
+# Leetcode: 540. Single Element in a Sorted Array.
+# https://leetcode.com/problems/single-element-in-a-sorted-array/
+# @param {Integer[]} nums
+# @return {Integer}
+def single_non_duplicate(nums)
+    
+end
