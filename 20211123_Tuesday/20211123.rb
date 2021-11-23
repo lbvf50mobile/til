767b79f1c1701a52a86b34eb8ff5bb6fe76bc98c
Dmitry <lbvf50.mobile@gmail.com>
2021-11-23 23:50:48 +0700

    1055. Leetcode: 952. Largest Component Size by Common Factor. (in action)

--- 20211123_Tuesday/20211123.rb
+++ 20211123_Tuesday/20211123.rb
@@ -3,5 +3,41 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def largest_component_size(nums)
-    
+  ans = 0
+  uf = Uf.new(nums.max)
+  count = {}
+  nums.each do |a|
+    (2..(Math.sqrt(a)+1).to_i).each do |num|
+      if 0 == a%num
+        uf.union(a,num)
+        uf.union(a,a/num)
+      end
+    end
+  end
+  nums.each do |a|
+    pa = uf.find(a)
+    count ||= 0
+    count[pa] += 1
+    ans = count[pa] if ans < count[pa]
+  end
+  ans
+end
+
+class Uf
+  def initialize(n)
+    @prnt = (1..n).to_a
+  end
+  def union(u,v)
+    pu, pv = find(u), find(v)
+    return false if pu == pv
+    @prnt[pu] = pv
+    true
+  end
+  def find(u)
+    p u
+    if @prnt[u] != u 
+      @prnt[u] = find(@prnt[u])
+    end
+    @prnt[u]
+  end
 end
