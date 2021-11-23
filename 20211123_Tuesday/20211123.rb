0e9d57adbeeb38fda177149a54174cdeda3757ec
Dmitry <lbvf50.mobile@gmail.com>
2021-11-23 23:31:51 +0700

    1055. Leetcode: 952. Largest Component Size by Common Factor. (begining)

---
+++ 20211123_Tuesday/20211123.rb
@@ -0,0 +1,7 @@
+# Leetcode: 952. Largest Component Size by Common Factor.
+# https://leetcode.com/problems/largest-component-size-by-common-factor/
+# @param {Integer[]} nums
+# @return {Integer}
+def largest_component_size(nums)
+    
+end
