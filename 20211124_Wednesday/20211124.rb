4a07bea777249e3762920b29779a41c313a90128
Dmitry <lbvf50.mobile@gmail.com>
2021-11-24 23:22:23 +0700

    1056. Leetcode: 986. Interval List Intersections. (begining)

---
+++ 20211124_Wednesday/20211124.rb
@@ -0,0 +1,8 @@
+# Leetcode: 986. Interval List Intersections.
+# https://leetcode.com/problems/interval-list-intersections/
+# @param {Integer[][]} first_list
+# @param {Integer[][]} second_list
+# @return {Integer[][]}
+def interval_intersection(first_list, second_list)
+    
+end
