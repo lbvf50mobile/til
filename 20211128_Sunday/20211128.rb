62da308a1e1861405a8b342ed1e7455927cbd2db
Dmitry <lbvf50.mobile@gmail.com>
2021-11-29 00:32:31 +0700

    1060. Leetcode: 797. All Paths From Source to Target. (check byte, uniq n)

--- 20211128_Sunday/20211128.rb
+++ 20211128_Sunday/20211128.rb
@@ -4,7 +4,7 @@
 # Accepted.
 # Thanks God!
 # = = = = = = =
-# Runtime: 124 ms, faster than 75.00% of Ruby online submissions for All Paths From Source to Target.
+# Runtime: 144 ms, faster than 37.50% of Ruby online submissions for All Paths From Source to Target.
 # Memory Usage: 213.3 MB, less than 75.00% of Ruby online submissions for All Paths From Source to Target.
 # @param {Integer[][]} graph
 # @return {Integer[][]}
@@ -13,18 +13,31 @@
 def all_paths_source_target(graph)
   @g = graph
   @visited, @ans = [], []
-  bt(0)
+  bt(0,0)
   @ans
 end
 
-def bt(i)
+def bt(i,n)
   @visited.push(i)
+  n = set_byte(n,i)
+  
   if i == @g.size - 1
     @ans.push(@visited.clone)
   else
     @g[i].each do |j|
-      bt(j)
+      bt(j,n) if ! check_byte(n,j)
     end
   end
+  n = reset_byte(n,i)
   @visited.pop()
+end
+def set_byte(number,index)
+  number | (1 << index)
+end
+def check_byte(number,index)
+  (number >> index) & 1 == 1
+end
+
+def reset_byte(number, index)
+  number &  ((16**2-1)^(1 << index))
 end
