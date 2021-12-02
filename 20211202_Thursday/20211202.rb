94ac0f229fa6834e3ccce9cf4b77cfeb438e3078
Dmitry <lbvf50.mobile@gmail.com>
2021-12-02 23:43:26 +0700

    1064. Leetcode: 328. Odd Even Linked List. (begining)

---
+++ 20211202_Thursday/20211202.rb
@@ -0,0 +1,15 @@
+# Leetcode: 328. Odd Even Linked List.
+# https://leetcode.com/problems/odd-even-linked-list/
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val = 0, _next = nil)
+#         @val = val
+#         @next = _next
+#     end
+# end
+# @param {ListNode} head
+# @return {ListNode}
+def odd_even_list(head)
+    
+end
