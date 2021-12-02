1cf7ce47123b6d431289a2798b9cc0bd0b37833c
Dmitry <lbvf50.mobile@gmail.com>
2021-12-03 02:54:15 +0700

    1064. Leetcode: 328. Odd Even Linked List. (article and solution)

--- 20211202_Thursday/20211202.rb
+++ 20211202_Thursday/20211202.rb
@@ -1,5 +1,11 @@
 # Leetcode: 328. Odd Even Linked List.
 # https://leetcode.com/problems/odd-even-linked-list/
+# = = = = = = =
+# Accepted.
+# Thanks God!
+# = = = = = = =
+# Runtime: 52 ms, faster than 90.32% of Ruby online submissions for Odd Even Linked List.
+# Memory Usage: 210.4 MB, less than 54.84% of Ruby online submissions for Odd Even Linked List.
 # Definition for singly-linked list.
 # class ListNode
 #     attr_accessor :val, :next
@@ -11,5 +17,24 @@
 # @param {ListNode} head
 # @return {ListNode}
 def odd_even_list(head)
-    
+  even = ListNode.new
+  odd = ListNode.new
+  even_head, odd_head = even, odd
+  i = 0
+  while head
+    if i.even?
+      odd.next = head
+      odd = head 
+    else
+      even.next = head
+      even = head
+    end
+    head = head.next
+    i += 1
+  end
+  odd.next = nil
+  even.next = nil
+  odd.next = even_head.next
+  even.next = nil
+  odd_head.next
 end
