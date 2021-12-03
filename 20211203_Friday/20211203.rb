a7ae9af99e2afb35c920d195333fc252787b0710
Dmitry <lbvf50.mobile@gmail.com>
2021-12-03 22:09:27 +0700

    1065. Leetcode: 152. Maximum Product Subarray. (begining)

---
+++ 20211203_Friday/20211203.rb
@@ -0,0 +1,7 @@
+# Leetcode: 152. Maximum Product Subarray.
+# https://leetcode.com/problems/maximum-product-subarray/
+# @param {Integer[]} nums
+# @return {Integer}
+def max_product(nums)
+    
+end
