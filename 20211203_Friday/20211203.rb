b4e669ef6a417291f0631837dc08a9cd1329a765
Dmitry <lbvf50.mobile@gmail.com>
2021-12-03 22:55:30 +0700

    1065. Leetcode: 152. Maximum Product Subarray. (article and solution)

--- 20211203_Friday/20211203.rb
+++ 20211203_Friday/20211203.rb
@@ -1,7 +1,35 @@
 # Leetcode: 152. Maximum Product Subarray.
 # https://leetcode.com/problems/maximum-product-subarray/
+# = = = = = = =
+# Accepted.
+# Thanks God!
+# = = = = = = =
+# Runtime: 60 ms, faster than 60.58% of Ruby online submissions for Maximum Product Subarray.
+# Memory Usage: 210.4 MB, less than 39.42% of Ruby online submissions for Maximum Product Subarray.
 # @param {Integer[]} nums
 # @return {Integer}
 def max_product(nums)
-    
+  minimums = Array.new(nums.size,0)
+  maximums = Array.new(nums.size,0)
+  minimums[nums.size-1] = nums.last
+  maximums[nums.size-1] = nums.last
+  max = nums.last
+  (0...(nums.size-1)).reverse_each do |i|
+    x, nmin, nmax = nums[i], minimums[i+1], maximums[i+1]
+    minimums[i] = min_from_three(x,x*nmin,x*nmax)
+    maximums[i] = max_from_three(x,x*nmin,x*nmax) 
+    max = maximums[i] if max < maximums[i]
+  end
+  max
+end
+
+def max_from_three(a,b,c)
+  return a if a >= b && a >= c
+  return b if b >= a && b >= c
+  return c
+end
+def min_from_three(a,b,c)
+  return a if a <= b && a <= c
+  return b if b <= a && b <= c
+  return c
 end
