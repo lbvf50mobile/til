818e3d9aeee0dcd67b831b8373006062ec9ec355
Dmitry <lbvf50.mobile@gmail.com>
2021-12-04 23:51:16 +0700

    1066. Leetcode: 1032. Stream of Characters. (begining)

---
+++ 20211204_Saturday/20211204.rb
@@ -0,0 +1,26 @@
+# Leetcode: 1032. Stream of Characters.
+# https://leetcode.com/problems/stream-of-characters/
+class StreamChecker
+
+=begin
+    :type words: String[]
+=end
+    def initialize(words)
+              
+          end
+
+
+=begin
+    :type letter: Character
+        :rtype: Boolean
+=end
+    def query(letter)
+              
+          end
+
+
+end
+
+# Your StreamChecker object will be instantiated and called as such:
+# # obj = StreamChecker.new(words)
+# # param_1 = obj.query(letter)
