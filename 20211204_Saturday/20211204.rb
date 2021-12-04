aabeb03f3bcd77a966f245fa2d61103b60b2dd3d
Dmitry <lbvf50.mobile@gmail.com>
2021-12-05 03:30:16 +0700

    1066. Leetcode: 1032. Stream of Characters. (tle)

--- 20211204_Saturday/20211204.rb
+++ 20211204_Saturday/20211204.rb
@@ -1,23 +1,56 @@
 # Leetcode: 1032. Stream of Characters.
 # https://leetcode.com/problems/stream-of-characters/
+# TLE;
 class StreamChecker
 
 =begin
-    :type words: String[]
+:type words: String[]
 =end
-    def initialize(words)
-              
-          end
+  def initialize(words)
+    @words,@trie = words, {}
+    @trie[:end] = false # Dose this node is an words end?
+    @array_of_pointers = []
+    create_trie
+  end
 
 
 =begin
-    :type letter: Character
-        :rtype: Boolean
+:type letter: Character
+:rtype: Boolean
 =end
-    def query(letter)
-              
-          end
+  def query(letter)
+    # First add new word pointer.
+    @array_of_pointers.push(@trie)
+    new_pointers = []
+    answer = false
+    # Let iterate over pointers.
+    # Only a pointer that can be moved further would be saved.
+    @array_of_pointers.each do |pointer|
+      if pointer[letter]
+          new_pointers.push  pointer[letter]
+          answer = true if pointer[letter][:end]
+      end
+    end
+    @array_of_pointers = new_pointers
+    answer
+  end
 
+  def create_trie
+    @words.each do |word|
+      # Each word starting from a root.
+      pointer = @trie
+      (0...word.size).each do |i|
+        char = word[i]
+        if ! pointer[char]
+          tmp = {}
+          tmp[:end] = false
+          pointer[char] = tmp
+        end
+        pointer = pointer[char]
+        pointer[:end] = true if i == word.size - 1
+      end
+    end
+  end
 
 end
 
