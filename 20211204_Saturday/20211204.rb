4ddd598de1dad813734cf974fe7c184e3445e8da
Dmitry <lbvf50.mobile@gmail.com>
2021-12-05 20:48:04 +0700

    1066. Leetcode: 1032. Stream of Characters. (solution)

--- 20211204_Saturday/20211204.rb
+++ 20211204_Saturday/20211204.rb
@@ -1,6 +1,11 @@
 # Leetcode: 1032. Stream of Characters.
 # https://leetcode.com/problems/stream-of-characters/
-# TLE;
+# = = = = = = =
+# Accepted.
+# Thanks God!
+# = = = = = = =
+# Runtime: 680 ms, faster than 84.62% of Ruby online submissions for Stream of Characters.
+# Memory Usage: 240.5 MB, less than 84.62% of Ruby online submissions for Stream of Characters.
 class StreamChecker
 
 =begin
@@ -9,8 +14,8 @@ class StreamChecker
   def initialize(words)
     @words,@trie = words, {}
     @trie[:end] = false # Dose this node is an words end?
-    @array_of_pointers = []
-    create_trie
+    @string = []
+    create_trie_of_reversed_words
   end
 
 
@@ -19,38 +24,35 @@ class StreamChecker
 :rtype: Boolean
 =end
   def query(letter)
-    # First add new word pointer.
-    @array_of_pointers.push(@trie) if @trie[letter]
-    answer = false
-    # Let iterate over pointers.
-    # Only a pointer that can be moved further would be saved.
-    i = 0
-    while i < @array_of_pointers.size
-      pointer = @array_of_pointers[i]
-      if pointer[letter]
-          @array_of_pointers[i] = pointer[letter]
-          answer = true if pointer[letter][:end]
-          i += 1
-      else
-        @array_of_pointers.delete_at(i)
+    @string.push(letter)
+    pointer = @trie
+    @string.reverse_each do |char|
+      if ! pointer[char]
+        # Can not move the pointer to the new char.
+        return false
       end
+      pointer = pointer[char]
+      # This is an end of a work.
+      return true if pointer[:end]
     end
-    answer
+    # Did not find a word.
+    return false
   end
 
-  def create_trie
+  def create_trie_of_reversed_words
     @words.each do |word|
       # Each word starting from a root.
       pointer = @trie
-      (0...word.size).each do |i|
+      (0...word.size).reverse_each do |i|
         char = word[i]
+        # Add path to the new char.
         if ! pointer[char]
           tmp = {}
           tmp[:end] = false
           pointer[char] = tmp
         end
         pointer = pointer[char]
-        pointer[:end] = true if i == word.size - 1
+        pointer[:end] = true if 0 == i
       end
     end
   end
