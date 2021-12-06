16954dbda2b8d34846c9eeb54e3fa94be3399bc1
Dmitry <lbvf50.mobile@gmail.com>
2021-12-06 23:49:11 +0700

    1067. Leetcode: 1217. Minimum Cost to Move Chips to The Same Position. (begining)

---
+++ 20211206_Monday/20211206.rb
@@ -0,0 +1,7 @@
+# Leetcode: 1217. Minimum Cost to Move Chips to The Same Position.
+# https://leetcode.com/problems/minimum-cost-to-move-chips-to-the-same-position/
+# @param {Integer[]} position
+# @return {Integer}
+def min_cost_to_move_chips(position)
+    
+end
