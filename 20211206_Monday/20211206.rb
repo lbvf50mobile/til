68e309cd29648493ce8a7fdd10684e418470da3d
Dmitry <lbvf50.mobile@gmail.com>
2021-12-07 02:32:57 +0700

    1067. Leetcode: 1217. Minimum Cost to Move Chips to The Same Position. (article and solution)

--- 20211206_Monday/20211206.rb
+++ 20211206_Monday/20211206.rb
@@ -1,7 +1,21 @@
 # Leetcode: 1217. Minimum Cost to Move Chips to The Same Position.
 # https://leetcode.com/problems/minimum-cost-to-move-chips-to-the-same-position/
+# = = = = = = =
+# Accepted.
+# Thanks God!
+# = = = = = = =
+# Runtime: 48 ms, faster than 100.00% of Ruby online submissions for Minimum Cost to Move Chips to The Same Position.
+# Memory Usage: 209.7 MB, less than 85.71% of Ruby online submissions for Minimum Cost to Move Chips to The Same Position.
 # @param {Integer[]} position
 # @return {Integer}
 def min_cost_to_move_chips(position)
-    
+  even_sum, odd_sum = 0, 0
+  (0...position.size).each do |i|
+    if position[i].even?
+      even_sum += 1
+    else
+      odd_sum += 1
+    end
+  end
+  even_sum < odd_sum ? even_sum : odd_sum
 end
