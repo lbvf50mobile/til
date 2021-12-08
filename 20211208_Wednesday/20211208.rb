65a909107a3005c91e5acea2f0a7768f34107434
Dmitry <lbvf50.mobile@gmail.com>
2021-12-08 23:33:18 +0700

    1069. Leetcode: 563. Binary Tree Tilt. (begining)

---
+++ 20211208_Wednesday/20211208.rb
@@ -0,0 +1,16 @@
+# Leetcode: 563. Binary Tree Tilt.
+# https://leetcode.com/problems/binary-tree-tilt/
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {Integer}
+def find_tilt(root)
+    
+end
