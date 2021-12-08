762d07fae7dcecdb1a0b79292823eb56b296d019
Dmitry <lbvf50.mobile@gmail.com>
2021-12-08 23:48:28 +0700

    1069. Leetcode: 563. Binary Tree Tilt. (solution)

--- 20211208_Wednesday/20211208.rb
+++ 20211208_Wednesday/20211208.rb
@@ -1,5 +1,11 @@
 # Leetcode: 563. Binary Tree Tilt.
 # https://leetcode.com/problems/binary-tree-tilt/
+# = = = = = = =
+# Accepted.
+# Thanks God!
+# = = = = = = =
+# Runtime: 143 ms, faster than 20.00% of Ruby online submissions for Binary Tree Tilt.
+# Memory Usage: 210.7 MB, less than 60.00% of Ruby online submissions for Binary Tree Tilt.
 # Definition for a binary tree node.
 # class TreeNode
 #     attr_accessor :val, :left, :right
@@ -12,5 +18,13 @@
 # @param {TreeNode} root
 # @return {Integer}
 def find_tilt(root)
-    
+  @sum = 0
+  dfs(root)
+  @sum
+end
+def dfs(root)
+  return 0 if root.nil?
+  left,right = dfs(root.left), dfs(root.right)
+  @sum += (right - left).abs
+  root.val + right + left
 end
