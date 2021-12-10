c2e90a7259fca2dd75f21139afee43fa44699427
Dmitry <lbvf50.mobile@gmail.com>
2021-12-11 03:17:46 +0700

    1072. Leetcode: 790. Domino and Tromino Tiling. (article and solution)

--- 20211209_Thursday/20211209_1.rb
+++ 20211209_Thursday/20211209_1.rb
@@ -1,7 +1,24 @@
 # Leetcode: 790. Domino and Tromino Tiling.
 # https://leetcode.com/problems/domino-and-tromino-tiling/
+# = = = = = = =
+# Accepted.
+# Thaks God!
+# = = = = = = =
+# Runtime: 72 ms, faster than 50.00% of Ruby online submissions for Domino and Tromino Tiling.
+# Memory Usage: 211.1 MB, less than 50.00% of Ruby online submissions for Domino and Tromino Tiling.
 # @param {Integer} n
 # @return {Integer}
 def num_tilings(n)
-    
+  dp = Array.new(n+2).map{ Array.new(4,0)}
+  states = [[0,1,2,3],[2,3],[1,3],[0]]
+  dp[1][0] = 1
+  (1..n).each do |pos|
+    (0...4).each do |state|
+      number = dp[pos][state]
+      states[state].each do |next_state|
+        dp[pos+1][next_state] += number
+      end
+    end
+  end
+  dp[n+1][0] % (10**9 + 7)
 end
