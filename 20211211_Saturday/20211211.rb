39eebec171401d68ae1ef4ce4e67cf32b234861d
Dmitry <lbvf50.mobile@gmail.com>
2021-12-11 23:34:47 +0700

    1073. Leetcode: 878. Nth Magical Number. (begining)

---
+++ 20211211_Saturday/20211211.rb
@@ -0,0 +1,9 @@
+# Leetcode: 878. Nth Magical Number.
+# https://leetcode.com/problems/nth-magical-number/
+# @param {Integer} n
+# @param {Integer} a
+# @param {Integer} b
+# @return {Integer}
+def nth_magical_number(n, a, b)
+    
+end
