f5f708400160d54dcd1920e966d3ab07b5245d85
Dmitry <lbvf50.mobile@gmail.com>
2021-12-13 01:16:20 +0700

    1074. Leetcode: 416. Partition Equal Subset Sum. (TLE)

--- 20211212_Sunday/20211212.rb
+++ 20211212_Sunday/20211212.rb
@@ -2,18 +2,54 @@
 # https://leetcode.com/problems/partition-equal-subset-sum/
 # @param {Integer[]} nums
 # @return {Boolean}
-# Wrong algorithm.
+# TLE.
 def can_partition(nums)
-  nums = nums.sort
-  # right_part_prefix_sum (suffix sum)
-  suffix_sum = nums.clone
-  (0...(nums.size-1)).reverse_each do |i|
-    suffix_sum[i] += suffix_sum[i+1]
+  sum = nums.sum
+  @d = false # debugging
+  return false if sum.odd?
+  @dp = {}
+  address = nums.join(?,) + ?| + ?0
+  dfs(address)
+end
+
+
+def dfs(address)
+  puts "I enter the #{address}." if @d
+  return @dp[address] if ! @dp[address].nil?
+  if /^\d+\|\d+/ === address
+    num,sum = address.split(?|)
+    answer = num.to_i == sum.to_i
+    puts "Address #{address} is two digits #{num} and #{sum}, result is #{answer.inspect}" if @d
+    puts "(Digits) Address #{address} is #{answer.inspect}." if @d
+    @dp[address] = answer
+    return answer
   end
-  prefix_sum = 0
-  (0...(nums.size-1)).each do |i|
-    prefix_sum += nums[i]
-    return true if prefix_sum == suffix_sum[i+1]
+  puts "Address #{address} contains array and number." if @d
+  arr,sum = address.split(?|)
+  arr = arr.split(?,).map(&:to_i)
+  sum = sum.to_i
+  arr_sum = arr.sum
+  puts "Address #{address} has array #{arr} = #{arr_sum} and a #{sum}" if @d
+  if arr_sum == sum
+    puts "(Sum) Address #{address} is true." if @d
+    @dp[address] = true
+    return true
   end
+  if arr_sum < sum
+    puts "(Sum) Address #{address} is false." if @d
+    @dp[address] = false
+    return false
+  end
+  (0...arr.size - 1).each do |i|
+    new_address = arr.reject.with_index{|_,j| j == i}.join(?,) + ?| + (sum + arr[i]).to_s
+    if dfs(new_address)
+      @dp[address] = true
+      puts "(Loop) Address #{address} is true." if @d
+      return true
+    end
+  end
+
+  @dp[address] = false
+  puts "(End) Address #{address} is False." if @d
   return false
 end
