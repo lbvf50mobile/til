575bb364569023c5771917b29b7a193e64fd4c5d
Dmitry <lbvf50.mobile@gmail.com>
2021-12-12 23:41:55 +0700

    1074. Leetcode: 416. Partition Equal Subset Sum. (wrong algorithm)

--- 20211212_Sunday/20211212.rb
+++ 20211212_Sunday/20211212.rb
@@ -2,6 +2,18 @@
 # https://leetcode.com/problems/partition-equal-subset-sum/
 # @param {Integer[]} nums
 # @return {Boolean}
+# Wrong algorithm.
 def can_partition(nums)
-    
+  nums = nums.sort
+  # right_part_prefix_sum (suffix sum)
+  suffix_sum = nums.clone
+  (0...(nums.size-1)).reverse_each do |i|
+    suffix_sum[i] += suffix_sum[i+1]
+  end
+  prefix_sum = 0
+  (0...(nums.size-1)).each do |i|
+    prefix_sum += nums[i]
+    return true if prefix_sum == suffix_sum[i+1]
+  end
+  return false
 end
