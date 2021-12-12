654ab385fd942a734e1c5c9145bcf1adb658bc3f
Dmitry <lbvf50.mobile@gmail.com>
2021-12-12 23:30:21 +0700

    1074. Leetcode: 416. Partition Equal Subset Sum. (begining)

---
+++ 20211212_Sunday/20211212.rb
@@ -0,0 +1,7 @@
+# Leetcode: 416. Partition Equal Subset Sum.
+# https://leetcode.com/problems/partition-equal-subset-sum/
+# @param {Integer[]} nums
+# @return {Boolean}
+def can_partition(nums)
+    
+end
