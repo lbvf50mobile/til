9ce5f6696292e0c915256daee7371ba73aa0df89
Dmitry <lbvf50.mobile@gmail.com>
2021-12-14 22:48:12 +0700

    1075. Leetcode: 938. Range Sum of BST. (begining)

---
+++ 20211214_Tuesday/20211214.rb
@@ -0,0 +1,18 @@
+# Leetcode: 938. Range Sum of BST.
+# https://leetcode.com/problems/range-sum-of-bst/
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @param {Integer} low
+# @param {Integer} high
+# @return {Integer}
+def range_sum_bst(root, low, high)
+    
+end
