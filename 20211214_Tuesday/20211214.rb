4312b92145c7f74b1df7f6e4e6854c0c91b152a4
Dmitry <lbvf50.mobile@gmail.com>
2021-12-14 23:16:56 +0700

    1075. Leetcode: 938. Range Sum of BST. (article and solution)

--- 20211214_Tuesday/20211214.rb
+++ 20211214_Tuesday/20211214.rb
@@ -1,5 +1,11 @@
 # Leetcode: 938. Range Sum of BST.
 # https://leetcode.com/problems/range-sum-of-bst/
+# = = = = = = =
+# Accepted.
+# Thanks God!
+# = = = = = = =
+# Runtime: 218 ms, faster than 26.00% of Ruby online submissions for Range Sum of BST.
+# Memory Usage: 214.7 MB, less than 54.00% of Ruby online submissions for Range Sum of BST.
 # Definition for a binary tree node.
 # class TreeNode
 #     attr_accessor :val, :left, :right
@@ -14,5 +20,14 @@
 # @param {Integer} high
 # @return {Integer}
 def range_sum_bst(root, low, high)
-    
+  return 0 if root.nil?
+  global_sum = 0
+  queue = [root]
+  while ! queue.empty?
+    el = queue.pop()
+    global_sum += el.val if el.val.between?(low,high)
+    queue.unshift(el.right) if el.right &&  el.val <= high
+    queue.unshift(el.left) if el.left && el.val >= low
+  end
+  global_sum
 end
