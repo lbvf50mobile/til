2c00a4b181508280960546e572189473cfece77c
Dmitry <lbvf50.mobile@gmail.com>
2021-12-15 23:17:54 +0700

    1075. Leetcode: 147. Insertion Sort List. (begining)

---
+++ 20211215_Wednesday/20211215.rb
@@ -0,0 +1,17 @@
+# Leetcode: 147. Insertion Sort List.
+# https://leetcode.com/problems/insertion-sort-list/
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val = 0, _next = nil)
+#         @val = val
+#         @next = _next
+#     end
+# end
+# @param {ListNode} head
+# @return {ListNode}
+def insertion_sort_list(head)
+  return head if head.nil?
+  return head if head.next.nil?
+    
+end
