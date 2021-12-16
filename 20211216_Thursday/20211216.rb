1f525ec3c45634b98b5ad3d0dafcefe6414ba2bd
Dmitry <lbvf50.mobile@gmail.com>
2021-12-16 23:04:33 +0700

    1076. Leetcode: 147. Insertion Sort List. (fix task number, tiny fix)

---
+++ 20211216_Thursday/20211216.rb
@@ -0,0 +1,8 @@
+# Leetcode: 310. Minimum Height Trees.
+# https://leetcode.com/problems/minimum-height-trees/
+# @param {Integer} n
+# @param {Integer[][]} edges
+# @return {Integer[]}
+def find_min_height_trees(n, edges)
+    
+end
