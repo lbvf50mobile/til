d5558c338fde1c68808a364d57a4c46290136ba6
Dmitry <lbvf50.mobile@gmail.com>
2021-12-18 23:33:07 +0700

    1079. Leetcode: 902. Numbers At Most N Given Digit Set. (begining)

---
+++ 20211218_Saturday/20211218.rb
@@ -0,0 +1,8 @@
+# Leetcode: 902. Numbers At Most N Given Digit Set.
+# https://leetcode.com/problems/numbers-at-most-n-given-digit-set/
+# @param {String[]} digits
+# @param {Integer} n
+# @return {Integer}
+def at_most_n_given_digit_set(digits, n)
+    
+end
