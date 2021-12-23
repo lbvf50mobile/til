b7a1eb76b00f1363818e19a9579d6aab91430653
Dmitry <lbvf50.mobile@gmail.com>
2021-12-24 03:22:16 +0700

    1084. Leetcode: 210. Course Schedule II. (keep on working)

--- 20211223_Thursday/20211223.rb
+++ 20211223_Thursday/20211223.rb
@@ -4,34 +4,44 @@
 # @param {Integer[][]} prerequisites
 # @return {Integer[]}
 def find_order(num_courses, prerequisites)
+  @d = false
   @al = {}
+  @depended = {}
   @n = num_courses
   prerequisites.each do |(second,first)|
-    @al[first] = []
+    @al[first] ||= []
     @al[first].push(second)
+    @depended[second] = true
+  end
+  puts "Input has #{@n} elements." if @d
+  puts "Input: #{prerequisites}" if @d
+  if @d
+    @al.keys.each do |i|
+      puts "#{i} = #{@al[i].inspect}"
+    end
   end
   @ans = []
+  @visited = {}
   (0...@n).each do |i|
-    @ans = []
-    bfs(i)
-    return @ans if @ans.size == @n
+    if (! @visited[i]) && (! @depended[i])
+      bfs(i)
+    end
   end
-  @ans
+  @ans.size == @n ? @ans : []
 end
 
 
 def bfs(i)
   q = [i]
-  visited = {}
-  visited[i] = true
+  @visited[i] = true
   while ! q.empty?
     el = q.pop
     @ans.push(el)
     next if !@al[el]
     @al[el].each do |j|
-      if ! visited[j]
+      if ! @visited[j]
         q.unshift(j)
-        visited[j] = true
+        @visited[j] = true
       end
     end
   end
