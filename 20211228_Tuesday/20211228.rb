2138c8d36a98ba4fa1287f5063d9d6704c73e697
Dmitry <lbvf50.mobile@gmail.com>
2021-12-28 23:29:13 +0700

    1089. Leetcode: 876. Middle of the Linked List. (article and solution)

--- 20211228_Tuesday/20211228.rb
+++ 20211228_Tuesday/20211228.rb
@@ -1,5 +1,11 @@
 # Leetcode: 876. Middle of the Linked List.
 # https://leetcode.com/problems/middle-of-the-linked-list/
+# = = = = = = =
+# Accepted.
+# Thanks God!
+# = = = = = = =
+# Runtime: 48 ms, faster than 84.31% of Ruby online submissions for Middle of the Linked List.
+# Memory Usage: 209.8 MB, less than 19.61% of Ruby online submissions for Middle of the Linked List.
 # Definition for singly-linked list.
 # class ListNode
 #     attr_accessor :val, :next
@@ -11,5 +17,11 @@
 # @param {ListNode} head
 # @return {ListNode}
 def middle_node(head)
-    
+  tmp = head
+  arr = []
+  while tmp 
+    arr.push(tmp)
+    tmp = tmp.next
+  end
+  arr[arr.size/2]
 end
