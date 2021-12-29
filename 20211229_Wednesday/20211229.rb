6762621730e11774b04de445e184dad5cb5a1de6
Dmitry <lbvf50.mobile@gmail.com>
2021-12-29 22:22:02 +0700

    1090. Leetcode: 116. Populating Next Right Pointers in Each Node. (begining)

---
+++ 20211229_Wednesday/20211229.rb
@@ -0,0 +1,16 @@
+# Leetcode: 116. Populating Next Right Pointers in Each Node.
+# https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
+# Definition for Node.
+# class Node
+#     attr_accessor :val, :left, :right, :next
+#     def initialize(val)
+#         @val = val
+#         @left, @right, @next = nil, nil, nil
+#     end
+# end
+
+# @param {Node} root
+# @return {Node}
+def connect(root)
+    
+end
