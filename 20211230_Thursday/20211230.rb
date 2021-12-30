f72a6823966823b7b581b9b54eee9f05a3e80e03
Dmitry <lbvf50.mobile@gmail.com>
2021-12-30 22:25:15 +0700

    1091. Leetcode: 1015. Smallest Integer Divisible by K. (beginig)

---
+++ 20211230_Thursday/20211230.rb
@@ -0,0 +1,7 @@
+# Leetcode: 1015. Smallest Integer Divisible by K.
+# https://leetcode.com/problems/smallest-integer-divisible-by-k/
+# @param {Integer} k
+# @return {Integer}
+def smallest_repunit_div_by_k(k)
+    
+end
