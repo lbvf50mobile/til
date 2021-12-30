1f7273a0e696c6d4446fdce365a5c42f2583ef5d
Dmitry <lbvf50.mobile@gmail.com>
2021-12-30 23:27:29 +0700

    1091. Leetcode: 1015. Smallest Integer Divisible by K. (translation and solutuion)

--- 20211230_Thursday/20211230.rb
+++ 20211230_Thursday/20211230.rb
@@ -1,7 +1,20 @@
 # Leetcode: 1015. Smallest Integer Divisible by K.
 # https://leetcode.com/problems/smallest-integer-divisible-by-k/
+# = = = = = = =
+# Accepted.
+# Thanks God!
+# = = = = = = =
+# Runtime: 113 ms, faster than 100.00% of Ruby online submissions for Smallest Integer Divisible by K.
+# Memory Usage: 209.7 MB, less than 100.00% of Ruby online submissions for Smallest Integer Divisible by K.
 # @param {Integer} k
 # @return {Integer}
 def smallest_repunit_div_by_k(k)
-    
+  # Solution based on:
+  # https://leetcode.com/problems/smallest-integer-divisible-by-k/solution/
+  reminder = 0
+  (1..k).each do |number|
+    reminder = (reminder*10 + 1) % k
+    return number if reminder.zero?
+  end
+  return -1
 end
