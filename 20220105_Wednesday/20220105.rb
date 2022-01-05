9b607eba25eed842c3f47a55f34f83f8f9a3c6fc
Dmitry <lbvf50.mobile@gmail.com>
2022-01-06 02:40:52 +0700

    1097. Leetcode: 131. Palindrome Partitioning. (article and solution)

--- 20220105_Wednesday/20220105.rb
+++ 20220105_Wednesday/20220105.rb
@@ -1,36 +1,35 @@
 # Leetcode: 131. Palindrome Partitioning.
 # https://leetcode.com/problems/palindrome-partitioning/
+# = = = = = = =
+# Accepted.
+# Thanks God!
+# = = = = = = =
+# Runtime: 2162 ms, faster than 25.00% of Ruby online submissions for Palindrome Partitioning.
+# Memory Usage: 248.5 MB, less than 25.00% of Ruby online submissions for Palindrome Partitioning.
 # @param {String} s
 # @return {String[][]}
 def partition(s)
   @s = s
   @answer = []
-  dfs(0,1,[])
+  dfs(0,[])
   return @answer
 end
-
-def dfs(start_point, current_point, path_array)
-  str = @s[start_point...current_point]
-  if str == str.reverse
-    if current_point == @s.size
-      path_array.push(str)
-      @answer.push(path_array.clone)
-      path_array.pop
-      return
-    end
-    # Just ignore.
-    dfs(start_point,current_point+1, path_array)
-    # Add new palindrome.
-    path_array.push(str)
-    dfs(current_point, current_point + 1, path_array)
-    # Return to its inital postion.
+def dfs(start_point, path_array)
+  if start_point == @s.size
+    @answer.push(path_array.clone)
     path_array.pop()
-  else
-    if current_point == @s.size
-      path_array.pop
-      return
-    end
-    # Just move forward.
-    dfs(start_point,current_point + 1, path_array)
+    return 
   end
+  str = ""
+  (start_point...@s.size).each do |i|
+    str += @s[i]
+    if is_palindrome(str)
+      path_array.push(str)
+      dfs(i+1,path_array)
+    end
+  end
+  path_array.pop() if (!path_array.empty?)
+end
+def is_palindrome(x)
+  (!x.empty?) && x == x.reverse 
 end
