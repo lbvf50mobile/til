0a9d5542e37e8a7a24e27b4a1751638f58d7d03c
Dmitry <lbvf50.mobile@gmail.com>
2022-01-05 23:27:44 +0700

    1097. Leetcode: 131. Palindrome Partitioning. (in action)

--- 20220105_Wednesday/20220105.rb
+++ 20220105_Wednesday/20220105.rb
@@ -3,5 +3,34 @@
 # @param {String} s
 # @return {String[][]}
 def partition(s)
-    
+  @s = s
+  @answer = []
+  dfs(0,1,[])
+  return @answer
+end
+
+def dfs(start_point, current_point, path_array)
+  str = @s[start_point...current_point]
+  if str == str.reverse
+    if current_point == @s.size
+      path_array.push(str)
+      @answer.push(path_array.clone)
+      path_array.pop
+      return
+    end
+    # Just ignore.
+    dfs(start_point,current_point+1, path_array)
+    # Add new palindrome.
+    path_array.push(str)
+    dfs(current_point, current_point + 1, path_array)
+    # Return to its inital postion.
+    path_array.pop()
+  else
+    if current_point == @s.size
+      path_array.pop
+      return
+    end
+    # Just move forward.
+    dfs(start_point,current_point + 1, path_array)
+  end
 end
