3ce1eec6e6164fab2ab8199ce31c5ac0f01f8c25
Dmitry <lbvf50.mobile@gmail.com>
2022-01-16 01:04:07 +0700

    1106. Leetcode: 1345. Jump Game IV. (TLE2)

--- 20220115_Saturday/20220115.rb
+++ 20220115_Saturday/20220115.rb
@@ -4,25 +4,11 @@
 # @return {Integer}
 # TLE.
 def min_jumps(arr)
-  require 'set'
   last_index = arr.size - 1
   common_values = {}
   arr.each_with_index do |value,index|
     common_values[value] ||= []
     common_values[value].push(index)
-  end
-  adjacency_list = Array.new(arr.size).map{ Set.new}
-  (0...arr.size).each do |i|
-    a,b = i-1, i+1
-    if a.between?(0,last_index)
-      adjacency_list[i].add(a)
-    end
-    if b.between?(0,last_index)
-      adjacency_list[i].add(b)
-    end
-    common_values[arr[i]].each do |j|
-      adjacency_list[i].add(j) if j != i
-    end
   end
   steps_queue = [0]
   index_queue = [0]
@@ -34,7 +20,16 @@ def min_jumps(arr)
     if last_index == index
       return step
     end
-    adjacency_list[index].each do |i|
+    [index+1,index-1].each do |i|
+      if (! used[i]) && i.between?(0,last_index)
+        used[i] = true
+        steps_queue.unshift(step+1)
+        index_queue.unshift(i)
+      end
+    end
+    value = arr[index]
+    next if ! common_values[value]
+    common_values[value].each do |i|
       if ! used[i]
         used[i] = true
         steps_queue.unshift(step+1)
