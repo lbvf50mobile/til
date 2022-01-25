025e669b9bc04f891d261f9472130036e0680aa1
Dmitry <lbvf50.mobile@gmail.com>
2022-01-26 02:38:58 +0700

    1116. Leetcode: 941. Valid Mountain Array. (article and solution)

--- 20220125_Tuesday/20220125.rb
+++ 20220125_Tuesday/20220125.rb
@@ -1,10 +1,18 @@
 # Leetcode: 941. Valid Mountain Array.
 # https://leetcode.com/problems/valid-mountain-array/
+# = = = = = = =
+# Accepted.
+# Thanks God!
+# = = = = = = =
+# Runtime: 151 ms, faster than 28.13% of Ruby online submissions for Valid Mountain Array.
+# Memory Usage: 211.7 MB, less than 84.38% of Ruby online submissions for Valid Mountain Array.
 # @param {Integer[]} arr
 # @return {Boolean}
 def valid_mountain_array(arr)
+  @d = false
   @a = arr
   @size = arr.size
+  puts "#{@a.inspect} size: #{@size}" if @d
   return false if @size < 3
   state_prev = 0
   state_cur = get_state(1)
@@ -13,14 +21,18 @@ def valid_mountain_array(arr)
   rules = [ # Previous state for current one. Current one is an index.
     ->x{ 0 == x},
     ->x{ 0 == x},
-    ->x{ 1 == x && 2 == x} 
+    ->x{ 1 == x || 2 == x} 
   ]   
   (2...@size).each do |i|
-    tmp = state_prev
+    puts "in #{state_prev} #{state_cur} a[#{i}] = #{@a[i]}" if @d
+    puts "#{@a.inspect}" if @d
+    puts "#{" "*(3*i+1)}^" if @d
     state_prev = state_cur
     state_cur = get_state(i)
     return false if ! state_cur
-    return false if rules[state_cur][tmp]
+    return false if ! rules[state_cur][state_prev]
+    puts "out #{state_prev} #{state_cur}" if @d
+    puts "-------------" if @d
   end
   2 == state_cur
 end
