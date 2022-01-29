d1ea3982036bcd3b4748932f996a27593cbb37c8
Dmitry <lbvf50.mobile@gmail.com>
2022-01-29 20:00:46 +0700

    1120. Leetcode: 84. Largest Rectangle in Histogram. (begining)

---
+++ 20220129_Saturday/20220129.rb
@@ -0,0 +1,7 @@
+# Leetcode: 84. Largest Rectangle in Histogram.
+# https://leetcode.com/problems/largest-rectangle-in-histogram/
+# @param {Integer[]} heights
+# @return {Integer}
+def largest_rectangle_area(heights)
+    
+end
