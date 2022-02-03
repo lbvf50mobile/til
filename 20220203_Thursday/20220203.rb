878e7da38003c14cd87dac3394332928ad2e3824
Dmitry <lbvf50.mobile@gmail.com>
2022-02-03 22:28:02 +0700

    1125. Leetcode: 454. 4Sum II. (still does not work)

--- 20220203_Thursday/20220203.rb
+++ 20220203_Thursday/20220203.rb
@@ -6,21 +6,22 @@
 # @param {Integer[]} nums4
 # @return {Integer}
 def four_sum_count(nums1, nums2, nums3, nums4)
-  a_array = possible_sum_values(nums1, nums2)
-  b_array = possible_sum_values(nums3, nums4)
+  a_array = possible_sum_values(nums1, nums2, nums3)
   a_hash = get_hash_counter(a_array)
   answer = 0
-  b_array.each do |b|
+  nums4.each do |b|
     answer += 1 if a_hash[-b]
   end
   answer
 end
 
-def possible_sum_values(a,b)
+def possible_sum_values(a,b,c)
   ans = []
   a.each do |x|
     b.each do |y|
-      ans.push(x*y)
+      c.each do |z|
+        ans.push(x+y+z)
+      end
     end
   end
   ans
