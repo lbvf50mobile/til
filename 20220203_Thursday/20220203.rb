d0065c6be99904e08f5b6f58cb5e4357db5401ae
Dmitry <lbvf50.mobile@gmail.com>
2022-02-03 23:20:20 +0700

    1125. Leetcode: 454. 4Sum II. (article and solution) Thanks God!

--- 20220203_Thursday/20220203.rb
+++ 20220203_Thursday/20220203.rb
@@ -1,27 +1,35 @@
 # Leetcode: 454. 4Sum II.
 # https://leetcode.com/problems/4sum-ii/
+# = = = = = = =
+# Accepted.
+# Thanks God!
+# = = = = = = =
+# Runtime: 1455 ms, faster than 11.11% of Ruby online submissions for 4Sum II.
+# Memory Usage: 239.3 MB, less than 22.22% of Ruby online submissions for 4Sum II.
 # @param {Integer[]} nums1
 # @param {Integer[]} nums2
 # @param {Integer[]} nums3
 # @param {Integer[]} nums4
 # @return {Integer}
 def four_sum_count(nums1, nums2, nums3, nums4)
-  a_array = possible_sum_values(nums1, nums2, nums3)
+  @d = false
+  a_array = possible_sum_values(nums1, nums2)
+  puts "#{a_array.inspect}" if @d
+  b_array = possible_sum_values(nums3, nums4)
+  puts "#{b_array.inspect}" if @d
   a_hash = get_hash_counter(a_array)
   answer = 0
-  nums4.each do |b|
-    answer += 1 if a_hash[-b]
+  b_array.each do |b|
+    answer += a_hash[-b] if a_hash[-b]
   end
   answer
 end
 
-def possible_sum_values(a,b,c)
+def possible_sum_values(a,b)
   ans = []
   a.each do |x|
     b.each do |y|
-      c.each do |z|
-        ans.push(x+y+z)
-      end
+      ans.push(x+y)
     end
   end
   ans
