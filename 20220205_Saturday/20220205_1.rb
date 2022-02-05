1b242f675f5ca716a7b81406acbc337e5a8b23aa
Dmitry <lbvf50.mobile@gmail.com>
2022-02-05 14:35:49 +0700

    1127. Leetcode: 954. Array of Doubled Pairs. (improving the algorithm)

--- 20220205_Saturday/20220205_1.rb
+++ 20220205_Saturday/20220205_1.rb
@@ -2,26 +2,28 @@
 # https://leetcode.com/problems/array-of-doubled-pairs/
 # @param {Integer[]} arr
 # @return {Boolean}
-# Fails: [2,1,2,1,1,1,2,2]
 def can_reorder_doubled(arr)
-  return false if arr.size.odd?
+  @d = true
+  puts "#{arr.inspect}" if @d
+  if arr.size.odd?
+    puts "return false because #{arr.size}." if @d
+    return false
+  end
   counter = arr.each_with_object(Hash.new){|el,o| o[el]||=0; o[el]+=1;}
-  if counter[0]
-    return false if counter[0].odd?
-    return false if counter.keys.size - 1 !=  (arr.size - counter[0])
-    pairs = counter[0]/2
-    counter.keys.each do |el|
-      if 0 != el
-        pairs += 1 if counter[el*2]
-      end
-    end
-    return pairs >= arr.size/2
-  else
-    return false if counter.keys.size != arr.size
-    pairs = 0
-    counter.keys.each do |el|
+  if counter[0] && counter[0].odd?
+    puts "return false because #{counter[0]}" if @d
+    return false
+  end
+  pairs = counter[0] ? counter[0]/2 : 0
+  puts "Number of pairs is #{pairs}." if @d
+  puts "Keys: #{counter.keys.inspect}." if @d
+  counter.keys.each do |el|
+    if 0 != el
       pairs += 1 if counter[el*2]
     end
-    return pairs >= arr.size/2
   end
+  ans = pairs >= arr.size/2
+  puts "Final: pairs = #{pairs}; size/2 = #{arr.size/2};" if @d
+  puts "Return: #{ans.inspect}." if @d
+  return ans
 end
