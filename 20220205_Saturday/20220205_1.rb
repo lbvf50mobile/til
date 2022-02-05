7ae39d140dbb1321a3e2fcd11cf32ad8e9465d8c
Dmitry <lbvf50.mobile@gmail.com>
2022-02-05 12:55:35 +0700

    1127. Leetcode: 954. Array of Doubled Pairs. (keep working)

--- 20220205_Saturday/20220205_1.rb
+++ 20220205_Saturday/20220205_1.rb
@@ -2,12 +2,26 @@
 # https://leetcode.com/problems/array-of-doubled-pairs/
 # @param {Integer[]} arr
 # @return {Boolean}
-# Fails on: [4,-2,2,-4]
+# Fails: [2,1,2,1,1,1,2,2]
 def can_reorder_doubled(arr)
   return false if arr.size.odd?
-  arr = arr.sort
-  (0...arr.size/2).each do |i|
-    return false if arr[i] * 2 != arr[i+arr.size/2]
+  counter = arr.each_with_object(Hash.new){|el,o| o[el]||=0; o[el]+=1;}
+  if counter[0]
+    return false if counter[0].odd?
+    return false if counter.keys.size - 1 !=  (arr.size - counter[0])
+    pairs = counter[0]/2
+    counter.keys.each do |el|
+      if 0 != el
+        pairs += 1 if counter[el*2]
+      end
+    end
+    return pairs >= arr.size/2
+  else
+    return false if counter.keys.size != arr.size
+    pairs = 0
+    counter.keys.each do |el|
+      pairs += 1 if counter[el*2]
+    end
+    return pairs >= arr.size/2
   end
-  true
 end
