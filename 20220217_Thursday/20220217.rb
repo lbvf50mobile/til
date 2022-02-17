7e4d31be1c5730c7c06d13c0d53a2ed9fcce8848
Dmitry <lbvf50.mobile@gmail.com>
2022-02-17 23:40:45 +0700

    1139. Leetcode: 39. Combination Sum. (solution)

---
+++ 20220217_Thursday/20220217.rb
@@ -0,0 +1,45 @@
+# Leetcode: 39. Combination Sum.
+# https://leetcode.com/problems/combination-sum/
+# = = = = = = =
+# Accepted.
+# Thanks God!
+# = = = = = = =
+# Runtime: 138 ms, faster than 60.18% of Ruby online submissions for Combination Sum.
+# Memory Usage: 211.1 MB, less than 36.28% of Ruby online submissions for Combination Sum.
+#
+# @param {Integer[]} candidates
+# @param {Integer} target
+# @return {Integer[][]}
+def combination_sum(candidates, target)
+  @d = false
+  @c,@t = candidates, target
+  @ans = []
+  (0...@c.size).each do |i|
+    back(i,0,[])
+  end
+  @ans
+end
+
+
+def back(i,sum,arr)
+  # Add.
+  # Check.
+  # Move.
+  return if i >= @c.size
+  sum += @c[i]
+  arr.push @c[i]
+  if @t == sum
+    puts "eq #{i}, #{sum}, #{arr.inspect}" if @d
+    @ans.push(arr.clone)
+    arr.pop
+    return
+  end
+  if @t < sum
+    arr.pop
+    return
+  end
+  (i...@c.size).each do |j|
+    back(j,sum,arr)
+  end
+  arr.pop
+end
