fc9944c65245d27ff19ade83fbbddeeee8a61a45
Dmitry <lbvf50.mobile@gmail.com>
2022-03-04 00:28:21 +0700

    1151. Leetcode: 413. Arithmetic Slices. (article and solution) Thanks God!

--- 20220303_Thursday/20220303.rb
+++ 20220303_Thursday/20220303.rb
@@ -1,5 +1,11 @@
 # Leetcode: 413. Arithmetic Slices.
 # https://leetcode.com/problems/arithmetic-slices/
+# = = = = = = =
+# Accepted.
+# Thanks God!
+# = = = = = = =
+# Runtime: 128 ms, faster than 40.00% of Ruby online submissions for Arithmetic Slices.
+# Memory Usage: 211 MB, less than 53.33% of Ruby online submissions for Arithmetic Slices.
 # @param {Integer[]} nums
 # @return {Integer}
 def number_of_arithmetic_slices(nums)
@@ -14,11 +20,9 @@ end
 def rec(i)
   return @dp[i] if @dp[i]
   if i < @n.size - 2 && @n[i] - @n[i+1] == @n[i+1] - @n[i+2]
-    @dp[i] = 1 + rec(i+1) + rec(i+2)
+    @dp[i] = 1 + rec(i+1)
     return @dp[i]
   end
   @dp[i] = 0
   return @dp[i]
 end
-
-
