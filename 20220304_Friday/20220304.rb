9ac76aec982963189386f4df55354355a1cbbf82
Dmitry <lbvf50.mobile@gmail.com>
2022-03-04 23:19:57 +0700

    1152. Leetcode: 799. Champagne Tower. (in action) Thanks God!

---
+++ 20220304_Friday/20220304.rb
@@ -0,0 +1,9 @@
+# Leetcode: 799. Champagne Tower.
+# https://leetcode.com/problems/champagne-tower/
+# @param {Integer} poured
+# @param {Integer} query_row
+# @param {Integer} query_glass
+# @return {Float}
+def champagne_tower(poured, query_row, query_glass)
+    
+end
