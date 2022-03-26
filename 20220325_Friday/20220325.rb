f31b3f29510ef5352ed1407eb1bb19a5bde1c583
Dmitry <lbvf50.mobile@gmail.com>
2022-03-26 23:29:05 +0700

    1170. Leetcode: 1029. Two City Scheduling. (article and solution) Thanks God!

--- 20220325_Friday/20220325.rb
+++ 20220325_Friday/20220325.rb
@@ -4,25 +4,21 @@
 # Accepted.
 # Thanks God!
 # = = = = = = =
-# Runtime: 121 ms, faster than 33.33% of Ruby online submissions for Two City Scheduling.
-# Memory Usage: 211.1 MB, less than 66.67% of Ruby online submissions for Two City Scheduling.
+# Runtime: 119 ms, faster than 33.33% of Ruby online submissions for Two City Scheduling.
+# Memory Usage: 211.1 MB, less than 66.67% of Ruby online submissions for Two City Scheduling
 # @param {Integer[][]} costs
 # @return {Integer}
 def two_city_sched_cost(costs)
   # Based on: 
   # https://leetcode.com/problems/two-city-scheduling/discuss/761411/python3-solution-that-beats-97-with-explanation
-  # Firs assume that every one is going to city A.
-  # Then identify N pople to go to city B.
-  # according to how much they can halp save
-  # when changing to destination to city B.
   n = costs.size/2
-  total_cost_a = 0
-  diff = []
+  all_to_a = 0
+  profit_if_got_to_b = []
   costs.each do |(a,b)|
-    total_cost_a += a
-    diff.push(a - b)
+    all_to_a += a
+    profit_if_got_to_b.push(a-b)
   end
-  diff = diff.sort.reverse
-  total_cost_a - diff[0...n].sum
+  half = profit_if_got_to_b.sort_by{|x| -x}[0...n].sum
+  all_to_a - half
 end
 
