3e6569c66a1a30f4828cebd2205696748119485c
Dmitry <lbvf50.mobile@gmail.com>
2022-04-01 01:59:26 +0700

    1175. Leetcode: 410. Split Array Largest Sum. (article and solution) Thanks God!

--- 20220331_Thursday/20220331.rb
+++ 20220331_Thursday/20220331.rb
@@ -1,35 +1,44 @@
 # Leetcode: 410. Split Array Largest Sum.
 # https://leetcode.com/problems/split-array-largest-sum/
+# = = = = = = =
+# Accepted.
+# Thanks God!
+# = = = = = = =
+# Runtime: 160 ms, faster than 16.67% of Ruby online submissions for Split Array Largest Sum.
+# Memory Usage: 211.1 MB, less than 33.33% of Ruby online submissions for Split Array Largest Sum.
 # @param {Integer[]} nums
 # @param {Integer} m
 # @return {Integer}
 def split_array(nums, m)
-  return nums.sum if 1 == m
+  @d = false
   # Based on: https://leetcode.com/problems/split-array-largest-sum/discuss/1899733/Python-Binary-Search-Solution
   @nums, @m, = nums, m
-  l, h = 0, @nums.sum
+  start, end_ = 0, @nums.sum
+  puts "start = #{start}, end = #{end_}" if @d
   ans = -1
-  while l < h
-    mid = l + (h-l)/2
+  while start <= end_
+    mid = start + (end_-start)/2
+    puts "start = #{start}, end = #{end_} mid = #{mid}" if @d
     if check(mid)
       ans = mid
-      h = mid - 1
+      end_ = mid - 1
     else
-      l = mid + 1
+      start = mid + 1
     end
   end
+  puts "#{ans}" if @d
   ans
 end
 
 def check(mid)
-  sum,count = 0, 1
+  sumz,c = 0, 1
   @nums.each do |el|
-    if el + sum <= mid
-      sum += el
+    if el + sumz <= mid
+      sumz += el
     else
-      count += 1
-      return false if count > @m || el > mid
-      sum = el
+      c += 1
+      return false if c > @m || el > mid
+      sumz = el
     end
   end
   true
