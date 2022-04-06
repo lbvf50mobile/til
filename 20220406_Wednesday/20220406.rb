501d30d1c653ad60d50f9cdb59c915f2791def8a
Dmitry <lbvf50.mobile@gmail.com>
2022-04-06 23:08:05 +0700

    1182. Leetcode: 923. 3Sum With Multiplicity. (solution) Thanks God!

--- 20220406_Wednesday/20220406.rb
+++ 20220406_Wednesday/20220406.rb
@@ -1,30 +1,54 @@
 # Leetcode: 923. 3Sum With Multiplicity.
 # https://leetcode.com/problems/3sum-with-multiplicity/
+# = = = = = = =
+# Accepted.
+# Thanks God!
+# = = = = = = =
+# Runtime: 1574 ms, faster than 100.00% of Ruby online submissions for 3Sum With Multiplicity.
+# Memory Usage: 211.3 MB, less than 100.00% of Ruby online submissions for 3Sum With Multiplicity.
 # @param {Integer[]} arr
 # @param {Integer} target
 # @return {Integer}
-# TLE.
 def three_sum_multi(arr, target)
+  # Based on: https://leetcode.com/problems/3sum-with-multiplicity/solution/
   arr.sort!
   ans = 0
+  a = arr
+  mod = 10**9 + 7
   n = arr.size
-  counter = {}
-  # i changes in 0...n-2 interval.
-  (0...n-2).each do |i|
-    a = arr[i]
-    counter.clear
-    counter[arr[i+1]] = 1
-    # k changes in i+2...n interval.
-    (i+2...n).each do |k|
-      c = arr[k]
-      break if target < a + c
-      # Find number of b by the hash map counter.
-      # a + b + c = target; b = target - (a+c);
-      b = target - (a+c)
-      ans += counter[b] if counter[b]
-      counter[c] ||= 0
-      counter[c] += 1
+  arr.each_with_index do |x,i|
+    # We'll try to find the number of i < j < k
+    # with a[j] + a[k] == t, where t = target - a[i];
+    t = target - a[i]
+    j,k = i+1, n - 1
+    while j < k
+      # These steps proceed as in a typcal two-sum.
+      if a[j] + a[k] < t
+        j += 1
+      elsif a[j] + a[k] > t
+        k -= 1
+      elsif a[j] != a[k]
+        # Whe have a[j] + a[k] == t.
+        # Let's count "left": the number of a[j] == a[j+1] == a[j+2] == ...
+        left, right = 1, 1
+        while j + 1 < k && a[j] == a[j+1]
+          left += 1; j += 1;
+        end
+        while k - 1 > j && a[k] == a[k-1]
+          right += 1
+          k -= 1
+        end
+        # We contributed left * right many pairs.
+        ans += left * right
+        ans %= mod; j += 1; k -= 1;
+      else
+        # M = k-j+1;
+        # We contribute M*(M-1)/2 pairs.
+        ans += (k-j+1)*(k-j)/2
+        ans %= mod
+        break
+      end
     end
   end
-  return ans % (10**9 + 7)
+  ans
 end
