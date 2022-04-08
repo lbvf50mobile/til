51120723c7c1b9ce8a5643b35e1a29810ee08774
Dmitry <lbvf50.mobile@gmail.com>
2022-04-09 00:29:43 +0700

    1184. 703. Kth Largest Element in a Stream. Fix typos, add link.

--- 20220408_Friday/20220408.rb
+++ 20220408_Friday/20220408.rb
@@ -1,4 +1,4 @@
-# 703. Kth Largest Element in a Stream.
+# Leetcode: 703. Kth Largest Element in a Stream.
 # https://leetcode.com/problems/kth-largest-element-in-a-stream/
 # TLE.
 class KthLargest
