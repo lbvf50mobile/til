ebf76a244c0759ad1b0703ab93be388c975ea907
Dmitry <lbvf50.mobile@gmail.com>
2022-04-17 01:19:13 +0700

    1190. Leetcode: 538. Convert BST to Greater Tree. (article and solution) Thanks God!

--- 20220416_Saturday/20220416.rb
+++ 20220416_Saturday/20220416.rb
@@ -1,5 +1,11 @@
 # Leetcode: 538. Convert BST to Greater Tree.
 # https://leetcode.com/problems/convert-bst-to-greater-tree/
+# = = = = = = =
+# Accepted.
+# Thanks God, Jesus Christ!
+# = = = = = = =
+# Runtime: 126 ms, faster than 66.67% of Ruby online submissions for Convert BST to Greater Tree.
+# Memory Usage: 214.8 MB, less than 16.67% of Ruby online submissions for Convert BST to Greater Tree.
 # Definition for a binary tree node.
 # class TreeNode
 #     attr_accessor :val, :left, :right
@@ -12,12 +18,27 @@
 # @param {TreeNode} root
 # @return {TreeNode}
 def convert_bst(root)
-    
+  return root if root.nil?
+  stack = [root]
+  sum = 0
+  while ! stack.empty?
+    # Move right as far as possible.
+    while stack.last.right
+      stack.push(stack.last.right)
+    end
+    # When there is no way rigth.
+    # Try to find way left.
+    while ! stack.empty?
+      # Because of pop. Can't got rigth same way.
+      node = stack.pop  
+      sum += node.val
+      node.val = sum
+      if node.left
+        stack.push(node.left)
+        break
+      end
+    end
+  end
+  root
 end
 
-def action(top_sum, node)
-  return 0 if node.nil?
-  right = action(top_sum, node.right)
-  left = action(top_sum + right +  )
-  
-end
