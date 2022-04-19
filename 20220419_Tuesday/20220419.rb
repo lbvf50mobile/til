ecd65ab89a7b5dee8e7a57edab8b0f1973956c6e
Dmitry <lbvf50.mobile@gmail.com>
2022-04-20 00:55:10 +0700

    1193. Leetcode: 99. Recover Binary Search Tree. (article and solution) Thanks God!

--- 20220419_Tuesday/20220419.rb
+++ 20220419_Tuesday/20220419.rb
@@ -1,5 +1,12 @@
 # Leetcode: 99. Recover Binary Search Tree.
 # https://leetcode.com/problems/recover-binary-search-tree/
+# = = = = = = =
+# Accepted.
+# Thanks God!
+# = = = = = = =
+# Runtime: 161 ms, faster than 50.00% of Ruby online submissions for Recover Binary Search Tree.
+# Memory Usage: 211.3 MB, less than 14.29% of Ruby online submissions for Recover Binary Search Tree.
+#
 # Definition for a binary tree node.
 # class TreeNode
 #     attr_accessor :val, :left, :right
@@ -11,30 +18,32 @@
 # end
 # @param {TreeNode} root
 # @return {Void} Do not return anything, modify root in-place instead.
-# This solution only work if error in root/child but not a the same level.
-# [2,3,1] - does not works.
 def recover_tree(root)
   return if root.nil?
-  a,b = find_bigger(root.left, root.val), find_smaller(root.right, root.val)
-  if a || b
-    x = a || b
-    x.val, root.val = root.val, x.val
-  else
-    recover_tree(root.left)
-    recover_tree(root.right)
+  stack = [root]
+  sorted = []
+  # Fill sorted.
+  while ! stack.empty?
+    while stack.last.left
+      stack.push stack.last.left
+    end
+    while ! stack.empty?
+      x = stack.pop
+      sorted.push x
+      if x.right
+        stack.push x.right
+        break
+      end
+    end
   end
-end
-
-def find_bigger(node, current_val)
-  return nil if node.nil?
-  # Need to find bigger than Current Val.
-  return node if node.val >= current_val
-  return find_bigger(node.right, current_val)
-end
-def find_smaller(node, current_val)
-  return nil if node.nil?
-  # Need to find smaller than Current Val.
-  return node if node.val <= current_val
-  return find_smaller(node.left, current_val)
+  i = 0
+  while sorted[i].val < sorted[i+1].val
+    i += 1
+  end
+  j = sorted.size - 1
+  while sorted[j-1].val < sorted[j].val
+    j -= 1
+  end
+  sorted[i].val, sorted[j].val =  sorted[j].val, sorted[i].val
 end
 
