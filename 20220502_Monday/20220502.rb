3e2e900e6ae267a038cfe2635860fa2e914af19c
Dmitry <lbvf50.mobile@gmail.com>
2022-05-02 22:52:10 +0700

    1205. Leetcode: 905. Sort Array By Parity. (article and solutions) Thanks God!

--- 20220502_Monday/20220502.rb
+++ 20220502_Monday/20220502.rb
@@ -4,10 +4,19 @@
 # Accepted.
 # Thanks God!
 # = = = = = = =
-# Runtime: 149 ms, faster than 33.33% of Ruby online submissions for Sort Array By Parity.
-# Memory Usage: 212.2 MB, less than 7.69% of Ruby online submissions for Sort Array By Parity.
+# Runtime: 216 ms, faster than 5.13% of Ruby online submissions for Sort Array By Parity.
+# Memory Usage: 211.8 MB, less than 46.15% of Ruby online submissions for Sort Array By Parity.
 # @param {Integer[]} nums
 # @return {Integer[]}
 def sort_array_by_parity(nums)
-  nums.select(&:even?) + nums.select(&:odd?)
+  ans = Array.new(nums.size)
+  i,j = 0, nums.size - 1
+  nums.each do |x|
+    if x.even?
+      ans[i] = x; i += 1;
+    else
+      ans[j] = x; j -= 1;
+    end
+  end
+  ans
 end
