787331e0eaa13e91a07261e448e2a9f147bec392
Dmitry <lbvf50.mobile@gmail.com>
2022-05-24 01:04:08 +0700

    1225. Leetcode: 474. Ones and Zeroes. (article and solution) Thanks God!

--- 20220523_Monday/20220523.rb
+++ 20220523_Monday/20220523.rb
@@ -1,23 +1,29 @@
 # Leetcode: 474. Ones and Zeroes.
 # https://leetcode.com/problems/ones-and-zeroes/
+# = = = = = = =
+# Accepted.
+# Thanks God!
+# = = = = = = =
+# Runtime: 1798 ms, faster than 50.00% of Ruby online submissions for Ones and Zeroes.
+# Memory Usage: 218.1 MB, less than 50.00% of Ruby online submissions for Ones and Zeroes.
 # @param {String[]} strs
 # @param {Integer} m
 # @param {Integer} n
 # @return {Integer}
 def find_max_form(strs, m, n)
-  # https://www.codespeedy.com/knapsack-problem-using-greedy-method-in-java/
-  @s,@m,@n = strs,m,n
-  @dp = Array.new(m+1).map{ Array.new(n+1)}
-  @s = @s.map{|x| [x.count(?0),[x.count(?1)]}
-    .sort_by{|(a,b)| -a.fdiv(b)}
-  used = Array.new(@s.size,0)
-  @s.each_with_index do |(mm,nn),i|
-    if m - mm >= 0 && n - nn >= 0
-      m -= mm
-      n -= nn
-      @used[i] = 1
+  # Based on:
+  # https://leetcode.com/problems/ones-and-zeroes/discuss/1025400/Top-Down-VS-Bottom-up-oror-easy-to-understand-oror-Dhruv-vavliya
+  dp = Array.new(m+1).map{ Array.new(n+1,0)}
+  strs.each do |s|
+    zero = s.count(?0)
+    one = s.count(?1)
+    (0..m).reverse_each do |i|
+      (0..n).reverse_each do |j|
+        if zero <= i && one <= j
+          dp[i][j] = (1+dp[i-zero][j-one]) if (1+dp[i-zero][j-one]) > dp[i][j]
+        end
+      end
     end
   end
-  @used.sum
-    
+  dp[-1][-1]
 end
