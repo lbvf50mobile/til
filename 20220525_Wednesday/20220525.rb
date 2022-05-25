09cfdb577da600fb4f3eb2e69562303f0ee824da
Dmitry <lbvf50.mobile@gmail.com>
2022-05-25 23:59:12 +0700

    1227. Leetcode: 354. Russian Doll Envelopes. (solution) Thanks God!

--- 20220525_Wednesday/20220525.rb
+++ 20220525_Wednesday/20220525.rb
@@ -1,13 +1,17 @@
 # Leetcode: 354. Russian Doll Envelopes. 
 # https://leetcode.com/problems/russian-doll-envelopes/
+# = = = = = = =
+# Accepted.
+# Thanks God!
+# = = = = = = =
+# Runtime: 1037 ms, faster than 50.00% of Ruby online submissions for Russian Doll Envelopes.
+# Memory Usage: 221.2 MB, less than 100.00% of Ruby online submissions for Russian Doll Envelopes.
 # @param {Integer[][]} envelopes
 # @return {Integer}
-# fails:
-# [[4,5],[4,6],[6,7],[2,3],[1,1]]
 def max_envelopes(envelopes)
   # Based on:
   # https://leetcode.com/problems/russian-doll-envelopes/discuss/1134011/JS-Python-Java-C%2B%2B-or-Easy-LIS-Solution-w-Explanation
-  e = envelopes.sort{|a,b| a[0] == b[0] ? a[1] <=> b[1] : a[0] <=> b[0]}
+  e = envelopes.sort{|a,b| a[0] == b[0] ? b[1] <=> a[1] : a[0] <=> b[0]}
   dp = []
   e.each do |(_,height)|
     left = 0 
