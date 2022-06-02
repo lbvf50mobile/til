d342bfe762aae4789f228d73e295d9a11d65bb62
Dmitry <lbvf50.mobile@gmail.com>
2022-06-02 23:51:14 +0700

    1234. Leetcode: 867. Transpose Matrix. (map solution) Thanks God!

--- 20220602_Thursday/20220602.rb
+++ 20220602_Thursday/20220602.rb
@@ -4,16 +4,10 @@
 # Accepted.
 # Thanks God!
 # = = = = = = =
-# Runtime: 152 ms, faster than 37.50% of Ruby online submissions for Transpose Matrix.
-# Memory Usage: 211.7 MB, less than 100.00% of Ruby online submissions for Transpose Matrix.
+# Runtime: 142 ms, faster than 37.50% of Ruby online submissions for Transpose Matrix.
+# Memory Usage: 211.8 MB, less than 87.50% of Ruby online submissions for Transpose Matrix.
 # @param {Integer[][]} matrix
 # @return {Integer[][]}
 def transpose(matrix)
-  ans = Array.new(matrix[0].size).map{ Array.new(matrix.size)}
-  (0...matrix.size).each do |row|
-    (0...matrix[0].size).each do |col|
-      ans[col][row] = matrix[row][col]
-    end
-  end
-  ans
+  matrix[0].map.with_index{|_,i| matrix.map{|x| x[i]}} 
 end
