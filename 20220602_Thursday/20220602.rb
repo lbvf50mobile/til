22fd488abde0e6dd6477c26ca2428e795edee04b
Dmitry <lbvf50.mobile@gmail.com>
2022-06-02 23:36:13 +0700

    1234. Leetcode: 867. Transpose Matrix. (article and solution) Thanks God!

---
+++ 20220602_Thursday/20220602.rb
@@ -0,0 +1,13 @@
+# Leetcode: 867. Transpose Matrix.
+# https://leetcode.com/problems/transpose-matrix/
+# = = = = = = =
+# Accepted.
+# Thanks God!
+# = = = = = = =
+# Runtime: 138 ms, faster than 50.00% of Ruby online submissions for Transpose Matrix.
+# Memory Usage: 212 MB, less than 25.00% of Ruby online submissions for Transpose Matrix.
+# @param {Integer[][]} matrix
+# @return {Integer[][]}
+def transpose(matrix)
+  matrix.transpose
+end
