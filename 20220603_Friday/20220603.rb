ddd36a534931a130d1055aa83c361a6f10714efa
Dmitry <lbvf50.mobile@gmail.com>
2022-06-03 23:46:50 +0700

    1235. 304. Range Sum Query 2D - Immutable (in action) Thanks God!

---
+++ 20220603_Friday/20220603.rb
@@ -0,0 +1,29 @@
+# Leetcode: 304. Range Sum Query 2D - Immutable.
+# https://leetcode.com/problems/range-sum-query-2d-immutable/
+class NumMatrix
+
+=begin
+    :type matrix: Integer[][]
+=end
+    def initialize(matrix)
+        
+    end
+
+
+=begin
+    :type row1: Integer
+    :type col1: Integer
+    :type row2: Integer
+    :type col2: Integer
+    :rtype: Integer
+=end
+    def sum_region(row1, col1, row2, col2)
+        
+    end
+
+
+end
+
+# Your NumMatrix object will be instantiated and called as such:
+# obj = NumMatrix.new(matrix)
+# param_1 = obj.sum_region(row1, col1, row2, col2)
