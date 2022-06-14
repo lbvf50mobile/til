b482a92a20d70cbfafa807457e59939259f7f7ad
Dmitry <lbvf50.mobile@gmail.com>
2022-06-14 23:58:59 +0700

    1244. Leetcode: 583. Delete Operation for Two Strings. (in action) Thanks God!

---
+++ 20220614_Tuesday/20220614.rb
@@ -0,0 +1,8 @@
+# Leetcode: 583. Delete Operation for Two Strings.
+# https://leetcode.com/problems/delete-operation-for-two-strings/
+# @param {String} word1
+# @param {String} word2
+# @return {Integer}
+def min_distance(word1, word2)
+    
+end
