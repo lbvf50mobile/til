8d440ddabbbf6f01a35210b1d2494f9516e557cc
Dmitry <lbvf50.mobile@gmail.com>
2022-06-15 02:09:32 +0700

    1244. Leetcode: 583. Delete Operation for Two Strings. (solution) Thanks God!

--- 20220614_Tuesday/20220614.rb
+++ 20220614_Tuesday/20220614.rb
@@ -1,23 +1,30 @@
 # Leetcode: 583. Delete Operation for Two Strings.
 # https://leetcode.com/problems/delete-operation-for-two-strings/
+# = = = = = = =
+# Accepted.
+# Thanks God!
+# = = = = = = =
+# Runtime: 372 ms, faster than 88.89% of Ruby online submissions for Delete Operation for Two Strings.
+# Memory Usage: 214.3 MB, less than 22.22% of Ruby online submissions for Delete Operation for Two Strings.
 # @param {String} word1
 # @param {String} word2
 # @return {Integer}
-# TLE.
 def min_distance(word1, word2)
   # Based on: https://leetcode.com/problems/delete-operation-for-two-strings/solution/
   @w1,@w2,@m,@n = word1, word2, word1.size, word2.size
-  @mem = Array.new(@m+1).map{ Array.new(@n+1,0)}
-  @m+@n - 2*lcs(@m,@n)
-end
-
-def lcs(m,n)
-  return 0 if 0 == m || 0 == n
-  return @mem[m][n] if @mem[m][n] > 0
-  if @w1[m-1] == @w2[n-1]
-    @mem[m][n] = 1 + lcs(m-1,n-1)
-  else
-    @mem[m][n] = [lcs(m-1,n),lcs(m,n-1)].max
+  @dp = Array.new(@m+1).map{ Array.new(@n+1,0)}
+  (0..@m).each do |i|
+    (0..@n).each do |j|
+      next if 0 == i || 0 == j
+      if @w1[i-1] == @w2[j-1]
+        @dp[i][j] =  1 + @dp[i-1][j-1]
+      else
+        @dp[i][j] = max(@dp[i-1][j],@dp[i][j-1])
+      end
+    end
   end
-  return @mem[m][n]
+  @m+@n - 2 * @dp[@m][@n] 
+end
+def max(a,b)
+  a > b ? a : b
 end
