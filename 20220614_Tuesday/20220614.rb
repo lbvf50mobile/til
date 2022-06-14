885eb720a7b543875849d980a00a2d9001ed2443
Dmitry <lbvf50.mobile@gmail.com>
2022-06-15 01:54:15 +0700

    1244. Leetcode: 583. Delete Operation for Two Strings. (TLE) Thanks God!

--- 20220614_Tuesday/20220614.rb
+++ 20220614_Tuesday/20220614.rb
@@ -3,58 +3,21 @@
 # @param {String} word1
 # @param {String} word2
 # @return {Integer}
+# TLE.
 def min_distance(word1, word2)
-  w1,w2 = word1, word2
-  @dp = {}
-  caller(w1,w2)
+  # Based on: https://leetcode.com/problems/delete-operation-for-two-strings/solution/
+  @w1,@w2,@m,@n = word1, word2, word1.size, word2.size
+  @mem = Array.new(@m+1).map{ Array.new(@n+1,0)}
+  @m+@n - 2*lcs(@m,@n)
 end
 
-def rec(w1,w2)
-  # What if there is a solution?
-  return @dp[w1][w2] if @dp[w1] && @dp[w1][w2]
-  # What if the w1 generate for a first time?
-  @dp[w1] ||= {}
-  # Words are the same, there is nothing to calculate!
-  if w1 == w2
-    @dp[w1][w2] = 0
-    return 0
-  end
-  # The w1 is empty, need to remove all chars from the w2.
-  # It would be an answer.
-  if w1.empty?
-    @dp[w1][w2] = w2.size
-    return w2.size
-  end
-  # Each string contains charaters that represented in both of them.
-  a1,a2 = w1.chars, w2.chars
-  h1 = a1.each_with_object(Hash.new){|e,o| o[e] ||= true}
-  h2 = a2.each_with_object(Hash.new){|e,o| o[e] ||= true}
-  a1clear = a1.select{|x| h1[x] && h2[x]}
-  a2clear = a2.select{|x| h1[x] && h2[x]}
-  # Number of steps need to implement any way. Remove char uniq for a string.
-  need_to_have = (a1.size + a2.size) - (a1clear.size + a2clear.size)
-  # Remove same from the back and from the front.
-  while (!a1clear.empty?) && (!a2clear.empty?) && a1clear[0] == a2clear[0]
-    a1clear.shift
-    a2clear.shift
-  end
-  while (!a1clear.empty?) && (!a2clear.empty?) && a1clear[-1] == a2clear[-1]
-    a1clear.pop
-    a2clear.pop
-  end
-  if a1clear.empty? || a2clear.empty?
-    @dp[w1][w2] = need_to_have + caller(a1clear.join,a2clear.join)
-    return @dp[w1][w2]
-  end
-  wn1,wn2 = a1clear.join, a2clear.join
-  steps = [caller(wn1[1..-1], wn2), caller(wn1,wn2[1..-1]), caller(wn1[1..-1],wn2[1..-1])].min + need_to_have
-  @dp[w1][w2] = steps
-end
-
-def caller(w1,w2)
-  if w1 < w2
-    rec(w1,w2)
+def lcs(m,n)
+  return 0 if 0 == m || 0 == n
+  return @mem[m][n] if @mem[m][n] > 0
+  if @w1[m-1] == @w2[n-1]
+    @mem[m][n] = 1 + lcs(m-1,n-1)
   else
-    rec(w2,w1)
+    @mem[m][n] = [lcs(m-1,n),lcs(m,n-1)].max
   end
+  return @mem[m][n]
 end
