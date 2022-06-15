69fbaf6273e886d93333370d102ea5215fa3f7c6
Dmitry <lbvf50.mobile@gmail.com>
2022-06-16 02:30:37 +0700

    1255. Leetcode: 1048. Longest String Chain. (article and solution) Thanks God!

--- 20220615_Wednesday/20220615.rb
+++ 20220615_Wednesday/20220615.rb
@@ -1,12 +1,20 @@
 # Leetcode: 1048. Longest String Chain.
 # https://leetcode.com/problems/longest-string-chain/
+# = = = = = = =
+# Accepted.
+# Thanks God!
+# = = = = = = =
+# Runtime: 671 ms, faster than 22.73% of Ruby online submissions for Longest String Chain.
+# Memory Usage: 211.7 MB, less than 36.36% of Ruby online submissions for Longest String Chain.
 # @param {String[]} words
 # @return {Integer}
 def longest_str_chain(words)
+  @d = false
   @w = words.sort_by{|x| -x.size} # Longest first.
   # Current word length has list of possible next element of the chain.
   # Possible parents of i+1 are i.
   @prev = @w.each_with_object(Hash.new){|e,o| o[e.size+1] ||= []; o[e.size+1].push(e)} 
+  puts @prev.inspect if @d
   @dp = {}
   @w.each do |w|
     rec(w)
@@ -16,13 +24,20 @@ end
 
 def rec(word)
   return @dp[word] if @dp[word]
+  puts word if @d
   if @prev[word.size].nil?
     @dp[word] = 1
     return 1
   end
-  @prev[word.size]
+  puts @prev[word.size].inspect if @d
+  puts @prev[word.size].select{|x| relative(x,word)}.inspect if @d
 
   prev = @prev[word.size].select{|x| relative(x,word)}.map{|x| rec(x)}.max
+  
+  if !prev
+    @dp[word] = 1
+    return 1
+  end
   @dp[word] = prev + 1
   return prev + 1
 end
@@ -39,5 +54,5 @@ def relative(prev,curr)
     return false if prev[i] != curr[j] && 0 != errors
     j += 1
   end
-  1 == errors
+  true
 end
