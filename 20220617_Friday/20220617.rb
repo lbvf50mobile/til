12b1a48a05ce179da425dad0b62fada0629c14a8
Dmitry <lbvf50.mobile@gmail.com>
2022-06-18 14:24:20 +0700

    Day 1256. Leetcode: 968. Binary Tree Cameras. (continue) Thanks God!

--- 20220617_Friday/20220617.rb
+++ 20220617_Friday/20220617.rb
@@ -11,25 +11,32 @@
 # end
 # @param {TreeNode} root
 # @return {Integer}
+# [0,0,null,0,0]
+# [0,0,null,0,null,0,null,null,0]
+# [0,0,null,null,0,0,null,null,0,0]
+# [0,null,0,null,0,0,0] Error.
 def min_camera_cover(root)
-  # Light that lights down.
-  # TODO: Need to fix. Need add light up.
+  # Light that lights down and UP.
   # 1 - light
-  # 0 - shadow
-  # -1 - total darknes, need new light.
-  a = dfs(root,-1) # Set camera at root.
-  b = dfs(root, 0) # set camera on second row.
+  # 0 - shadow no need camera. The light is enough.
+  # -1 - total darknes, need new light, but light could be at child node.
+  # -2 - second level of full darknes, put camera any way. 
+  a = dfs(root,-2) # Set camera at root.
+  b = dfs(root, -1) # set camera on second row.
   a < b ? a : b
 end
 
 
 def dfs(node,light)
-  # TODO: Need to check do it possible to light with by a child!
   return 0 if  node.nil?
   camera = 0
-  if light < 0
+  children = node.left || node.right
+  if -2 == light 
     light = 1
-    camera = 1
+    camera = 1 
+  elsif -1 == light && (!children)
+    light = 1
+    camera = 1 
   end
   ans = camera
   ans += dfs(node.left, light-1)
