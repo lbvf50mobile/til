2004718478308c59b67312c8f1a1ec2aaff48df6
Dmitry <lbvf50.mobile@gmail.com>
2022-06-18 23:54:47 +0700

    1256. Leetcode: 745. Prefix and Suffix Search. (in action) Thanks God!

---
+++ 20220618_Saturday/20220618.rb
@@ -0,0 +1,27 @@
+# Leetcode: 745. Prefix and Suffix Search.
+# https://leetcode.com/problems/prefix-and-suffix-search/
+class WordFilter
+
+=begin
+    :type words: String[]
+=end
+    def initialize(words)
+        
+    end
+
+
+=begin
+    :type prefix: String
+    :type suffix: String
+    :rtype: Integer
+=end
+    def f(prefix, suffix)
+        
+    end
+
+
+end
+
+# Your WordFilter object will be instantiated and called as such:
+# obj = WordFilter.new(words)
+# param_1 = obj.f(prefix, suffix)
