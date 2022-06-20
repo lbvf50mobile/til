ecc754bf1929241a40434a718c8b9e3c5f3d66fd
Dmitry <lbvf50.mobile@gmail.com>
2022-06-20 23:58:30 +0700

    1257. Leetcode: 820. Short Encoding of Words. (in action) Thanks God!

---
+++ 20220620_Monday/20220620.rb
@@ -0,0 +1,12 @@
+# Leetcode: 820. Short Encoding of Words.
+# https://leetcode.com/problems/short-encoding-of-words/
+# @param {String[]} words
+# @return {Integer}
+def minimum_length_encoding(words)
+  require "set"
+  good =  Set.new(words)
+  words.each do |word|
+    (1..
+  end
+    
+end
