14820e77ae866771d26c7f77c884ea0a6fc90ed9
Dmitry <lbvf50.mobile@gmail.com>
2022-06-22 00:34:24 +0700

    1258. Leetcode: 1642. Furthest Building You Can Reach. (in action) Thanks God!

--- 20220621_Tuesday/20220621.rb
+++ 20220621_Tuesday/20220621.rb
@@ -6,21 +6,28 @@
 # @return {Integer}
 def furthest_building(heights, bricks, ladders)
   return 0 if 1 == heights.size
+  @d = false
+  puts heights.inspect if @d
   max = []
   total = 0
   (1...heights.size).each do |i|
+    puts "i=#{i} h=#{heights[i]}" if @d
     if heights[i] > heights[i-1]
+      puts "Need to jump." if @d
       delta = heights[i] - heights[i-1]
+      puts "delta = #{delta}" if @d
       total += delta
       max.push(delta)
-      max.sort
+      max.sort! # <= here was an error.
       sum = 0
-      (0...max.size - ladders).each do |i|
-        sum += max[i]
+      (0...max.size - ladders).each do |j|
+        sum += max[j]
       end
       if sum > bricks
         return i-1
       end
+    else
+      puts "no need to jump." if @d
     end
   end
   return heights.size - 1
