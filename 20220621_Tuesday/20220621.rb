6aaac962a81e1a77b4a47ff81d7547de5e113783
Dmitry <lbvf50.mobile@gmail.com>
2022-06-22 01:21:52 +0700

    1258. Leetcode: 1642. Furthest Building You Can Reach. (emulator works) Thanks God!

--- 20220621_Tuesday/20220621.rb
+++ 20220621_Tuesday/20220621.rb
@@ -7,20 +7,28 @@
 def furthest_building(heights, bricks, ladders)
   return 0 if 1 == heights.size
   total = 0
+  @d = true
+  puts heights.inspect if @d
+  max = []
   heap = MinHeap.new([])
   heap_sum = 0
   (1...heights.size).each do |i|
     delta = heights[i] - heights[i-1]
     if delta > 0
       total += delta
-      if heap.size < ladders
-        heap_sum += delta
-        heap.push(delta)
-      elsif heap.size == ladders && ladders > 0 && heap.min < delta
-        tmp = heap.pop
-        heap_sum += (delta - tmp)
+      if max.size < ladders
+        p [i,?a]
+        max.push(delta)
+        max.sort!
+        heap_sum = max.sum
+      elsif max.size == ladders && ladders > 0 && max[0] < delta
+        p [i,?b]
+        tmp = max.shift
+        max.push(delta)
+        max.sort!
+        heap_sum = max.sum
       end
-      if bricks < total - heap_sum
+      if bricks < (total - heap_sum)
         return i - 1
       end
     end
