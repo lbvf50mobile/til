cd961579ff81ea5e5bca30160f2dc8321ffa0654
Dmitry <lbvf50.mobile@gmail.com>
2022-06-27 23:46:25 +0700

    1264. Leetcode: 1689. Partitioning Into Minimum Number Of Deci-Binary Numbers. (article and solution) Thanks God!

---
+++ 20220627_Monday/20220627.rb
@@ -0,0 +1,13 @@
+# Leetcode: 1689. Partitioning Into Minimum Number Of Deci-Binary Numbers.
+# https://leetcode.com/problems/partitioning-into-minimum-number-of-deci-binary-numbers/
+# = = = = = = =
+# Accepted.
+# Thanks God!
+# = = = = = = =
+# Runtime: 368 ms, faster than 100.00% of Ruby online submissions for Partitioning Into Minimum Number Of Deci-Binary Numbers.
+# Memory Usage: 227.6 MB, less than 100.00% of Ruby online submissions for Partitioning Into Minimum Number Of Deci-Binary Numbers.
+# @param {String} n
+# @return {Integer}
+def min_partitions(n)
+  n.chars.max.to_i
+end
