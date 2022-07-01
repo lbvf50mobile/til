5feebd670c5297952b43665d79e6c0dfcd4d780e
Dmitry <lbvf50.mobile@gmail.com>
2022-07-02 00:04:02 +0700

    1268. Leetcode: 1710. Maximum Units on a Truck. (solitiong) Thanks God!

--- 20220701_Friday/20220701.rb
+++ 20220701_Friday/20220701.rb
@@ -1,12 +1,18 @@
 # Leetcode: 1710. Maximum Units on a Truck.
 # https://leetcode.com/problems/maximum-units-on-a-truck/
+# = = = = = = =
+# Accepted.
+# Thanks God!
+# = = = = = = =
+# Runtime: 399 ms, faster than 16.00% of Ruby online submissions for Maximum Units on a Truck.
+# Memory Usage: 210.9 MB, less than 100.00% of Ruby online submissions for Maximum Units on a Truck.
 # @param {Integer[][]} box_types
 # @param {Integer} truck_size
 # @return {Integer}
 def maximum_units(box_types, truck_size)
   box_types = box_types.sort_by{|x| - x.last}
   p box_types
-  units1 = 0
+  units1 = 0 # ! Variables name confilct! Leadsto dubbuging.
   box_types.each do |amount, units|
     p [amount, units, truck_size]
     if truck_size > 0
