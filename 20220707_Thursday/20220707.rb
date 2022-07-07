21371641390fe31e36f121f5667a1ce683867798
Dmitry <lbvf50.mobile@gmail.com>
2022-07-08 00:08:26 +0700

    1274. Leetcode: 97. Interleaving String. (in action 2) Thanks God!

--- 20220707_Thursday/20220707.rb
+++ 20220707_Thursday/20220707.rb
@@ -5,7 +5,38 @@
 # @param {String} s3
 # @return {Boolean}
 def is_interleave(s1, s2, s3)
-  a = (s1+s2).chars.sort.tally.values.join(?,)
-  b = s3.chars.sort.tally.values.join(?,)
-  a == b
+  @s1,@s2,@s3 = s1, s2, s3
+  # need to flip turns.
+  @dp = {} 
+  rec(true,0,0,0) || rec(false,0,0,0)
+end
+
+def rec(turn,i,j,k)
+  label =  "#{turn.inspect},#{i},#{j},#{k}"
+  return @dp[label] if @dp[label]
+  if @s1.size == i && @s2.size == j && @s3.size == k
+    @dp[label] = true
+    return true
+  end
+  if turn
+    while @s1[i] == @s3[k]
+      if true == rec(false,i+1, j, k+1)
+        @dp[label] = true
+        return true
+      end
+      i += 1; k += 1;
+    end
+    @dp[label] = false
+    return false
+  else
+    while @s1[j] == @s3[k]
+      if true == rec(true,i, j+1, k+1)
+        @dp[label] = true
+        return true
+      end
+      j += 1; k += 1;
+    end
+    @dp[label] = false
+    return false
+  end
 end
