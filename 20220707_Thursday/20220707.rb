f2ff12b1fed82c0000d2f92064a182666f3fd002
Dmitry <lbvf50.mobile@gmail.com>
2022-07-08 01:40:39 +0700

    1274. Leetcode: 97. Interleaving String. (in action 3) Thanks God!

--- 20220707_Thursday/20220707.rb
+++ 20220707_Thursday/20220707.rb
@@ -5,38 +5,46 @@
 # @param {String} s3
 # @return {Boolean}
 def is_interleave(s1, s2, s3)
-  @s1,@s2,@s3 = s1, s2, s3
+  return false if (s1.size + s2.size) != s3.size
+  return false if counter(s1+s2) != counter(s3)
+  @s1,@s2,@s3 = s1, s2, s3 # Save all varaiables as an object variables.
   # need to flip turns.
-  @dp = {} 
-  rec(true,0,0,0) || rec(false,0,0,0)
+  @dp = Array.new(2) # turn 0 - s1, 1 - s2.  
+    .map{ Array.new(s1.size+1).map{ Array.new(s2.size+1).map{Array.new(s3.size+1)}}}
+  true == rec(0,0,0,0) || true == rec(1,0,0,0) # Start from first or start from second.
+end
+
+def counter(str)
+  ans = Array.new(30, 0)
+  str.chars.each{|x| ans[x.ord - ?a.ord] += 1}
+  ans
 end
 
 def rec(turn,i,j,k)
-  label =  "#{turn.inspect},#{i},#{j},#{k}"
-  return @dp[label] if @dp[label]
+  return  @dp[turn][i][j][k] if ! @dp[turn][i][j][k].nil?
   if @s1.size == i && @s2.size == j && @s3.size == k
-    @dp[label] = true
-    return true
+    return @dp[turn][i][j][k] = true
+  end
+  if @s3.size == k 
+    return @dp[turn][i][j][k] = false
   end
   if turn
-    while @s1[i] == @s3[k]
-      if true == rec(false,i+1, j, k+1)
-        @dp[label] = true
-        return true
-      end
-      i += 1; k += 1;
-    end
-    @dp[label] = false
-    return false
+    return @dp[turn][i][j][k] = rec0(i,j,k)
   else
-    while @s1[j] == @s3[k]
-      if true == rec(true,i, j+1, k+1)
-        @dp[label] = true
-        return true
-      end
-      j += 1; k += 1;
-    end
-    @dp[label] = false
-    return false
+    return @dp[turn][i][j][k] = rec1(i,j,k)
   end
 end
+
+def rec0(i,j,k)
+  while @s1[i] == @s3[k]
+  end
+  return false
+end
+
+
+def rec1(i,j,k)
+  while @s2[j] == @s3[k]
+  end
+  return false
+end
+
