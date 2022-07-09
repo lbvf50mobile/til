ad99d22b46a0dc95bcc47e96c1eccac973afd846
Dmitry <lbvf50.mobile@gmail.com>
2022-07-10 01:48:42 +0700

    1276. Leetcode: 1696. Jump Game VI. (search for a solution) Thanks God!

--- 20220709_Saturday/20220709.rb
+++ 20220709_Saturday/20220709.rb
@@ -3,20 +3,42 @@
 # @param {Integer[]} nums
 # @param {Integer} k
 # @return {Integer}
-# TLE.
+# Wrong answer.
 def max_result(nums, k)
   # Let's use pull dp.
+  @d = false
   dp = nums.clone
-  (0...dp.size).each do |i|
-    right = i-1
-    left = (i-k) >= 0 ? i-k : 0
-    next if right < 0
-    max = nil
-    (left..right).each do |j|
-      max = dp[j] if max.nil?
-      max = dp[j] if dp[j] > max
-    end
+  max = dp[0]
+  hp = MaxHeap.new([dp[0]])
+  right = k >= dp.size ? dp.size : k
+  puts "from 1 till #{right}" if @d
+
+  (1...right).each do |i|
     dp[i] += max
+    max = dp[i] if dp[i] > max
+    hp.push(dp[i])
+    puts "dp[#{i}] = #{dp[i]}" if @d
+  end
+  return dp[-1] if right >= dp.size
+  # Error was in index with numbers. Need to start from k not form k+1.
+  # And need to draw images.
+  (k...nums.size).each do |i|
+    puts "Current dp[#{i}] == #{dp[i]}, and max = #{hp.max}." if @d
+    dp[i] += hp.max
+    puts "And current value is dp[#{i}] == #{dp[i]}." if @d
+    # Who exits?
+    # If maximum exit. Need to pop it.
+    # and add current value. May be it is a maximum.
+    # And here is the same arror. dp[i-1-k] instead of dp[i-k].
+    puts "i-k = #{i-k} i = #{i} dp[i-k] == #{dp[i-k]}." if @d
+    if dp[i-k] == hp.max
+      # I think need to pop every time. Every element we leave outside the window.
+      puts "i-k = #{i-k} i = #{i} dp[i-k] == #{dp[i-k]}." if @d 
+      hp.pop
+      if hp.empty? || dp[i] >= hp.max
+        hp.push(dp[i])
+      end
+    end
   end
   dp[-1]
 end
