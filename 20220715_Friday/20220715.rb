16db437e0181eb6a54387176f31099e1bd91a226
Dmitry <lbvf50.mobile@gmail.com>
2022-07-16 00:10:43 +0700

    1281. Leetcode: 695. Max Area of Island. (looking for error) Thanks God!

--- 20220715_Friday/20220715.rb
+++ 20220715_Friday/20220715.rb
@@ -2,16 +2,19 @@
 # https://leetcode.com/problems/max-area-of-island/
 # @param {Integer[][]} grid
 # @return {Integer}
+# Error: [[1,1,0,0,0],[1,1,0,0,0],[0,0,0,1,1],[0,0,0,1,1]]
 def max_area_of_island(grid)
+  @d = false
   @g = grid
-  @v = Array.new(@g.size).map{ @g[0].size}
+  @v = Array.new(@g.size).map{ Array.new(@g[0].size,false)}
   ans = 0
   (0...@g.size).each do |i|
     (0...@g[0].size).each do |j|
       next if @v[i][j]
-      next if 0 == @v[i][j]
+      next if 0 == @g[i][j]
       tmp = bfs_size(i,j)
       ans = tmp if tmp > ans
+      puts "Start island: #{i},#{j}. Size #{tmp}. Ans #{ans}." if @d
     end
   end
   ans 
@@ -22,7 +25,8 @@ def correct(i,j)
 end
 
 def bfs_size(i,j)
-  size = 1
+  puts "Start with #{i},#{j}." if @d
+  size = 0 # Error was here.
   q1 = [i]
   q2 = [j]
   while (!q1.empty?) && (!q2.empty?)
@@ -30,12 +34,14 @@ def bfs_size(i,j)
     j = q2.shift
     @v[i][j] = true
     size += 1
+    puts "#{size}, #{i},#{j}, #{@v[i][j].inspect}, #{@g[i][j]}" if @d
     [[i+1,j],[i,j+1],[i-1,j],[i,j-1]].each do |ii,jj|
+      puts "Add:#{ii},#{jj}, #{correct(ii,jj).inspect}, #{@v[i][j].inspect}" if @d
       next if ! correct(ii,jj)
       next if @v[ii][jj]
       next if 0 == @g[ii][jj]
-      q1.push(i)
-      q2.push(j)
+      q1.push(ii) # Error was here.
+      q2.push(jj)
     end
   end
   return size
