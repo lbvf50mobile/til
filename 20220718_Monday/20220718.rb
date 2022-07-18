8371a6a5ea023dbb4ad6d0f2cfafa2a6b3ca8f13
Dmitry <lbvf50.mobile@gmail.com>
2022-07-18 23:55:59 +0700

    1283. Leetcode: 1074. Number of Submatrices That Sum to Target. (in action) Thanks God!

---
+++ 20220718_Monday/20220718.rb
@@ -0,0 +1,8 @@
+# Leetcode: 1074. Number of Submatrices That Sum to Target.
+# https://leetcode.com/problems/number-of-submatrices-that-sum-to-target/
+# @param {Integer[][]} matrix
+# @param {Integer} target
+# @return {Integer}
+def num_submatrix_sum_target(matrix, target)
+    
+end
