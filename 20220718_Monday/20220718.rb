17e50aa5437bbfa8dc3235663529493aece5e344
Dmitry <lbvf50.mobile@gmail.com>
2022-07-19 23:16:58 +0700

    1283. Leetcode: 1074. Number of Submatrices That Sum to Target. (article and solution) Thanks God!

--- 20220718_Monday/20220718.rb
+++ 20220718_Monday/20220718.rb
@@ -1,42 +1,34 @@
 # Leetcode: 1074. Number of Submatrices That Sum to Target.
 # https://leetcode.com/problems/number-of-submatrices-that-sum-to-target/
+# = = = = = = =
+# Accepted.
+# Thanks God!
+# = = = = = = =
+# Runtime: 630 ms, faster than 100.00% of Ruby online submissions for Number of Submatrices That Sum to Target.
+# Memory Usage: 227.5 MB, less than 100.00% of Ruby online submissions for Number of Submatrices That Sum to Target.
 # @param {Integer[][]} matrix
 # @param {Integer} target
 # @return {Integer}
-# TLE.
 def num_submatrix_sum_target(matrix, target)
-  @t = target
-  @m = matrix.clone.map{|x| [0] + x.clone} # Add zero column.
-  @m.unshift( Array.new(@m[0].size,0)) # Add zero row.
-  # Make prefix array for each row.
-  w = @m.size
-  h = @m[0].size
-  (1...w).each do |i|
-    (1...h).each do |j|
-      @m[i][j] += @m[i][j-1] # Take data from the left.
+  # Based on: https://leetcode.com/problems/number-of-submatrices-that-sum-to-target/discuss/1162790/Ruby-DP
+  m, n = matrix.size, matrix[0].size
+  # Prefix array.
+  (0...m).each do |i|
+    (1...n).each do |j|
+      matrix[i][j] += matrix[i][j-1]
     end
   end
-  # Make 2d prefix array. Use columns.
-  (1...h).each do |j|
-    (1...w).each do |i|
-      @m[i][j] += @m[i-1][j] # Take data from the top.
-    end
-  end
-  answer = 0
-  (1...w).each do |i0|
-    (1...h).each do |j0|
-      (i0...w).each do |i1|
-        (j0...h).each do |j1|
-          answer += 1 if @t == sum(i0,j0,i1,j1)
-        end
+  result = 0
+  (0...n).each do |j1|
+    (j1...n).each do |j2|
+      h = {}; h[0] = 1
+      s = 0
+      (0...m).each do |i|
+        s += matrix[i][j2] - (j1 > 0 ? matrix[i][j1-1] : 0)
+        result += h[s-target] || 0
+        h[s] = (h[s] || 0) + 1
       end
     end
   end
-  answer
-end
-
-# i0,j0 - top left.
-# i1,j1 - bottom right.
-def sum(i0,j0, i1, j1) 
-  @m[i1][j1] - @m[i0-1][j1] - @m[i1][j0-1] + @m[i0-1][j0-1] 
+  result
 end
