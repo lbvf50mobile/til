27178a738c5f780269b96b85655c8c75c07f6c22
Dmitry <lbvf50.mobile@gmail.com>
2022-07-21 00:06:42 +0700

    1285. Leetcode: 792. Number of Matching Subsequences. (tle) Thanks God!

--- 20220720_Wednesday/20220720.rb
+++ 20220720_Wednesday/20220720.rb
@@ -3,16 +3,29 @@
 # @param {String} s
 # @param {String[]} words
 # @return {Integer}
+# TLE.
 def num_matching_subseq(s, words)
+  @d = false
   sa = create_array(s)
   wa = words.map{|x| create_array(x)}
   answer = 0
   words.zip(wa).each do |(word,arr)|
     next if word.size > s.size
     next if ! (same_set(arr,sa))
-    answer += 1
-    # Todo need to check word is subset.
-
+    i,j = 0,0
+    while i < s.size && j < word.size
+      if s[i] !=  word[j]
+        i += 1
+      else
+        i += 1
+        j += 1
+      end
+    end
+    # Need to reach the end of a word.
+    # End of a word is a "terminating zero".
+    # Can not skip chars from a word.
+    puts "#{s} #{word} #{(word.size == j).inspect}" if @d
+    answer += 1 if  word.size == j
   end
   answer
 end
