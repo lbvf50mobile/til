e98454c14177104b386d6a171cb9dd74e7348ed1
Dmitry <lbvf50.mobile@gmail.com>
2022-07-24 23:26:26 +0700

    1289. Leetcode: 240. Search a 2D Matrix II. (article and solution) Thanks God!

---
+++ 20220724_Sunday/20220724.rb
@@ -0,0 +1,23 @@
+# Leetcode: 240. Search a 2D Matrix II.
+# https://leetcode.com/problems/search-a-2d-matrix-ii/
+# = = = = = = =
+# Accepted.
+# Thanks God!
+# = = = = = = =
+# Runtime: 621 ms, faster than 25.00% of Ruby online submissions for Search a 2D Matrix II.
+# Memory Usage: 219.5 MB, less than 12.50% of Ruby online submissions for Search a 2D Matrix II.
+# @param {Integer[][]} matrix
+# @param {Integer} target
+# @return {Boolean}
+def search_matrix(matrix, target)
+  @d = true
+  @target = target
+  matrix.select{|x| target.between?(x.first, x.last)}
+    .any?{|x| check_row(x)}
+end
+def check_row(row)
+  value = row.bsearch{|x| x >= @target}
+  return false unless value
+  p [value, @target] if @d
+  value == @target
+end
