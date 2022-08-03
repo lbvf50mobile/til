7063f182b4f12d18550a0b7fbc5f428983b53c62
Dmitry <lbvf50.mobile@gmail.com>
2022-08-03 23:32:18 +0700

    1298. Leetcode: 378. Kth Smallest Element in a Sorted Matrix. (Binary search) Thanks God!

--- 20220802_Tuesday/20220802.rb
+++ 20220802_Tuesday/20220802.rb
@@ -3,32 +3,41 @@
 # = = = = = = = = = = = = = =
 # Thanks God, Jesus Chirst!
 # = = = = = = = = = = = = = =
+# Runtime: 227 ms, faster than 52.63% of Ruby online submissions for Kth Smallest Element in a Sorted Matrix.
+# Memory Usage: 214.9 MB, less than 42.11% of Ruby online submissions for Kth Smallest Element in a Sorted Matrix.
 # @param {Integer[][]} matrix
 # @param {Integer} k
 # @return {Integer}
-# TLE.
+# Based on:
+# https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/discuss/1547354/3-Different-Approaches-for-Interview-With-Comments
 def kth_smallest(matrix, k)
-  @d = true
-  heap = MaxHeap.new([])
-  tmp = [] if @d
-  matrix.each do |row|
-    row.each do |cell|
-      puts "cell = #{cell}" if @d
-      if heap.size < k
-        heap.push(cell)
-        tmp.push(cell) if @d
-        tmp.sort! if @d
-        tmp.reverse! if @d
-      elsif heap.max >= cell
-        heap.pop
-        heap.push(cell)
-        tmp.pop if @d
-        tmp.push(cell) if @d
-        tmp.sort! if @d
-        tmp.reverse! if @d
+  # Based on:
+  # https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/discuss/1547354/3-Different-Approaches-for-Interview-With-Comments
+  rows, cols = matrix.size, matrix[0].size
+  lo, hi = matrix[0][0], matrix[rows-1][cols-1]
+  while lo <= hi # Error was here.
+    mid = lo + (hi-lo)/2
+    count, maxNum = 0, lo
+    (0...rows).each do |r|
+      c = cols - 1
+      while( c >=0 && matrix[r][c] > mid)
+        c -= 1;
       end
-      puts "heap = #{tmp.to_a.inspect}" if @d
+      if  c >= 0
+        count += (c+1)
+        maxNum = [maxNum, matrix[r][c]].max
+      else
+        break
+      end
+    end
+    if k == count
+      return maxNum
+    elsif count < k
+      lo = mid + 1
+    else
+      hi = mid - 1
     end
   end
-  heap.max
+  return lo
 end
+
