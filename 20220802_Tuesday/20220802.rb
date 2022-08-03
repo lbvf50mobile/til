64f61e007d5748c9748e616426809f0d02bb85db
Dmitry <lbvf50.mobile@gmail.com>
2022-08-03 22:32:31 +0700

    1298. Leetcode: 378. Kth Smallest Element in a Sorted Matrix. (Heap solution) Thanks God!

--- 20220802_Tuesday/20220802.rb
+++ 20220802_Tuesday/20220802.rb
@@ -1,14 +1,34 @@
 # Leetcode: 378. Kth Smallest Element in a Sorted Matrix.
 # https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/
 # = = = = = = = = = = = = = =
-# Accepted.
 # Thanks God, Jesus Chirst!
 # = = = = = = = = = = = = = =
-# Runtime: 158 ms, faster than 73.68% of Ruby online submissions for Kth Smallest Element in a Sorted Matrix.
-# Memory Usage: 216.9 MB, less than 10.53% of Ruby online submissions for Kth Smallest Element in a Sorted Matrix.
 # @param {Integer[][]} matrix
 # @param {Integer} k
 # @return {Integer}
+# TLE.
 def kth_smallest(matrix, k)
-  matrix.flatten.sort[k-1]
+  @d = true
+  heap = MaxHeap.new([])
+  tmp = [] if @d
+  matrix.each do |row|
+    row.each do |cell|
+      puts "cell = #{cell}" if @d
+      if heap.size < k
+        heap.push(cell)
+        tmp.push(cell) if @d
+        tmp.sort! if @d
+        tmp.reverse! if @d
+      elsif heap.max >= cell
+        heap.pop
+        heap.push(cell)
+        tmp.pop if @d
+        tmp.push(cell) if @d
+        tmp.sort! if @d
+        tmp.reverse! if @d
+      end
+      puts "heap = #{tmp.to_a.inspect}" if @d
+    end
+  end
+  heap.max
 end
