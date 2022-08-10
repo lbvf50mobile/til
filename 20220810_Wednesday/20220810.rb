cf51d2f2bf3a403682db76a8e428a7320983bdb9
Dmitry <lbvf50.mobile@gmail.com>
2022-08-10 23:59:46 +0700

    1305. Leetcode: 108. Convert Sorted Array to Binary Search Tree. (in action) Thanks God!

---
+++ 20220810_Wednesday/20220810.rb
@@ -0,0 +1,14 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {Integer[]} nums
+# @return {TreeNode}
+def sorted_array_to_bst(nums)
+    
+end
