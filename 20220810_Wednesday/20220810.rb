544ee77ff19046780d0c828c5ea67872cb6b759e
Dmitry <lbvf50.mobile@gmail.com>
2022-08-11 01:10:40 +0700

    1305. Leetcode: 108. Convert Sorted Array to Binary Search Tree. (article and solution) Thanks God!

--- 20220810_Wednesday/20220810.rb
+++ 20220810_Wednesday/20220810.rb
@@ -1,4 +1,13 @@
+# Leetcode: 108. Convert Sorted Array to Binary Search Tree.
+# https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
 # Definition for a binary tree node.
+# = = = = = = = = = = = = = =
+# Accepted.
+# Thanks God, Jesus Christ!
+# = = = = = = = = = = = = = =
+# Runtime: 246 ms, faster than 25.00% of Ruby online submissions for Convert Sorted Array to Binary Search Tree.
+# Memory Usage: 277.7 MB, less than 71.67% of Ruby online submissions for Convert Sorted Array to Binary Search Tree.
+#
 # class TreeNode
 #     attr_accessor :val, :left, :right
 #     def initialize(val = 0, left = nil, right = nil)
@@ -10,5 +19,11 @@
 # @param {Integer[]} nums
 # @return {TreeNode}
 def sorted_array_to_bst(nums)
-    
+  return nil if nums.empty?
+  return TreeNode.new(nums[0]) if 1 == nums.size
+  return TreeNode.new(nums[1],sorted_array_to_bst(nums[0...1])) if 2 == nums.size
+  left = sorted_array_to_bst(nums[0...nums.size/2]) 
+  right = sorted_array_to_bst(nums[nums.size/2+1..-1])
+  return TreeNode.new(nums[nums.size/2],left,right)
 end
+
