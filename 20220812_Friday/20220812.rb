22215ced47cd62fcfdda4d8597726ed6df3c7cf6
Dmitry <lbvf50.mobile@gmail.com>
2022-08-12 23:56:01 +0700

    1307. Leetcode: 235. Lowest Common Ancestor of a Binary Search Tree. (in acton) Thanks God!

---
+++ 20220812_Friday/20220812.rb
@@ -0,0 +1,18 @@
+# Leetcode: 235. Lowest Common Ancestor of a Binary Search Tree.
+# https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val)
+#         @val = val
+#         @left, @right = nil, nil
+#     end
+# end
+
+# @param {TreeNode} root
+# @param {TreeNode} p
+# @param {TreeNode} q
+# @return {TreeNode}
+def lowest_common_ancestor(root, p, q)
+    
+end
