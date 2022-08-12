407dc6b354dc6d7de180bc56789f0ff340b84cc7
Dmitry <lbvf50.mobile@gmail.com>
2022-08-13 00:18:22 +0700

    1307. Leetcode: 235. Lowest Common Ancestor of a Binary Search Tree. (article and solution) Thanks God!

--- 20220812_Friday/20220812.rb
+++ 20220812_Friday/20220812.rb
@@ -1,5 +1,12 @@
 # Leetcode: 235. Lowest Common Ancestor of a Binary Search Tree.
 # https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+# = = = = = = = = = = = = = =
+# Accepted.
+# Thanks God, Jesus Christ!
+# = = = = = = = = = = = = = =
+# Runtime: 189 ms, faster than 21.21% of Ruby online submissions for Lowest Common Ancestor of a Binary Search Tree.
+# Memory Usage: 212.5 MB, less than 56.36% of Ruby online submissions for Lowest Common Ancestor of a Binary Search Tree.
+#
 # Definition for a binary tree node.
 # class TreeNode
 #     attr_accessor :val, :left, :right
@@ -14,5 +21,13 @@
 # @param {TreeNode} q
 # @return {TreeNode}
 def lowest_common_ancestor(root, p, q)
-    
+  return root if root.nil?
+  return p if root == p
+  return q if root == q
+
+  l = lowest_common_ancestor(root.left, p, q)
+  r = lowest_common_ancestor(root.right, p, q)
+  return root if l && r
+  return l if l
+  return r 
 end
