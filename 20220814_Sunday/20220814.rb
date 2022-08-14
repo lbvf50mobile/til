13968aacc7da16cd22a90360e0824d2e50b0c79f
Dmitry <lbvf50.mobile@gmail.com>
2022-08-14 23:55:40 +0700

    1309. Leetcode: 126. Word Ladder II. (in action) Thanks God!

---
+++ 20220814_Sunday/20220814.rb
@@ -0,0 +1,9 @@
+# Leetcode: 126. Word Ladder II.
+# https://leetcode.com/problems/word-ladder-ii/
+# @param {String} begin_word
+# @param {String} end_word
+# @param {String[]} word_list
+# @return {String[][]}
+def find_ladders(begin_word, end_word, word_list)
+    
+end
