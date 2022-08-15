d1c8563e6546de058d464b3ecfc1f6c81a3bee03
Dmitry <lbvf50.mobile@gmail.com>
2022-08-16 00:16:12 +0700

    1310. Leetcode: 13. Roman to Integer. (article and solution) Thanks God!

--- 20220815_Monday/20220815.rb
+++ 20220815_Monday/20220815.rb
@@ -1,31 +1,46 @@
 # Leetcode: 13. Roman to Integer.
 # https://leetcode.com/problems/roman-to-integer/
+# = = = = = = = = = = = = = =
+# Accepted.
+# Thanks God, Jesus Christ!
+# = = = = = = = = = = = = = =
+# Runtime: 174 ms, faster than 51.06% of Ruby online submissions for Roman to Integer.
+# Memory Usage: 211 MB, less than 43.90% of Ruby online submissions for Roman to Integer.
 # @param {String} s
 # @return {Integer}
 def roman_to_int(s)
   answer = 0
   i = 0
-  two = [
+  two = {
     "IV" => 4,
     "IX" => 9,
-    "XL" => 50,
+    "XL" => 40,
     "XC" => 90,
-    "CD" => 500,
-    "CM" => 
-  ]
+    "CD" => 400,
+    "CM" => 900
+  }
+  one = {
+    ?I => 1,
+    ?V => 5,
+    ?X => 10,
+    ?L => 50,
+    ?C => 100,
+    ?D => 500,
+    ?M => 1000,
+  }
   while i < s.size
-    if i < s.size - 1
-      if "IV" == (s[i] + s[i+1])
-        answer += 4
-      end
-      if "IX" == (s[i] + s[i+1])
-        answer += 9
-      end
-      if "XL" == (s[i] + s[i+1])
-        answer += 50
-      end
+    if (i < s.size - 1) && two[s[i]+s[i+1]]
+      answer += two[s[i] + s[i+1]]
+      i += 2
+      next
     end
-    i += 1
+    if one[s[i]]
+      answer += one[s[i]]
+      i += 1
+      next
+    else
+      raise "Cannot find value for #{s[i]}."
+    end
   end
-    
+  return answer
 end
