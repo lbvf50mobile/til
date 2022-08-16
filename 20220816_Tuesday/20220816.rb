6ec184315df7262b605d455e4d42d5ec36d444bf
Dmitry <lbvf50.mobile@gmail.com>
2022-08-16 23:42:08 +0700

    1311. Leetcode: 387. First Unique Character in a String. (one more solution) Thanks God!

--- 20220816_Tuesday/20220816.rb
+++ 20220816_Tuesday/20220816.rb
@@ -4,24 +4,12 @@
 # Accepted.
 # Thanks God, Jesus Christ!
 # = = = = = = = = = = = = = =
-# Runtime: 469 ms, faster than 35.90% of Ruby online submissions for First Unique Character in a String.
-# Memory Usage: 217.2 MB, less than 27.18% of Ruby online submissions for First Unique Character in a String.
+# Runtime: 504 ms, faster than 31.80% of Ruby online submissions for First Unique Character in a String.
+# Memory Usage: 214.8 MB, less than 46.67% of Ruby online submissions for First Unique Character in a String.
 # @param {String} s
 # @return {Integer}
 def first_uniq_char(s)
-  counter = Array.new(26,0)
-  position = Array.new(26,nil)
-  s.chars.each_with_index do |char,i|
-    code = char.ord - ?a.ord
-    next if 2 == counter[code]
-    counter[code] += 1
-    position[code] = i if ! position[code]
-  end
-  min = s.size 
-  (0...26).each do |i|
-    next if 1 != counter[i]
-    min = position[i] if position[i] < min
-  end
-  return -1 if s.size == min
-  return min
+  hash = s.chars.tally
+  s.chars.each_with_index{|c,i| return i if 1 == hash[c]}
+  -1
 end
