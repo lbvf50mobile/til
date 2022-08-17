5ca974f27f21a9b6213e2024cbf4a5fee31baa59
Dmitry <lbvf50.mobile@gmail.com>
2022-08-18 00:06:18 +0700

    1312. Leetcode: 804. Unique Morse Code Words. (article and solution) Thanks God!

--- 20220817_Wednesday/20220817.rb
+++ 20220817_Wednesday/20220817.rb
@@ -1,7 +1,18 @@
 # Leetcode: 804. Unique Morse Code Words.
 # https://leetcode.com/problems/unique-morse-code-words/
+# = = = = = = = = = = = = = =
+# Accepted.
+# Thanks God, Jesus Christ!
+# = = = = = = = = = = = = = =
+# Runtime: 139 ms, faster than 40.74% of Ruby online submissions for Unique Morse Code Words.
+# Memory Usage: 210.9 MB, less than 70.37% of Ruby online submissions for Unique Morse Code Words.
 # @param {String[]} words
 # @return {Integer}
 def unique_morse_representations(words)
-    
+  @map = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+  words.map{|x| word2morse(x)}.uniq.size
+end
+
+def word2morse(word)
+  word.chars.map{|x| @map[x.ord - ?a.ord]}.join
 end
