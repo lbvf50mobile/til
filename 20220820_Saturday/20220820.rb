213a4b9140f6af9e7b4a36a8271f0ba80b581c08
Dmitry <lbvf50.mobile@gmail.com>
2022-08-20 23:54:46 +0700

    1315. Leetcode: 871. Minimum Number of Refueling Stops. (in action) Thanks God!

---
+++ 20220820_Saturday/20220820.rb
@@ -0,0 +1,9 @@
+# Leetcode: 871. Minimum Number of Refueling Stops.
+# https://leetcode.com/problems/minimum-number-of-refueling-stops/
+# @param {Integer} target
+# @param {Integer} start_fuel
+# @param {Integer[][]} stations
+# @return {Integer}
+def min_refuel_stops(target, start_fuel, stations)
+    
+end
