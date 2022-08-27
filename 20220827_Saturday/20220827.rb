d1623b1fc9e7ccf775d8c5ec1703cc425d26bfff
Dmitry <lbvf50.mobile@gmail.com>
2022-08-28 02:20:45 +0700

    1321. Leetcode: 363. Max Sum of Rectangle No Larger Than K. (in action) Thanks God!

--- 20220827_Saturday/20220827.rb
+++ 20220827_Saturday/20220827.rb
@@ -1,6 +1,7 @@
 # Leetcode: 363. Max Sum of Rectangle No Larger Than K.
 # https://leetcode.com/problems/max-sum-of-rectangle-no-larger-than-k/
-# TLE.
+# Error in the code.
+# 
 # @param {Integer[][]} matrix
 # @param {Integer} k
 # @return {Integer}
@@ -12,7 +13,7 @@ def max_sum_submatrix(matrix, k)
     (l...@m[0].size).each do |r| # End of the interval.
       (0...@m.size).each{|i| arr[i] += @m[i][r]} # Fill by rows.
        tmp = find_max(arr)
-       max = tmp if tmp > max
+       max = tmp if tmp && tmp > max
        return max if max == @k
     end
   end
@@ -20,16 +21,37 @@ def max_sum_submatrix(matrix, k)
 end
 
 def find_max(arr)
-  max = -Float::INFINITY
-  (0...arr.size).each do |i|
-    s = 0 
-    (i...arr.size).each do |j|
-      s += arr[j]
-      if s <= @k
-        max = s if s > max
-        return max if max == @k
-      end
-    end
+  # require 'sorted_set'
+  # set = SortedSet.new([])
+  require 'set'
+  set = Set.new([])
+  # https://github.com/ruby/set/pull/2
+  # https://stackoverflow.com/questions/68208165/why-sortedset-was-removed-from-stdlib-in-ruby-3-0
+  sum = 0
+  arr.each do |x|
+    sum += x
+    set.add(sum)
   end
-  return max
+  ans = set.to_a.sort.reverse.bsearch{|x| x <= @k}
+  ans1 = old(arr) 
+  p @k
+  p arr
+  p set.to_a.sort.reverse
+  p [ans,ans1]
+  ans
+end 
+
+def old(arr)
+  max = -Float::INFINITY
+    (0...arr.size).each do |i|
+      s = 0 
+      (i...arr.size).each do |j|
+        s += arr[j]
+        if s <= @k
+          max = s if s > max
+          return max if max == @k
+        end
+        end
+      end
+    max
 end
