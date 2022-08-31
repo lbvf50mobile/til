771ae8464816920f17ad7e762c4d8d62f18247a4
Dmitry <lbvf50.mobile@gmail.com>
2022-09-01 01:52:38 +0700

    1325. Leetcode: 417. Pacific Atlantic Water Flow. (tle after dp 2) Thanks God!

--- 20220831_Wednesday/20220831.rb
+++ 20220831_Wednesday/20220831.rb
@@ -14,15 +14,13 @@ def pacific_atlantic(heights)
   answer = []
   (0...@h.size).each do |i|
     (0...@h[0].size).each do |j|
-      @v = get_visited
-      # @v[i][j] = true # Error always break here.
-      # Because in DFS. Reject visited places.
-      # Error here. Instad of Infinity pass curent value.
-      # Always return 0.
-      tmp = dfs(i,j, Float::INFINITY) # Water come frome the sky. 
-      @dp[i][j] = tmp
-      # The sky is a hightes point.
-      # p [i,j,tmp] if @d
+      if @dp[i][j]
+        tmp = @dp[i][j]
+      else
+        @v = get_visited
+        tmp = dfs(i,j, Float::INFINITY) # Water come frome the sky. 
+        @dp[i][j] = tmp
+      end
       answer.push([i,j]) if 3 == tmp 
     end
   end
@@ -41,16 +39,12 @@ def dfs(i,j,value)
   return 0 if @h[i][j] > value
   return 0 if @v[i][j]
   return @dp[i][j] if @dp[i][j]
-  # p [i,j,value, "dfs"] if @d
   val = @h[i][j]
   answer = 0
   @v[i][j] = true
-  # Here forget a comma. Error.
   [[i+1,j],[i-1,j],[i,j+1],[i,j-1]].each do |ii,jj|
-    # p [ii,jj,val, "dfs loop"] if @d
     answer |= dfs(ii,jj,val)
   end
-  # answer = 0 # One more error. Always 0.
   return answer
 end
 
