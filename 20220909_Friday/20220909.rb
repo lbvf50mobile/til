8e55b0d24e7018d00cff175e673b80c90cd5fca4
Dmitry <lbvf50.mobile@gmail.com>
2022-09-10 01:06:21 +0700

    1334. Leetcode: 1996. The Number of Weak Characters in the Game. (article and soltuion) Thanks God!

--- 20220909_Friday/20220909.rb
+++ 20220909_Friday/20220909.rb
@@ -1,15 +1,20 @@
 # Leetcode: 1996. The Number of Weak Characters in the Game.
 # https://leetcode.com/problems/the-number-of-weak-characters-in-the-game/
+# = = = = = = = = = = = = = =
+# Accepted.
+# Thanks God, Jesus Christ!
+# = = = = = = = = = = = = = =
+# Runtime: 1569 ms, faster than 100.00% of Ruby online submissions for The Number of Weak Characters in the Game.
+# Memory Usage: 225.3 MB, less than 100.00% of Ruby online submissions for The Number of Weak Characters in the Game.
 # @param {Integer[][]} properties
 # @return {Integer}
-# [[1,1],[2,1],[2,2],[1,2]]
-# error
 def number_of_weak_characters(properties)
   # Based on:
   # https://leetcode.com/problems/the-number-of-weak-characters-in-the-game/discuss/1452696/O(n)
   answer, max = 0, -Float::INFINITY
-  properties.sort{|a,b| a[0] <=> b[0]}
-    .reverse
+  # Attack => DECS;
+  # Defence => ASC;
+  properties.sort{|a,b| a[0] == b[0] ? (a[1] <=> b[1]) : (b[0] <=> a[0])}
     .each do |_,defence|
       answer += 1 if defence < max
       max = [defence,max].max
