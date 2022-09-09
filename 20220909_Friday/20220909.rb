7859b008c69a8a185c4cb05a8f25b48b2aa5aac9
Dmitry <lbvf50.mobile@gmail.com>
2022-09-09 23:52:59 +0700

    1334. Leetcode: 1996. The Number of Weak Characters in the Game. (still in action) Thanks God!

--- 20220909_Friday/20220909.rb
+++ 20220909_Friday/20220909.rb
@@ -2,27 +2,17 @@
 # https://leetcode.com/problems/the-number-of-weak-characters-in-the-game/
 # @param {Integer[][]} properties
 # @return {Integer}
+# [[1,1],[2,1],[2,2],[1,2]]
+# error
 def number_of_weak_characters(properties)
-  pr = properties.sort{|a,b| compare(a,b)}
-  groups = [1]
-  (1...pr.size).each do |j|
-    a = pr[j-1]
-    b = pr[j]
-    if -1 == compare(a,b)
-      groups.push(1) 
-    else
-      groups[groups.size-1] += 1
+  # Based on:
+  # https://leetcode.com/problems/the-number-of-weak-characters-in-the-game/discuss/1452696/O(n)
+  answer, max = 0, -Float::INFINITY
+  properties.sort{|a,b| a[0] <=> b[0]}
+    .reverse
+    .each do |_,defence|
+      answer += 1 if defence < max
+      max = [defence,max].max
     end
-  end
-  groups[0...-1].sum
-end
-
-def compare(a,b)
-  if a[0] > b[0] && a[1] > b[1]
-    1
-  elsif b[0] > a[0] && b[1] > a[1]
-    -1
-  else
-    0
-  end
+  answer
 end
