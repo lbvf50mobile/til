9afbc6f790dec2c76285b7829cd57472285f626f
Dmitry <lbvf50.mobile@gmail.com>
2022-09-16 02:13:25 +0700

    1340. Leetcode: 2007. Find Original Array From Doubled Array. (article and solution) Thanks God!

--- 20220915_Thursday/20220915.rb
+++ 20220915_Thursday/20220915.rb
@@ -1,25 +1,29 @@
 # Leetcode: 2007. Find Original Array From Doubled Array.
 # https://leetcode.com/problems/find-original-array-from-doubled-array/
+# = = = = = = = = = = = = = =
+# Accepted.
+# Thanks God, Jesus Christ!
+# = = = = = = = = = = = = = =
+# Runtime: 1005 ms, faster than 100.00% of Ruby online submissions for Find Original Array From Doubled Array.
+# Memory Usage: 234.2 MB, less than 100.00% of Ruby online submissions for Find Original Array From Doubled Array.
+# Completion Streak: 444 Days.
 # @param {Integer[]} changed
 # @return {Integer[]}
 def find_original_array(changed)
   return [] if changed.size.odd?
-  exclude = {}
-  require 'set'
-  values = Set.new(changed)
-  zero_counter = 0
+  counter = changed.tally
   answer = [] 
-  changed.sort.each do |x|
+  changed.uniq.sort.each do |x|
     if 0 == x
-      zero_counter += 1
+      ;
     else
-      next if exclude[x]
-      if values.include?(x*2)
-        exclude[x*2] = true
-        answer.push(x)
+      if counter[x*2] && counter[x*2] >= counter[x] 
+        answer.push(*([x]*counter[x]))
+        counter[x*2] -= counter[x]
       end
     end
   end
+  zero_counter = counter[0] || 0
   if zero_counter.even? 
     answer = answer + ([0]*(zero_counter/2))
   end
