bff3e24ab98f520ebcba55d93d124fbb23a274c9
Dmitry <lbvf50.mobile@gmail.com>
2022-09-20 23:53:26 +0700

    1345. Leetcode: 718. Maximum Length of Repeated Subarray. (article and solution) Thanks God!

--- 20220920_Tuesday/20220920.rb
+++ 20220920_Tuesday/20220920.rb
@@ -6,26 +6,16 @@
 # TLE.
 def find_length(nums1, nums2)
   @n1, @n2 = nums1, nums2
-  @dp = Array.new(nums1.size).map{ Array.new(nums2.size)}
-  @max = 0
-  (0...nums1.size).each do |i|
-    (0...nums2.size).each do |j|
-      next if @dp[i][j]
-      rec(i,j)
+  @dp = Array.new(nums1.size+1).map{ Array.new(nums2.size+1,0)}
+  max = 0
+  (0...@n1.size).reverse_each do |i|
+    (0...@n2.size).reverse_each do |j|
+      if nums1[i] == nums2[j]
+        @dp[i][j] = (@dp[i+1][j+1] + 1)
+        max = @dp[i][j] if max < @dp[i][j]
+      end
     end
   end
-  @max
-end
-
-def rec(i,j)
-  return 0 if ! (i.between?(0,@n1.size-1) && j.between?(0,@n2.size-1))
-  return @dp[i][j] if @dp[i][j]
-  if @n1[i] != @n2[j]
-    @dp[i][j] = 0
-  else
-    @dp[i][j] = 1 + rec(i+1, j+1)
-  end
-  @max = @dp[i][j] if @dp[i][j] > @max
-  return @dp[i][j]
+  max
 end
 
