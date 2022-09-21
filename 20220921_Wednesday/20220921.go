068c8a3c7b412acd7e7401691b0cff10d1d4b625
Dmitry <lbvf50.mobile@gmail.com>
2022-09-22 02:21:34 +0700

    1346. Leetcode: 985. Sum of Even Numbers After Queries. (optimized go soltuion) Thanks God!

--- 20220921_Wednesday/20220921.go
+++ 20220921_Wednesday/20220921.go
@@ -4,10 +4,10 @@
 // Accepted.
 // Thanks God, Jesus Christ!
 // = = = = = = = = = = = = = = 
-// Runtime: 180 ms, faster than 50.00% of Go online submissions for Sum of Even Numbers After Queries.
-// Memory Usage: 8.8 MB, less than 7.14% of Go online submissions for Sum of Even Numbers After Queries
+// Runtime: 179 ms, faster than 50.00% of Go online submissions for Sum of Even Numbers After Queries.
+// Memory Usage: 7.6 MB, less than 64.29% of Go online submissions for Sum of Even Numbers After Queries
 func sumEvenAfterQueries(nums []int, queries [][]int) []int {
-  var answer []int  
+  answer := make([]int, len(queries))
   total := 0
   // Iterate over nums and get sum of even values.
   for _, val := range nums {
@@ -15,9 +15,9 @@ func sumEvenAfterQueries(nums []int, queries [][]int) []int {
       total += val
     }
   }
-  // Iterate over quereis.
+  // Iterate over queries and calculate new value of the total.
   var  value, index int
-  for _, pair := range queries {
+  for query_i, pair := range queries {
     value, index = pair[0], pair[1]
     if 0 == nums[index]%2 {
       total -= nums[index]
@@ -26,7 +26,7 @@ func sumEvenAfterQueries(nums []int, queries [][]int) []int {
     if 0 == nums[index]%2 {
       total += nums[index]
     }
-    answer = append(answer, total)
+    answer[query_i] = total
   }
   return answer
 }
