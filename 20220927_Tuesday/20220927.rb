4efc9067a5600c8f083fb7daf530c2d5e36dbf01
Dmitry <lbvf50.mobile@gmail.com>
2022-09-28 00:36:09 +0700

    1351. Leetcode: 838. Push Dominoes. (ruby solution) Thanks God!

--- 20220927_Tuesday/20220927.rb
+++ 20220927_Tuesday/20220927.rb
@@ -1,15 +1,24 @@
 # Leetcode: 838. Push Dominoes.
 # https://leetcode.com/problems/push-dominoes/
+# = = = = = = = = = = = = = =
+# Accepted.
+# Thanks God, Jesus Christ!
+# = = = = = = = = = = = = = =
+# Runtime: 1916 ms, faster than 100.00% of Ruby online submissions for Push Dominoes.
+# Memory Usage: 214.7 MB, less than 100.00% of Ruby online submissions for Push Dominoes.
 # @param {String} dominoes
 # @return {String}
 def push_dominoes(dominoes)
-  @d = dominoes
-  @ans = " " * d.size
-  (0...@d.size).each do |i|
+  @d = false
+  @do = dominoes
+  @ans = " " * @do.size
+  (0...@do.size).each do |i|
     next if " " != @ans[i]
-    if ?. == @d[i]
+    if ?. == @do[i]
       c,j = find_r(i+1)
+      puts "[#{i},#{j}] [.,#{c}] Dot." if @d
       if ?E == c
+        # All dots till the end.
         color_with_dots(i,j)
       elsif ?L == c
         # All fall left including j.
@@ -20,10 +29,14 @@ def push_dominoes(dominoes)
       else
         raise "Start dot, unknown end."
       end
-    elsif ?L == @d[i]
+      puts @ans if @d
+    elsif ?L == @do[i]
+      puts "[#{i}] [#{c}] L." if @d
       @ans[i] = ?L
-    elsif ?R == @d[i]
+      puts @ans if @d
+    elsif ?R == @do[i]
       c,j = find_r(i+1)
+      puts "[#{i},#{j}] [.,#{c}] R." if @d
       if ?E == c
         # Fall right till the end.
         color_with_r(i,j)
@@ -36,6 +49,7 @@ def push_dominoes(dominoes)
       else
         raise "Start R, unknown end."
       end
+      puts @ans if @d
     else
       raise "Unkonws Start."
     end
@@ -44,12 +58,12 @@ def push_dominoes(dominoes)
 end
 
 def find_r(i)
-  (i...@d.size).each do |j| 
-    c = @d[j]
-    [?L, j] if ?L == c
-    [?R, j] if ?R == c
+  (i...@do.size).each do |j| 
+    c = @do[j]
+    return [?L, j] if ?L == c
+    return [?R, j] if ?R == c
   end
-  [?E, @d.size-1]
+  [?E, @do.size-1]
 end
 
 def color_with_dots(i,j)
@@ -70,11 +84,13 @@ end
 def color_half(i,j)
   l = j-i+1
   half = l/2
+  # Several erros fixed. Left part R, Right part L.
   if l.even?
-    (i...(i+half)).each{|k| @ans[k] = ?L}
-    (i+half..j).each{|k| @ans[k] = ?R}
+    (i...(i+half)).each{|k| @ans[k] = ?R}
+    (i+half..j).each{|k| @ans[k] = ?L}
   else
-    (i...(i+half)).each{|k| @ans[k] = ?L}
-    (i+half+1..j).each{|k| @ans[k] = ?R}
+    (i...(i+half)).each{|k| @ans[k] = ?R}
+    @ans[i+half] = ?. # Fixed error. Must be dot in the middle.
+    (i+half+1..j).each{|k| @ans[k] = ?L}
   end
 end
