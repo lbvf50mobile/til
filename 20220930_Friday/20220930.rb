96a48df85dfa2f4b8da08da23b39a58dbd69d808
Dmitry <lbvf50.mobile@gmail.com>
2022-09-30 23:24:25 +0700

    1354. 218. The Skyline Problem. (in action) Thanks God!

---
+++ 20220930_Friday/20220930.rb
@@ -0,0 +1,7 @@
+# Leetcode: 218. The Skyline Problem.
+# https://leetcode.com/problems/the-skyline-problem/
+# @param {Integer[][]} buildings
+# @return {Integer[][]}
+def get_skyline(buildings)
+    
+end
