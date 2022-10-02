f9fdbf28a6153a7c3c32ebd4182a8c4a73e0ae2e
Dmitry <lbvf50.mobile@gmail.com>
2022-10-02 23:52:54 +0700

    1356. Leetcode: 1155. Number of Dice Rolls With Target Sum. (in action) Thanks God!

---
+++ 20221002_Sunday/20221002.rb
@@ -0,0 +1,9 @@
+# Leetcode: 1155. Number of Dice Rolls With Target Sum.
+# https://leetcode.com/problems/number-of-dice-rolls-with-target-sum/
+# @param {Integer} n
+# @param {Integer} k
+# @param {Integer} target
+# @return {Integer}
+def num_rolls_to_target(n, k, target)
+    
+end
