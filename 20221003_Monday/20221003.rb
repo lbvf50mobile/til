2f27f6dd92490a198070cd76308aef83d6157fb0
Dmitry <lbvf50.mobile@gmail.com>
2022-10-03 23:48:57 +0700

    1357. Leetcode: 1578. Minimum Time to Make Rope Colorful. (article and gist) Thanks God!

--- 20221003_Monday/20221003.rb
+++ 20221003_Monday/20221003.rb
@@ -3,27 +3,19 @@
 # @param {String} colors
 # @param {Integer[]} needed_time
 # @return {Integer}
-# TLE.
 def min_cost(colors, needed_time)
-  @d = false
   counter = 1
   sum = needed_time[0]
   max  = needed_time[0]
   answer = 0
   (1...needed_time.size).each do |i|
-    puts "#{colors[i-1]} and #{colors[i]}" if @d
-    puts "sum = #{sum}, max = #{max}, answer = #{answer}" if @d
-
-
     if colors[i] == colors[i-1]
       sum += needed_time[i]
       max = needed_time[i] if needed_time[i] > max
-      puts "Update: sum=#{sum}, max=#{max},answer=#{answer}" if @d
     else
       answer += (sum - max)
       sum = needed_time[i]
       max = needed_time[i]
-      puts "Reset: sum=#{sum}, max=#{max},answer=#{answer}" if @d
     end
   end
   answer += (sum - max)
