aa1e3fd7b54226fa797ed48e42b0aff88e06928f
Dmitry <lbvf50.mobile@gmail.com>
2022-10-07 02:17:11 +0700

    1359. Leetcode: 981. Time Based Key-Value Store. (ruby solution) Thanks God!

--- 20221006_Thursday/20221006.rb
+++ 20221006_Thursday/20221006.rb
@@ -1,5 +1,12 @@
 # Leetcode: 981. Time Based Key-Value Store.
 # https://leetcode.com/problems/time-based-key-value-store/
+# = = = = = = = = = = = = = =
+# Accepted.
+# Thanks God, Jesus Christ!
+# = = = = = = = = = = = = = =
+# Runtime: 377 ms, faster than 100.00% of Ruby online submissions for Time Based Key-Value Store.
+# Memory Usage: 266.4 MB, less than 11.54% of Ruby online submissions for Time Based Key-Value Store.
+#
 class TimeMap
     def initialize()
       @hsh = {}
@@ -14,8 +21,8 @@ class TimeMap
     :rtype: Void
 =end
     def set(key, value, timestamp)
-      @hsh[key] ||= {}
-      @hsh[key][timestamp] = value
+      @hsh[key] ||= [] 
+      @hsh[key].push([timestamp,@hsh[key].size,value])
     end
 
 
@@ -25,10 +32,19 @@ class TimeMap
     :rtype: String
 =end
     def get(key, timestamp)
-      return  @hsh[key][timestamp]
+      arr = @hsh[key]
+      return "" if !arr
+      return "" if arr.first.first > timestamp
+      return arr.last.last if arr.last.first <= timestamp
+      x = arr.bsearch{|t,i,v| t >= timestamp} # First bigger.
+      return "" if x.nil?
+      t,i,v = x
+      if t == timestamp
+        return v
+      else
+        return arr[i-1].last
+      end
     end
-
-
 end
 
 # Your TimeMap object will be instantiated and called as such:
