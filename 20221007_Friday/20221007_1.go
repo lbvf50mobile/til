beb7924aeb0e3adbe96a2632bdb7e891b1873d24
Dmitry <lbvf50.mobile@gmail.com>
2022-10-08 03:22:23 +0700

    1360. Leetcode: 732. My Calendar III. (go solution lc14) Thanks God!

---
+++ 20221007_Friday/20221007_1.go
@@ -0,0 +1,31 @@
+// Leetcode: 14. Longest Common Prefix.
+// https://leetcode.com/problems/longest-common-prefix/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 3 ms, faster than 51.49% of Go online submissions for Longest Common Prefix.
+// Memory Usage: 2.4 MB, less than 54.17% of Go online submissions for Longest Common Prefix.
+func longestCommonPrefix(strs []string) string {
+    answer := ""
+    for i := 0; i <= 200; i += 1{
+      // take char from the first string.
+      var x string
+      if i < len(strs[0]) {
+        x = string(strs[0][i])
+      } else {
+        return answer
+      }
+      // Check rest of the strings have the same char at
+      // the same postion.
+      for j := 1; j < len(strs); j += 1{
+        if i < len(strs[j]) && x == string(strs[j][i]) {
+          ;
+        } else {
+          return answer
+        }
+      }
+      answer += x
+    }
+    return answer
+}
