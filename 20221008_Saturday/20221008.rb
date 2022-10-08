8adbdeaecb2d2501dda0a468776eeb315c9ce796
Dmitry <lbvf50.mobile@gmail.com>
2022-10-08 23:37:32 +0700

    1361. Leetcode: 16. 3Sum Closest. (ruby solution) Thanks God!

--- 20221008_Saturday/20221008.rb
+++ 20221008_Saturday/20221008.rb
@@ -1,10 +1,17 @@
 # Leetcode: 16. 3Sum Closest.
 # https://leetcode.com/problems/3sum-closest/
+# = = = = = = = = = = = = = =
+# Accepted.
+# Thanks God, Jesus Christ!
+# = = = = = = = = = = = = = =
+# Runtime: 1794 ms, faster than 71.74% of Ruby online submissions for 3Sum Closest.
+# Memory Usage: 212.3 MB, less than 84.78% of Ruby online submissions for 3Sum Closest.
 # @param {Integer[]} nums
 # @param {Integer} target
 # @return {Integer}
 # FAIL
 def three_sum_closest(nums, target)
+  nums.sort! # Forget to sort. Fixed.
   ans = nums[0] + nums[1] + nums[2]
   ans_abs = (target - ans).abs
   (0...(nums.size-2)).each do |i|
