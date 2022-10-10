d852a7e87aca5f9e1572f36e14c667650daeffa4
Dmitry <lbvf50.mobile@gmail.com>
2022-10-10 23:49:51 +0700

    1363. Leetcode: 1328. Break a Palindrome. (ruby solution) Thanks God!

--- 20221010_Monday/20221010.rb
+++ 20221010_Monday/20221010.rb
@@ -1,22 +1,26 @@
 # Leetcode: 1328. Break a Palindrome.
 # https://leetcode.com/problems/break-a-palindrome/
+# = = = = = = = = = = = = = =
+# Accepted.
+# Thanks God, Jesus Christ!
+# = = = = = = = = = = = = = =
+# Runtime: 84 ms, faster than 100.00% of Ruby online submissions for Break a Palindrome.
+# Memory Usage: 211 MB, less than 50.00% of Ruby online submissions for Break a Palindrome
+# 2022.10.10 Daily Challenge.
 # @param {String} palindrome
 # @return {String}
 def break_palindrome(palindrome)
+  return "" if  1 == palindrome.size
   pal = palindrome
-  if pal.chars.uniq.join == ?a
-    if 1 == pal.size
-      return ""
-    else
-      return pal[0...-1] + ?b
-    end
-  end
   ans = pal.chars
-  (0...ans.size).each do |i|
+  done = false
+  (0...(ans.size)/2).each do |i|
     char = ans[i]
     next if ?a == char
-    ans[i] = (ans[i].ord - 1).chr
+    ans[i] = ?a
+    done = true
     break
   end
-  ans.join
+  return ans.join if done
+  pal[0...-1]  + ?b
 end
