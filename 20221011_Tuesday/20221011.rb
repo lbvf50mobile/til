8b33f8531c659c264f6ccb35e3a6554b0ee294de
Dmitry <lbvf50.mobile@gmail.com>
2022-10-11 23:49:57 +0700

    1364. Leetcode: 334. Increasing Triplet Subsequence. (in action) Thanks God!

---
+++ 20221011_Tuesday/20221011.rb
@@ -0,0 +1,15 @@
+# Leetcode: 334. Increasing Triplet Subsequence. 
+# https://leetcode.com/problems/increasing-triplet-subsequence/
+# @param {Integer[]} nums
+# @return {Boolean}
+def increasing_triplet(nums)
+  (0...@n.size-2).each do |i|
+    (i+1...@n.size-1).each do |j|
+      (j+1...@n.size).each do |k|
+        return true if @n[i] < @n[j] && @n[j] < @n[k] && @n[i] < @n[k]
+      end
+    end
+  end
+  return false
+end
+
