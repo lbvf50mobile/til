61db191a562a9bd5cae820e26a6eaa88b2b8f582
Dmitry <lbvf50.mobile@gmail.com>
2022-10-13 23:09:07 +0700

    1366. Leetcode: 237. Delete Node in a Linked List. (ruby solution) Thanks God!

--- 20221013_Thursday/20221013.rb
+++ 20221013_Thursday/20221013.rb
@@ -1,5 +1,12 @@
 # Leetcode: 237. Delete Node in a Linked List.
 # https://leetcode.com/problems/delete-node-in-a-linked-list/
+# = = = = = = = = = = = = = =
+# Accepted.
+# Thanks God, Jesus Christ!
+# = = = = = = = = = = = = = =
+# Runtime: 183 ms, faster than 8.00% of Ruby online submissions for Delete Node in a Linked List.
+# Memory Usage: 210.9 MB, less than 40.00% of Ruby online submissions for Delete Node in a Linked List.
+# 2022.10.13 Daily Challenge.
 # Definition for singly-linked list.
 # class ListNode
 #     attr_accessor :val, :next
