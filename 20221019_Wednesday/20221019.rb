a5179b570683bcc4ba2d983670ff3e1588d216d8
Dmitry <lbvf50.mobile@gmail.com>
2022-10-19 18:50:50 +0700

    1372. Leetcode: 692. Top K Frequent Words. (ruby solution) Thanks God!

---
+++ 20221019_Wednesday/20221019.rb
@@ -0,0 +1,15 @@
+# Leetcode: 692. Top K Frequent Words.
+# https://leetcode.com/problems/top-k-frequent-words/
+# = = = = = = = = = = = = = =
+# Accepted.
+# Thanks God, Jesus Christ!
+# = = = = = = = = = = = = = =
+# Runtime: 87 ms, faster than 100.00% of Ruby online submissions for Top K Frequent Words.
+# Memory Usage: 211.2 MB, less than 21.57% of Ruby online submissions for Top K Frequent Words.
+# @param {String[]} words
+# @param {Integer} k
+# @return {String[]}
+def top_k_frequent(words, k)
+  hash = words.tally
+  hash.to_a.sort{|x,y| x[1] == y[1] ? x[0]<=>y[0] : y[1] <=> x[1]}[0...k].map(&:first)
+end
