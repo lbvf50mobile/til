5aa905207d770ae07f446bcc5497a4045b55ac35
Dmitry <lbvf50.mobile@gmail.com>
2022-10-21 02:16:24 +0700

    1373. Leetcode: 12. Integer to Roman. (ruby solution 2) Thanks God!

--- 20221020_Thursday/20221020.rb
+++ 20221020_Thursday/20221020.rb
@@ -4,54 +4,38 @@
 # Accepted.
 # Thanks God, Jesus Christ!
 # = = = = = = = = = = = = = =
-# Runtime: 215 ms, faster than 44.78% of Ruby online submissions for Integer to Roman.
-# Memory Usage: 211.2 MB, less than 15.34% of Ruby online submissions for Integer to Roman.
+# Runtime: 259 ms, faster than 14.72% of Ruby online submissions for Integer to Roman.
+# Memory Usage: 211 MB, less than 39.88% of Ruby online submissions for Integer to Roman.
 # @param {Integer} num
 # @return {String}
 # @param {Integer} num
 # @return {String}
 def int_to_roman(num)
   ans = ""
+  values = [
+    [1000, ?M],
+    [900, "CM"],
+    [500, ?D],
+    [400, "CD"],
+    [100, ?C],
+    [90, "XC"],
+    [50, ?L],
+    [40, "XL"],
+    [10, ?X],
+    [9, "IX"],
+    [5, ?V],
+    [4, "IV"],
+    [1,?I]
+  ]
   while num > 0
-    if 1000 <= num
-      num -= 1000
-      ans += ?M
-    elsif 900 <= num
-      num -= 900
-      ans += 'CM'
-    elsif 500 <= num
-      num -= 500
-      ans += ?D
-    elsif 400 <= num
-      num -= 400
-      ans += 'CD'
-    elsif 100 <= num
-      num -= 100
-      ans += ?C
-    elsif 90 <= num
-      num -= 90
-      ans += 'XC'
-    elsif 50 <= num
-      num -= 50
-      ans += ?L 
-    elsif 40 <= num
-      num -= 40
-      ans += 'XL'
-    elsif 10 <= num
-      num -= 10
-      ans += ?X
-    elsif 9 <= num
-      num -= 9
-      ans += 'IX'
-    elsif 5 <= num
-      num -= 5
-      ans += ?V
-    elsif 4 <= num
-      num -= 4
-      ans += "IV"
-    elsif 1 <= num
-      num -= 1
-      ans += ?I
+    values.each do |n,str|
+      if n <= num
+        num -= n
+        ans += str
+        break # Important!
+      else
+        next
+      end
     end
   end
   return ans
