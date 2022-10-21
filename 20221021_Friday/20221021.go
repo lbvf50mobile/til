0d966dae6742ecee1fe14e6e9e0b4326c840627c
Dmitry <lbvf50.mobile@gmail.com>
2022-10-21 23:37:48 +0700

    1374. Leetcode: 219. Contains Duplicate II. (go solution) Thanks God!

---
+++ 20221021_Friday/20221021.go
@@ -0,0 +1,20 @@
+// Leetcode: 219. Contains Duplicate II.
+// https://leetcode.com/problems/contains-duplicate-ii/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 238 ms, faster than 41.89% of Go online submissions for Contains Duplicate II.
+// Memory Usage: 14.2 MB, less than 18.92% of Go online submissions for Contains Duplicate II.
+// 2022.10.21 Daily Challange.
+func containsNearbyDuplicate(nums []int, k int) bool {
+  hash := make(map[int]int)
+  for j,el := range nums{
+    i, exists := hash[el]
+    if exists && (j-i) <= k {
+      return true
+    }
+    hash[el] = j
+  }
+  return false
+}
