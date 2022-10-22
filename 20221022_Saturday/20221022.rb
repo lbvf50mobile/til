e1ddd4269b283e804e7890fdc3b6996ac493b66f
Dmitry <lbvf50.mobile@gmail.com>
2022-10-22 23:45:44 +0700

    1375. Leetcode: 76. Minimum Window Substring. (in action) Thanks God!

---
+++ 20221022_Saturday/20221022.rb
@@ -0,0 +1,8 @@
+# Leetcode: 76. Minimum Window Substring.
+# https://leetcode.com/problems/minimum-window-substring/
+# @param {String} s
+# @param {String} t
+# @return {String}
+def min_window(s, t)
+    
+end
