c78bb66760fe1dcc15b9ea4e896117d12fabb2c5
Dmitry <lbvf50.mobile@gmail.com>
2022-10-23 01:55:09 +0700

    1375. Leetcode: 76. Minimum Window Substring. (ruby solution) Thanks God!

--- 20221022_Saturday/20221022.rb
+++ 20221022_Saturday/20221022.rb
@@ -1,5 +1,12 @@
 # Leetcode: 76. Minimum Window Substring.
 # https://leetcode.com/problems/minimum-window-substring/
+# = = = = = = = = = = = = = =
+# Accepted.
+# Thanks God, Jesus Christ!
+# = = = = = = = = = = = = = =
+# Runtime: 2752 ms, faster than 5.48% of Ruby online submissions for Minimum Window Substring.
+# Memory Usage: 215.6 MB, less than 30.14% of Ruby online submissions for Minimum Window Substring.
+# 20221022 Daily Challenge.
 # @param {String} s
 # @param {String} t
 # @return {String}
@@ -12,9 +19,11 @@ def min_window(s, t)
   end
   # t cannot be bigger then s.
   return "" if t.size > s.size
-  # if t == s they must be equal.
+  # if t == s they "must be equal".
+  # if t == s they must have eaual Counters.
+  # need to sort s and t. (FIX)
   if t.size == s.size
-    return t if s == t
+    return s if s.chars.sort == t.chars.sort
     return ""
   end
   # Set minimum huge.
@@ -27,12 +36,16 @@ def min_window(s, t)
   # Find i to be in ideal hash.
   while i < s.size && ! @ideal_hash[s[i]]; i += 1; end
   return "" if i == s.size
+  hash = {}
+  hash.default = 0
+  j = i
   hash[s[i]] = 1
   while j < s.size
     # Hash contains all characters?
     is = check(hash)
     if is
-      while i <= j; && ! @ideal_hash[s[i]]; i += 1; end
+      # Skip unused characters.
+      while i <= j && ! @ideal_hash[s[i]]; i += 1; end
       # Update
       if j-i + 1 < min_size
         return s[i..j] if (j-i+1) == t.size
