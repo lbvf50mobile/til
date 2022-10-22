6f5e88ad66ab870327eb7dff5c16f728def9435b
Dmitry <lbvf50.mobile@gmail.com>
2022-10-23 02:39:06 +0700

    1375. Leetcode: 76. Minimum Window Substring. (lc58 add urls) Thanks God!

--- 20221022_Saturday/20221022.go
+++ 20221022_Saturday/20221022.go
@@ -7,6 +7,8 @@
 // Runtime: 0 ms, faster than 100.00% of Go online submissions for Length of Last Word.
 // Memory Usage: 2.1 MB, less than 34.00% of Go online submissions for Length of Last Word.
 // 20221023;
+// https://gist.github.com/lbvf50mobile/cdab719ad843d37385a7bd066a5547c8
+// https://leetcode.com/problems/length-of-last-word/discuss/2733430/Go%3A-Two-consecutive-loops.
 func lengthOfLastWord(s string) int {
   // Find right most non space symbol.
   j := len(s) - 1
