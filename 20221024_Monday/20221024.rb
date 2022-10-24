3884371345ef0dc0e3ef76086905ab7ff3c44019
Dmitry <lbvf50.mobile@gmail.com>
2022-10-25 01:46:44 +0700

    1377. Leetcode: 1239. Maximum Length of a Concatenated String with Unique Characters. (ruby solution) Thanks God!

--- 20221024_Monday/20221024.rb
+++ 20221024_Monday/20221024.rb
@@ -1,48 +1,41 @@
 # Leetcode: 1239. Maximum Length of a Concatenated String with Unique Characters.
 # https://leetcode.com/problems/maximum-length-of-a-concatenated-string-with-unique-characters/
+# = = = = = = = = = = = = = =
+# Accepted.
+# Thanks God, Jesus Christ!
+# = = = = = = = = = = = = = =
+# Runtime: 157 ms, faster than 100.00% of Ruby online submissions for Maximum Length of a Concatenated String with Unique Characters.
+# Memory Usage: 211.2 MB, less than 100.00% of Ruby online submissions for Maximum Length of a Concatenated String with Unique Characters.
+# 20221024 Daily Challenge.
 # @param {String[]} arr
 # @return {Integer}
 def max_length(arr)
-  @d = true
   @max = 0
-  # Filter string with duplicates.
+  # Remove string with duplicate characters.
   @a = arr.select{|x| x.chars.size == x.chars.uniq.size}
   @masks = @a.map{|x| str2mask(x)}
   @lens = @a.map(&:size)
   (0...@a.size).each do |i|
-    dfs(i,0,0,"")
+    dfs(i,0,0)
   end
   return @max
 end
 
-def dfs(i,total_mask, total_size, str)
-  puts "#{i} #{@a[i]}"
+def dfs(i,total_mask, total_size)
   total_mask |= @masks[i]
   total_size += @lens[i]
-  str = str + @a[i]
-  if @max < total_size
-    @max = total_size
-    p [str,total_size] if @d
-  end
+  @max = total_size if @max < total_size
   (i+1...@a.size).each do |j|
     check = 0 == (total_mask & @masks[j])
-    puts "i=#{i} j=#{j} str=#{str} check=#{check.inspect}" if @d
-    puts "#{str_mask(total_mask)} : #{str}" if @d
-    puts "#{str_mask(@masks[j])} : #{@a[j]}" if @d
-    puts "#{str_mask(total_mask & @masks[j])} : #{str + "&" + @a[j]}" if @d
     # Fix! Need to have j instead of i.
-    dfs(j,total_mask, total_size, str) if 0 == (total_mask & @masks[j]) 
+    dfs(j,total_mask, total_size) if 0 == (total_mask & @masks[j]) 
   end
-end
-
-def str_mask(mask)
-  mask.to_s(2).rjust(26,?0)
 end
 
 def str2mask(str)
   mask = 0
   str.chars.each do |x|
-    # Fix! Forget to move one.
+    # Fix! Forget to right shift one.
     mask = mask | (1 << (x.ord - ?a.ord)) # Fix!
   end
   return mask
