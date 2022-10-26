fac759169616ce750c419d9bdbd986a33a9b4fea
Dmitry <lbvf50.mobile@gmail.com>
2022-10-27 02:27:55 +0700

    1379. Leetcode: 523. Continuous Subarray Sum. (ruby solution) Thanks God!

--- 20221026_Wednesday/20221026.rb
+++ 20221026_Wednesday/20221026.rb
@@ -1,12 +1,17 @@
 # Leetcode: 523. Continuous Subarray Sum.
 # https://leetcode.com/problems/continuous-subarray-sum/
+# = = = = = = = = = = = = = =
+# Accepted.
+# Thanks God, Jesus Christ!
+# = = = = = = = = = = = = = =
+# Runtime: 479 ms, faster than 13.79% of Ruby online submissions for Continuous Subarray Sum.
+# Memory Usage: 226 MB, less than 89.66% of Ruby online submissions for Continuous Subarray Sum.
+# 2022.10.26 Daily Challenge.
 # @param {Integer[]} nums
 # @param {Integer} k
 # @return {Boolean}
-# TLE
-# [1,1000000000]
-# 1
 def check_subarray_sum(nums, k)
+  return true if 1 == k && nums.size > 1
   pref_sum = Array.new(nums.size+1,0)
   hash = {}
   hash[0] = 0 
