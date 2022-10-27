2003a618bd8c3b8ed401868f8c7da372eb26983e
Dmitry <lbvf50.mobile@gmail.com>
2022-10-27 21:27:39 +0700

    1380. Leetcode: 835. Image Overlap. (in action 1) Thanks God!

--- 20221027_Thursday/20221027.rb
+++ 20221027_Thursday/20221027.rb
@@ -3,24 +3,23 @@
 # @param {Integer[][]} img1
 # @param {Integer[][]} img2
 # @return {Integer}
-# Fails.
 def largest_overlap(img1, img2)
   @img1, @img2 = img1, img2
   @n = @img1.size
   max = 0
-  # Move upper-left corter.
   (0...@n).each do |i|
     (0...@n).each do |j|
       # [i,j] postioton of img1 corner on img2.
       tmp = upleft(i,j)
       max = tmp if tmp > max
-    end
-  end
-  # Move down-rigth corter.
-  (0...@n).reverse_each do |i|
-    (0...@n).reverse_each do |j|
-      # [i,j] position of img1 corner on img2.
+
+      tmp = upright(i,j)
+      max = tmp if tmp > max
+
       tmp = downright(i,j)
+      max = tmp if tmp > max
+
+      tmp = downleft(i,j)
       max = tmp if tmp > max
     end
   end
@@ -41,6 +40,20 @@ def upleft(i,j)
   end
   return ans 
 end
+def upright(i,j)
+  i1, j1 = 0, @n-1 # Always up-right.
+  i2, j2 = i, j
+  di_limit, dj_limit = @n-i, j+1
+  ans = 0
+  (0...di_limit).each do |di|
+    (0...dj_limit).each do |dj|
+      cell1 = @img1[i1+di][j1 - dj]
+      cell2 = @img2[i2+di][j2 - dj]
+      ans += 1 if 1 == cell1 && 2 == cell2
+    end
+  end
+  return ans
+end
 
 def downright(i,j)
   i1, j1 = @n-1, @n-1 # Always bottom-right.
@@ -52,6 +65,21 @@ def downright(i,j)
       cell1 = @img1[i1-di][j1 - dj]
       cell2 = @img2[i2-di][j2 - dj]
       ans += 1 if 1 == cell1 && 1 == cell2
+    end
+  end
+  return ans
+end
+
+def downleft(i,j)
+  i1, j1 = @n-1,0 # Always down-left.
+  i2, j2 = i, j
+  di_limit, dj_limit = i+1, @n - j
+  ans = 0
+  (0...di_limit).each do |di|
+    (0...dj_limit).each do |dj|
+      cell1 = @img1[i1 - di][j1 + dj]
+      cell2 = @img2[i2 - di][j2 + dj]
+      ans += 1 if 1 == cell1 && 2 == cell2
     end
   end
   return ans
