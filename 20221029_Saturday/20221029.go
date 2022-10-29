46faacc8b2ebb5cee5880dea3c68b94a578d4eab
Dmitry <lbvf50.mobile@gmail.com>
2022-10-30 03:10:07 +0700

    1382. Leetcode: 2136. Earliest Possible Day of Full Bloom. (article and solution) Thanks God!

--- 20221029_Saturday/20221029.go
+++ 20221029_Saturday/20221029.go
@@ -1,13 +1,14 @@
 // Leetcode: 2136. Earliest Possible Day of Full Bloom.
 // https://leetcode.com/problems/earliest-possible-day-of-full-bloom/
-// Fails:
-// [3,11,29,4,4,26,26,12,13,10,30,19,27,2,10]
-// [10,13,22,17,18,15,21,11,24,14,18,23,1,30,6]
-// 232 instead 227.
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 283 ms, faster than 92.86% of Go online submissions for Earliest Possible Day of Full Bloom.
+// Memory Usage: 10.3 MB, less than 100.00% of Go online submissions for Earliest Possible Day of Full Bloom.
+// 2022.10.29 Daily Challenge.
 import "sort"
-import "fmt"
 func earliestFullBloom(plantTime []int, growTime []int) int {
-  p := fmt.Println
   // Based on:
   // https://leetcode.com/problems/earliest-possible-day-of-full-bloom/solution/
   current_plant_time := 0
@@ -17,11 +18,11 @@ func earliestFullBloom(plantTime []int, growTime []int) int {
     indices[i] = i
   }
   sort.SliceStable(indices,func (i,j int) bool{
+    // Here was a bug. Fixed. i and j are incices of array.
+    // and in array indices need to use value of this array 
+    // in this case.
     return growTime[indices[i]] > growTime[indices[j]]
   })
-  p(indices)
-  // [2 1 0 4 3 6 5 8 7 11 10 9 13 12 14]
-  // [13, 8, 11, 2, 6, 4, 10, 3, 5, 9, 1, 7, 0, 14, 12]
   for _, i := range indices{
     current_plant_time += plantTime[i]
     result = max(result, current_plant_time + growTime[i])
