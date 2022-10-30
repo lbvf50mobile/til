80e3ac4a208e72bcf3fa660fedd36f2d5577238f
Dmitry <lbvf50.mobile@gmail.com>
2022-10-31 03:34:42 +0700

    1383. Leetcode: 1293. Shortest Path in a Grid with Obstacles Elimination. (urls) Thanks God!

--- 20221030_Sunday/20221030.go
+++ 20221030_Sunday/20221030.go
@@ -7,6 +7,8 @@
 // Runtime: 26 ms, faster than 8.40% of Go online submissions for Concatenation of Array.
 // Memory Usage: 6.4 MB, less than 67.16% of Go online submissions for Concatenation of Array.
 // 2022.10.31 Monday;
+// https://gist.github.com/lbvf50mobile/47ab703cfa5dd18724745f091010276e
+// https://leetcode.com/problems/concatenation-of-array/discuss/2761080/Go%3A-One-loop-two-assignments.
 func getConcatenation(nums []int) []int {
   ans := make([]int,2*len(nums))
   for i,data := range nums {
