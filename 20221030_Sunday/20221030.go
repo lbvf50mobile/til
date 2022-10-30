ef947143189064290057029579fd909df8f3ff36
Dmitry <lbvf50.mobile@gmail.com>
2022-10-31 03:30:31 +0700

    1383. Leetcode: 1293. Shortest Path in a Grid with Obstacles Elimination. (lc1929) Thanks God!

--- 20221030_Sunday/20221030.go
+++ 20221030_Sunday/20221030.go
@@ -1,9 +1,17 @@
-    package main
-
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20221030_Sunday/20221030.go'")
-    }
+// Leetcode: 1929. Concatenation of Array.
+// https://leetcode.com/problems/concatenation-of-array/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 26 ms, faster than 8.40% of Go online submissions for Concatenation of Array.
+// Memory Usage: 6.4 MB, less than 67.16% of Go online submissions for Concatenation of Array.
+// 2022.10.31 Monday;
+func getConcatenation(nums []int) []int {
+  ans := make([]int,2*len(nums))
+  for i,data := range nums {
+    ans[i] = data
+    ans[i+len(nums)] = data
+  }
+  return ans
+}
