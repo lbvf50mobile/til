cb6e00068f9d4cbb717d0195c12d1812bb95d0ee
Dmitry <lbvf50.mobile@gmail.com>
2022-10-30 23:53:45 +0700

    1383. Leetcode: 1293. Shortest Path in a Grid with Obstacles Elimination. (go solution) Thanks God!

---
+++ 20221030_Sunday/20221030.go
@@ -0,0 +1,9 @@
+    package main
+
+    import (
+        "fmt"
+    )
+    
+    func main() {
+        fmt.Println("alias x='go run 20221030_Sunday/20221030.go'")
+    }
---
+++ 20221030_Sunday/20221030.rb
@@ -0,0 +1,40 @@
+# Leetcode: 1293. Shortest Path in a Grid with Obstacles Elimination.
+# https://leetcode.com/problems/shortest-path-in-a-grid-with-obstacles-elimination/
+# = = = = = = = = = = = = = =
+# Accepted.
+# Thanks God, Jesus Christ!
+# = = = = = = = = = = = = = =
+# Runtime: 830 ms, faster than 33.33% of Ruby online submissions for Shortest Path in a Grid with Obstacles Elimination.
+# Memory Usage: 210.9 MB, less than 100.00% of Ruby online submissions for Shortest Path in a Grid with Obstacles Elimination.
+# @param {Integer[][]} grid
+# @param {Integer} k
+# @return {Integer}
+def shortest_path(grid, k)
+  # Based on:
+  # https://leetcode.com/problems/shortest-path-in-a-grid-with-obstacles-elimination/discuss/451832/Python-Short-BFS-Solution
+  rows, cols = grid.size, grid[0].size
+  q = [] # row, col, num of obstacles met so far.
+  q.push([0,0,0])
+  visited = {}
+  visited[[0,0]] = 0
+  steps = 0
+  while ! q.empty?
+    q.size.times do
+      r,c,obs = q.pop()
+      next if obs > k
+      return steps if rows - 1 == r && cols - 1 == c
+      [[r+1,c],[r-1,c],[r,c+1],[r,c-1]].each do |nr,nc|
+        next if ! (nr.between?(0,rows-1) && nc.between?(0,cols-1))
+        nobs = (1 == grid[nr][nc]) ? obs + 1 : obs
+        arr = [nr,nc]
+        # The key is here!
+        if visited[arr].nil? || visited[arr] > nobs
+          visited[arr] = nobs
+          q.unshift([nr,nc,nobs])
+        end
+      end
+    end
+    steps += 1
+  end
+  -1 
+end
