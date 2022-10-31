f17dea96f7b02e9543d40a3032d820a08b8c9235
Dmitry <lbvf50.mobile@gmail.com>
2022-11-01 00:01:30 +0700

    1384. Leetcode: 766. Toeplitz Matrix. (go solution) Thanks God!

--- 20221031_Monday/20221031.rb
+++ 20221031_Monday/20221031.rb
@@ -1,5 +1,12 @@
 # Leetcode: 766. Toeplitz Matrix.
 # https://leetcode.com/problems/toeplitz-matrix/
+# = = = = = = = = = = = = = =
+# Accepted.
+# Thanks God, Jesus Christ!
+# = = = = = = = = = = = = = =
+# Runtime: 88 ms, faster than 81.82% of Ruby online submissions for Toeplitz Matrix.
+# Memory Usage: 211.5 MB, less than 18.18% of Ruby online submissions for Toeplitz Matrix.
+# 2022.10.31 Daily Challenge.
 # @param {Integer[][]} matrix
 # @return {Boolean}
 def is_toeplitz_matrix(matrix)
@@ -7,13 +14,25 @@ def is_toeplitz_matrix(matrix)
   j_start = 0
   (0...rows).each do |i_start|
     previous = nil 
-    (i_start...rows).each do |i|
-      (j_start...cols).each do |j|
-        if previous.nil?
-          previous = matrix[i][j] 
-          return false if previous != matrix[i][j]
-        end
-      end
+    i, j = i_start, j_start
+    while i.between?(0,rows-1) && j.between?(0,cols-1)
+      previous = matrix[i][j] if previous.nil?
+      return false if previous != matrix[i][j]
+      previous = matrix[i][j]
+      i+=1
+      j+=1
+    end
+  end
+  i_start = 0
+  (0...cols).each do |j_start|
+    previous = nil
+    i, j = i_start, j_start
+    while i.between?(0,rows-1) && j.between?(0,cols-1)
+      previous = matrix[i][j] if previous.nil?
+      return false if previous != matrix[i][j]
+      previous = matrix[i][j]
+      i+=1
+      j+=1
     end
   end
   return true
