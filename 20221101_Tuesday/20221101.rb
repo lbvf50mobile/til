96bfac0594507e322719d8efb3f733445a85ced0
Dmitry <lbvf50.mobile@gmail.com>
2022-11-02 00:04:40 +0700

    1385. Leetcode: 1706. Where Will the Ball Fall. (ruby solution) Thanks God!

--- 20221101_Tuesday/20221101.rb
+++ 20221101_Tuesday/20221101.rb
@@ -1,5 +1,12 @@
 # Leetcode: 1706. Where Will the Ball Fall.
 # https://leetcode.com/problems/where-will-the-ball-fall/
+# = = = = = = = = = = = = = =
+# Accepted.
+# Thanks God, Jesus Christ!
+# = = = = = = = = = = = = = =
+# Runtime: 202 ms, faster than 65.85% of Ruby online submissions for Where Will the Ball Fall.
+# Memory Usage: 214.2 MB, less than 14.63% of Ruby online submissions for Where Will the Ball Fall.
+# 2022.11.01 Daily Challenge.
 # @param {Integer[][]} grid
 # @return {Integer[]}
 def find_ball(grid)
@@ -15,12 +22,16 @@ end
 
 def rec(i,j)
   return @dp[i][j] if @dp[i][j]
-  return -1 if 0 == j && -1 == @g[i][j]
+  # Cannot step back.
+  return -1 if 0 == j && -1 == @g[i][j] 
+  # Cannot step forward.
   return -1 if @c - 1 == j && 1 == @g[i][j] 
+  # Cannot step forard.
   return -1 if 1 == @g[i][j] && -1 == @g[i][j+1]
+  # Cannot step back.
   return -1 if -1 == @g[i][j] && 1 == @g[i][j-1]
   if @r - 1 == i
-    @dp[i][j] = j
+    @dp[i][j] = 1 == @g[i][j] ? j + 1 : j - 1
     return @dp[i][j]
   end
   if -1 == @g[i][j]
