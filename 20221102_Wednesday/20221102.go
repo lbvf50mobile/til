bf163a2e08a4f25412b8c8d68779598e2fc83540
Dmitry <lbvf50.mobile@gmail.com>
2022-11-03 02:28:33 +0700

    1386. Leetcode: 433. Minimum Genetic Mutation. (lc26) Thanks God!

--- 20221102_Wednesday/20221102.go
+++ 20221102_Wednesday/20221102.go
@@ -1,9 +1,22 @@
-    package main
-
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20221102_Wednesday/20221102.go'")
+// Leetcode: 26. Remove Duplicates from Sorted Array.
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 20 ms, faster than 38.52% of Go online submissions for Remove Duplicates from Sorted Array.
+// Memory Usage: 4.6 MB, less than 10.58% of Go online submissions for Remove Duplicates from Sorted Array.
+// 2022.11.03 Thursday;
+func removeDuplicates(nums []int) int {
+  if 1 == len(nums){
+    return 1
+  }
+  w,r := 0, 1 // Writer and Reader.
+  for ; r < len(nums); r += 1 {
+    if nums[w] != nums[r] {
+      nums[w+1] = nums[r] 
+      w += 1
     }
+  }
+  return w+1
+}
