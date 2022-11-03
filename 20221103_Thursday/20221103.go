849964f260f735f63aebb62d32ae74aefd99ba5f
Dmitry <lbvf50.mobile@gmail.com>
2022-11-04 02:25:31 +0700

    1387. Leetcode: 2131. Longest Palindrome by Concatenating Two Letter Words. (lc2114) Thanks God!

--- 20221103_Thursday/20221103.go
+++ 20221103_Thursday/20221103.go
@@ -1,9 +1,20 @@
-    package main
-
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20221103_Thursday/20221103.go'")
+// Leetcode: 2114. Maximum Number of Words Found in Sentences.
+// https://leetcode.com/problems/maximum-number-of-words-found-in-sentences/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 2 ms, faster than 92.23% of Go online submissions for Maximum Number of Words Found in Sentences.
+// Memory Usage: 4.2 MB, less than 10.68% of Go online submissions for Maximum Number of Words Found in Sentences.
+// 2022.11.04 Friday.
+import "strings"
+func mostWordsFound(sentences []string) int {
+  max := 0
+  for _, sentence := range sentences {
+    size := len(strings.Split(sentence," "))
+    if max < size {
+      max = size
     }
+  }
+  return max
+}
