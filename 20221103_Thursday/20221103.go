224d78d0588aaa146a7948b2e875c5cf06de4cb5
Dmitry <lbvf50.mobile@gmail.com>
2022-11-04 02:27:41 +0700

    1387. Leetcode: 2131. Longest Palindrome by Concatenating Two Letter Words. (lc2114 and urls) Thanks God!

--- 20221103_Thursday/20221103.go
+++ 20221103_Thursday/20221103.go
@@ -7,6 +7,8 @@
 // Runtime: 2 ms, faster than 92.23% of Go online submissions for Maximum Number of Words Found in Sentences.
 // Memory Usage: 4.2 MB, less than 10.68% of Go online submissions for Maximum Number of Words Found in Sentences.
 // 2022.11.04 Friday.
+// https://gist.github.com/lbvf50mobile/4bd7d42d47aa80cf3afc416919db0e77
+// https://leetcode.com/problems/maximum-number-of-words-found-in-sentences/discuss/2774931/Go%3A-One-loop-and-strings.Split.
 import "strings"
 func mostWordsFound(sentences []string) int {
   max := 0
