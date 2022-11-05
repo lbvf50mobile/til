5d0206f6d8f1ca2fdc7f41e25343851ce3a5dcfa
Dmitry <lbvf50.mobile@gmail.com>
2022-11-06 03:08:12 +0700

    1389. Leetcode: 212. Word Search II. (lc2236 urls) Thnks God!

--- 20221105_Saturday/20221105.go
+++ 20221105_Saturday/20221105.go
@@ -7,6 +7,8 @@
 // Runtime: 6 ms, faster than 13.06% of Go online submissions for Root Equals Sum of Children.
 // Memory Usage: 2.1 MB, less than 71.64% of Go online submissions for Root Equals Sum of Children.
 // 2022.11.06 Saturday.
+// https://gist.github.com/lbvf50mobile/345c113d3dd84457f2b4d690ff58485a
+// https://leetcode.com/problems/root-equals-sum-of-children/discuss/2782421/Go%3A-Compare-root-value-with-sum-of-left-and-right-values.
 /**
  * Definition for a binary tree node.
  * type TreeNode struct {
