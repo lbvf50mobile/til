0848bfe345f96241c3150d769b4919aab1a90e59
Dmitry <lbvf50.mobile@gmail.com>
2022-11-06 00:28:54 +0700

    1389. Leetcode: 212. Word Search II. (debugging log) Thanks God!

--- 20221105_Saturday/20221105.rb
+++ 20221105_Saturday/20221105.rb
@@ -4,6 +4,7 @@
 # @param {String[]} words
 # @return {String[]}
 def find_words(board, words)
+  @d = true
   @tire_head = [Hash.new,nil] # Pointers, word.
   @answer = []
   @b = board
@@ -11,17 +12,22 @@ def find_words(board, words)
     write_in_trie(w)
   end
   @used = set_used
+  @path = [] if @d
+  @answer_path = [] if @d
   (0...board.size).each do |i|
     (0...board[0].size).each do |j|
       backtracking(i,j,@tire_head)
     end
   end
+  p @answer_path if @d
   return @answer
 end
 
 def backtracking(i,j,trie)
   @used[i][j] = true
+  @path.push(@b[i][j] + "(#{i},#{j})") if @d
   @answer.push(trie[1]) if trie[1]
+  @answer_path.push(@path.join) if trie[1] if @d
   [[i+1,j],[i-1,j],[i,j+1],[i,j-1]].each do |ni,nj|
     next if !(ni.between?(0,@b.size-1) && nj.between?(0,@b[0].size-1))
     next if @used[ni][nj]
@@ -29,6 +35,7 @@ def backtracking(i,j,trie)
     backtracking(ni,nj,trie[0][@b[ni][nj]])
   end
   @used[i][j] = false
+  @path.pop if @d
 end
 
 def set_used
