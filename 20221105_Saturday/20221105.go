afb377a6949f7479f6b3c5985e77568f9251dbc2
Dmitry <lbvf50.mobile@gmail.com>
2022-11-06 03:06:12 +0700

    1389. Leetcode: 212. Word Search II. (lc2236) Thnks God!

--- 20221105_Saturday/20221105.go
+++ 20221105_Saturday/20221105.go
@@ -1,9 +1,20 @@
-    package main
-
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20221105_Saturday/20221105.go'")
-    }
+// Leetcode: 2236. Root Equals Sum of Children.
+// https://leetcode.com/problems/root-equals-sum-of-children/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 6 ms, faster than 13.06% of Go online submissions for Root Equals Sum of Children.
+// Memory Usage: 2.1 MB, less than 71.64% of Go online submissions for Root Equals Sum of Children.
+// 2022.11.06 Saturday.
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func checkTree(root *TreeNode) bool {
+  return root.Val == root.Left.Val + root.Right.Val
+}
