e1bdd2fc72d06d353628a35da7ea54747bff2c6a
Dmitry <lbvf50.mobile@gmail.com>
2022-11-09 00:10:56 +0700

    1392. Leetcode: 1544. Make The String Great. (ruby solution) Thanks God!

--- 20221108_Tuesday/20221108.rb
+++ 20221108_Tuesday/20221108.rb
@@ -1,21 +1,35 @@
 # Leetcode: 1544. Make The String Great.
 # https://leetcode.com/problems/make-the-string-great/
+# = = = = = = = = = = = = = =
+# Accepted.
+# Thanks God, Jesus Christ!
+# = = = = = = = = = = = = = =
+# Runtime: 247 ms, faster than 11.11% of Ruby online submissions for Make The String Great.
+# Memory Usage: 211.1 MB, less than 33.33% of Ruby online submissions for Make The String Great.
 # @param {String} s
 # @return {String}
 def make_good(s)
   marks = Array.new(s.size,false)
   while true
-    delete = false
-    (0...(s.size-1)).each do |i|
-      a,b = s[i], s[i+1]
-      p [a,b]
-      if a == a.downcase && b == b.upcase && a == b.downcase
-        marks[i] = true
-        marks[i+1] = true
-        delete = true
+    stop = true
+    prev = -1
+    (0...s.size).each do |i|
+      if ! marks[i]
+        if prev >= 0 
+          a,b = s[prev],s[i]
+          same = a.upcase == b.upcase
+          diff = a != b
+          if same && diff
+            marks[i],marks[prev] = true, true
+            stop = false
+            prev = -1
+            break
+          end
+        end
+        prev = i
       end
     end
-    break
+    break if stop
   end
   answer = []
   (0...marks.size).each do |i|
