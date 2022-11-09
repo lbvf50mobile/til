37328901df37dd5eb2510c97fc333d8b47eae84e
Dmitry <lbvf50.mobile@gmail.com>
2022-11-10 04:10:14 +0700

    1393. Leetcode: 901. Online Stock Span. (go debug) Thanks God!

--- 20221109_Wednesday/20221109.go
+++ 20221109_Wednesday/20221109.go
@@ -1,9 +1,45 @@
-    package main
+// Leetcode: 901. Online Stock Span.
+// https://leetcode.com/problems/online-stock-span/
+import "math"
+import "fmt"
+var p = fmt.Println
+type StockSpanner struct {
+  i int // inedex of last element. Number of an input price.
+  sv []int // stack values.
+  si [] int // stack indices.
+}
 
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20221109_Wednesday/20221109.go'")
-    }
+
+func Constructor() StockSpanner {
+  data := StockSpanner{i:0}
+  data.i = 0
+  data.sv = make([]int,1)
+  data.si = make([]int,1) 
+  data.sv[0] = int(math.Pow10(7))
+  data.si[0] = 0
+  return data
+}
+
+
+func (this *StockSpanner) Next(price int) int {
+ p(price) 
+  this.i += 1
+  last := len(this.sv) - 1
+  for ; 0 < len(this.sv) && this.sv[last] <= price ; last -= 1 {
+    this.sv = this.sv[0:last]
+    this.si = this.si[0:last]
+  }
+  this.sv = append(this.sv,price)
+  this.si = append(this.si,this.i)
+ p(this.sv)
+  s := len(this.si)
+  return  this.i - this.si[s-2]
+  
+}
+
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * obj := Constructor();
+ * param_1 := obj.Next(price);
+ */
