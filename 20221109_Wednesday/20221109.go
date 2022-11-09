06e12fbf6bc8ba29a04846e6df1d6c3b8771a67a
Dmitry <lbvf50.mobile@gmail.com>
2022-11-09 23:50:39 +0700

    1393. Leetcode: 901. Online Stock Span. (in action) Thanks God!

---
+++ 20221109_Wednesday/20221109.go
@@ -0,0 +1,9 @@
+    package main
+
+    import (
+        "fmt"
+    )
+    
+    func main() {
+        fmt.Println("alias x='go run 20221109_Wednesday/20221109.go'")
+    }
---
+++ 20221109_Wednesday/20221109.rb
@@ -0,0 +1,35 @@
+# Leetcode: 901. Online Stock Span.
+# https://leetcode.com/problems/online-stock-span/
+class StockSpanner
+    def initialize()
+      @sv = [] # stack values.
+      @si = [] # stack indices.
+      @i = -1
+    end
+
+
+=begin
+    :type price: Integer
+    :rtype: Integer
+=end
+    def next(price)
+      @i += 1
+      while (!@sv.empty?) && @sv.last <= price
+        @sv.pop()
+        @si.pop()
+      end
+      @sv.push(price)
+      @si.push(@i)
+      if 0 == @i
+        return 1
+      elsif 1 == @sv.size
+        return @i
+      else
+        return @i - @si[-2]
+      end
+    end
+end
+
+# Your StockSpanner object will be instantiated and called as such:
+# obj = StockSpanner.new()
+# param_1 = obj.next(price)
