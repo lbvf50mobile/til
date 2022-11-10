f8d08b411b7f7e043fc405da6bcc1413f634229b
Dmitry <lbvf50.mobile@gmail.com>
2022-11-10 14:25:04 +0700

    1394. Leetcode: 1047. Remove All Adjacent Duplicates In String. (new solution) Thanks God!

--- 20221110_Thursday/20221110.rb
+++ 20221110_Thursday/20221110.rb
@@ -4,16 +4,21 @@
 # Accepted.
 # Thanks God, Jesus Christ!
 # = = = = = = = = = = = = = =
-# Runtime: 536 ms, faster than 21.43% of Ruby online submissions for Remove All Adjacent Duplicates In String.
-# Memory Usage: 219.6 MB, less than 10.71% of Ruby online submissions for Remove All Adjacent Duplicates In String.
+# Runtime: 474 ms, faster than 53.57% of Ruby online submissions for Remove All Adjacent Duplicates In String.
+# Memory Usage: 218.5 MB, less than 10.71% of Ruby online submissions for Remove All Adjacent Duplicates In String.
 # 2022.11.10 Daily Challenge.
 # @param {String} s
 # @return {String}
 def remove_duplicates(s)
   ans = []
   s.chars.each do |c|
-    ans.push(c)
-    ans.pop(2) if ans.size >= 2 && ans[-2] == ans[-1]
+    # No need to push.
+    # It is possible to compare.
+    if ans.last == c
+      ans.pop
+    else
+      ans.push(c)
+    end
   end
   return ans.join
 end
