0f1bd3537755791e67b21a6eecdf793409d2b985
Dmitry <lbvf50.mobile@gmail.com>
2022-11-10 14:52:33 +0700

    1394. Leetcode: 1047. Remove All Adjacent Duplicates In String. (article and solution) Thanks God!

--- 20221110_Thursday/20221110.go
+++ 20221110_Thursday/20221110.go
@@ -1,9 +1,20 @@
-    package main
-
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20221110_Thursday/20221110.go'")
+// Leetcode: 1047. Remove All Adjacent Duplicates In String.
+// https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 15 ms, faster than 46.73% of Go online submissions for Remove All Adjacent Duplicates In String.
+// Memory Usage: 6.7 MB, less than 61.68% of Go online submissions for Remove All Adjacent Duplicates In String.
+// 2022.10.11 Daily Challenge.
+func removeDuplicates(s string) string {
+  ans := make([]rune, 0, len(s))
+  for _,c := range s {
+    if 0 < len(ans) && ans[len(ans)-1] == c{
+      ans = ans[0:len(ans)-1]
+    } else {
+      ans = append(ans,rune(c))
     }
+  }
+  return string(ans)
+}
