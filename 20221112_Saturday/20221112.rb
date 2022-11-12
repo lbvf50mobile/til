f2b81ef4884a7bd9def1983673dd4cc0429889b6
Dmitry <lbvf50.mobile@gmail.com>
2022-11-13 00:04:20 +0700

    1396. Leetcode: 295. Find Median from Data Stream. (ruby solution) Thanks God!

--- 20221112_Saturday/20221112.rb
+++ 20221112_Saturday/20221112.rb
@@ -1,9 +1,19 @@
 # Leetcode: 295. Find Median from Data Stream.
 # https://leetcode.com/problems/find-median-from-data-stream/
+# = = = = = = = = = = = = = =
+# Accepted.
+# Thanks God, Jesus Christ!
+# = = = = = = = = = = = = = =
+# Runtime: 1417 ms, faster than 84.62% of Ruby online submissions for Find Median from Data Stream.
+# Memory Usage: 229.2 MB, less than 15.38% of Ruby online submissions for Find Median from Data Stream.
+# 2022.11.12 Daily Challenge.
+# Based on:
+# https://leetcode.com/problems/find-median-from-data-stream/discuss/2807068/One-liner-fast-and-simple-solution-using-binary-search
 class MedianFinder
+  # Based on:
+  # https://leetcode.com/problems/find-median-from-data-stream/discuss/2807068/One-liner-fast-and-simple-solution-using-binary-search
     def initialize()
-      @sum = 0
-      @counter = 0
+      @x = []
     end
 
 
@@ -12,8 +22,12 @@ class MedianFinder
     :rtype: Void
 =end
     def add_num(num)
-      @sum += num
-      @counter += 1
+      i = @x.bsearch_index{|n| num <= n}
+      if i
+        @x.insert(i,num)
+      else
+        @x.push(num)
+      end
     end
 
 
@@ -21,8 +35,12 @@ class MedianFinder
     :rtype: Float
 =end
     def find_median()
-      @sum.fdiv(@counter)
-        
+      n = @x.size
+      if n.odd?
+        @x[n/2]
+      else
+        (@x[n/2] + @x[(n/2) - 1])/2.0
+      end
     end
 
 
