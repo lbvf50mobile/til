e37eda2b90f3b3c92f061c89bac880eb1b612273
Dmitry <lbvf50.mobile@gmail.com>
2022-11-13 03:16:01 +0700

    1396. Leetcode: 295. Find Median from Data Stream. (lc70) Thanks God!

--- 20221112_Saturday/20221112.go
+++ 20221112_Saturday/20221112.go
@@ -1,9 +1,19 @@
-    package main
-
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20221112_Saturday/20221112.go'")
-    }
+// Leetcode: 70. Climbing Stairs.
+// https://leetcode.com/problems/climbing-stairs/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 2 ms, faster than 39.94% of Go online submissions for Climbing Stairs.
+// Memory Usage: 1.9 MB, less than 90.47% of Go online submissions for Climbing Stairs.
+// 2022.11.13 Sunday.
+func climbStairs(n int) int {
+  a,b,c := 1, 1, 0
+  for i := 1; i < n; i += 1 {
+    b += a
+    c += a
+    a,b = b,c
+    c = 0
+  }
+  return a
+}
