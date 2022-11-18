31e4db9334de26a07a11a79d9cc099d2f282acb9
Dmitry <lbvf50.mobile@gmail.com>
2022-11-18 23:23:36 +0700

    1402. Leetcode: 263. Ugly Number. (go solution) Thanks God!

--- 20221118_Friday/20221118.go
+++ 20221118_Friday/20221118.go
@@ -1,9 +1,25 @@
-    package main
+// Leetcode: 263. Ugly Number.
+// https://leetcode.com/problems/ugly-number/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 0 ms, faster than 100.00% of Go online submissions for Ugly Number.
+// Memory Usage: 2.1 MB, less than 73.13% of Go online submissions for Ugly Number.
+// 2022.11.18 Daily Challenge.
+func isUgly(n int) bool {
+  if 0 >= n {
+    return false
+  }
+  n = div_till(n,2)
+  n = div_till(n,3)
+  n = div_till(n,5)
+  return 1 == n
+}
 
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20221118_Friday/20221118.go'")
-    }
+func div_till(dv,dz int) int {
+  for ; 0 == dv % dz ; {
+    dv /= dz
+  }
+  return dv
+}
