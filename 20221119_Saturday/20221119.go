2578ba2af3e5fff0621c4d86511184e1089a0453
Dmitry <lbvf50.mobile@gmail.com>
2022-11-20 03:31:10 +0700

    1403. Leetcode: 587. Erect the Fence. (lc141 urls) Thanks God!

--- 20221119_Saturday/20221119.go
+++ 20221119_Saturday/20221119.go
@@ -7,6 +7,8 @@
 // Runtime: 19 ms, faster than 31.86% of Go online submissions for Linked List Cycle.
 // Memory Usage: 6.7 MB, less than 5.03% of Go online submissions for Linked List Cycle.
 // 2022.11.20 Sunday.
+// https://gist.github.com/lbvf50mobile/9202e976411952b2f12c030a72ef1902
+// https://leetcode.com/problems/linked-list-cycle/discuss/2831175/Go%3A-Hash-Map-and-Loop.
 /**
  * Definition for singly-linked list.
  * type ListNode struct {
