b0f71fed7ebc2f4df8f8fe2f81b8ff75c619532f
Dmitry <lbvf50.mobile@gmail.com>
2022-11-21 03:17:15 +0700

    1404. Leetcode: 224. Basic Calculator. (+1 ruby solution) Thanks God!

--- 20221120_Sunday/20221120.rb
+++ 20221120_Sunday/20221120.rb
@@ -1,44 +1,44 @@
 # Leetcode: 224. Basic Calculator.
 # https://leetcode.com/problems/basic-calculator/
 # = = = = = = = = = = = = = =
-# Accepted.
 # Thanks God, Jesus Christ!
 # = = = = = = = = = = = = = =
-# Runtime: 697 ms, faster than 10.00% of Ruby online submissions for Basic Calculator.
-# Memory Usage: 212.2 MB, less than 40.00% of Ruby online submissions for Basic Calculator.
+# Runtime: 283 ms, faster than 54.55% of Ruby online submissions for Basic Calculator.
+# Memory Usage: 225.6 MB, less than 9.09% of Ruby online submissions for Basic Calculator.
 # 2022.11.20 Daily Challenge.
 # @param {String} s
 # @return {Integer}
 def calculate(s)
-  # Based on:
-  # https://leetcode.com/problems/basic-calculator/discuss/62344/Easy-18-lines-C%2B%2B-16-lines-Python
-  # Original code solution do not pass: 
-  # But with: 
-  # https://leetcode.com/problems/basic-calculator/discuss/62344/Easy-18-lines-C++-16-lines-Python/1674262
-  # Added comment:
-  # https://leetcode.com/problems/basic-calculator/discuss/62344/Easy-18-lines-C++-16-lines-Python/1690945
+  # Signs. outer and current.
+  # - == -1
+  # + == 1
+  outer = [1] # start with +.
+  current = 1 # Begin as +;
+  number = 0
   total = 0
-  stack = [1,1]
-  i = 0
-  while i < s.size
-    c = s[i]
-    if ?0 <= c
-      n = 0
-      while i < s.size && ?0 <= s[i]
-        n = n*10 + s[i].to_i
-        i += 1
-      end
-      total += (stack.pop) * n
-      i -= 1
-      # https://leetcode.com/problems/basic-calculator/discuss/62344/Easy-18-lines-C++-16-lines-Python/1674262
-    elsif ?( == c && ?- == s[i+1]
-      ;
+  s.chars.each_with_index do |c,i|
+    if ?+ == c
+      current = outer.last
+    elsif ?- == c
+      current = - outer.last
+    elsif ?( == c
+      outer.push(current)
     elsif ?) == c
-      stack.pop
-    elsif ' ' != c
-      stack.push(stack.last * (?- == c ? -1 : 1))
+      outer.pop
+      current = outer.last
+    elsif " " == c
+      ;
+    elsif ?0 <= c && c <= ?9
+      number = number*10 + c.to_i
+      # Last digit.
+      if s.size - 1 == i || (! s[i+1].ord.between?(?0.ord,?9.ord)) 
+        total += current * number
+        current = outer.last
+        number = 0
+      end
+    else
+      raise "Unknown position."
     end
-    i += 1
   end
   return total
 end
