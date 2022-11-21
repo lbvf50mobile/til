e37cb220fba4adfe641f0ad7d375b56e780625b3
Dmitry <lbvf50.mobile@gmail.com>
2022-11-22 03:10:34 +0700

    1405. Leetcode: 1926. Nearest Exit from Entrance in Maze. (go in action) Thanks God!

--- 20221121_Monday/20221121.go
+++ 20221121_Monday/20221121.go
@@ -1,9 +1,39 @@
-    package main
+// Leetcode: 1926. Nearest Exit from Entrance in Maze.
+// https://leetcode.com/problems/nearest-exit-from-entrance-in-maze/
+type element struct{
+  i int
+  j int
+  l int
+}
+type queue struct {
+  data []*element
+}
+func (q queue) init(){
+  q.data = make([]*element,0)
+}
+func (q queue) push(x *element){
+  q.data = append(q.data,x)
+}
+func (q queue) shift() *element{
+  x := q.data[0]
+  q.data =q.data[1:]
+  return x
+}
+func (q queue) size() int{
+  return len(q.data)
+}
+var n, m int
+var used [][]bool
 
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20221121_Monday/20221121.go'")
-    }
+func nearestExit(maze [][]byte, entrance []int) int {
+  n = len(maze)
+  m = len(maze[0])
+  init_used()
+  return 1
+}
+func init_used(){
+  used = make([][]bool,n)
+  for i := range used {
+    used[i] = make([]bool,m)
+  }
+}
