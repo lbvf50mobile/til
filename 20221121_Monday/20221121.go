73d3c74c484d35a57559dd73ba310b71274d1f71
Dmitry <lbvf50.mobile@gmail.com>
2022-11-22 03:26:03 +0700

    1405. Leetcode: 1926. Nearest Exit from Entrance in Maze. (go1) Thanks God!

--- 20221121_Monday/20221121.go
+++ 20221121_Monday/20221121.go
@@ -19,8 +19,8 @@ func (q queue) shift() *element{
   q.data =q.data[1:]
   return x
 }
-func (q queue) size() int{
-  return len(q.data)
+func (q queue) empty() bool{
+  return 0 != len(q.data)
 }
 var n, m int
 var used [][]bool
@@ -28,12 +28,45 @@ var used [][]bool
 func nearestExit(maze [][]byte, entrance []int) int {
   n = len(maze)
   m = len(maze[0])
+  delta := [][]int{
+    {-1,0},
+    {1,0},
+    {0, -1},
+    {0, 1},
+  }
+  q := queue{}
+  q.init()
   init_used()
-  return 1
+  i,j := entrance[0],entrance[1]
+  used[i][j] = true
+  q.push(&element{i,j,0}
+  for ; ! q.empty() ; {
+    x := q.shift()
+    i,j,l := x.i, x.j, x.l
+    for _,dlt := range delta{
+      di,dj := dlt[0],dlt[1]
+      ni,nj,nl := i+di, j+dj, l+1
+      if out_(ni,nj){
+        if 0 != l { return l }
+      }else{
+        if used[ni][nj] {continue}
+        if '+' == maze[ni][nj] {continue}
+        used[ni][nj] = true
+        q.push(&element{ni,nj,nl})
+      }
+    }
+  }
+  return  -1
 }
 func init_used(){
   used = make([][]bool,n)
   for i := range used {
     used[i] = make([]bool,m)
   }
+}
+func in_(i,j int) bool{
+  return 0 <= i && i < n && 0 <= j && j < m
+}
+func out_(i,j int) bool {
+  return ! in_(i,j)
 }
