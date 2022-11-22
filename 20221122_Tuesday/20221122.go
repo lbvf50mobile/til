b009aabea2f6c90e99fa2a37eb7a282b574e6c9d
Dmitry <lbvf50.mobile@gmail.com>
2022-11-23 02:22:52 +0700

    Day 1406. Leetcode: 279. Perfect Squares. (go solution) Thanks God!

--- 20221122_Tuesday/20221122.go
+++ 20221122_Tuesday/20221122.go
@@ -1,9 +1,31 @@
-    package main
+// Leetcode: 279. Perfect Squares.
+// https://leetcode.com/problems/perfect-squares/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 0 ms, faster than 100.00% of Go online submissions for Perfect Squares.
+// Memory Usage: 2 MB, less than 93.58% of Go online submissions for Perfect Squares.
+// 2022.11.22 Daily Challenge.
+import "math"
+func numSquares(n int) int {
+  // Based on:
+  // https://leetcode.com/problems/perfect-squares/discuss/707526/Python-Fastest-O(sqrt(n))-solution-with-math-explanied.
+  q := sqrt(n)
+  if n == sq(q){ return 1}
+  for j:=0; j <= q; j += 1 {
+    val := n - sq(j) 
+    if  val == sq(sqrt(val)) { return 2}
+  }
+  for ; 0 == n%4 ; { n /= 4 }
+  if 7 == n % 8 { return 4 }
+  return 3
+}
+func sqrt(x int) int{
+  return int(math.Sqrt(float64(x)))
+}
+func sq(x int) int {
+  return x * x
+}
 
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20221122_Tuesday/20221122.go'")
-    }
+
