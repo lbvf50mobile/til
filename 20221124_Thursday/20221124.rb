0503ce28799ba79097d1ae876135cf75a1317754
Dmitry <lbvf50.mobile@gmail.com>
2022-11-25 00:10:03 +0700

    1407. Leetcode: 79. Word Search. (ruby solution) Thanks God!

--- 20221124_Thursday/20221124.rb
+++ 20221124_Thursday/20221124.rb
@@ -1,9 +1,15 @@
 # Leetcode: 79. Word Search.
 # https://leetcode.com/problems/word-search/
+# = = = = = = = = = = = = = =
+# Accepted.
+# Thanks God, Jesus Christ!
+# = = = = = = = = = = = = = =
+# Runtime: 111 ms, faster than 97.78% of Ruby online submissions for Word Search.
+# Memory Usage: 211.1 MB, less than 15.56% of Ruby online submissions for Word Search.
+# 2022.11.24 Daily Challenge.
 # @param {Character[][]} board
 # @param {String} word
 # @return {Boolean}
-# TLE.
 def exist(board, word)
   @b, @w = board, word
   @m,@n = @b.size, @b[0].size
@@ -14,6 +20,11 @@ def exist(board, word)
     return false if !b[c]
     return false if a[c] > b[c]
   end
+  # Trick based on:
+  # https://leetcode.com/problems/word-search/discuss/2844479/Ruby-solution
+  a = @w[0]
+  b = @w[-1]
+  @w.reverse! if @w.match("^#{a}+")[0].size > @w.match("#{b}+$")[0].size
   @used = Array.new(@b.size).map{ Array.new(@b[0].size,false)}
   (0...@b.size).each do |i|
     (0...@b[0].size).each do |j|
