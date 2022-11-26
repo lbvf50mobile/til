504f543b775a5a5b030164c7f186f9a1f9a6a931
Dmitry <lbvf50.mobile@gmail.com>
2022-11-27 03:49:10 +0700

    1409. Leetcode: 1235. Maximum Profit in Job Scheduling. (lc83 urls) Thanks God!

--- 20221126_Saturday/20221126.go
+++ 20221126_Saturday/20221126.go
@@ -7,6 +7,8 @@
 // Runtime: 4 ms, faster than 78.48% of Go online submissions for Remove Duplicates from Sorted List.
 // Memory Usage: 3.1 MB, less than 42.01% of Go online submissions for Remove Duplicates from Sorted List.
 // 2022.11.27 Sunday.
+// https://leetcode.com/problems/remove-duplicates-from-sorted-list/discuss/2851483/Go%3A-Re-point-next-of-prev-if-equal-move-prev-otherwise.
+// https://gist.github.com/lbvf50mobile/743eef407777f601db061b731a5f68a8
 /**
  * Definition for singly-linked list.
  * type ListNode struct {
