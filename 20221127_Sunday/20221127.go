69ae1b911d7000c20898f3148d954badb0fd2a14
Dmitry <lbvf50.mobile@gmail.com>
2022-11-27 23:45:20 +0700

    1410. Leetcode: 1235. Maximum Profit in Job Scheduling. (in action) Thanks God!

---
+++ 20221127_Sunday/20221127.go
@@ -0,0 +1,9 @@
+    package main
+
+    import (
+        "fmt"
+    )
+    
+    func main() {
+        fmt.Println("alias x='go run 20221127_Sunday/20221127.go'")
+    }
---
+++ 20221127_Sunday/20221127.rb
@@ -0,0 +1,8 @@
+# Leetcode: 446. Arithmetic Slices II - Subsequence.
+# https://leetcode.com/problems/arithmetic-slices-ii-subsequence/
+# @param {Integer[]} nums
+# @return {Integer}
+def number_of_arithmetic_slices(nums)
+    
+end
+
