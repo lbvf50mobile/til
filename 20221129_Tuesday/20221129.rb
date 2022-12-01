716720e8ecfe7af26c046ec4be94445f80812705
Dmitry <lbvf50.mobile@gmail.com>
2022-12-01 19:50:04 +0700

    1412. Leetcode: 380. Insert Delete GetRandom O(1). (fixed) Thanks God!

--- 20221129_Tuesday/20221129.rb
+++ 20221129_Tuesday/20221129.rb
@@ -6,11 +6,9 @@
 # = = = = = = = = = = = = = =
 # 2022.11.29 Daily Challenge.
 # 2022.12.01 Updated.
-# Error.
-# ["RandomizedSet","insert","insert","remove","insert","remove","getRandom"]
-# [[],[0],[1],[0],[2],[1],[]]
 class RandomizedSet
     def initialize()
+      @d = true
       @h = {}
       @a = []
     end
@@ -19,9 +17,11 @@ class RandomizedSet
     :rtype: Boolean
 =end
     def insert(val)
+      puts "Insert #{val} in #{@a}." if @d
       return false if @h[val]
       @a.push(val)
       @h[val] = @a.size - 1
+      puts "Now @h[#{val}] = #{@h[val]} in #{@a.inspect} return true." if @d
       return true
     end
 =begin
@@ -29,15 +29,19 @@ class RandomizedSet
     :rtype: Boolean
 =end
     def remove(val)
+      puts "Delte #{val} in #{@a}." if @d
       return false if ! @h[val]
       # Swap in the array last element
       # and elemet to be delted.
       i,j = @h[val], @a.size - 1
-      a,b = val,@h[j]
-      @h[a],@h[b] = @h[b],@h[j]
+      puts "@a[#{i}] = #{@a[i]}, @a[#{j}] = #{@a[j]}." if @d
+      a,b = val,@a[j]
+      @h[a],@h[b] = @h[b],@h[a]
       @a[i],@a[j] = @a[j],@a[i]
+      puts "@a[#{i}] = #{@a[i]}, @a[#{j}] = #{@a[j]}." if @d
       @a.pop
       @h[val] = nil
+      puts "#{@a.inspect} return true." if @d
       return true
     end
 
@@ -46,10 +50,9 @@ class RandomizedSet
     :rtype: Integer
 =end
     def get_random()
+      puts "Random in #{@a.inspect}." if @d
       @a.sample
     end
-
-
 end
 
 # Your RandomizedSet object will be instantiated and called as such:
