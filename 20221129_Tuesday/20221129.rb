a883a26f351df75819e5a25704a9d420091beaeb
Dmitry <lbvf50.mobile@gmail.com>
2022-12-01 19:58:19 +0700

    1412. Leetcode: 380. Insert Delete GetRandom O(1). (ruby solution) Thanks God!

--- 20221129_Tuesday/20221129.rb
+++ 20221129_Tuesday/20221129.rb
@@ -4,11 +4,12 @@
 # Accepted.
 # Thanks God, Jesus Christ!
 # = = = = = = = = = = = = = =
+# Runtime: 368 ms, faster than 100.00% of Ruby online submissions for Insert Delete GetRandom O(1).
+# Memory Usage: 248.5 MB, less than 100.00% of Ruby online submissions for Insert Delete GetRandom O(1).
 # 2022.11.29 Daily Challenge.
 # 2022.12.01 Updated.
 class RandomizedSet
     def initialize()
-      @d = true
       @h = {}
       @a = []
     end
@@ -17,11 +18,9 @@ class RandomizedSet
     :rtype: Boolean
 =end
     def insert(val)
-      puts "Insert #{val} in #{@a}." if @d
       return false if @h[val]
       @a.push(val)
       @h[val] = @a.size - 1
-      puts "Now @h[#{val}] = #{@h[val]} in #{@a.inspect} return true." if @d
       return true
     end
 =begin
@@ -29,19 +28,15 @@ class RandomizedSet
     :rtype: Boolean
 =end
     def remove(val)
-      puts "Delte #{val} in #{@a}." if @d
       return false if ! @h[val]
       # Swap in the array last element
       # and elemet to be delted.
       i,j = @h[val], @a.size - 1
-      puts "@a[#{i}] = #{@a[i]}, @a[#{j}] = #{@a[j]}." if @d
       a,b = val,@a[j]
       @h[a],@h[b] = @h[b],@h[a]
       @a[i],@a[j] = @a[j],@a[i]
-      puts "@a[#{i}] = #{@a[i]}, @a[#{j}] = #{@a[j]}." if @d
       @a.pop
       @h[val] = nil
-      puts "#{@a.inspect} return true." if @d
       return true
     end
 
@@ -50,7 +45,6 @@ class RandomizedSet
     :rtype: Integer
 =end
     def get_random()
-      puts "Random in #{@a.inspect}." if @d
       @a.sample
     end
 end
