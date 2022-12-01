0094efc1084a349d5e912a3558dc4170e701bb1d
Dmitry <lbvf50.mobile@gmail.com>
2022-12-01 23:25:52 +0700

    1414. Leetcode: 1704. Determine if String Halves Are Alike. (go solution) Thanks God!

--- 20221201_Thursday/20221201.go
+++ 20221201_Thursday/20221201.go
@@ -1,9 +1,28 @@
-    package main
+// Leetcode: 1704. Determine if String Halves Are Alike.
+// https://leetcode.com/problems/determine-if-string-halves-are-alike/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 5 ms, faster than 36.67% of Go online submissions for Determine if String Halves Are Alike.
+// Memory Usage: 2.2 MB, less than 81.67% of Go online submissions for Determine if String Halves Are Alike.
+// 2022.12.01 Daily Challenge.
+func halvesAreAlike(s string) bool {
+  i,j := 0, len(s) - 1
+  a,b := 0, 0
+  for ; i < j; i,j = i+1, j-1 {
+    if vowel(s[i]){ a += 1}
+    if vowel(s[j]){ b += 1}
+  }
+  return a == b
+}
 
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20221201_Thursday/20221201.go'")
-    }
+func vowel(s byte) bool {
+  if 'a' == s || 'e' == s || 'i' == s || 'o' == s || 'u' == s {
+    return true
+  }
+  if 'A' == s || 'E' == s || 'I' == s || 'O' == s || 'U' == s {
+    return true
+  }
+  return false
+}
