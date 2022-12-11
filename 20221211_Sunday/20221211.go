0b4b1874b5c53a7fe441f01cbb56491fd1af78c6
Dmitry <lbvf50.mobile@gmail.com>
2022-12-12 02:58:47 +0700

    1424. Leetcode: 124. Binary Tree Maximum Path Sum. (go solution) Thanks God!

--- 20221211_Sunday/20221211.go
+++ 20221211_Sunday/20221211.go
@@ -1,9 +1,38 @@
-    package main
+// Leetcode: 124. Binary Tree Maximum Path Sum.
+// https://leetcode.com/problems/binary-tree-maximum-path-sum/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 18 ms, faster than 85.37% of Go online submissions for Binary Tree Maximum Path Sum.
+// Memory Usage: 7.5 MB, less than 83.62% of Go online submissions for Binary Tree Maximum Path Sum.
+// 2022.12.11 Daily Challenge.
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+var max int
+func maxPathSum(root *TreeNode) int {
+  max = root.Val
+  dfs(root)
+  return max
+}
 
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20221211_Sunday/20221211.go'")
-    }
+func dfs(r *TreeNode) int{
+  if nil == r {
+    return 0
+  }
+  a,b,c := dfs(r.Left),dfs(r.Right), r.Val
+  if a < 0 { a = 0 }
+  if b < 0 { b = 0 }
+  x := a + b + c
+  if max < x  { max = x}
+  ans := a + c
+  if (b + c) > ans { ans = b + c}
+  return ans
+}
+
