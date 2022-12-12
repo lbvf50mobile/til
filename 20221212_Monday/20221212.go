2a3d39f7c3a002b24d9e54fe02ee733a09b2c6c9
Dmitry <lbvf50.mobile@gmail.com>
2022-12-12 23:57:58 +0700

    1425. Leetcode: 70. Climbing Stairs. (go solution) Thanks God!

--- 20221212_Monday/20221212.go
+++ 20221212_Monday/20221212.go
@@ -1,9 +1,16 @@
-    package main
-
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20221212_Monday/20221212.go'")
-    }
+// Leetcode: 70. Climbing Stairs.
+// https://leetcode.com/problems/climbing-stairs/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 0 ms, faster than 100.00% of Go online submissions for Climbing Stairs.
+// Memory Usage: 1.9 MB, less than 89.49% of Go online submissions for Climbing Stairs.
+// 2022.12.12 Daily Challenge.
+func climbStairs(n int) int {
+  a,b,c := 1, 0, 0
+  for i := 1; i <= n; i +=1 {
+    a,b,c = b+a, c+a, 0
+  }
+  return a
+}
