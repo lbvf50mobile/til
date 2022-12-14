8b3e557f4f9e6afc1a0c0216dc40b6a5bdcb50cd
Dmitry <lbvf50.mobile@gmail.com>
2022-12-14 23:08:51 +0700

    1412. Leetcode: 380. Insert Delete GetRandom O(1). (go solution) Thanks God!

--- 20221129_Tuesday/20221129.go
+++ 20221129_Tuesday/20221129.go
@@ -1,9 +1,50 @@
-    package main
+type RandomizedSet struct {
+  n int
+  a []int
+  h map[int]int
+}
 
-    import (
-        "fmt"
-    )
+
+func Constructor() RandomizedSet {
+  return RandomizedSet{0, make([]int,0), make(map[int]int));
     
-    func main() {
-        fmt.Println("alias x='go run 20221129_Tuesday/20221129.go'")
-    }
+}
+
+
+func (this *RandomizedSet) Insert(val int) bool {
+  if _, ok := this.h[val]; ! ok {
+    return false
+  }
+  this.a = append(this.a,val)
+  this.h[val] = this.n 
+  this.n += 1
+  return true
+}
+
+
+func (this *RandomizedSet) Remove(val int) bool {
+  if i, ok := this.h[val]; ! ok {
+    return false
+  }
+  // Write last at the deleted place.
+  last := this.a[this.n-1]
+  this.h[last] = i
+  delete(this.h, val)
+  this.a = this.a[:this.n-1]
+  this.n -= 1
+  return true
+}
+
+
+func (this *RandomizedSet) GetRandom() int {
+    
+}
+
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * obj := Constructor();
+ * param_1 := obj.Insert(val);
+ * param_2 := obj.Remove(val);
+ * param_3 := obj.GetRandom();
+ */
