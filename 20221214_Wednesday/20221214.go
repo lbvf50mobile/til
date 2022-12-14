b683d5ee6709d80e1ef42e9fd0ea2411938455bd
Dmitry <lbvf50.mobile@gmail.com>
2022-12-14 23:40:23 +0700

    1427. Leetcode: 198. House Robber. (in action) Thanks God!

---
+++ 20221214_Wednesday/20221214.go
@@ -0,0 +1,9 @@
+    package main
+
+    import (
+        "fmt"
+    )
+    
+    func main() {
+        fmt.Println("alias x='go run 20221214_Wednesday/20221214.go'")
+    }
---
+++ 20221214_Wednesday/20221214.rb
@@ -0,0 +1,7 @@
+# Leetcode: 198. House Robber.
+# https://leetcode.com/problems/house-robber/
+# @param {Integer[]} nums
+# @return {Integer}
+def rob(nums)
+    
+end
