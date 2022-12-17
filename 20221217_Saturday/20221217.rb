e02aa5c99068d3f9aab898bb7c79e4fe3c124583
Dmitry <lbvf50.mobile@gmail.com>
2022-12-18 02:08:52 +0700

    1430. Leetcode: 150. Evaluate Reverse Polish Notation. (ruby solution) Thanks God!

--- 20221217_Saturday/20221217.rb
+++ 20221217_Saturday/20221217.rb
@@ -1,58 +1,39 @@
 # Leetcode: 150. Evaluate Reverse Polish Notation.
 # https://leetcode.com/problems/evaluate-reverse-polish-notation/
+# = = = = = = = = = = = = = =
+# Accepted.
+# Thanks God, Jesus Christ!
+# = = = = = = = = = = = = = =
+# Runtime: 106 ms, faster than 83.33% of Ruby online submissions for Evaluate Reverse Polish Notation.
+# Memory Usage: 212 MB, less than 37.50% of Ruby online submissions for Evaluate Reverse Polish Notation.
+# 2022.12.17 Daily Challenge.
 # @param {String[]} tokens
 # @return {Integer}
 def eval_rpn(tokens)
+  return tokens[0].to_i if 1 == tokens.size
+  # Based on:
+  # https://en.wikibooks.org/wiki/A-level_Computing_2009/AQA/Problem_Solving,_Programming,_Operating_Systems,_Databases_and_Networking/Problem_Solving/Reverse_Polish_Notation
   s = []
-  first, second, action = nil, nil, nil
-  # first + second = answer
   tokens.each_with_index do |x,i|
     s.push(x)
-    p x
     if ?+ == x || ?* == x || ?- == x || ?/ == x
       action = s.pop
       second = (s.pop()).to_i
-      first = (s.pop()).to_i if first.nil?
-      it = i == tokens.size - 1 || in_(tokens[i+1])
-      if  it
-        p "#{first} #{action} #{second}"
-      else
-        p "#{second} #{action} #{first}"
-      end
+      first = (s.pop()).to_i 
+      # p "#{first} #{action} #{second}"
       if ?+ == action
-        if it
-          first = first + second
-        else
-          first = second + first
-        end
+        s.push(first + second)
       elsif ?* == action
-        if it
-          first = first * second
-        else
-          first = second * first
-        end
+        s.push(first * second)
       elsif ?- == action
-        if it
-          first = first - second
-        else
-          first = second - first
-        end
+        s.push(first - second)
       elsif ?/ == action
-        if it
-          first = first / second
-        else
-          first = second / first
-        end
+        # https://leetcode.com/problems/evaluate-reverse-polish-notation/discuss/1229933/Ruby-Stack
+        s.push((first.to_f/second).to_i)
       else
         raise "Unknown action."
       end
-      p "#{first}"
     end
   end
-  return first
+  return s.pop()
 end
-
-def in_(x)
-  ?+ == x || ?* == x || ?- == x || ?/ == x
-end
-
