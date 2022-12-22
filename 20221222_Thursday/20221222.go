aeef38c6cda0f0668c9cbd9e7be049a992d373fb
Dmitry <lbvf50.mobile@gmail.com>
2022-12-22 23:53:24 +0700

    1434. Leetcode: 834. Sum of Distances in Tree. (in action) Thanks God!

---
+++ 20221222_Thursday/20221222.go
@@ -0,0 +1,9 @@
+    package main
+
+    import (
+        "fmt"
+    )
+    
+    func main() {
+        fmt.Println("alias x='go run 20221222_Thursday/20221222.go'")
+    }
---
+++ 20221222_Thursday/20221222.rb
@@ -0,0 +1,8 @@
+# Leetcode: 834. Sum of Distances in Tree.
+# https://leetcode.com/problems/sum-of-distances-in-tree/
+# @param {Integer} n
+# @param {Integer[][]} edges
+# @return {Integer[]}
+def sum_of_distances_in_tree(n, edges)
+    
+end
