07d64b73a95e48c1fcd089c0e3a3b825bd33818c
Dmitry <lbvf50.mobile@gmail.com>
2022-12-27 23:50:18 +0700

    1439. Leetcode: 2279. Maximum Bags With Full Capacity of Rocks. (go solution) Thanks God!

--- 20221227_Tuesday/20221227.go
+++ 20221227_Tuesday/20221227.go
@@ -1,9 +1,26 @@
-    package main
-
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20221227_Tuesday/20221227.go'")
+// Leetcode: 2279. Maximum Bags With Full Capacity of Rocks.
+// https://leetcode.com/problems/maximum-bags-with-full-capacity-of-rocks/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 155 ms, faster than 100.00% of Go online submissions for Maximum Bags With Full Capacity of Rocks.
+// Memory Usage: 9.2 MB, less than 7.14% of Go online submissions for Maximum Bags With Full Capacity of Rocks.
+// 2022.12.27 Daily Challenge.
+import "sort"
+func maximumBags(capacity []int, rocks []int, additionalRocks int) int {
+  delta := make([]int,len(rocks))
+  for i,_ := range delta {
+    delta[i] = capacity[i] - rocks[i]
+  }
+  sort.Ints(delta)
+  counter := 0
+  for _, v := range delta{
+    if v <= additionalRocks {
+      additionalRocks -= v
+      v = 0
     }
+    if 0 == v { counter += 1}
+  }
+  return counter
+}
