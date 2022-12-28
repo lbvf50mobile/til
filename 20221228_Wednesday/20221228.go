2e7d122905bff7f48c9d0305233daab84a2261ce
Dmitry <lbvf50.mobile@gmail.com>
2022-12-29 00:08:32 +0700

    1440. Leetcode: 1962. Remove Stones to Minimize the Total. (go in action) Thanks God!

--- 20221228_Wednesday/20221228.go
+++ 20221228_Wednesday/20221228.go
@@ -1,9 +1,9 @@
-    package main
+// Leetcode: 1962. Remove Stones to Minimize the Total.
+// https://leetcode.com/problems/remove-stones-to-minimize-the-total/
 
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20221228_Wednesday/20221228.go'")
-    }
+// https://pkg.go.dev/container/heap
+// https://leetcode.com/problems/last-stone-weight/discuss/549801/golang-using-containerheap
+import "container/heap"
+func minStoneSum(piles []int, k int) int {
+  return 12
+}
