198e6d9210454d038b7bf0b583a3a35576bf7ae6
Dmitry <lbvf50.mobile@gmail.com>
2022-12-30 03:10:30 +0700

    1441. Leetcode: 1834. Single-Threaded CPU. (ruby fix) Thanks God!

--- 20221229_Thursday/20221229.rb
+++ 20221229_Thursday/20221229.rb
@@ -2,11 +2,13 @@
 # https://leetcode.com/problems/single-threaded-cpu/
 # @param {Integer[][]} tasks
 # @return {Integer[]}
+# Error:
+# [[19,13],[16,9],[21,10],[32,25],[37,4],[49,24],[2,15],[38,41],[37,34],[33,6],[45,4],[18,18],[46,39],[12,24]]
 
 # https://github.com/kanwei/algorithms/blob/master/lib/containers/heap.rb#L475
 class Containers::MinHeap < Containers::Heap
   def initialize(ary=[])
-    super(ary) { |x, y| x[1] == y[1]? (x[0] <=> y[0]) == -1: (x[1] <=> y[1]) == -1 }
+    super(ary) { |x, y| x[1] == y[1]? (x[2] <=> y[2]) == -1: (x[1] <=> y[1]) == -1 } # Fix. 
   end
 end
 
@@ -21,7 +23,7 @@ def get_order(tasks)
     if a[0] == b[0] && a[1] == b[1]
       a[2] <=> b[2]
     elsif a[0] == b[0]
-      a[1] <=> b[2]
+      a[1] <=> b[1]
     else
       a[0] <=> b[0]
     end
