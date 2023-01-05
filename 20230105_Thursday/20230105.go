ef2a59bd3e99e90ec3291abf4dcc4f81012bea62
Dmitry <lbvf50.mobile@gmail.com>
2023-01-06 02:57:03 +0700

    1448. Leetcode: 452. Minimum Number of Arrows to Burst Balloons. (go solution) Thanks God!

--- 20230105_Thursday/20230105.go
+++ 20230105_Thursday/20230105.go
@@ -1,9 +1,24 @@
-    package main
-
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20230105_Thursday/20230105.go'")
-    }
+// Leetcode: 452. Minimum Number of Arrows to Burst Balloons.
+// https://leetcode.com/problems/minimum-number-of-arrows-to-burst-balloons/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 491 ms, faster than 44.83% of Go online submissions for Minimum Number of Arrows to Burst Balloons.
+// Memory Usage: 16.1 MB, less than 68.97% of Go online submissions for Minimum Number of Arrows to Burst Balloons.
+// 2023.01.05 Daily Challenge.
+import "sort"
+func findMinArrowShots(points [][]int) int {
+  counter, last_arrow := 0, 0
+  sort.Slice(points,func(i, j int) bool{
+    return points[i][1] < points[j][1]
+   })
+   for _,v := range points {
+     l,r := v[0],v[1]
+     if 0 == counter || last_arrow < l {
+       counter += 1
+       last_arrow = r
+     }
+   }
+  return counter
+}
