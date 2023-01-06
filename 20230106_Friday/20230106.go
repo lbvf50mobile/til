7d02fb7c4c72266a0b481a4e5333b8432dbb339a
Dmitry <lbvf50.mobile@gmail.com>
2023-01-06 23:58:54 +0700

    1449. Leetcode: 1833. Maximum Ice Cream Bars. (go solution) Thanks God!

--- 20230106_Friday/20230106.go
+++ 20230106_Friday/20230106.go
@@ -1,9 +1,23 @@
-    package main
-
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20230106_Friday/20230106.go'")
+// Leetcode: 1833. Maximum Ice Cream Bars.
+// https://leetcode.com/problems/maximum-ice-cream-bars/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 413 ms, faster than 23.53% of Go online submissions for Maximum Ice Cream Bars.
+// Memory Usage: 9.9 MB, less than 17.65% of Go online submissions for Maximum Ice Cream Bars.
+// 2023.01.06 Daily Challenge.
+import "sort"
+func maxIceCream(costs []int, coins int) int {
+  sort.Ints(costs)
+  counter := 0
+  for _,price := range costs {
+    if price <= coins {
+      counter += 1
+      coins -= price
+    } else {
+      break
     }
+  }
+  return counter
+}
