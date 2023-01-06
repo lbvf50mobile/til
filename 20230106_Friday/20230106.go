eb859be1d2df7db310472308a0bce472da973cca
Dmitry <lbvf50.mobile@gmail.com>
2023-01-06 23:49:16 +0700

    1449. Leetcode: 1833. Maximum Ice Cream Bars. (in action) Thanks God!

---
+++ 20230106_Friday/20230106.go
@@ -0,0 +1,9 @@
+    package main
+
+    import (
+        "fmt"
+    )
+    
+    func main() {
+        fmt.Println("alias x='go run 20230106_Friday/20230106.go'")
+    }
---
+++ 20230106_Friday/20230106.rb
@@ -0,0 +1,8 @@
+# Leetcode: 1833. Maximum Ice Cream Bars.
+# https://leetcode.com/problems/maximum-ice-cream-bars/
+# @param {Integer[]} costs
+# @param {Integer} coins
+# @return {Integer}
+def max_ice_cream(costs, coins)
+    
+end
