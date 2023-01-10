4f3536af6031c77a27a2587c658cb70435d74590
Dmitry <lbvf50.mobile@gmail.com>
2023-01-10 23:51:18 +0700

    1453. Leetcode: 100. Same Tree. (ruby solution) Thanks God!

--- 20230110_Tuesday/20230110.rb
+++ 20230110_Tuesday/20230110.rb
@@ -1,5 +1,12 @@
 # Leetcode: 100. Same Tree.
 # https://leetcode.com/problems/same-tree/
+# = = = = = = = = = = = = = =
+# Accepted.
+# Thanks God, Jesus Christ!
+# = = = = = = = = = = = = = =
+# Runtime: 111 ms, faster than 43.43% of Ruby online submissions for Same Tree.
+# Memory Usage: 211 MB, less than 76.77% of Ruby online submissions for Same Tree.
+# 2023.01.10 Daily Challenge!
 # Definition for a binary tree node.
 # class TreeNode
 #     attr_accessor :val, :left, :right
@@ -12,22 +19,20 @@
 # @param {TreeNode} pe
 # @param {TreeNode} q
 # @return {Boolean}
-# fails:
-# [10,5,15]
-# [10,5,null,null,15]
 def is_same_tree(pe, q)
   return true if pe.nil? && q.nil?
   return false if pe.nil? || q.nil?
-  qu1,qu2 = [[pe,0]],[[q,0]]
+  qu1,qu2 = [[pe,0,0]],[[q,0,0]]
   while (!qu1.empty?)&&(!qu2.empty?)
-    x1,s1 = qu1.shift 
-    x2,s2 = qu2.shift
+    x1,s1,l1 = qu1.shift 
+    x2,s2,l2 = qu2.shift
+    return false if l1 != l2
     return false if s1 != s2
     return false if x1.val != x2.val
-    qu1.push([x1.left,0]) if x1.left
-    qu1.push([x1.right,1]) if x1.right
-    qu2.push([x2.left,0]) if x2.left
-    qu2.push([x2.right,1]) if x2.right
+    qu1.push([x1.left,0,l1+1]) if x1.left
+    qu1.push([x1.right,1,l1+1]) if x1.right
+    qu2.push([x2.left,0,l2+1]) if x2.left
+    qu2.push([x2.right,1,l2+1]) if x2.right
   end
   return false if (!qu1.empty?) || (!qu2.empty?)
   return true
