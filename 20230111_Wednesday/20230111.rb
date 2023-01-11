e41a7b480409fac5a929f16851ee4c341d470e1d
Dmitry <lbvf50.mobile@gmail.com>
2023-01-12 00:40:44 +0700

    1454. Leetcode: 1443. Minimum Time to Collect All Apples in a Tree. (ruby solution) Thanks God!

--- 20230111_Wednesday/20230111.rb
+++ 20230111_Wednesday/20230111.rb
@@ -1,25 +1,31 @@
 # Leetcode: 1443. Minimum Time to Collect All Apples in a Tree.
 # https://leetcode.com/problems/minimum-time-to-collect-all-apples-in-a-tree/
+# = = = = = = = = = = = = = =
+# Accepted.
+# Thanks God, Jesus Christ!
+# = = = = = = = = = = = = = =
+# 2023.01.11 Daily Challenge.
 # @param {Integer} n
 # @param {Integer[][]} edges
 # @param {Boolean[]} has_apple
 # @return {Integer}
-# Error:
-# 4
-# [[0,2],[0,3],[1,2]]
-# [false,true,false,false]
 def min_time(n, edges, has_apple)
   @n,@e,@h = n, edges, has_apple
   @l = {}
+  @v = [false] * n
   @e.each do |pr,ch|
     @l[pr] ||= []
+    @l[ch] ||=[]
     @l[pr].push(ch)
+    @l[ch].push(pr)
   end
   time, has_fruit = dfs(0)
-  return  time
+  return time
 end
 
 def dfs(i)
+  return [0, false] if @v[i]
+  @v[i] = true
   time = 0
   has_fruit = @h[i]
   (@l[i] || []).each do |j|
