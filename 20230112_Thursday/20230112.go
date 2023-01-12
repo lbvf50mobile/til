9442c95575acc9f2d0a3a594aed7cb75ea30f8a7
Dmitry <lbvf50.mobile@gmail.com>
2023-01-12 23:53:15 +0700

    1455. Leetcode: 1519. Number of Nodes in the Sub-Tree With the Same Label. (in action) Thanks God!

---
+++ 20230112_Thursday/20230112.go
@@ -0,0 +1,9 @@
+    package main
+
+    import (
+        "fmt"
+    )
+    
+    func main() {
+        fmt.Println("alias x='go run 20230112_Thursday/20230112.go'")
+    }
---
+++ 20230112_Thursday/20230112.rb
@@ -0,0 +1,34 @@
+# Leetcode: 1519. Number of Nodes in the Sub-Tree With the Same Label.
+# https://leetcode.com/problems/number-of-nodes-in-the-sub-tree-with-the-same-label/
+# @param {Integer} n
+# @param {Integer[][]} edges
+# @param {String} labels
+# @return {Integer[]}
+def count_sub_trees(n, edges, labels)
+  @n, @l = n, labels.chars
+  @al = {}
+  @cnt = Array.new(@n).map{ Hash.new(0)}
+  @v = Array.new(@n,false)
+  edges.each do |a,b|
+    @al[a] ||= []; @al[b] ||= []
+    @al[a].push(b); @al[b].push(a)
+  end
+  answer = Array.new(@n,0)
+  @n.times do |i|
+    answer[i] = @cnt[i][@l[i]]
+  end
+  return answer
+end
+
+def dfs(i)
+  return if @v[i]
+  @v[i] = true
+  @al[i].each do |j|
+    if ! @v[j]
+      dfs(j)
+      (?a..?z).each do |c|
+        @cnt[i][c] += @cnt[j][c]
+      end
+    end
+  end
+end
