0a1ebfe09e5a38e59c3c97b11592ee04704fd6a7
Dmitry <lbvf50.mobile@gmail.com>
2023-01-13 23:50:12 +0700

    1456. Leetcode: 2246. Longest Path With Different Adjacent Characters. (in action) Thanks God!

---
+++ 20230113_Friday/20230113.go
@@ -0,0 +1,9 @@
+    package main
+
+    import (
+        "fmt"
+    )
+    
+    func main() {
+        fmt.Println("alias x='go run 20230113_Friday/20230113.go'")
+    }
---
+++ 20230113_Friday/20230113.rb
@@ -0,0 +1,8 @@
+# Leetcode: 2246. Longest Path With Different Adjacent Characters.
+# https://leetcode.com/problems/longest-path-with-different-adjacent-characters/
+# @param {Integer[]} parent
+# @param {String} s
+# @return {Integer}
+def longest_path(parent, s)
+    
+end
