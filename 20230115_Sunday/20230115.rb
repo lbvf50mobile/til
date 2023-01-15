87a2a83f75f3c9ace9c815d99e8a013757cf6cfd
Dmitry <lbvf50.mobile@gmail.com>
2023-01-15 23:33:30 +0700

    1458. Leetcode: 2421. Number of Good Paths. (in action) Thanks God!

---
+++ 20230115_Sunday/20230115.rb
@@ -0,0 +1,8 @@
+# Leetcode: 2421. Number of Good Paths.
+# https://leetcode.com/problems/number-of-good-paths/
+# @param {Integer[]} vals
+# @param {Integer[][]} edges
+# @return {Integer}
+def number_of_good_paths(vals, edges)
+    
+end
