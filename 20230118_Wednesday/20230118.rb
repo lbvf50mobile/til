aadd5af028b5470c15268b8502fd9d539365158d
Dmitry <lbvf50.mobile@gmail.com>
2023-01-18 23:58:06 +0700

    1461. Leetcode: 918. Maximum Sum Circular Subarray. (ruby code) Thanks God!

--- 20230118_Wednesday/20230118.rb
+++ 20230118_Wednesday/20230118.rb
@@ -1,5 +1,12 @@
 # Leetcode: 918. Maximum Sum Circular Subarray.
 # https://leetcode.com/problems/maximum-sum-circular-subarray/
+# = = = = = = = = = = = = = =
+# Accepted.
+# Thanks God, Jesus Christ!
+# = = = = = = = = = = = = = =
+# Runtime: 141 ms, faster than 100.00% of Ruby online submissions for Maximum Sum Circular Subarray.
+# Memory Usage: 215.4 MB, less than 75.00% of Ruby online submissions for Maximum Sum Circular Subarray.
+# 2023.01.18 Daily Challenge.
 # @param {Integer[]} nums
 # @return {Integer}
 def max_subarray_sum_circular(nums)
@@ -13,8 +20,8 @@ def max_subarray_sum_circular(nums)
   (1...nums.size).each do |i|
     x = nums[i]
     total += x
-    sum1 = [x,sum1].min
-    sum2 = [x,sum2].max
+    sum1 = [x,sum1+x].min
+    sum2 = [x,sum2+x].max
     min_sum = sum1 if sum1 < min_sum
     max_sum = sum2 if sum2 > max_sum
   end
