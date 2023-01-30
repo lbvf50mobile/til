22e6644f72a690ac2be90e0c576c4a573a8f6cb8
Dmitry <lbvf50.mobile@gmail.com>
2023-01-31 02:52:23 +0700

    1473. Leetcode: 1137. N-th Tribonacci Number. (go solution) Thanks God!

--- 20230130_Monday/20230130.go
+++ 20230130_Monday/20230130.go
@@ -1,9 +1,19 @@
-    package main
-
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20230130_Monday/20230130.go'")
-    }
+// Leetcode: 1137. N-th Tribonacci Number.
+// https://leetcode.com/problems/n-th-tribonacci-number/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 0 ms, faster than 100.00% of Go online submissions for N-th Tribonacci Number.
+// Memory Usage: 1.9 MB, less than 73.74% of Go online submissions for N-th Tribonacci Number.
+// 2023.01.30 Daily Challenge.
+func tribonacci(n int) int {
+  a,b,c := 0, 1, 1
+  if 0 == n { return a }
+  if 1 == n  { return b }
+  if 2 == n { return c }
+  for i := 3; i <= n ; i += 1 {
+    a,b,c = b, c, a+b+c 
+  }
+  return c
+}
