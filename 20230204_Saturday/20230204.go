2fade6a6569bd86c7855693d5f37c625ca734f5f
Dmitry <lbvf50.mobile@gmail.com>
2023-02-04 23:52:26 +0700

    1478. Leetcode: 567. Permutation in String. (in action) Thanks God!

---
+++ 20230204_Saturday/20230204.go
@@ -0,0 +1,9 @@
+    package main
+
+    import (
+        "fmt"
+    )
+    
+    func main() {
+        fmt.Println("alias x='go run 20230204_Saturday/20230204.go'")
+    }
---
+++ 20230204_Saturday/20230204.rb
@@ -0,0 +1,12 @@
+# Leetcode: 567. Permutation in String..
+# https://leetcode.com/problems/permutation-in-string/
+# @param {String} s1
+# @param {String} s2
+# @return {Boolean}
+def check_inclusion(s1, s2)
+  return false if s1.size > s2
+  a = [0] * 26
+
+end
+
+
