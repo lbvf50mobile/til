00a6ba616ddee6ffa8ece7a8e0575845e6db7e82
Dmitry <lbvf50.mobile@gmail.com>
2023-02-09 00:11:06 +0700

    1482. Leetcode: 45. Jump Game II. (tle) Thanks God!

--- 20230208_Wednesday/20230208.rb
+++ 20230208_Wednesday/20230208.rb
@@ -1,15 +1,16 @@
 # Leetcode: 45. Jump Game II.
 # https://leetcode.com/problems/jump-game-ii/
+# TLE.
 # @param {Integer[]} nums
 # @return {Integer}
 def jump(nums)
-  return 1 if 1 == nums.size
+  return 0 if 1 == nums.size
   dp = [100000] * nums.size
   dp[0] = 0 
   (0...nums.size-1).each do |i|
-    (1..nums[i]).each do |j|
-      k = i+j
-      dp[k] = [dp[k],dp[i]+1].min
+    next if 0 == nums[i] # No need to jump from zero.
+    (1..[nums[i]+i,nums.size-1].min).reverse_each do |j|
+      dp[j] = [dp[j],dp[i]+1].min
     end
   end
   return dp[nums.size-1]
