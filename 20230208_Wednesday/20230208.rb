a84c1a9a2e179fe346fe21321cea65551fd4bc08
Dmitry <lbvf50.mobile@gmail.com>
2023-02-09 01:21:02 +0700

    1482. Leetcode: 45. Jump Game II. (in action) Thanks God!

--- 20230208_Wednesday/20230208.rb
+++ 20230208_Wednesday/20230208.rb
@@ -1,17 +1,21 @@
 # Leetcode: 45. Jump Game II.
 # https://leetcode.com/problems/jump-game-ii/
-# TLE.
 # @param {Integer[]} nums
 # @return {Integer}
+# Fails.
+# [7,0,9,6,9,6,1,7,9,0,1,2,9,0,3]
 def jump(nums)
+  # Hint from the Leetcode's solution.
   return 0 if 1 == nums.size
-  dp = [100000] * nums.size
-  dp[0] = 0 
-  (0...nums.size-1).each do |i|
-    next if 0 == nums[i] # No need to jump from zero.
-    (1..[nums[i]+i,nums.size-1].min).reverse_each do |j|
-      dp[j] = [dp[j],dp[i]+1].min
+  farest, steps = -1, 0
+  nums.each_with_index do |i,range|
+    if i+range > farest
+      farest = i + range
+      steps += 1
+    end
+    if farest >= nums.size - 1
+      return steps
     end
   end
-  return dp[nums.size-1]
+  raise "This line could not be implmeneted."
 end
