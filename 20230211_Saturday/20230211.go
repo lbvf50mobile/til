25c0ab7a6f4f2e0bef0d978e4bed7164f4332b01
Dmitry <lbvf50.mobile@gmail.com>
2023-02-11 23:45:19 +0700

    1486. Leetcode: 1129. Shortest Path with Alternating Colors. (ruby in action) Thanks God!

---
+++ 20230211_Saturday/20230211.go
@@ -0,0 +1,9 @@
+    package main
+
+    import (
+        "fmt"
+    )
+    
+    func main() {
+        fmt.Println("alias x='go run 20230211_Saturday/20230211.go'")
+    }
---
+++ 20230211_Saturday/20230211.rb
@@ -0,0 +1,16 @@
+# Leetcode: 1129. Shortest Path with Alternating Colors.
+# https://leetcode.com/problems/shortest-path-with-alternating-colors/
+# @param {Integer} n
+# @param {Integer[][]} red_edges
+# @param {Integer[][]} blue_edges
+# @return {Integer[]}
+def shortest_alternating_paths(n, red_edges, blue_edges)
+  # Based on:
+  # https://leetcode.com/problems/shortest-path-with-alternating-colors/solution/
+  # Create an adjacency list adj that contains a list of pairs of integers such that adj[node] contains the neighbors of node in the form (neighbor, color) where neighbor is the neighbor of node and color denotes the edge color that connects node and neighbor. We use the number 0 for red and the number 1 for blue.
+  adj = {}
+  red_edges.each do |x|
+
+  end
+    
+end
