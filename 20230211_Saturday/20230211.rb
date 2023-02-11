736e5600167b26eb31bb83caae6becd4f4951853
Dmitry <lbvf50.mobile@gmail.com>
2023-02-12 03:34:21 +0700

    1486. Leetcode: 1129. Shortest Path with Alternating Colors. (in action) Thanks God!

--- 20230211_Saturday/20230211.rb
+++ 20230211_Saturday/20230211.rb
@@ -7,10 +7,30 @@
 def shortest_alternating_paths(n, red_edges, blue_edges)
   # Based on:
   # https://leetcode.com/problems/shortest-path-with-alternating-colors/solution/
-  # Create an adjacency list adj that contains a list of pairs of integers such that adj[node] contains the neighbors of node in the form (neighbor, color) where neighbor is the neighbor of node and color denotes the edge color that connects node and neighbor. We use the number 0 for red and the number 1 for blue.
+  # 1. One.
+  # Create an adjacency list adj that contains a list of pairs of integers 
+  # such that adj[node] contains the neighbors of node in the form (neighbor, color) 
+  # where neighbor is the neighbor of node and color denotes the edge color 
+  # that connects node and neighbor. We use the number 0 for red and the number 1 for blue.
   adj = {}
-  red_edges.each do |x|
-
+  red_edges.each do |i,j|
+    adj[i] ||= []
+    adj[i].push([j,0]) # 0 - RED;
   end
+  blue_edges.each do |i,j|
+    adj[i] ||= []
+    adj[i].push([j,1]) # 1 - BLUE;
+  end
+  # 2. Two.
+  # Create a answer array with the value -1 where answer[i] 
+  # is the answer for the i-th node.
+  ans = Array.new(n,-1)
+  # 3. Three.
+  # Create a 2D visit array in which visit[node][color] 
+  # indicates whether node has yet been 
+  # visited using an edge of color.
+  vst = Array.new(n).map{ [-1,-1]}
+
+
     
 end
