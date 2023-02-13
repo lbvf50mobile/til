883970e89751c6e86023b4d5b7895fa2337995b8
Dmitry <lbvf50.mobile@gmail.com>
2023-02-13 23:17:25 +0700

    1488. Leetcode: 1523. Count Odd Numbers in an Interval Range. (go solution) Thanks God!

--- 20230213_Monday/20230213.go
+++ 20230213_Monday/20230213.go
@@ -1,9 +1,24 @@
-    package main
-
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20230213_Monday/20230213.go'")
+// Leetcode: 1523. Count Odd Numbers in an Interval Range.
+// https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 0 ms, faster than 100.00% of Go online submissions for Count Odd Numbers in an Interval Range.
+// Memory Usage: 1.9 MB, less than 55.20% of Go online submissions for Count Odd Numbers in an Interval Range.
+// 2023.02.13 Daily Challenge.
+func countOdds(low int, high int) int {
+  size := high - low + 1
+  if 0 == low%2 {
+    // 10 - 8 + 1 = 3; 3-1 = 2; 2/2 = 1;
+    if 0 != size%2 {
+      size -= 1
     }
+  }else{
+    // 7-3+1 = 5; 5+1 = 6; 6/2 = 3;
+    if 0 !=  size%2{
+      size += 1
+    }
+  }
+  return size/2;
+}
