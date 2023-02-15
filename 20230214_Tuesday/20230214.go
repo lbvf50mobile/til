aa6ade37a26785ecc035878c38b466a9a665c3ae
Dmitry <lbvf50.mobile@gmail.com>
2023-02-15 13:01:39 +0700

    1489. Leetcode: 67. Add Binary. (go bits overflow) Thanks God!

--- 20230214_Tuesday/20230214.go
+++ 20230214_Tuesday/20230214.go
@@ -1,9 +1,8 @@
-    package main
-
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20230214_Tuesday/20230214.go'")
-    }
+// Leetcode: 67. Add Binary.
+// https://leetcode.com/problems/add-binary/
+import "strconv"
+func addBinary(a string, b string) string {
+  ai, _ := strconv.ParseUint(a,2,64)
+  bi, _ := strconv.ParseUint(b,2,64)
+  return strconv.FormatInt( int64(ai) + int64(bi) ,2)
+}
