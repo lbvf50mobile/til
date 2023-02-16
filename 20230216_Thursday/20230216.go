f56d2609446be95240761d9f8ed85d0bef556c84
Dmitry <lbvf50.mobile@gmail.com>
2023-02-16 23:54:30 +0700

    1495. Leetcode: 104. Maximum Depth of Binary Tree. (go solution) Thanks God!

--- 20230216_Thursday/20230216.go
+++ 20230216_Thursday/20230216.go
@@ -1,9 +1,29 @@
-    package main
-
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20230216_Thursday/20230216.go'")
-    }
+// Leetcode: 104. Maximum Depth of Binary Tree.
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 3 ms, faster than 85.87% of Go online submissions for Maximum Depth of Binary Tree.
+// Memory Usage: 4.2 MB, less than 48.75% of Go online submissions for Maximum Depth of Binary Tree.
+// 2023.02.16 Daily Challenge.
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+var max int
+func maxDepth(root *TreeNode) int {
+  max = 0
+  dfs(root,1)
+  return max
+}
+func dfs(x *TreeNode, deep int){
+  if nil == x { return }
+  if deep > max { max = deep }
+  dfs(x.Left, deep+1)
+  dfs(x.Right, deep+1)
+}
