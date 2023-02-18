fa2633bdf9ddf39698252a3b597d87629f6aa611
Dmitry <lbvf50.mobile@gmail.com>
2023-02-18 23:54:22 +0700

    1497. Leetcode: 226. Invert Binary Tree. (go solution) Thanks God!

--- 20230218_Saturday/20230218.go
+++ 20230218_Saturday/20230218.go
@@ -1,9 +1,24 @@
-    package main
-
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20230218_Saturday/20230218.go'")
-    }
+// Leetcode: 226. Invert Binary Tree.
+// https://leetcode.com/problems/invert-binary-tree/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 2 ms, faster than 58.43% of Go online submissions for Invert Binary Tree.
+// Memory Usage: 2.1 MB, less than 100.00% of Go online submissions for Invert Binary Tree.
+// 2023.02.18 Daily Challenge.
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func invertTree(root *TreeNode) *TreeNode {
+  if nil == root { return root }
+  root.Left, root.Right = root.Right, root.Left
+  invertTree(root.Left)
+  invertTree(root.Right)
+  return root
+}
