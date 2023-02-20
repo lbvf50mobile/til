f697ce5bd3b9cdd9988daea9599dd2dd8d07a5f2
Dmitry <lbvf50.mobile@gmail.com>
2023-02-21 03:26:00 +0700

    1499. Leetcode: 35. Search Insert Position. (go solution) Thanks God!

--- 20230220_Monday/20230220.go
+++ 20230220_Monday/20230220.go
@@ -1,9 +1,23 @@
-    package main
-
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20230220_Monday/20230220.go'")
+// Leetcode: 35. Search Insert Position.
+// https://leetcode.com/problems/search-insert-position/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 5 ms, faster than 59.23% of Go online submissions for Search Insert Position.
+// Memory Usage: 2.9 MB, less than 100.00% of Go online submissions for Search Insert Position.
+// 2023.02.20 Daily Challenge.
+func searchInsert(nums []int, target int) int {
+  l,r,m, v := 0, len(nums) - 1, 0, 0
+  for l <= r {
+    m = l + (r-l)/2
+    v = nums[m]
+    if v == target { return m }
+    if v < target {
+      l = m + 1
+    } else {
+      r = m - 1
     }
+  }
+  return l
+}
