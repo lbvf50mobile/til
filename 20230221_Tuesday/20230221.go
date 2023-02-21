5647139bd59cc0e2ea229fa38839738276936af6
Dmitry <lbvf50.mobile@gmail.com>
2023-02-22 02:42:25 +0700

    1500. Leetcode: 540. Single Element in a Sorted Array. (go solution) Thanks God!

--- 20230221_Tuesday/20230221.go
+++ 20230221_Tuesday/20230221.go
@@ -1,9 +1,25 @@
-    package main
-
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20230221_Tuesday/20230221.go'")
+// Leetcode: 540. Single Element in a Sorted Array.
+// https://leetcode.com/problems/single-element-in-a-sorted-array/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 25 ms, faster than 56.41% of Go online submissions for Single Element in a Sorted Array.
+// Memory Usage: 7.7 MB, less than 94.87% of Go online submissions for Single Element in a Sorted Array.
+// 2023.02.21 Daily Challenge.
+func singleNonDuplicate(nums []int) int {
+  l,r,m := 0, len(nums)-1, 0
+  lst := len(nums) - 1
+  for l < r {
+    m = l + (r-l)/2
+    if 1 == m%2 {
+      m -= 1
     }
+    if m == lst || nums[m] != nums[m+1] {
+      r = m
+    } else {
+      l = m + 2
+    }
+  }
+  return nums[l]
+}
