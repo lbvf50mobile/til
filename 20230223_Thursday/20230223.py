c518d06304d93dc004022423828ef0a59137b28a
Dmitry <lbvf50.mobile@gmail.com>
2023-02-24 01:55:08 +0700

    1502. Leetcode: 502. IPO. (ruby half solution, python soltuion copied) Thanks God!

--- 20230223_Thursday/20230223.py
+++ 20230223_Thursday/20230223.py
@@ -1,41 +1,4 @@
 # Leetcode: 502. IPO.
-
-- https://gist.github.com/lbvf50mobile/f8970b441e30808c413c870e44bc60ae
-
-
-Ruby code:
-```Ruby
-# Leetcode: 502. IPO.
-# https://leetcode.com/problems/ipo/
-# @param {Integer} k
-# @param {Integer} w
-# @param {Integer[]} profits
-# @param {Integer[]} capital
-# @return {Integer}
-# TLE
-def find_maximized_capital(k, w, profits, capital)
-  # Based on: 
-  # https://leetcode.com/problems/ipo/solution/
-  n = profits.size
-  projects = capital.zip(profits)
-  projects.sort!
-  q = []
-  ptr = 0
-  k.times do |i|
-    while ptr < n && projects[ptr][0] <= w
-      q.push(projects[ptr][1])
-      q.sort!
-      ptr += 1
-    end
-    break if q.empty?
-    w += q.pop()
-  end
-  return w
-end
-```
-
-```Python
-# Leetcode: 502. IPO.
 # https://leetcode.com/problems/ipo/
 # = = = = = = = = = = = = = =
 # Accepted.
@@ -66,4 +29,3 @@ class Solution:
             # pop a negated element
             w += -heappop(q)
         return w
-```
--- 20230223_Thursday/20230223.rb
+++ 20230223_Thursday/20230223.rb
@@ -5,21 +5,23 @@
 # @param {Integer[]} profits
 # @param {Integer[]} capital
 # @return {Integer}
+# TLE
 def find_maximized_capital(k, w, profits, capital)
   # Based on: 
   # https://leetcode.com/problems/ipo/solution/
   n = profits.size
-  projects = capital.zip(profits).to_h
+  projects = capital.zip(profits)
+  projects.sort!
   q = []
   ptr = 0
   k.times do |i|
     while ptr < n && projects[ptr][0] <= w
-      q.push(-projects[ptr][1])
+      q.push(projects[ptr][1])
       q.sort!
       ptr += 1
     end
     break if q.empty?
-    w += -q.shift()
+    w += q.pop()
   end
   return w
 end
