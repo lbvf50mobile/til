fe24c8bbbceda8f7ad82ea994d48aa7cb9b78c94
Dmitry <lbvf50.mobile@gmail.com>
2023-03-02 02:58:17 +0700

    1507. Leetcode: 912. Sort an Array. (go build in solution) Thanks God!

--- 20230301_Wednesday/20230301.go
+++ 20230301_Wednesday/20230301.go
@@ -1,9 +1,16 @@
-    package main
-
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20230301_Wednesday/20230301.go'")
-    }
+// Leetcode: 912. Sort an Array.
+// https://leetcode.com/problems/sort-an-array/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 131 ms, faster than 73.71% of Go online submissions for Sort an Array.
+// Memory Usage: 7.8 MB, less than 82.47% of Go online submissions for Sort an Array.
+// 2023.03.01 Daily Challenge.
+import "sort"
+func sortArray(nums []int) []int {
+  // It is a basic built in solution.
+  // Need to revrite.
+  sort.Ints(nums)
+  return nums
+}
