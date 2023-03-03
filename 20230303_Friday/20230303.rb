54626567a1b24b9f6025375106757443db69e85c
Dmitry <lbvf50.mobile@gmail.com>
2023-03-03 23:43:42 +0700

    1509. Leetcode: 28. Find the Index of the First Occurrence in a String. (ruby algorithmic solution) Thanks God!

--- 20230303_Friday/20230303.rb
+++ 20230303_Friday/20230303.rb
@@ -4,12 +4,23 @@
 # Accepted.
 # Thanks God, Jesus Christ!
 # = = = = = = = = = = = = = =
+# Runtime: 85 ms, faster than 61.06% of Ruby online submissions for Find the Index of the First Occurrence in a String.
+# Memory Usage: 211.2 MB, less than 8.85% of Ruby online submissions for Find the Index of the First Occurrence in a String.
 # 2023.03.03 Daily Challenge.
 # @param {String} haystack
 # @param {String} needle
 # @return {Integer}
 def str_str(haystack, needle)
-  ans = haystack.index(needle)
-  return -1 if ! ans
-  return ans
+  return -1 if needle.size > haystack.size
+  (0...(haystack.size - needle.size + 1)).each do |i|
+    if haystack[i] == needle[0]
+      count = 0
+      (0...needle.size).each do |delta|
+        break if haystack[i+delta] != needle[delta]
+        count += 1
+      end
+      return i if count == needle.size
+    end
+  end
+  return -1
 end
