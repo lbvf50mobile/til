6330daa545a6659d4caed3d04110e396f2c62445
Dmitry <lbvf50.mobile@gmail.com>
2023-03-03 23:10:07 +0700

    1509. Leetcode: 28. Find the Index of the First Occurrence in a String. (go solution) Thanks God!

--- 20230303_Friday/20230303.go
+++ 20230303_Friday/20230303.go
@@ -1,9 +1,13 @@
-    package main
-
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20230303_Friday/20230303.go'")
-    }
+// Leetcode: 28. Find the Index of the First Occurrence in a String.
+// https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 2 ms, faster than 18.41% of Go online submissions for Find the Index of the First Occurrence in a String.
+// Memory Usage: 2 MB, less than 36.04% of Go online submissions for Find the Index of the First Occurrence in a String.
+// 2023.03.03 Daily Challenge.
+import "strings"
+func strStr(haystack string, needle string) int {
+  return strings.Index(haystack, needle)
+}
