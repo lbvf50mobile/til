f4f61e0bca722e33f807924945413e3996edc0ee
Dmitry <lbvf50.mobile@gmail.com>
2023-03-10 01:27:14 +0700

    1515. Leetcode: 142. Linked List Cycle II. (go solution) Thanks God!

--- 20230309_Thursday/20230309.go
+++ 20230309_Thursday/20230309.go
@@ -1,9 +1,28 @@
-    package main
-
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20230309_Thursday/20230309.go'")
-    }
+// Leetcode: 142. Linked List Cycle II.
+// https://leetcode.com/problems/linked-list-cycle-ii/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 11 ms, faster than 18.39% of Go online submissions for Linked List Cycle II.
+// Memory Usage: 5.2 MB, less than 15.58% of Go online submissions for Linked List Cycle II.
+// 2023.03.09 Daily Challenge.
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func detectCycle(head *ListNode) *ListNode {
+	mp := make(map[*ListNode]bool)
+	for head != nil {
+		if _, ok := mp[head]; ok {
+			return head
+		} else {
+			mp[head] = true
+		}
+		head = head.Next
+	}
+	return nil
+}
