e82506b5a2a8ae53e618c85d6b5f6d7021633433
Dmitry <lbvf50.mobile@gmail.com>
2023-03-13 00:21:39 +0700

    1518. Leetcode: 23. Merge k Sorted Lists. (experementing with TLE) Thanks God!

--- 20230312_Sunday/20230312.rb
+++ 20230312_Sunday/20230312.rb
@@ -1,12 +1,5 @@
 # Leetcode: 23. Merge k Sorted Lists.
 # https://leetcode.com/problems/merge-k-sorted-lists/
-# = = = = = = = = = = = = = =
-# Accepted.
-# Thanks God, Jesus Christ!
-# = = = = = = = = = = = = = =
-# Runtime: 97 ms, faster than 90.16% of Ruby online submissions for Merge k Sorted Lists.
-# Memory Usage: 212.4 MB, less than 62.30% of Ruby online submissions for Merge k Sorted Lists.
-# 2023.03.12 Daily Challenge.
 # Definition for singly-linked list.
 # class ListNode
 #     attr_accessor :val, :next
@@ -17,20 +10,61 @@
 # end
 # @param {ListNode[]} lists
 # @return {ListNode}
+# TLE!?
 def merge_k_lists(lists)
   return nil if 0 == lists.size
-  arr = []
-  lists.each do |x|
-    while x
-      arr.push(x)
-      x = x.next
-      arr.last.next = nil
+  # Select head.
+  head = nil
+  head_index = -1
+  lists.each_with_index do |x,i|
+    next if x.nil? # Important.
+    if head.nil? || head.val > x.val
+      head = x
+      head_index = i
     end
   end
-  arr.sort_by!{|x| x.val}
-  return nil if arr.empty?
-  (0...(arr.size-1)).each do |i|
-    arr[i].next = arr[i+1]
+  return head if ! head
+  # Fill a tail of the list.
+  lists[head_index] = lists[head_index].next # Move pointer to next.
+  prev = head
+  curr = nil
+  prev.next = curr
+  curr_i = -1
+  new_lists = []
+  while true
+    # Need to remove nil's from the array.
+    # Fill a new array if an element is not nil.
+    # Save index of last element of not_nil array in `i` var.
+    i = -1
+    lists.each do |x|
+      # Remove nils from the list.
+      if x
+        new_lists.push(x)
+        i += 1
+      else
+        next # it is nil, no need to check seek for minimum.
+      end
+      # Next step is to select minimum.
+      if curr.nil? || curr.val > x.val
+        curr_i = i 
+        curr = x
+      end
+    end
+    break if -1 == i # All element in the list are nils.
+    # Only one list exists. Return immediately.
+    if 0 == i
+      prev.next = curr # Just append.
+      return head
+    end
+    new_lists[curr_i] = new_lists[curr_i].next # Move selected min to a next element.
+    prev.next = curr # Add new element to a list.
+    prev = curr # Move prev pointer.
+    curr = nil
+    prev.next = curr # Nill new added next pointer.
+    curr_i = -1
+    # Use two lists. Without adding new one.
+    lists.clear()
+    lists, new_lists = new_lists, lists
   end
-  return arr[0]
+  return head
 end
