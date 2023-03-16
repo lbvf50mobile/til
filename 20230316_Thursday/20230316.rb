2ed89c53edc7ceb04cf8dae61e6568a70c83cba4
Dmitry <lbvf50.mobile@gmail.com>
2023-03-17 00:15:41 +0700

    1522. Leetcode: 106. Construct Binary Tree from Inorder and Postorder Traversal. (ruby solution) Thanks God!

--- 20230316_Thursday/20230316.rb
+++ 20230316_Thursday/20230316.rb
@@ -1,5 +1,12 @@
 # Leetcode: 106. Construct Binary Tree from Inorder and Postorder Traversal.
 # https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/
+# = = = = = = = = = = = = = =
+# Accepted.
+# Thanks God, Jesus Christ!
+# = = = = = = = = = = = = = =
+# Runtime: 115 ms, faster than 75.00% of Ruby online submissions for Construct Binary Tree from Inorder and Postorder Traversal.
+# Memory Usage: 211.8 MB, less than 75.00% of Ruby online submissions for Construct Binary Tree from Inorder and Postorder Traversal.
+# 2023.03.16 Daily Challenge.
 # Definition for a binary tree node.
 # class TreeNode
 #     attr_accessor :val, :left, :right
@@ -13,15 +20,13 @@
 # @param {Integer[]} postorder
 # @return {TreeNode}
 def build_tree(inorder, postorder)
-  p inorder
-  p postorder
   # Based on:
   # https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/discuss/221681/A-better-Python-solution
   return nil if inorder.empty? || postorder.empty?
   root = TreeNode.new(postorder.pop())
   ii = inorder.find_index(root.val) # inorder index.
-  root.left = build_tree(inorder[ii+1..],postorder)
-  root.right = build_tree(inorder[0...ii],postorder)
-  p [root.left, root.right]
+  # Bug was here. Order of calls is important.
+  root.right = build_tree(inorder[ii+1..-1],postorder)
+  root.left = build_tree(inorder[0...ii],postorder)
   return root
 end
