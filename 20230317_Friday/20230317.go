a5ac33d8a613e4a8b346384fea5f9c72182cfc60
Dmitry <lbvf50.mobile@gmail.com>
2023-03-18 02:07:45 +0700

    1523. Leetcode: 208. Implement Trie (Prefix Tree). (go solution) Thanks God!

--- 20230317_Friday/20230317.go
+++ 20230317_Friday/20230317.go
@@ -1,9 +1,75 @@
-    package main
+// Leetcode: 208. Implement Trie (Prefix Tree).
+// https://leetcode.com/problems/implement-trie-prefix-tree/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 81 ms, faster than 18.16% of Go online submissions for Implement Trie (Prefix Tree).
+// Memory Usage: 17.6 MB, less than 48.34% of Go online submissions for Implement Trie (Prefix Tree).
+// 2023.03.17 Daily Challenge.
+type Trie struct {
+  x map[string] *Trie
+}
 
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20230317_Friday/20230317.go'")
+
+func Constructor() Trie {
+  trie :=  new(Trie)
+  trie.x = make(map[string] *Trie)
+  return *trie
+}
+
+
+func (this *Trie) Insert(word string)  {
+  pnt := this
+  for _,v := range word {
+    str := string(v)
+    if _, ok := pnt.x[str]; !ok {
+      trie := new(Trie)
+      trie.x = make(map[string]*Trie)
+      pnt.x[str] = trie
     }
+    pnt = pnt.x[str]
+  }
+  trie := new(Trie)
+  pnt.x["end"] = trie
+}
+
+
+func (this *Trie) Search(word string) bool {
+  pnt := this
+  for _, v := range word {
+    str := string(v)
+    if npnt,ok := pnt.x[str]; ok {
+      pnt = npnt
+    } else {
+      return false
+    }
+  }
+  if _, ok := pnt.x["end"]; ok {
+    return true
+  }
+  return false
+}
+
+
+func (this *Trie) StartsWith(prefix string) bool {
+  pnt := this
+  for _,v := range prefix {
+    str := string(v)
+    if npnt, ok := pnt.x[str]; ok {
+      pnt = npnt
+    } else {
+      return false
+    }
+  }
+  return true
+}
+
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Insert(word);
+ * param_2 := obj.Search(word);
+ * param_3 := obj.StartsWith(prefix);
+ */
