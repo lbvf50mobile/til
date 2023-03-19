5eace1191941f843ad7c63e355fd3385cb16ba92
Dmitry <lbvf50.mobile@gmail.com>
2023-03-20 04:37:50 +0700

    1525. Leetcode: 211. Design Add and Search Words Data Structure. (cpp copied) Thanks God!

--- 20230319_Sunday/20230319.go
+++ 20230319_Sunday/20230319.go
@@ -1,9 +1,40 @@
-    package main
+// Leetcode: 211. Design Add and Search Words Data Structure.
+// https://leetcode.com/problems/design-add-and-search-words-data-structure/
+type WordDictionary struct {
+  p map[string]*WordDictionary
+  e bool
+}
 
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20230319_Sunday/20230319.go'")
+
+func Constructor() WordDictionary {
+  x := WordDictionary{}
+  x.p = make(map[string]*WordDictionary)
+  x.e = false
+}
+
+
+func (this *WordDictionary) AddWord(word string)  {
+  pnt := this
+  for _,c := range word {
+    ch := string(c)
+    if v,o := pnt.p[ch]; ! o {
+      pnt.p[ch] = new(WordDictionary)
+      pnt.p[ch].p = make(map[string]*WordDictionary)
     }
+    pnt = pnt.p[ch]
+  }
+  pnt.e = true
+}
+
+
+func (this *WordDictionary) Search(word string) bool {
+    
+}
+
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.AddWord(word);
+ * param_2 := obj.Search(word);
+ */
