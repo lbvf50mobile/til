ce35badc435e0b778e0e7ceeae961163ea05456f
Dmitry <lbvf50.mobile@gmail.com>
2023-03-21 23:57:15 +0700

    1527. Leetcode: 2348. Number of Zero-Filled Subarrays. (go solution) Thanks God!

--- 20230321_Tuesday/20230321.go
+++ 20230321_Tuesday/20230321.go
@@ -1,9 +1,22 @@
-    package main
+// Leetcode: 2348. Number of Zero-Filled Subarrays.
+// https://leetcode.com/problems/number-of-zero-filled-subarrays/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 154 ms, faster than 14.29% of Go online submissions for Number of Zero-Filled Subarrays.
+// Memory Usage: 10.8 MB, less than 14.29% of Go online submissions for Number of Zero-Filled Subarrays.
+// 2023.03.21 Daily Challenge.
 
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20230321_Tuesday/20230321.go'")
-    }
+func zeroFilledSubarray(nums []int) int64 {
+	ans, zrs := 0, 0
+	for _, v := range nums {
+		if 0 == v {
+			zrs += 1
+			ans += zrs
+		} else {
+			zrs = 0
+		}
+	}
+	return int64(ans)
+}
