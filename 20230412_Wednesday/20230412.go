5004a0b47fb95e58e652597527194b6bab5b6aa3
Dmitry <lbvf50.mobile@gmail.com>
2023-04-14 17:35:39 +0700

    1548. Leetcode: 71. Simplify Path. (go solution) Thanks God!

--- 20230412_Wednesday/20230412.go
+++ 20230412_Wednesday/20230412.go
@@ -1,9 +1,46 @@
-    package main
+// Leetcode: 71. Simplify Path.
+// https://leetcode.com/problems/simplify-path/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 4 ms, faster than 42.08% of Go online submissions for Simplify Path.
+// Memory Usage: 3.1 MB, less than 38.24% of Go online submissions for Simplify
+// Path.
+// 2023.04.14 Update.
+import "strings"
 
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20230412_Wednesday/20230412.go'")
-    }
+func simplifyPath(path string) string {
+	// Create a splited slice of elements.
+	els := strings.Split(path, "/")
+
+	// Generate answer slices.
+	answer := []string{}
+
+	// Make a loop where skip unappropriate elements.
+	// And push and pop values to the answer.
+	for _, x := range els {
+		if "" == x {
+			continue
+		}
+		if "." == x {
+			continue
+		}
+		if ".." == x {
+			if 0 != len(answer) {
+				// Pop.
+				answer = answer[:len(answer)-1]
+			}
+		} else {
+			// Push.
+			answer = append(answer, x)
+		}
+	}
+	// Make first return. On condintion.
+	if 0 == len(answer) {
+		return "/"
+	}
+
+	// Make general return.
+	return "/" + strings.Join(answer, "/")
+}
