f2de4fb12a01d449f33ab3b79fddb861408191d0
Dmitry <lbvf50.mobile@gmail.com>
2023-04-19 00:52:22 +0700

    1554. Leetcode: 1768. Merge Strings Alternately. (go solution 2) Thanks God!

--- 20230418_Tuesday/20230418.go
+++ 20230418_Tuesday/20230418.go
@@ -4,29 +4,34 @@
 // Accepted.
 // Thanks God, Jesus Christ!
 // = = = = = = = = = = = = = =
-// Runtime: 3 ms, faster than 12.00% of Go online submissions for Merge Strings
+// Runtime: 2 ms, faster than 60.00% of Go online submissions for Merge Strings
 // Alternately.
-// Memory Usage: 2.6 MB, less than 26.00% of Go online submissions for Merge
+// Memory Usage: 2.1 MB, less than 45.00% of Go online submissions for Merge
 // Strings Alternately.
 // 2023.04.18 Daily Challenge.
 package main
 
 func mergeAlternately(word1 string, word2 string) string {
-	i, j := 0, 0
+	i, j, k := 0, 0, 0
 	m, n := len(word1), len(word2)
-	ans := ""
+	ans := make([]byte, m+n)
 	for i < m && j < n {
-		ans += string(word1[i]) + string(word2[j])
+		ans[k] = word1[i]
+		k += 1
 		i += 1
+		ans[k] = word2[j]
+		k += 1
 		j += 1
 	}
 	for i < m {
-		ans += string(word1[i])
+		ans[k] = word1[i]
+		k += 1
 		i += 1
 	}
 	for j < n {
-		ans += string(word2[j])
+		ans[k] = word2[j]
+		k += 1
 		j += 1
 	}
-	return ans
+	return string(ans)
 }
