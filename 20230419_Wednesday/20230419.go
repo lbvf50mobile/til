c3fa7f42be35f941e582a80c8a641933b6d70493
Dmitry <lbvf50.mobile@gmail.com>
2023-04-20 00:53:54 +0700

    1555. Leetcode: 1372. Longest ZigZag Path in a Binary Tree. (go solution) Thanks God!

--- 20230419_Wednesday/20230419.go
+++ 20230419_Wednesday/20230419.go
@@ -4,9 +4,9 @@
 // Accepted.
 // Thanks God, Jesus Christ!
 // = = = = = = = = = = = = = =
-// Runtime: 128 ms, faster than 40.00% of Go online submissions for Longest
+// Runtime: 104 ms, faster than 95.00% of Go online submissions for Longest
 // ZigZag Path in a Binary Tree.
-// Memory Usage: 13.8 MB, less than 65.00% of Go online submissions for Longest
+// Memory Usage: 13 MB, less than 85.00% of Go online submissions for Longest
 // ZigZag Path in a Binary Tree.
 // 2023.04.19 Daily Challenge.
 package main
@@ -22,25 +22,28 @@ package main
 var max int
 
 func longestZigZag(root *TreeNode) int {
-	// Based on Leetcode's solution.
+	// Based on Frank's Luo (https://leetcode.com/longluo/) solution.
+	// https://leetcode.com/problems/longest-zigzag-path-in-a-binary-tree/solution/1868016
 	max = 0
-	dfs(root, true, 0)
-	dfs(root, false, 0)
+	dfs(root, 0, 0)
+	dfs(root, 0, 0)
 	return max
 }
 
-func dfs(x *TreeNode, lft bool, n int) {
+func dfs(x *TreeNode, l int, r int) {
 	if nil == x {
 		return
 	}
-	if max < n {
-		max = n
+	if max < l {
+		max = l
 	}
-	if lft {
-		dfs(x.Left, true, 1)
-		dfs(x.Right, false, n+1)
-	} else {
-		dfs(x.Left, true, n+1)
-		dfs(x.Right, false, 1)
+	if max < r {
+		max = r
+	}
+	if nil != x.Left {
+		dfs(x.Left, r+1, 0)
+	}
+	if nil != x.Right {
+		dfs(x.Right, 0, l+1)
 	}
 }
