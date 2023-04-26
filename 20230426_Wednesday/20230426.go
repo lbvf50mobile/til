fd54f447f01afc30716e31d5bcd82983a4bed4d8
Dmitry <lbvf50.mobile@gmail.com>
2023-04-27 01:16:53 +0700

    1562. Leetcode: 258. Add Digits. (article and new solutions) Thanks God!

--- 20230426_Wednesday/20230426.go
+++ 20230426_Wednesday/20230426.go
@@ -4,7 +4,7 @@
 // Accepted.
 // Thanks God, Jesus Christ!
 // = = = = = = = = = = = = = =
-// Runtime: 3 ms, faster than 53.46% of Go online submissions for Add Digits.
+// Runtime: 0 ms, faster than 100.00% of Go online submissions for Add Digits.
 // Memory Usage: 2.1 MB, less than 13.21% of Go online submissions for Add
 // Digits.
 // 2023.04.26 Daily Challenge.
@@ -14,9 +14,7 @@ package main
 func addDigits(num int) int {
 	if 0 == num {
 		return 0
+	} else {
+		return 1 + (num-1)%9
 	}
-	if 0 == num%9 {
-		return 9
-	}
-	return num % 9
 }
--- 20230426_Wednesday/20230426.rb
+++ 20230426_Wednesday/20230426.rb
@@ -4,15 +4,13 @@
 # Accepted.
 # Thanks God, Jesus Christ!
 # = = = = = = = = = = = = = =
-# Runtime: 91 ms, faster than 23.08% of Ruby online submissions for Add Digits.
-# Memory Usage: 211.1 MB, less than 7.69% of Ruby online submissions for Add
+# Runtime: 84 ms, faster than 42.31% of Ruby online submissions for Add Digits.
+# Memory Usage: 210.9 MB, less than 92.31% of Ruby online submissions for Add
 # Digits.
 # 2023.04.26 Daily Challenge.
 # @param {Integer} num
 # @return {Integer}
 def add_digits(num)
   # Based on Leetcode's solution.
-  return 0 if 0 == num
-  return 9 if 0 == num % 9
-  return num % 9
+  0 == num ? 0 : 1 + (num-1)%9
 end
