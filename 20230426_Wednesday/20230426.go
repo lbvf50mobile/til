0ac18797c43fb69d50b0377dedc12cd47df38e8b
Dmitry <lbvf50.mobile@gmail.com>
2023-04-26 23:58:54 +0700

    1562. Leetcode: 258. Add Digits. (go solution) Thanks God!

--- 20230426_Wednesday/20230426.go
+++ 20230426_Wednesday/20230426.go
@@ -1,9 +1,31 @@
-    package main
+// Leetcode: 258. Add Digits.
+// https://leetcode.com/problems/add-digits/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 7 ms, faster than 12.58% of Go online submissions for Add Digits.
+// Memory Usage: 2.1 MB, less than 98.11% of Go online submissions for Add
+// Digits.
+// 2023.04.26 Daily Challenge.
 
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20230426_Wednesday/20230426.go'")
-    }
+package main
+
+import (
+	"strconv"
+)
+
+func addDigits(num int) int {
+	for num >= 10 {
+		num = sumDigits(num)
+	}
+	return num
+}
+
+func sumDigits(x int) int {
+	str, sum := strconv.Itoa(x), 0
+	for _, x := range str {
+		sum += int(x - '0')
+	}
+	return sum
+}
