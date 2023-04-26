6bc7f51e6b0d5bc8b4a776c00d58e12b0df2c116
Dmitry <lbvf50.mobile@gmail.com>
2023-04-27 01:07:16 +0700

    1562. Leetcode: 258. Add Digits. (article and soltuion) Thanks God!

--- 20230426_Wednesday/20230426.go
+++ 20230426_Wednesday/20230426.go
@@ -4,6 +4,9 @@
 // Accepted.
 // Thanks God, Jesus Christ!
 // = = = = = = = = = = = = = =
+// Runtime: 3 ms, faster than 53.46% of Go online submissions for Add Digits.
+// Memory Usage: 2.1 MB, less than 13.21% of Go online submissions for Add
+// Digits.
 // 2023.04.26 Daily Challenge.
 
 package main
