b90a68c5d29453c8a5386fd73d35a813f8e888d8
Dmitry <lbvf50.mobile@gmail.com>
2023-04-27 23:47:33 +0700

    1563. Leetcode: 319. Bulb Switcher. (ruby solution) Thanks God!

---
+++ 20230427_Thursday/20230427.go
@@ -0,0 +1,9 @@
+    package main
+
+    import (
+        "fmt"
+    )
+    
+    func main() {
+        fmt.Println("alias x='go run 20230427_Thursday/20230427.go'")
+    }
---
+++ 20230427_Thursday/20230427.rb
@@ -0,0 +1,18 @@
+# Leetcode: 319. Bulb Switcher.
+# https://leetcode.com/problems/bulb-switcher/
+# = = = = = = = = = = = = = =
+# Accepted.
+# Thanks God, Jesus Christ!
+# = = = = = = = = = = = = = =
+# Runtime: 79 ms, faster than 100.00% of Ruby online submissions for Bulb
+# Switcher.
+# Memory Usage: 211.1 MB, less than 33.33% of Ruby online submissions for Bulb
+# Switcher.
+# 2023.04.27 Daily Challenge.
+# @param {Integer} n
+# @return {Integer}
+def bulb_switch(n)
+  # Based on:
+  # https://leetcode.com/problems/bulb-switcher/solution/
+  Math.sqrt(n).to_i
+end
