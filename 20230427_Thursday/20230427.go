03e0672fe01ab8939739213286541dea6d8402dc
Dmitry <lbvf50.mobile@gmail.com>
2023-04-27 23:53:03 +0700

    1563. Leetcode: 319. Bulb Switcher. (go solution) Thanks God!

--- 20230427_Thursday/20230427.go
+++ 20230427_Thursday/20230427.go
@@ -1,9 +1,17 @@
-    package main
+// Leetcode: 319. Bulb Switcher.
+// https://leetcode.com/problems/bulb-switcher/
+// = = = = = = = = = = = = = =
+// Accpeted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 1 ms, faster than 71.43% of Go online submissions for Bulb Switcher.
+// Memory Usage: 1.9 MB, less than 100.00% of Go online submissions for Bulb
+// Switcher.
+// 2023.04.27 Daily Challenge.
+package main
 
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20230427_Thursday/20230427.go'")
-    }
+import "math"
+
+func bulbSwitch(n int) int {
+	return int(math.Sqrt(float64(n)))
+}
