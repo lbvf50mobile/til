ac7dfc678187d6e733d8b1360acb4a41df2063b4
Dmitry <lbvf50.mobile@gmail.com>
2023-04-28 23:41:06 +0700

    1564. Leetcode: 839. Similar String Groups. (tle in ruby solution) Thanks God!

--- 20230428_Friday/20230428.rb
+++ 20230428_Friday/20230428.rb
@@ -1,30 +1,45 @@
 # Leetcode: 839. Similar String Groups.
 # https://leetcode.com/problems/similar-string-groups/
+# = = = = = = = = = = = = = =
+# TLE. But correct.
+# Thanks God, Jesus Christ!
+# = = = = = = = = = = = = = =
+# TLE.
+# 2023.04.28 Daily Challenge.
 # @param {String[]} strs
 # @return {Integer}
 def num_similar_groups(strs)
-  @strs = strs
-  @v = Array.new(@strs.size,false)
-  ans = 0
-  (0...@strs.size).each do |i|
-    if ! @v[i]
-      dfs(i)
-      ans += 1
+  # Hint from the Leetcode's solution.
+  @n = strs[0].size
+  @al = Array.new(strs.size){ Array.new }
+  (0...strs.size).each do |i|
+    (i+1...strs.size).each do |j|
+      next if ! similar(strs[i],strs[j])
+      @al[i].push(j)
+      @al[j].push(i)
     end
   end
-  return ans
+  @v = Array.new(strs.size,false)
+  ans = 0
+  (0...strs.size).each do |i|
+    next if @v[i]
+    dfs(i)
+    ans += 1
+  end
+  return ans 
 end
+
 def dfs(i)
   @v[i] = true
-  (i+1...@strs.size).each do |j|
+  @al[i].each do |j|
     next if @v[j]
-    next if ! similar(@strs[i], @strs[j])
     dfs(j)
   end
 end
+
 def similar(a,b)
   cnt = 0
-  (0...a.size).each do |i|
+  (0...@n).each do |i|
     if a[i] != b[i]
       cnt += 1
     end
