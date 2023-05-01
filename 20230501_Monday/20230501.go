5e35e1d69f7a2ab50c50561b9ea4c19ac16644c5
Dmitry <lbvf50.mobile@gmail.com>
2023-05-01 23:43:22 +0700

    1567. Leetcode: 1491. Average Salary Excluding the Minimum and Maximum Salary. (article and solution) Thanks God!

--- 20230501_Monday/20230501.go
+++ 20230501_Monday/20230501.go
@@ -1,5 +1,14 @@
 // Leetcode: 1491. Average Salary Excluding the Minimum and Maximum Salary.
 // https://leetcode.com/problems/average-salary-excluding-the-minimum-and-maximum-salary/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 2 ms, faster than 68.66% of Go online submissions for Average Salary
+// Excluding the Minimum and Maximum Salary.
+// Memory Usage: 2 MB, less than 99.00% of Go online submissions for Average
+// Salary Excluding the Minimum and Maximum Salary.
+// 2023.05.01 Daily Challenge.
 package main
 
 func average(salary []int) float64 {
