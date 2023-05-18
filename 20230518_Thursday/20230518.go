ccf86f59f3683f0a0bd426b5fe86ff68aaa61ee5
Dmitry <lbvf50.mobile@gmail.com>
2023-05-19 03:20:20 +0700

    1584. Leetcode: 1557. Minimum Number of Vertices to Reach All Nodes. (go solution) Thanks God!

--- 20230518_Thursday/20230518.go
+++ 20230518_Thursday/20230518.go
@@ -1,9 +1,27 @@
-    package main
+// Leetcode: 1557. Minimum Number of Vertices to Reach All Nodes.
+// https://leetcode.com/problems/minimum-number-of-vertices-to-reach-all-nodes/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 208 ms, faster than 27.62% of Go online submissions for Minimum
+// Number of Vertices to Reach All Nodes.
+// Memory Usage: 17.8 MB, less than 51.43% of Go online submissions for Minimum
+// Number of Vertices to Reach All Nodes.
+// 2023.05.18 Daily Challenge.
+package main
 
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20230518_Thursday/20230518.go'")
-    }
+func findSmallestSetOfVertices(n int, edges [][]int) []int {
+	out := make([]bool, n)
+	for _, edge := range edges {
+		_, b := edge[0], edge[1]
+		out[b] = true
+	}
+	ans := []int{}
+	for i := 0; i < n; i += 1 {
+		if !out[i] {
+			ans = append(ans, i)
+		}
+	}
+	return ans
+}
