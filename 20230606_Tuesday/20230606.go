d3a374a6853cf9bd08c778e71a353c9e3cb538f1
Dmitry <lbvf50.mobile@gmail.com>
2023-06-08 22:08:00 +0700

    1603. Leetcode: 1502. Can Make Arithmetic Progression From Sequence. (go solution) Thanks God!

--- 20230606_Tuesday/20230606.go
+++ 20230606_Tuesday/20230606.go
@@ -1,9 +1,26 @@
-    package main
+// Leetcode: 1502. Can Make Arithmetic Progression From Sequence.
+// https://leetcode.com/problems/can-make-arithmetic-progression-from-sequence/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 4 ms, faster than 54.23% of Go online submissions for Can Make
+// Arithmetic Progression From Sequence.
+// Memory Usage: 2.5 MB, less than 100.00% of Go online submissions for Can Make
+// Arithmetic Progression From Sequence.
+// 2023.06.07 Update.
+package main
 
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20230606_Tuesday/20230606.go'")
-    }
+import "sort"
+
+func canMakeArithmeticProgression(arr []int) bool {
+	a := arr
+	sort.Ints(a)
+	d := a[1] - a[0]
+	for i := 2; i < len(a); i += 1 {
+		if a[i]-a[i-1] != d {
+			return false
+		}
+	}
+	return true
+}
