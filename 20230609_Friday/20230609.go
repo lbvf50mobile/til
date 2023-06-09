7d26053cdecc17587c7fb7857346fe71805a573f
Dmitry <lbvf50.mobile@gmail.com>
2023-06-09 23:26:35 +0700

    1606. Leetcode: 744. Find Smallest Letter Greater Than Target. (go solution) Thanks God!

--- 20230609_Friday/20230609.go
+++ 20230609_Friday/20230609.go
@@ -1,9 +1,27 @@
-    package main
+// Leetcode: 744. Find Smallest Letter Greater Than Target.
+// https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 3 ms, faster than 53.19% of Go online submissions for Find Smallest
+// Letter Greater Than Target.
+// Memory Usage: 2.6 MB, less than 46.10% of Go online submissions for Find
+// Smallest Letter Greater Than Target.
+// 2023.06.09 Daily Challenge.
+package main
 
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20230609_Friday/20230609.go'")
-    }
+import ()
+
+func nextGreatestLetter(letters []byte, target byte) byte {
+	fst, lst := letters[0], letters[len(letters)-1]
+	if lst <= target {
+		return fst
+	}
+	for _, v := range letters {
+		if v > target {
+			return v
+		}
+	}
+	return fst
+}
