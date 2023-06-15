abae74cf4b3db12992ddb4cb3b9e88890255b98c
Dmitry <lbvf50.mobile@gmail.com>
2023-06-16 00:13:20 +0700

    1612. Leetcode: 1161. Maximum Level Sum of a Binary Tree. (not optimal ruby solution) Thanks God!

--- 20230615_Thursday/20230615.rb
+++ 20230615_Thursday/20230615.rb
@@ -1,5 +1,14 @@
 # Leetcode: 1161. Maximum Level Sum of a Binary Tree.
 # https://leetcode.com/problems/maximum-level-sum-of-a-binary-tree/
+# = = = = = = = = = = = = = =
+# Accepted.
+# Thanks God, Jesus Christ!
+# = = = = = = = = = = = = = =
+# Runtime: 306 ms, faster than 7.14% of Ruby online submissions for Maximum
+# Level Sum of a Binary Tree.
+# Memory Usage: 214.1 MB, less than 100.00% of Ruby online submissions for
+# Maximum Level Sum of a Binary Tree.
+# 2023.06.15 Daily Challenge.
 # Definition for a binary tree node.
 # class TreeNode
 #     attr_accessor :val, :left, :right
@@ -18,8 +27,9 @@ def max_level_sum(root)
     x,l = q.shift
     h[l] ||= 0
     h[l] += x.val
-    q.push([x.left,l+1]) if x.left
-    q.push([x.right,l+1]) if x.right
+   q.push([x.left,l+1]) if x.left
+   q.push([x.right,l+1]) if x.right
   end
-  return h.to_a.max{|x| x.last}.first 
+  max = h.values.max
+  h.to_a.select{|x| x.last == h.values.max}.sort_by(&:first).first.first 
 end
