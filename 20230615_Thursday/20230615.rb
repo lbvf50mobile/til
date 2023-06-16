8f3c53d39eb82e37e637ea74e09c682a19046e5d
Dmitry <lbvf50.mobile@gmail.com>
2023-06-16 17:46:18 +0700

    1612. Leetcode: 1161. Maximum Level Sum of a Binary Tree. (ruby solution) Thanks God!

--- 20230615_Thursday/20230615.rb
+++ 20230615_Thursday/20230615.rb
@@ -4,11 +4,11 @@
 # Accepted.
 # Thanks God, Jesus Christ!
 # = = = = = = = = = = = = = =
-# Runtime: 306 ms, faster than 7.14% of Ruby online submissions for Maximum
+# Runtime: 279 ms, faster than 7.14% of Ruby online submissions for Maximum
 # Level Sum of a Binary Tree.
-# Memory Usage: 214.1 MB, less than 100.00% of Ruby online submissions for
+# Memory Usage: 214.6 MB, less than 100.00% of Ruby online submissions for
 # Maximum Level Sum of a Binary Tree.
-# 2023.06.15 Daily Challenge.
+# 2023.06.16 Updated.
 # Definition for a binary tree node.
 # class TreeNode
 #     attr_accessor :val, :left, :right
@@ -21,15 +21,23 @@
 # @param {TreeNode} root
 # @return {Integer}
 def max_level_sum(root)
-  h = {}
   q = [[root,1]]
+  max = root.val
+  l = 1
   while !q.empty?
-    x,l = q.shift
-    h[l] ||= 0
-    h[l] += x.val
-   q.push([x.left,l+1]) if x.left
-   q.push([x.right,l+1]) if x.right
+    sum = 0
+    nq = []
+    current_level = q[0].last 
+    q.each do |node,lvl|
+      sum += node.val 
+      nq.push([node.left,lvl+1]) if node.left
+      nq.push([node.right,lvl+1]) if node.right
+    end
+    q = nq
+    if max < sum 
+      max = sum
+      l = current_level
+    end
   end
-  max = h.values.max
-  h.to_a.select{|x| x.last == h.values.max}.sort_by(&:first).first.first 
+  l
 end
