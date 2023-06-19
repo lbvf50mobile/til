bf944429bbeca32bf39cb70cd130e8d74b1fa3b3
Dmitry <lbvf50.mobile@gmail.com>
2023-06-19 23:31:44 +0700

    1616. Leetcode: 1732. Find the Highest Altitude. (go solution) Thanks God!

--- 20230619_Monday/20230619.go
+++ 20230619_Monday/20230619.go
@@ -1,9 +1,26 @@
-    package main
+// Leetcode: 1732. Find the Highest Altitude.
+// https://leetcode.com/problems/find-the-highest-altitude/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 2 ms, faster than 65.58% of Go online submissions for Find the
+// Highest Altitude.
+// Memory Usage: 2.2 MB, less than 97.21% of Go online submissions for Find the
+// Highest Altitude.
+// 2023.06.19 Daily Challenge.
+package main
 
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20230619_Monday/20230619.go'")
-    }
+import ()
+
+func largestAltitude(gain []int) int {
+	curr := 0
+	max := 0
+	for _, x := range gain {
+		curr += x
+		if curr > max {
+			max = curr
+		}
+	}
+	return max
+}
