52f418c4f58265db6ce32c94080c91921592735a
Dmitry <lbvf50.mobile@gmail.com>
2023-06-27 02:04:18 +0700

    1623. Leetcode: 2462. Total Cost to Hire K Workers. (ruby in action 2) Thanks God!

--- 20230626_Monday/20230626.rb
+++ 20230626_Monday/20230626.rb
@@ -5,8 +5,12 @@
 # @param {Integer} candidates
 # @return {Integer}
 # Error:
-#
+# [31,25,72,79,74,65,84,91,18,59,27,9,81,33,17,58]
+# 11
+# 2
 def total_cost(costs, k, candidates)
+  # Hint from:
+  # https://leetcode.com/problems/total-cost-to-hire-k-workers/solution/
   return costs.sum if k == costs.size
   return costs.sort[0...k].sum if 2*candidates >= costs.size
   ans = 0
@@ -15,16 +19,25 @@ def total_cost(costs, k, candidates)
   a = MinHeap.new(costs[0...candidates])
   b = MinHeap.new(costs[-candidates..-1])
   k.times do 
-    if a.min < b.min
+    if i > j
+      if a.empty?
+        ans += b.pop
+      elsif b.empty?
+        ans += a.pop
+      elsif a.min <= b.min
+        ans += a.pop
+      elsif b.min < a.min
+        ans += b.pop
+      else
+        raise "Not enought space to fill k elements."
+      end
+      next
+    end
+    if a.min <= b.min
       ans += a.min
       a.pop
       a.push(costs[i])
       i += 1
-    elsif b.min < a.min
-      ans += b.min
-      b.pop
-      b.push(costs[j])
-      j -= 1
     else
       ans += a.min
       a.pop
