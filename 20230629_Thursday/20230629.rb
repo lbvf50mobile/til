942db4e7ddcd3f794d4116d455348f3d0fed582c
Dmitry <lbvf50.mobile@gmail.com>
2023-06-29 23:51:33 +0700

    1626. Leetcode: 864. Shortest Path to Get All Keys. (ruby in action) Thanks God!

---
+++ 20230629_Thursday/20230629.rb
@@ -0,0 +1,7 @@
+# Leetcode: 864. Shortest Path to Get All Keys.
+# https://leetcode.com/problems/shortest-path-to-get-all-keys/
+# @param {String[]} grid
+# @return {Integer}
+def shortest_path_all_keys(grid)
+    
+end
