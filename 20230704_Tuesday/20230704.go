d49b83467546d900371e398a0ce1fede1d5613e0
Dmitry <lbvf50.mobile@gmail.com>
2023-07-05 02:22:37 +0700

    1631. Leetcode: 137. Single Number II. (go code) Thanks God!

--- 20230704_Tuesday/20230704.go
+++ 20230704_Tuesday/20230704.go
@@ -1,9 +1,30 @@
-    package main
+// Leetcode: 137. Single Number II.
+// https://leetcode.com/problems/single-number-ii/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 8 ms, faster than 21.19% of Go online submissions for Single Number
+// II.
+// Memory Usage: 3.9 MB, less than 26.27% of Go online submissions for Single
+// Number II.
+// 2023.07.04 Daily Challenge.
+package main
 
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20230704_Tuesday/20230704.go'")
-    }
+import (
+	"fmt"
+)
+
+func singleNumber(nums []int) int {
+	hash := make(map[int]int)
+	for _, v := range nums {
+		hash[v] += 1
+		if 3 == hash[v] {
+			delete(hash, v)
+		}
+	}
+	for k, _ := range hash {
+		return k  
+	}
+	return -1
+}
