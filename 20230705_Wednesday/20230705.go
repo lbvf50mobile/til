c0173201aa64d5314612f7b40e92fd0a21b49e5f
Dmitry <lbvf50.mobile@gmail.com>
2023-07-05 23:22:46 +0700

    1632. Leetcode: 1493. Longest Subarray of 1's After Deleting One Element. (go solution 2) Thanks God!

--- 20230705_Wednesday/20230705.go
+++ 20230705_Wednesday/20230705.go
@@ -4,10 +4,10 @@
 // Accepted.
 // Thanks God, Jesus Christ!
 // = = = = = = = = = = = = = =
-// Runtime: 52 ms, faster than 7.20% of Go online submissions for Longest
+// Runtime: 46 ms, faster than 30.30% of Go online submissions for Longest
 // Subarray of 1's After Deleting One Element.
-// Memory Usage: 8 MB, less than 9.09% of Go online submissions for Longest
-// Subarray of 1's After Deleting One Element.
+//Memory Usage: 7.1 MB, less than 90.53% of Go online submissions for Longest
+//Subarray of 1's After Deleting One Element.
 // 2023.07.05 Daily Challenge.
 
 package main
@@ -17,39 +17,45 @@ import (
 )
 
 func longestSubarray(nums []int) int {
-	i, j := 0, 0
-	n := len(nums)
-	zeros := 0
-	if 0 == nums[0] {
-		zeros = 1
-	}
+	// Adding concept of step. Step is 1 when previos is zero or begining of the
+	// aray.
+	stepIndices := make([]int, 0)
 	ans := 0
-	for i < n {
-		// Calculate.
-		// Always take one element from lenght.
-		l := j - i
-		if zeros < 2 && ans < l {
+	zeros := 0
+	for i, v := range nums {
+		// Nothing to do with zeros.
+		if 0 == v {
+			zeros += 1
+			if 2 == zeros {
+				stepIndices = stepIndices[:0]
+			}
+			continue
+		}
+		zeros = 0
+		// May be it is a step.
+		if 1 == v && (0 == i || 0 == nums[i-1]) {
+			stepIndices = append(stepIndices, i)
+			// Need only two step indices. Not more then two.
+			if 2 < len(stepIndices) {
+				stepIndices = stepIndices[1:]
+			}
+		}
+		// Time to calculate current length.
+		// Do not add one because always need.
+		l := i - stepIndices[0]
+
+		// Take zero left.
+		if 1 == len(stepIndices) && 0 != stepIndices[0] {
+			l += 1 // take one zero.
+		}
+		// Take zero right.
+		if 1 == len(stepIndices) && 0 == stepIndices[0] && i < len(nums) - 1 && 0 == nums[i+1] {
+			l += 1
+		}
+
+		// And save ans if l > ans.
+		if l > ans {
 			ans = l
-		}
-		// Condition to grow.
-		// If there is a place to grow.
-		// And less then two zeros.
-		if zeros < 2 && j < n-1 {
-			j += 1
-			if 0 == nums[j] {
-				zeros += 1
-			}
-			continue
-		}
-		// Condition to shirnk.
-		// Two or more zeros.
-		// No place to grow for j.
-		if zeros >= 2 || n-1 == j {
-			if 0 == nums[i] {
-				zeros -= 1
-			}
-			i += 1
-			continue
 		}
 	}
 	return ans
