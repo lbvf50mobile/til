45d4b0d794b815500544b772cb6afcff2a76b160
Dmitry <lbvf50.mobile@gmail.com>
2023-07-07 00:15:21 +0700

    1633. Leetcode: 209. Minimum Size Subarray Sum. (ruby solution) Thanks God!

--- 20230706_Thursday/20230706.rb
+++ 20230706_Thursday/20230706.rb
@@ -1,5 +1,14 @@
 # Leetcode: 209. Minimum Size Subarray Sum.
 # https://leetcode.com/problems/minimum-size-subarray-sum/
+# = = = = = = = = = = = = = =
+# Accepted.
+# Thanks God, Jesus Christ!
+# = = = = = = = = = = = = = =
+# Runtime: 142 ms, faster than 9.09% of Ruby online submissions for Minimum Size
+# Subarray Sum.
+# Memory Usage: 213.9 MB, less than 90.91% of Ruby online submissions for
+# Minimum Size Subarray Sum.
+# 2023.07.06 Daily Challenge.
 # @param {Integer} target
 # @param {Integer[]} nums
 # @return {Integer}
@@ -14,9 +23,10 @@ def min_sub_array_len(target, nums)
     if sum >= target && l < ans
       ans = l
     end
-    while sum >= target && j <= i
+    while (sum >= target || nums.size - 1 == i) && j <= i
       sum -= nums[j]
-      l = i -j + 1
+      j += 1
+      l = i - j + 1
       ans = l if sum >= target && l < ans
     end
   end
