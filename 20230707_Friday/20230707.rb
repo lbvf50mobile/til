027fac9d08dca1c67114707109da1a8c4c267d54
Dmitry <lbvf50.mobile@gmail.com>
2023-07-08 00:10:55 +0700

    1634. Leetcode: 2024. Maximize the Confusion of an Exam. (ruby solution) Thanks God!

--- 20230707_Friday/20230707.rb
+++ 20230707_Friday/20230707.rb
@@ -1,13 +1,21 @@
 # Leetcode: 2024. Maximize the Confusion of an Exam.
 # https://leetcode.com/problems/maximize-the-confusion-of-an-exam/
+# = = = = = = = = = = = = = =
+# Accepted.
+# Thanks God, Jesus Christ!
+# = = = = = = = = = = = = = =
+# Runtime: 1696 ms, faster than 100.00% of Ruby online submissions for Maximize
+# the Confusion of an Exam.
+# Memory Usage: 227.7 MB, less than 100.00% of Ruby online submissions for
+# Maximize the Confusion of an Exam.
+# 2023.07.07 Daily Challenge.
 # @param {String} answer_key
 # @param {Integer} k
 # @return {Integer}
-# TLE.
 def max_consecutive_answers(answer_key, k)
   # Based on:
   # https://leetcode.com/problems/maximize-the-confusion-of-an-exam/solution/
-  @ak, @k, @n = answer_key, k, answer_key.size
+  @ak, @k, @n = answer_key.chars, k, answer_key.size
 
   l,r = @k, @n
   while l < r
@@ -22,13 +30,13 @@ def max_consecutive_answers(answer_key, k)
 end
 
 def is_valid(s)
-  c = @ak.chars[0...s].tally
+  c = @ak[0...s].tally
   c.default = 0
-  return true if [c[?T],c[?F]].min <= @k
+  return true if c[?T] <= @k || c[?F] <= @k
   (s...@n).each do |i|
     c[@ak[i]] += 1
     c[@ak[i-s]] -= 1
-    return true if [c[?T],c[?F]].min <= @k
+    return true if c[?T] <= @k || c[?F] <= @k
   end
   return false
 end
