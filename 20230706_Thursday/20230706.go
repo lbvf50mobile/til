fc4109806ba5040ed8e3d2d2dac32f2ee2ab266d
Dmitry <lbvf50.mobile@gmail.com>
2023-07-07 14:13:46 +0700

    1633. Leetcode: 209. Minimum Size Subarray Sum. (go solution) Thanks God!

--- 20230706_Thursday/20230706.go
+++ 20230706_Thursday/20230706.go
@@ -1,9 +1,36 @@
-    package main
+// Leetcode: 209. Minimum Size Subarray Sum.
+// https://leetcode.com/problems/minimum-size-subarray-sum/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 34 ms, faster than 83.18% of Go online submissions for Minimum Size
+// Subarray Sum.
+// Memory Usage: 6.9 MB, less than 93.27% of Go online submissions for Minimum
+// Size Subarray Sum.
+// 2023.07.07 Updated.
+package main
 
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20230706_Thursday/20230706.go'")
-    }
+func minSubArrayLen(target int, nums []int) int {
+	n, ans := len(nums), len(nums)+1
+	i, sum := 0, 0
+	for j, x := range nums {
+		l := j - i + 1
+		sum += x
+		if sum >= target && l < ans {
+			ans = l
+		}
+		for (sum >= target || n-1 == j) && i < j {
+			sum -= nums[i]
+			i += 1
+			l := j - i + 1
+			if sum >= target && l < ans {
+				ans = l
+			}
+		}
+	}
+	if n+1 == ans {
+		return 0
+	}
+	return ans
+}
