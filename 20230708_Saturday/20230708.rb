6f3576aa98f8c812bf3b3c3c9266b0f3b9e12b3b
Dmitry <lbvf50.mobile@gmail.com>
2023-07-08 23:47:40 +0700

    1635. Leetcode: 2551. Put Marbles in Bags. (ruby in action) Thanks God!

---
+++ 20230708_Saturday/20230708.rb
@@ -0,0 +1,10 @@
+# Leetcode: 2551. Put Marbles in Bags.
+# https://leetcode.com/problems/put-marbles-in-bags/
+# @param {Integer[]} weights
+# @param {Integer} k
+# @return {Integer}
+def put_marbles(weights, k)
+  # Based on:
+  # https://leetcode.com/problems/put-marbles-in-bags/solution/
+    
+end
