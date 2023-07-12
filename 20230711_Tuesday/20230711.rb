242dbe46ad7061810323d0485d7f2fa6c1bc0b75
Dmitry <lbvf50.mobile@gmail.com>
2023-07-12 22:21:43 +0700

    1638. Leetcode: 863. All Nodes Distance K in Binary Tree. (debug free ruby code) Thanks God!

--- 20230711_Tuesday/20230711.rb
+++ 20230711_Tuesday/20230711.rb
@@ -4,9 +4,9 @@
 # Accepted.
 # Thanks God, Jesus Christ!
 # = = = = = = = = = = = = = =
-# Runtime: 80 ms, faster than 100.00% of Ruby online submissions for All Nodes
+# Runtime: 95 ms, faster than 25.00% of Ruby online submissions for All Nodes
 # Distance K in Binary Tree.
-# Memory Usage: 211.5 MB, less than 25.00% of Ruby online submissions for All
+# Memory Usage: 211.3 MB, less than 100.00% of Ruby online submissions for All
 # Nodes Distance K in Binary Tree.
 # 2023.07.12 Updated.
 # Definition for a binary tree node.
@@ -28,22 +28,13 @@ class Tn
   end
 end
 def distance_k(root, target, k)
-  @d = false
-  # Debugging summary.
-  # 1. Read types.
-  # 2. Graph need visited map for BFS.
-  # 3. Vairable with a method name.
   @root, @target, @k = root, target, k
-  p [@root.nil?, @target.nil?, @k, "Begining."] if @d
   # Target node.
   @tn = nil
   # Build tree.
-  p "Start bulid tree." if @d
   build_t(root,nil)
-  p "Tree is builded." if @d
   ans = []
   # No target in the tree.
-  p [@tn.nil?, "target node"] if @d
   return ans if @tn.nil?
   if 0 == @k
     ans.push @tn.v # here was an error!
@@ -61,10 +52,6 @@ def distance_k(root, target, k)
       nq.push x.r if x.r && (! visited[x.r])
       nq.push x.t if x.t && (! visited[x.t])
     end
-    if @d
-      p c
-      p nq.map{|x| x.v }
-    end
     # Need to generate answer.
     # Answer's layer.
     if @k == c
@@ -78,12 +65,10 @@ end
 
 # Build tree.
 def build_t(node,t = nil)
-  p [node.nil? ? "nil" : node.val, t.nil? ? "nil" : t.v] if @d
   return nil if node.nil?
   l = node.left
   r = node.right
   v = node.val
-  p v if @d
   nd = Tn.new(v,nil,nil,t)
   nd.l = build_t(l,nd)
   nd.r = build_t(r,nd)
