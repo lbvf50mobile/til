0827ca992d485f58ccbdb429e0ba3f7576951a5d
Dmitry <lbvf50.mobile@gmail.com>
2023-07-18 00:45:46 +0700

    1644. Leetcode: 445. Add Two Numbers II. (go in action) Thanks God!

--- 20230717_Monday/20230717.go
+++ 20230717_Monday/20230717.go
@@ -1,9 +1,21 @@
-    package main
+// Leetcode: 445. Add Two Numbers II.
+// https://leetcode.com/problems/add-two-numbers-ii/
+package lc445
 
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20230717_Monday/20230717.go'")
-    }
+import (
+)
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+
+}
+func AddTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	return addTwoNumbers(l1,l2)
+
+}
---
+++ 20230717_Monday/20230717_test.go
@@ -0,0 +1,19 @@
+package lc445
+
+import (
+	"test"
+)
+
+func TestAddTwoNumbers(t *testing.T) {
+	var tsts = []struct {
+		l1  []int
+		l2  []int
+		ans []int
+	}{
+		{
+			[]int{},
+			[]int{},
+			[]int{},
+		},
+	}
+}
