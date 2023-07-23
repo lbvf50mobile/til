944a93674ae374fa4fcd3f8836c96b8fc8a9b2f9
Dmitry <lbvf50.mobile@gmail.com>
2023-07-23 23:58:00 +0700

    1650. Leetcode: 894. All Possible Full Binary Trees. (article and solution) Thanks God!

--- 20230723_Sunday/20230723.py
+++ 20230723_Sunday/20230723.py
@@ -1,3 +1,5 @@
+# Leetcode: 894. All Possible Full Binary Trees.
+# https://leetcode.com/problems/all-possible-full-binary-trees/
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -5,6 +7,8 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    # Leetcode's solution.
+    # https://leetcode.com/problems/all-possible-full-binary-trees/solution/
     def allPossibleFBT(self, n: int) -> List[TreeNode]:
         if n % 2 == 0:
             return []
