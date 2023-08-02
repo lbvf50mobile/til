abf1bbf61b5e920c1ff5c2dd12d6f868a60824de
Dmitry <lbvf50.mobile@gmail.com>
2023-08-02 23:51:28 +0700

    1660. Leetcode: 46. Permutations. (ruby solution) Thanks God!

---
+++ 20230802_Wednesday/20230802.go
@@ -0,0 +1,9 @@
+    package main
+
+    import (
+        "fmt"
+    )
+    
+    func main() {
+        fmt.Println("alias x='go run 20230802_Wednesday/20230802.go'")
+    }
---
+++ 20230802_Wednesday/20230802.rb
@@ -0,0 +1,16 @@
+# Leetcode: 46. Permutations.
+# https://leetcode.com/problems/permutations/
+# = = = = = = = = = = = = = =
+# Accepted.
+# Thanks God, Jesus Christ!
+# = = = = = = = = = = = = = =
+# Runtime: 58 ms, faster than 98.25% of Ruby online submissions for
+# Permutations.
+# Memory Usage: 211 MB, less than 50.88% of Ruby online submissions for
+# Permutations.
+# 2023.08.02 Daily Challenge.
+# @param {Integer[]} nums
+# @return {Integer[][]}
+def permute(nums)
+  nums.permutation.to_a
+end
