3c1c59ef547b41b42d6ae162795f5b3245ed5af2
Dmitry <lbvf50.mobile@gmail.com>
2023-08-05 23:52:35 +0700

    1663. Leetcode: 95. Unique Binary Search Trees II. (ruby solution) Thanks God!

---
+++ 20230805_Saturday/20230805.py
@@ -0,0 +1,30 @@
+# Leetcode: 95. Unique Binary Search Trees II.
+# https://leetcode.com/problems/unique-binary-search-trees-ii/
+class Solution:
+    # Copied from the:
+    # https://leetcode.com/problems/unique-binary-search-trees-ii/solution/
+    def allPossibleBST(self, start, end, memo):
+        res = []
+        if start > end:
+            res.append(None)
+            return res
+        if (start, end) in memo:
+            return memo[(start, end)]
+        
+        # Iterate through all values from start to end to construct left and right subtree recursively.
+        for i in range(start, end + 1):
+            leftSubTrees = self.allPossibleBST(start, i - 1, memo)
+            rightSubTrees = self.allPossibleBST(i + 1, end, memo)
+            
+            # Loop through all left and right subtrees and connect them to ith root.
+            for left in leftSubTrees:
+                for right in rightSubTrees:
+                    root = TreeNode(i, left, right)
+                    res.append(root)
+        
+        memo[(start, end)] = res
+        return res
+    
+    def generateTrees(self, n: int) -> List[Optional[TreeNode]]:
+        memo = {}
+        return self.allPossibleBST(1, n, memo)
