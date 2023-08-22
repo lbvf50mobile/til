e199e0514c77705de41f1f3a4d979372f95a2215
Dmitry <lbvf50.mobile@gmail.com>
2023-08-23 01:14:47 +0700

    1680. Leetcode: 168. Excel Sheet Column Title. (some idea) Thanks God!

--- 20230822_Tuesday/20230822.rb
+++ 20230822_Tuesday/20230822.rb
@@ -2,33 +2,19 @@
 # https://leetcode.com/problems/excel-sheet-column-title/
 # @param {Integer} column_number
 # @return {String}
-# TLE.
 def convert_to_title(column_number)
-  counter = [0]
-  (1..column_number).each do |i|
-    carry = 1
-    j = 0
-    while 1 == carry
-      if counter.size > j
-        if counter[j] < 26
-          counter[j] += 1
-          carry = 0
-        else
-          counter[j] = 1
-          j += 1
-          carry = 1
-        end
-      else
-        counter.push(1)
-        carry = 0
-      end
-    end
+  cn = column_number
+  min = 1
+  top = 27
+  ans = []
+  while cn >= min
+    area = cn % top
+    ans.unshift(area/min)
+    min = top
+    top *= 26
   end
-  counter.reverse.map{|x| (?A.ord + (x-1)).chr}.join
+  ans.map{|x| (?A.ord + (x-1)).chr}.join
 end
-# 1 - 26  [A - Z]
-# 27 : 27+25=52 [AA - AZ] 26
-# 53 : 53+25=76 [BA - BZ] 26
-#
-# 1 - 26 A-z
-# 2 - A 1
+# 1 - 26  [A - Z] 
+# 26*26 [A* - B*]
+# 26*26*26 [A** - B**]
