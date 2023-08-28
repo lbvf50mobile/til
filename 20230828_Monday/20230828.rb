30693c9acd831d6c185d472f0c8a46970fcb8241
Dmitry <lbvf50.mobile@gmail.com>
2023-08-28 23:44:00 +0700

    1686. Leetcode: 225. Implement Stack using Queues. (ruby in action) Thanks God!

---
+++ 20230828_Monday/20230828.rb
@@ -0,0 +1,53 @@
+# Leetcode: 225. Implement Stack using Queues.
+# https://leetcode.com/problems/implement-stack-using-queues/
+
+class MyStack
+    def initialize()
+      
+      @q1 = []
+      @q2 = []
+        
+    end
+
+
+=begin
+    :type x: Integer
+    :rtype: Void
+=end
+    def push(x)
+        
+    end
+
+
+=begin
+    :rtype: Integer
+=end
+    def pop()
+        
+    end
+
+
+=begin
+    :rtype: Integer
+=end
+    def top()
+        
+    end
+
+
+=begin
+    :rtype: Boolean
+=end
+    def empty()
+        
+    end
+
+
+end
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack.new()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
