2684eb571d494c532be7e51eda1300379087d931
Dmitry <lbvf50.mobile@gmail.com>
2023-09-09 23:59:04 +0700

    1679. Leetcode: 377. Combination Sum IV. (ruby solution) Thanks God\!

---
+++ 20230909_Saturday/20230909.rb
@@ -0,0 +1,30 @@
+# Leetcode: 377. Combination Sum IV.
+# https://leetcode.com/problems/combination-sum-iv
+# = = = = = = = = = = = = = =
+# Accepted.
+# Thanks God, Jesus Christ!
+# = = = = = = = = = = = = = =
+# Runtime: 68 ms, faster than 85.71% of Ruby online submissions for Combination
+# Sum IV.
+# Memory Usage: 211.1 MB, less than 42.86% of Ruby online submissions for
+# Combination Sum IV.
+# 2023.09.09 Daily Challenge.
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer}
+def combination_sum4(nums, target)
+  nums, combs = nums.sort, [1] + [0]*target
+  (0..target).each do |i|
+    nums.each do |num|
+      break if num > i
+      if num == i
+        combs[i] += 1
+      end
+      if num < i
+        combs[i] += combs[i-num]
+      end
+    end
+  end
+  return combs[target]
+end
+
