0d4801c3e4f4c75967ca4ffc798dd8734c7e1992
Dmitry <lbvf50.mobile@gmail.com>
2023-09-16 01:23:32 +0700

    1685. Leetcode: 1584. Min Cost to Connect All Points. (python solution) Thanks God!

---
+++ 20230915_Friday/20230915.py
@@ -0,0 +1,43 @@
+# Leetcode: 1584. Min Cost to Connect All Points.
+# https://leetcode.com/problems/min-cost-to-connect-all-points/
+# = = = = = = = = = = = = = =
+# Accepted. 
+# Thanks God, Jesus Christ!
+# = = = = = = = = = = = = = =
+# Runtime: 766 ms, faster than 93.05% of Python3 online submissions for Min Cost
+# to Connect All Points.
+# Memory Usage: 20.1 MB, less than 94.90% of Python3 online submissions for Min
+# Cost to Connect All Points.
+# 2023.09.15 Daily Challenge.
+def manhattan_distance(p1: List[int], p2: List[int]) -> int:
+    return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+
+class Solution:
+    # Copied form:
+    # https://leetcode.com/problems/min-cost-to-connect-all-points/discuss/4045874/94.85-Prim-and-Kruskal-with-Min-Heap
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        n = len(points)
+        visited = [False] * n
+        heap_dict = {0: 0}  
+        min_heap = [(0, 0)]
+        
+        mst_weight = 0
+        
+        while min_heap:
+            w, u = heappop(min_heap)
+            
+            if visited[u] or heap_dict.get(u, float('inf')) < w:
+                continue
+            
+            visited[u] = True
+            mst_weight += w
+            
+            for v in range(n):
+                if not visited[v]:
+                    new_distance = manhattan_distance(points[u], points[v])
+      
+                    if new_distance < heap_dict.get(v, float('inf')):
+                        heap_dict[v] = new_distance
+                        heappush(min_heap, (new_distance, v))
+        
+        return mst_weight
--- 20230915_Friday/20230915.rb
+++ 20230915_Friday/20230915.rb
@@ -1,1 +1,9 @@
 # Leetcode: 1584. Min Cost to Connect All Points.
+# https://leetcode.com/problems/min-cost-to-connect-all-points/
+# @param {Integer[][]} points
+# @return {Integer}
+def min_cost_connect_points(points)
+  # Based on:
+  # https://leetcode.com/problems/min-cost-to-connect-all-points/discuss/4045874/94.85-Prim-and-Kruskal-with-Min-Heap
+    
+end
