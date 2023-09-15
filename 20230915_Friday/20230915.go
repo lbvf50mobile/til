4ca55ed5dec7012056e248f24f708b40cf420607
Dmitry <lbvf50.mobile@gmail.com>
2023-09-15 23:53:30 +0700

    1685. Leetcode: 1584. Min Cost to Connect All Points. (in action) Thanks God!

---
+++ 20230915_Friday/20230915.go
@@ -0,0 +1,9 @@
+    package main
+
+    import (
+        "fmt"
+    )
+    
+    func main() {
+        fmt.Println("alias x='go run 20230915_Friday/20230915.go'")
+    }
---
+++ 20230915_Friday/20230915.rb
@@ -0,0 +1,1 @@
+# Leetcode: 1584. Min Cost to Connect All Points.
