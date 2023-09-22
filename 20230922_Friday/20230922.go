68d6f43663110c64d8a8cb71367e37fcca6ee654
Dmitry <lbvf50.mobile@gmail.com>
2023-09-23 01:09:28 +0700

    1692. Leetcode: 392. Is Subsequence. (go solution) Thanks God!

--- 20230922_Friday/20230922.go
+++ 20230922_Friday/20230922.go
@@ -1,9 +1,28 @@
-    package main
+// Leetcode: 392. Is Subsequence.
+// https://leetcode.com/problems/is-subsequence
+// = = = = = = = = = = = = = = 
+// Accepted.
+// Thanks God, Jesus Chirst!
+// = = = = = = = = = = = = = =
+// Runtime: 1 ms, faster than 81.63% of Go online submissions for Is
+// Subsequence. Memory Usage: 2 MB, less than 95.67% of Go online submissions
+// for Is Subsequence.
+// 2023.09.22 Daily Challenge.
+package main
 
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20230922_Friday/20230922.go'")
-    }
+func isSubsequence(s string, t string) bool {
+	m, n := len(s), len(t)
+	i, j := 0, 0
+	for i < m && j < n {
+		a, b := s[i], t[j]
+		if a == b {
+			i += 1
+		}
+		j += 1
+	}
+
+	if m == i {
+		return true
+	}
+	return false
+}
