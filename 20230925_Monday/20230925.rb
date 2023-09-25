d63070c5d1ab3bc082f7468de0bc161342ce5e6e
Dmitry <lbvf50.mobile@gmail.com>
2023-09-25 21:30:28 +0700

    1695. Leetcode: 389. Find the Difference. (ruby solution) Thanks God!

--- 20230925_Monday/20230925.rb
+++ 20230925_Monday/20230925.rb
@@ -1,12 +1,24 @@
 # Leetcode: 389. Find the Difference.
 # https://leetcode.com/problems/find-the-difference/
+# = = = = = = = = = = = = = =
+# Accepted.
+# Thanks God, Jesus Christ!
+# = = = = = = = = = = = = = =
+# Runtime: 76 ms, faster than 64.86% of Ruby online submissions for Find the
+# Difference.
+# Memory Usage: 211 MB, less than 59.46% of Ruby online submissions for Find the
+# Difference.
+# 2023.09.25 Daily Challenge.
 # @param {String} s
 # @param {String} t
 # @return {Character}
 # Wrong soluition.
 def find_the_difference(s, t)
-  s.size.times do |i|
-    return t[i] if s[i] != t[i]
+  a, b = Array.new(26,0), Array.new(26,0)
+  s.chars.each{|x| a[x.ord - ?a.ord] += 1}
+  t.chars.each{|x| b[x.ord - ?a.ord] += 1}
+  26.times do |i|
+    return (i + ?a.ord).chr if a[i] + 1 == b[i]
   end
-  return t[-1]
+  raise "Unknown."
 end
