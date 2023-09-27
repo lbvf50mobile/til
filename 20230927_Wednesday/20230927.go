66752dbdcca8717375a1c68d472fb261997b6bf6
Dmitry <lbvf50.mobile@gmail.com>
2023-09-27 23:55:40 +0700

    1697. Leetcode: 880. Decoded String at Index. (ruby solution) Thanks God!

---
+++ 20230927_Wednesday/20230927.go
@@ -0,0 +1,9 @@
+    package main
+
+    import (
+        "fmt"
+    )
+    
+    func main() {
+        fmt.Println("alias x='go run 20230927_Wednesday/20230927.go'")
+    }
---
+++ 20230927_Wednesday/20230927.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/decoded-string-at-index/discuss/156747/JavaC%2B%2BPython-O(N)-Time-O(1)-Space
+ def decodeAtIndex(self, S, K):
+        N = 0
+        for i, c in enumerate(S):
+            N = N * int(c) if c.isdigit() else N + 1
+            if K <= N: break
+        for j in range(i, -1, -1):
+            c = S[j]
+            if c.isdigit():
+                N /= int(c)
+                K %= N
+            else:
+                if K == N or K == 0: return c
+                N -= 1
