243cd34224212348b4b16892852f05604e4482b2
Dmitry <lbvf50.mobile@gmail.com>
2023-09-28 23:37:03 +0700

    1698. Leetcode: 905. Sort Array By Parity. (go solution) Thanks God!

--- 20230928_Thursday/20230928.go
+++ 20230928_Thursday/20230928.go
@@ -1,9 +1,29 @@
-    package main
-
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20230928_Thursday/20230928.go'")
-    }
+// Leetcode: 905. Sort Array By Parity.
+// https://leetcode.com/problems/sort-array-by-parity/
+// = = = = = = = = = = = = = =
+// Accpted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 9 ms, faster than 57.51% of Go online submissions for Sort Array By
+// Parity.
+// Memory Usage: 5.1 MB, less than 19.74% of Go online submissions for Sort
+// Array By Parity.
+// 2023.09.28 Daily Challenge.
+package main
+func sortArrayByParity(nums []int) []int {
+	e,o := 0, len(nums) - 1 // even, odd;
+	ans := make([]int, len(nums))
+	for _,v := range nums {
+		if e > o {
+			break
+		}
+		if 0 == v%2 {
+			ans[e] = v
+			e += 1
+		} else {
+			ans[o] = v
+			o -= 1
+		}
+	}
+	return ans
+}
