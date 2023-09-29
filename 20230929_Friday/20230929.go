bfe5a12990996328d91c1d6654a734c6045faf70
Dmitry <lbvf50.mobile@gmail.com>
2023-09-29 23:54:24 +0700

    1699. Leetcode: 896. Monotonic Array. (go solution) Thanks God!

--- 20230929_Friday/20230929.go
+++ 20230929_Friday/20230929.go
@@ -1,9 +1,33 @@
-    package main
+// Leetcode: 896. Monotonic Array.
+// https://leetcode.com/problems/monotonic-array
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 140 ms, faster than 55.32% of Go online submissions for Monotonic
+// Array.
+// Memory Usage: 8.5 MB, less than 89.36% of Go online submissions for Monotonic
+// Array.
+// 2023.09.29 Daily Challenge.
+package main
 
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20230929_Friday/20230929.go'")
-    }
+func isMonotonic(nums []int) bool {
+	x := nums
+	up, down := false, false
+	n := len(x)
+	for i := 1; i < n; i += 1 {
+		if up && down {
+			return false
+		}
+		if x[i-1] > x[i] {
+			down = true
+		}
+		if x[i-1] < x[i] {
+			up = true
+		}
+	}
+	if up && down {
+		return false
+	}
+	return true
+}
