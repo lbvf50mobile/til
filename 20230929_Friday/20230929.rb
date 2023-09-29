74b429d0b94a3306cb71eb76a21865f8287fdddb
Dmitry <lbvf50.mobile@gmail.com>
2023-09-29 23:45:20 +0700

    1699. Leetcode: 896. Monotonic Array. (ruby solution 2) Thanks God!

--- 20230929_Friday/20230929.rb
+++ 20230929_Friday/20230929.rb
@@ -4,27 +4,22 @@
 # Accpeted.
 # Thanks God, Jesus Christ!
 # = = = = = = = = = = = = = =
-# Runtime: 132 ms, faster than 80.65% of Ruby online submissions for Monotonic
+# Runtime: 151 ms, faster than 35.48% of Ruby online submissions for Monotonic
 # Array.
-# Memory Usage: 223.4 MB, less than 80.65% of Ruby online submissions for
+# Memory Usage: 223.4 MB, less than 29.03% of Ruby online submissions for
 # Monotonic Array.
 # 2023.09.29 Daily Challenge.
 # @param {Integer[]} nums
 # @return {Boolean}
 def is_monotonic(nums)
-  return up(nums) || down(nums)
-end
-
-def up(x)
+  x = nums
+  up = false
+  down = false
   (1...x.size).each do |i|
-    return false if x[i-1] > x[i]
+    return false if up && down
+    up = true if x[i-1] < x[i]
+    down = true if x[i-1] > x[i]
   end
-  return true
-end
-
-def down(x)
-  (1...x.size).each do |i|
-    return false if x[i-1] < x[i]
-  end
+  return false if up && down
   return true
 end
