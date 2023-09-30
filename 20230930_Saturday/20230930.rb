94b88dd34ae19a994ba786d32df0ef5c1c1e0cbe
Dmitry <lbvf50.mobile@gmail.com>
2023-09-30 23:55:17 +0700

    1700. Leetcode: 456. 132 Pattern. (ruby solution) Thanks God!

--- 20230930_Saturday/20230930.rb
+++ 20230930_Saturday/20230930.rb
@@ -1,28 +1,29 @@
 # Leetcode: 456. 132 Pattern.
 # https://leetcode.com/problems/132-pattern
+# = = = = = = = = = = = = = =
+# Accepted.
+# Thanks God, Jesus Christ!
+# = = = = = = = = = = = = = =
+# Runtime: 138 ms, faster than 50.00% of Ruby online submissions for 132
+# Pattern.
+# Memory Usage: 218.2 MB, less than 50.00% of Ruby online submissions for 132
+# Pattern.
+# 2023.09.30 Daily Challenge.
 # @param {Integer[]} nums
 # @return {Boolean}
 # Incorrect: [3,5,0,3,4]
 def find132pattern(nums)
-  # Hint from:
+  # Based on:
   # https://leetcode.com/problems/132-pattern/discuss/94071/Single-pass-C%2B%2B-O(n)-space-and-time-solution-(8-lines)-with-detailed-explanation.
   # Snap shot of monotonic stack.
-  s = []
-  nums.each do |x|
-    do_pop = false
-    while (!s.empty?) && s[-1] >= x
-      if s[-1] == x
-        s.pop
-      else
-        # If it is a second pop.
-        return true if s.size > 1 && do_pop
-        do_pop = true
-        s.pop
-      end
+  st = []
+  s3 = -Float::INFINITY 
+  nums.reverse_each do |x|
+    return true if x < s3
+    while !st.empty? && x > st[-1]
+      s3 = st.pop 
     end
-    # If pop only ones.
-    return true if !s.empty? && do_pop
-    s.push x
+    st.push(x)
   end
   return false
 end
