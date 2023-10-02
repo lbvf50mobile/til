55092c25eb4782a879c481beeaffc9c5f4f097af
Dmitry <lbvf50.mobile@gmail.com>
2023-10-02 23:58:28 +0700

    1702. Leetcode: 2038. Remove Colored Pieces if Both Neighbors are the Same Color. (go solutione) Thanks God!

--- 20231002_Monday/20231002.go
+++ 20231002_Monday/20231002.go
@@ -1,9 +1,31 @@
-    package main
+// Leetcode: 2038. Remove Colored Pieces if Both Neighbors are the Same Color.
+// https://leetcode.com/problems/remove-colored-pieces-if-both-neighbors-are-the-same-color
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 13 ms, faster than 54.55% of Go online submissions for Remove
+// Colored Pieces if Both Neighbors are the Same Color.
+// Memory Usage: 6.3 MB, less than 100.00% of Go online submissions for Remove
+// Colored Pieces if Both Neighbors are the Same Color.
+// 2023.10.02 Daily Challenge.
+package main
 
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20231002_Monday/20231002.go'")
-    }
+func winnerOfGame(colors string) bool {
+	c := colors
+	a,b := 0, 0
+	n := len(colors)
+	var x,y,z byte
+	for i := 1; i < n-1; i += 1 {
+		x,y,z = c[i-1], c[i], c[i+1]
+		if 65 == x && 65 == y && 65 == z {
+			a += 1
+			continue
+		}
+		if 66 == x && 66 == y && 66 == z {
+			b += 1
+			continue
+		}
+	}
+	return a > b
+}
