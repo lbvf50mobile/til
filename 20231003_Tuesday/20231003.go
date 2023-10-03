fd4d90c791070eaa9f3048398d896a75fa58cb62
Dmitry <lbvf50.mobile@gmail.com>
2023-10-04 00:10:22 +0700

    1703. Leetcode: 1512. Number of Good Pairs. (go solution) Thanks God!

--- 20231003_Tuesday/20231003.go
+++ 20231003_Tuesday/20231003.go
@@ -1,9 +1,22 @@
-    package main
+// Leetcode: 1512. Number of Good Pairs.
+// https://leetcode.com/problems/number-of-good-pairs/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 0 ms, faster than 100.00% of Go online submissions for Number of
+// Good Pairs.
+// Memory Usage: 2 MB, less than 19.68% of Go online submissions for Number of
+// Good Pairs.
+// 2023.10.03 Daily Challenge.
+package main
 
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20231003_Tuesday/20231003.go'")
-    }
+func numIdenticalPairs(nums []int) int {
+	hash :=make(map[int]int)
+	ans := 0
+	for _,x := range nums {
+		ans += hash[x]
+		hash[x] += 1
+	}
+	return ans
+}
