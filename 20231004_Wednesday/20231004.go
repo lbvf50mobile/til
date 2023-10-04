bd606b767b96dc4d825386a5e4e499dc479a0941
Dmitry <lbvf50.mobile@gmail.com>
2023-10-04 23:47:50 +0700

    1704. Leetcode: 706. Design HashMap. (go solution) Thanks God!

--- 20231004_Wednesday/20231004.go
+++ 20231004_Wednesday/20231004.go
@@ -1,9 +1,50 @@
-    package main
+// Leetcode: 706. Design HashMap.
+// https://leetcode.com/problems/design-hashmap/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 105 ms, faster than 43.96% of Go online submissions for Design
+// HashMap.
+// Memory Usage: 39 MB, less than 6.28% of Go online submissions for Design
+// HashMap.
+// 2023.10.04 Daily Challenge.
 
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20231004_Wednesday/20231004.go'")
-    }
+package main
+
+import "fmt"
+
+var p = fmt.Println
+
+type MyHashMap struct {
+	a [1_000_001]int
+}
+
+func Constructor() MyHashMap {
+	ans := MyHashMap{}
+	for i := 0; i < 1_000_001; i += 1 {
+		ans.a[i] = -1
+	}
+	return ans
+}
+
+func (this *MyHashMap) Put(key int, value int) {
+	this.a[key] = value
+}
+
+func (this *MyHashMap) Get(key int) int {
+	return this.a[key]
+
+}
+
+func (this *MyHashMap) Remove(key int) {
+	this.a[key] = -1
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Put(key,value);
+ * param_2 := obj.Get(key);
+ * obj.Remove(key);
+ */
