79ec8f108c37727cdd64636b810a85ac9e79d67e
Dmitry <lbvf50.mobile@gmail.com>
2023-10-05 23:54:54 +0700

    1705. Leetcode: 229. Majority Element II. (go solution) Thanks God!

--- 20231005_Thursday/20231005.go
+++ 20231005_Thursday/20231005.go
@@ -1,9 +1,27 @@
-    package main
+// Leetcode: 229. Majority Element II.
+// https://leetcode.com/problems/majority-element-ii/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 8 ms, faster than 92.20% of Go online submissions for Majority
+// Element II.
+// Memory Usage: 5 MB, less than 43.97% of Go online submissions for Majority
+// Element II.
+// 2023.10.05 Daily Challenge.
+package main
 
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20231005_Thursday/20231005.go'")
-    }
+func majorityElement(nums []int) []int {
+	h := make(map[int]int)
+	bound := len(nums) / 3
+	for _, k := range nums {
+		h[k] += 1
+	}
+	ans := make([]int, 0, 2)
+	for k, v := range h {
+		if v > bound {
+			ans = append(ans, k)
+		}
+	}
+	return ans
+}
