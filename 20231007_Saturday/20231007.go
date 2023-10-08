b2db853f420c5453c1c9816400477a9495e081c2
Dmitry <lbvf50.mobile@gmail.com>
2023-10-08 22:52:42 +0700

    1707. Leetcode: 1420. Build Array Where You Can Find The Maximum Exactly K Comparisons. (golang in action) Thanks God!

--- 20231007_Saturday/20231007.go
+++ 20231007_Saturday/20231007.go
@@ -1,9 +1,18 @@
-    package main
+// Leetcode: 1420. Build Array Where You Can Find The Maximum Exactly K Comparisons.
+// https://leetcode.com/problems/build-array-where-you-can-find-the-maximum-exactly-k-comparisons/
+package main
 
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20231007_Saturday/20231007.go'")
-    }
+func numOfArrays(n int, m int, k int) int {
+	mod := 1_000_000_007
+	dp := make([][][]int,n+1)
+	for i := 0; i <= n ; i += 1 {
+		dp[i] := make([][]int,m+1)
+		for j := 0; j <= m; j += 1 {
+			dp[i][j] = make([]int, k+1)
+		}
+	}
+	for j := 0; j <= m; j += 1 {
+		dp[n][j][0] = 1
+	}
+	return dp[0][0][k]
+}
