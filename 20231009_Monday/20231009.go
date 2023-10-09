3c818816bb156cfb20bbe47f6a1253baedc7eda2
Dmitry <lbvf50.mobile@gmail.com>
2023-10-09 23:48:46 +0700

    1709. Leetcode: 34. Find First and Last Position of Element in Sorted Array. (ruby solution) Thanks God!

---
+++ 20231009_Monday/20231009.go
@@ -0,0 +1,9 @@
+    package main
+
+    import (
+        "fmt"
+    )
+    
+    func main() {
+        fmt.Println("alias x='go run 20231009_Monday/20231009.go'")
+    }
---
+++ 20231009_Monday/20231009.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/discuss/1054742/Python-O(logn)
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        
+        def search(x):
+            lo, hi = 0, len(nums)           
+            while lo < hi:
+                mid = (lo + hi) // 2
+                if nums[mid] < x:
+                    lo = mid+1
+                else:
+                    hi = mid                    
+            return lo
+        
+        lo = search(target)
+        hi = search(target+1)-1
+        
+        if lo <= hi:
+            return [lo, hi]
+                
+        return [-1, -1]
