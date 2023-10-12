cc1feb9b5e3dc5845c404688b76ddfbe139cf800
Dmitry <lbvf50.mobile@gmail.com>
2023-10-12 23:54:41 +0700

    1712. Leetcode: 1095. Find in Mountain Array. (ruby in action) Thanks God!

---
+++ 20231012_Thursday/20231012.go
@@ -0,0 +1,9 @@
+    package main
+
+    import (
+        "fmt"
+    )
+    
+    func main() {
+        fmt.Println("alias x='go run 20231012_Thursday/20231012.go'")
+    }
---
+++ 20231012_Thursday/20231012.rb
@@ -0,0 +1,20 @@
+# Leetcode: 1095. Find in Mountain Array.
+# https://leetcode.com/problems/find-in-mountain-array/
+# This is MountainArray's API interface.
+# You should not implement it, or speculate about its implementation
+# class MountainArray
+#    def get(index):
+#       
+#    end
+#
+#    def length()
+#		
+#	 end
+# end
+
+# @param {int} int
+# @param {MountainArray} mountain_arr
+# @return {int}
+def findInMountainArray(target, mountain_arr)
+    
+end
