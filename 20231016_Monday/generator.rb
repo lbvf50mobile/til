7f6bada216e546cbd9ab3c5cca1a1ad2333a3a89
Dmitry <lbvf50.mobile@gmail.com>
2023-10-16 23:34:27 +0700

    1716. Leetcode: 119. Pascal's Triangle II. (ruby solution) Thanks God!

---
+++ 20231016_Monday/generator.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+ans = Array.new(34)
+(0...34).each do |i|
+  ans[i] = Array.new(i+1,1)
+end
+ans[0][0] = 1
+ans[1][0] = 1
+ans[1][1] = 1
+
+(2...34).each do |i|
+  (1...i).each do |j|
+    ans[i][j] = ans[i-1][j-1] + ans[i-1][j]
+  end
+end
+
+puts "# Start data."
+puts  "["
+ans.each{ |x| puts "[" + x.join(?,) + "],"}
+puts  "]"
+puts "# End data."
