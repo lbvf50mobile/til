1e43ce41e88fe6245a0463ac1f7b76179d15c7ad
Dmitry <lbvf50.mobile@gmail.com>
2023-10-20 00:21:57 +0700

    1719. Leetcode: 844. Backspace String Compare. (article and solution) Thanks God!

--- 20231019_Thursday/20231019.go
+++ 20231019_Thursday/20231019.go
@@ -1,9 +1,53 @@
-    package main
+// Leetcode: 844. Backspace String Compare.
+// https://leetcode.com/problems/backspace-string-compare/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 1 ms, faster than 75.78% of Go online submissions for Backspace
+// String Compare.
+// Memory Usage: 2 MB, less than 33.54% of Go online submissions for Backspace
+// String Compare.
+// 2023.10.19 Daily Challenge.
+package main
 
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20231019_Thursday/20231019.go'")
-    }
+type bstring struct {
+	size int
+	chrs []byte
+}
+
+func backspaceCompare(s string, t string) bool {
+	a, b := &bstring{len(s), []byte(s)}, &bstring{len(t), []byte(t)}
+	a.perform()
+	b.perform()
+	return a.compare(b)
+}
+
+func (s *bstring) perform() {
+	// W is index of last written.
+	w := -1 // -1 meand did not write at all.
+	for r := 0; r < s.size; r += 1 {
+		x := s.chrs[r]
+		if '#' == x {
+			if w > -1 {
+				w -= 1
+			}
+		} else {
+			w += 1
+			s.chrs[w] = x
+		}
+	}
+	s.size = w + 1
+}
+
+func (s *bstring) compare(x *bstring) bool {
+	if s.size != x.size {
+		return false
+	}
+	for i := 0; i < s.size; i += 1 {
+		if s.chrs[i] != x.chrs[i] {
+			return false
+		}
+	}
+	return true
+}
