a699970dde8561ae95ccaf382b986b27b6731de5
Dmitry <lbvf50.mobile@gmail.com>
2023-10-21 03:06:21 +0700

    1720. Leetcode: 341. Flatten Nested List Iterator. (go solution) Thanks God!

--- 20231020_Friday/20231020.go
+++ 20231020_Friday/20231020.go
@@ -1,9 +1,82 @@
-    package main
+// Leetcode: 341. Flatten Nested List Iterator.
+// https://leetcode.com/problems/flatten-nested-list-iterator
+// = = = = = = = = = = = = = 
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = =
+// Runtime: 5 ms, faster than 63.08% of Go online submissions for Flatten Nested
+// List Iterator.
+// Memory Usage: 5.6 MB, less than 30.77% of Go online submissions for Flatten
+// Nested List Iterator.
+// 2023.10.20 Daily Challenge.
 
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20231020_Friday/20231020.go'")
-    }
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * type NestedInteger struct {
+ * }
+ *
+ * // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ * func (this NestedInteger) IsInteger() bool {}
+ *
+ * // Return the single integer that this NestedInteger holds, if it holds a single integer
+ * // The result is undefined if this NestedInteger holds a nested list
+ * // So before calling this method, you should have a check
+ * func (this NestedInteger) GetInteger() int {}
+ *
+ * // Set this NestedInteger to hold a single integer.
+ * func (n *NestedInteger) SetInteger(value int) {}
+ *
+ * // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ * func (this *NestedInteger) Add(elem NestedInteger) {}
+ *
+ * // Return the nested list that this NestedInteger holds, if it holds a nested list
+ * // The list length is zero if this NestedInteger holds a single integer
+ * // You can access NestedInteger's List element directly if you want to modify it
+ * func (this NestedInteger) GetList() []*NestedInteger {}
+ */
+package main
+
+type NestedIterator struct {
+	i    int
+	data []int
+}
+
+type pair struct {
+	i int
+	l []*NestedInteger
+}
+
+func Constructor(nestedList []*NestedInteger) *NestedIterator {
+	stack := make([]*pair, 0)
+	stack = append(stack, &pair{0, nestedList})
+	var dt []int
+	for 0 != len(stack) {
+		ln := len(stack)
+		tmp := stack[ln-1]
+		stack = stack[0 : ln-1]
+		i, lst := tmp.i, tmp.l
+		for i < len(lst) {
+			if lst[i].IsInteger() {
+				dt = append(dt, lst[i].GetInteger())
+				i += 1
+			} else {
+				stack = append(stack, &pair{i + 1, lst})
+				lst = lst[i].GetList()
+				i = 0
+			}
+		}
+	}
+
+	return &NestedIterator{0, dt}
+}
+
+func (this *NestedIterator) Next() int {
+	i := this.i
+	this.i += 1
+	return this.data[i]
+}
+
+func (this *NestedIterator) HasNext() bool {
+	return this.i < len(this.data)
+}
--- 20231020_Friday/20231020.rb
+++ 20231020_Friday/20231020.rb
@@ -1,10 +1,20 @@
 # Leetcode: 341. Flatten Nested List Iterator.
 # https://leetcode.com/problems/flatten-nested-list-iterator
+# = = = = = = = = = = = = = =
+# Accepted.
+# Thanks God, Jesus Christ!
+# = = = = = = = = = = = = = =
+# Runtime: 88 ms, faster than 14.29% of Ruby online submissions for Flatten
+# Nested List Iterator.
+# Memory Usage: 212.6 MB, less than 71.43% of Ruby online submissions for
+# Flatten Nested List Iterator.
+# 2023.10.20 Daily Challenge.
+
+# [[]] - one of the test cases. Test cases do not fulfill description's
+# constrains.
+# 1 <= nestedList.length <= 500
+
 # This is the interface that allows for creating nested lists.
-#
-# Error. What a surprice. Test cases do not support the descriptions.
-# [[]] - it is possible to have empty lists.
-#
 # You should not implement it, or speculate about its implementation
 #
 #class NestedInteger
@@ -31,63 +41,42 @@
 class NestedIterator
     # @param {NestedInteger[]} nested_list
     def initialize(nested_list)
-      # If it is a flat list need to store only index.
-      # If it is a list of list also need a stack. When enter some list need to
-      # do something.
-      # When current index is equal to size need to go up.
-      @l = nested_list # Full list.
-      @c = nested_list # Current list.
-      @s = [] # Stack. Stack stores next element. Element after entered one. Pop and use.
-      @i = 0 # Pointer of the current list.
-        
+      # Because there are could be an empty lists. 
+      # And test do not fulfill the conditions. On every has next have to
+      # iterate till the end. And better to save elements ones. 
+      @a = []
+      fill(nested_list)
     end
+
+    # Fills @a with numbers from nested_list.
+    def fill(x)
+      s = [[0,x]]
+      while !s.empty?
+        i,curr = s.pop
+        while i < curr.size
+          if curr[i].is_integer()
+            @a.push(curr[i].get_integer())
+            i += 1
+          else
+            s.push([i+1,curr])
+            s.push([0,curr[i].get_list()])
+            break
+          end
+        end
+      end
+    end
+    
 
     # @return {Boolean}
     # Idea. There is something to return if there is index were to step next
     # time.
     def has_next
-      return true if  @i < @c.size
-      # Moving down into the stack.
-      @s.reverse_each do |pair|
-        i,l = pair
-        # Found one element in the stack that is not empty.
-        # There is a place were to at this layer.
-        return true if i < l.size
-      end
-      return false
+      !@a.empty?
     end
 
     # @return {Integer}
     def next
-      norm
-    end
-    def norm
-      # Simplest situation just return ineger and move pointer.
-      if @i < @c.size && @c[@i].is_integer()
-        ans = @c[@i].get_integer()
-        @i += 1
-        return ans
-      end
-      # More complicate situation go into a subarray.
-      # Moving till first element is not sub array.
-      if @i < @c.size && ! @c[@i].is_integer()
-        # There is no empty list according to description.
-        # So if not integer it means list. There is always something at the
-        # index 0.
-        while @i < @c.size && !@c[@i].is_integer()
-          @s.push([@i+1,@c]) # Save next position for current layer.
-          @i, @c = 0, @c[@i].get_list() # Define new position, in a new layer.
-        end
-        # Just reference to a simplest solution.
-        # Because current element is not a list.
-        return norm
-      end
-      # One more situation out of the bound of current layer.
-      # Just climb back to a previos one.
-      if @i >= @c.size
-        @i,@c = @s.pop
-        return norm
-      end
+      @a.shift
     end
 end
 
