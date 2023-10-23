4cd2e83d76c53cb047b5a9a52c114d01e1de06e2
Dmitry <lbvf50.mobile@gmail.com>
2023-10-23 23:19:55 +0700

    1723. Leetcode: 342. Power of Four. (go code) Thanks God!

--- 20231023_Monday/20231023.go
+++ 20231023_Monday/20231023.go
@@ -1,9 +1,24 @@
-    package main
+// Leetcode: 342. Power of Four.
+// https://leetcode.com/problems/power-of-four
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 5 ms, faster than 17.48% of Go online submissions for Power of Four.
+// Memory Usage: 2.1 MB, less than 77.62% of Go online submissions for Power of
+// Four.
+// 2023.10.23 Daily Challenge.
+package main
 
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20231023_Monday/20231023.go'")
-    }
+func isPowerOfFour(n int) bool {
+	if 1 == n {
+		return true
+	}
+	if 0 >= n {
+		return false
+	}
+	for 0 == n%4 {
+		n /= 4
+	}
+	return 1 == n
+}
