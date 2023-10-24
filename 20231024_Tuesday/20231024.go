db10b3f2fca51f50db5e1e67d681cf1d92e47846
Dmitry <lbvf50.mobile@gmail.com>
2023-10-25 00:12:05 +0700

    1724. Leetcode: 515. Find Largest Value in Each Tree Row. (go solution) Thanks God!

--- 20231024_Tuesday/20231024.go
+++ 20231024_Tuesday/20231024.go
@@ -1,9 +1,48 @@
-    package main
+// Leetcode: 515. Find Largest Value in Each Tree Row.
+// https://leetcode.com/problems/find-largest-value-in-each-tree-row/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 4 ms, faster than 90.63% of Go online submissions for Find Largest
+// Value in Each Tree Row.
+// Memory Usage: 5.9 MB, less than 75.00% of Go online submissions for Find
+// Largest Value in Each Tree Row.
+// 2023.10.24 Daily Challenge.
+package main
 
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20231024_Tuesday/20231024.go'")
-    }
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func largestValues(root *TreeNode) []int {
+	superMin := -2147483648
+	if nil == root {
+		return []int{}
+	}
+	ans := make([]int, 0)
+	q := []*TreeNode{root}
+	for 0 < len(q) {
+		max := superMin
+		tmpQ := []*TreeNode{}
+		for _, n := range q {
+			val := n.Val
+			if max < val {
+				max = val
+			}
+			if nil != n.Left {
+				tmpQ = append(tmpQ, n.Left)
+			}
+			if nil != n.Right {
+				tmpQ = append(tmpQ, n.Right)
+			}
+		}
+		ans = append(ans, max)
+		q = tmpQ
+	}
+	return ans
+}
