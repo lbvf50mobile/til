ac9ba96b6716553d511be1fbd44918e6943dd0fb
Dmitry <lbvf50.mobile@gmail.com>
2023-10-25 23:26:24 +0700

    1725. Leetcode: 779. K-th Symbol in Grammar. (go solution) Thanks God!

--- 20231025_Wednesday/20231025.go
+++ 20231025_Wednesday/20231025.go
@@ -1,9 +1,43 @@
-    package main
+// Leetcode: 779. K-th Symbol in Grammar.
+// https://leetcode.com/problems/k-th-symbol-in-grammar
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 1 ms, faster than 82.50% of Go online submissions for K-th Symbol in
+// Grammar.
+// Memory Usage: 1.9 MB, less than 92.50% of Go online submissions for K-th
+// Symbol in Grammar.
+// 2023.10.25 Daily Challenge.
+package main
 
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20231025_Wednesday/20231025.go'")
-    }
+func kthGrammar(n int, k int) int {
+	// Based on:
+	// https://leetcode.com/problems/k-th-symbol-in-grammar/solution/
+	return dfs(n, k, 0)
+}
+
+func dfs(n, k, v int) int {
+	// v is the value of a root.
+	if 1 == n {
+		return v
+	}
+	total := 1 << (n - 1)
+	// Target node will be present in the right half sub-tree of the current
+	// root node.
+	if k > total/2 {
+		nv := 0
+		if 0 == v {
+			nv = 1
+		}
+		return dfs(n-1, k-total/2, nv)
+		// Otherwise, the target node is the left sub-tree of the current root
+		// node.
+	} else {
+		nv := 1
+		if 0 == v {
+			nv = 0
+		}
+		return dfs(n-1, k, nv)
+	}
+}
