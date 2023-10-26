0e53e87f51bb191a7e3616e1e90d38408378b637
Dmitry <lbvf50.mobile@gmail.com>
2023-10-27 01:07:38 +0700

    1726. Leetcode: 823. Binary Trees With Factors. (go solution) Thanks God!

--- 20231026_Thursday/20231026.go
+++ 20231026_Thursday/20231026.go
@@ -1,9 +1,42 @@
-    package main
+// Leetcode: 823. Binary Trees With Factors.
+// https://leetcode.com/problems/binary-trees-with-factors
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 57 ms, faster than 25.00% of Go online submissions for Binary Trees
+// With Factors.
+// Memory Usage: 3.8 MB, less than 75.00% of Go online submissions for Binary
+// Trees With Factors.
+// 2023.10.26 Daily Challenge.
+package main
 
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20231026_Thursday/20231026.go'")
-    }
+import (
+	"sort"
+)
+
+func numFactoredBinaryTrees(arr []int) int {
+	// Based on:
+	// https://leetcode.com/problems/binary-trees-with-factors/discuss/4209155/Beats-100-oror-Easiest-Approach-in-3-Points-oror-DP-and-HashMap
+	sort.Ints(arr)
+	mod := 1000000007
+	dp := make(map[int]int)
+	for i, _ := range arr {
+		dp[arr[i]] = 1
+		for j := 0; j < i; j += 1 {
+			md := arr[i] % arr[j]
+			dv := arr[i] / arr[j]
+			val, ok := dp[dv]
+			if 0 == md && ok {
+				one := dp[arr[i]]
+				two := (dp[arr[j]] * val) % mod
+				dp[arr[i]] = (one + two) % mod
+			}
+		}
+	}
+	ans := 0
+	for _, v := range dp {
+		ans = (ans + v) % mod
+	}
+	return ans
+}
