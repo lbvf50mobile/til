358f9ac4b8fa661bfc87731427dc442118a13b2b
Dmitry <lbvf50.mobile@gmail.com>
2023-10-29 22:29:49 +0700

    1729. Leetcode: 458. Poor Pigs. (go solution) Thanks God!

--- 20231029_Sunday/20231029.go
+++ 20231029_Sunday/20231029.go
@@ -1,9 +1,29 @@
-    package main
+// Leetcode: 458. Poor Pigs.
+// https://leetcode.com/problems/poor-pigs/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 1 ms, faster than 66.67% of Go online submissions for Poor Pigs.
+// Memory Usage: 1.9 MB, less than 33.33% of Go online submissions for Poor
+// Pigs.
+// 2023.10.29 Daily Challenge.
+package main
 
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20231029_Sunday/20231029.go'")
-    }
+func poorPigs(buckets int, minutesToDie int, minutesToTest int) int {
+	// Based on:
+	// https://leetcode.com/problems/poor-pigs/discuss/94266/Another-explanation-and-solution
+	ans := 0
+	for pow(minutesToTest/minutesToDie+1, ans) < buckets {
+		ans += 1
+	}
+	return ans
+}
+
+func pow(a, b int) int {
+	ans := 1
+	for i := 0; i < b; i += 1 {
+		ans *= a
+	}
+	return ans
+}
