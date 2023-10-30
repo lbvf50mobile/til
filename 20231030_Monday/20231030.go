a44ed286d4e236bbccdc6ed307a499d3fbd95dec
Dmitry <lbvf50.mobile@gmail.com>
2023-10-30 23:42:58 +0700

    1730. Leetcode: 1356. Sort Integers by The Number of 1 Bits. (go solution) Thanks God!

--- 20231030_Monday/20231030.go
+++ 20231030_Monday/20231030.go
@@ -1,9 +1,38 @@
-    package main
+// Leetcode: 1356. Sort Integers by The Number of 1 Bits.
+// https://leetcode.com/problems/sort-integers-by-the-number-of-1-bits
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 7 ms, faster than 69.77% of Go online submissions for Sort Integers
+// by The Number of 1 Bits.
+// Memory Usage: 3.4 MB, less than 100.00% of Go online submissions for Sort
+// Integers by The Number of 1 Bits.
+// 2023.10.30 Daily Challenge.
+package main
 
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20231030_Monday/20231030.go'")
-    }
+import (
+	"sort"
+)
+
+func sortByBits(arr []int) []int {
+	sort.SliceStable(arr, func(i, j int) bool {
+		a, b := arr[i], arr[j]
+		ai, bi := bitsN(a), bitsN(b)
+		if ai == bi {
+			return a < b
+		} else {
+			return ai < bi
+		}
+	})
+	return arr
+}
+
+func bitsN(x int) int {
+	ans := 0
+	for x != 0 {
+		ans += (x % 2)
+		x >>= 1
+	}
+	return ans
+}
