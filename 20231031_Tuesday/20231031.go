4d805bec761e8e69ff5e5ce173304a522cf862ed
Dmitry <lbvf50.mobile@gmail.com>
2023-10-31 23:20:56 +0700

    1731. Leetcode: 2433. Find The Original Array of Prefix Xor. (go solution) Thanks God!

--- 20231031_Tuesday/20231031.go
+++ 20231031_Tuesday/20231031.go
@@ -1,9 +1,22 @@
-    package main
+// Leetcode: 2433. Find The Original Array of Prefix Xor.
+// https://leetcode.com/problems/find-the-original-array-of-prefix-xor
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 93 ms, faster than 96.15% of Go online submissions for Find The
+// Original Array of Prefix Xor.
+// Memory Usage: 9.1 MB, less than 57.69% of Go online submissions for Find The
+// Original Array of Prefix Xor.
+// 2023.10.31 Daily Challenge.
+package main
 
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20231031_Tuesday/20231031.go'")
-    }
+func findArray(pref []int) []int {
+	n := len(pref)
+	ans := make([]int, n)
+	ans[0] = pref[0]
+	for i := 1; i < n; i += 1 {
+		ans[i] = pref[i] ^ pref[i-1]
+	}
+	return ans
+}
