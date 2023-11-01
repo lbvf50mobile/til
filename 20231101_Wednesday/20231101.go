025902b21479cf69a7587c0b468b27f2eaaf7460
Dmitry <lbvf50.mobile@gmail.com>
2023-11-01 23:35:40 +0700

    1732. Leetcode: 501. Find Mode in Binary Search Tree. (go code) Thanks God!

--- 20231101_Wednesday/20231101.go
+++ 20231101_Wednesday/20231101.go
@@ -1,9 +1,57 @@
-    package main
+// Leetcode: 501. Find Mode in Binary Search Tree.
+// https://leetcode.com/problems/find-mode-in-binary-search-tree/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 12 ms, faster than 37.88% of Go online submissions for Find Mode in
+// Binary Search Tree.
+// Memory Usage: 6.7 MB, less than 45.45% of Go online submissions for Find Mode
+// in Binary Search Tree.
+// 2023.11.01 Daily Challenge.
+package main
 
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20231101_Wednesday/20231101.go'")
-    }
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+var c map[int]int
+
+func findMode(root *TreeNode) []int {
+	c = make(map[int]int)
+	dfs(root)
+	if 0 == len(c) {
+		return []int{}
+	}
+	flag := true
+	max := 0
+	for _, v := range c {
+		if flag {
+			max = v
+			flag = false
+		}
+		if v > max {
+			max = v
+		}
+	}
+	ans := make([]int, 0)
+	for i, v := range c {
+		if v == max {
+			ans = append(ans, i)
+		}
+	}
+	return ans
+}
+
+func dfs(root *TreeNode) {
+	if nil == root {
+		return
+	}
+	c[root.Val] += 1
+	dfs(root.Left)
+	dfs(root.Right)
+}
