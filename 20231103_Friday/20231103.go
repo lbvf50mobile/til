ee3d22f40af6132e4eb5352e69b4163c23312313
Dmitry <lbvf50.mobile@gmail.com>
2023-11-04 01:48:11 +0700

    1734. Leetcode: 1441. Build an Array With Stack Operations. (go solution) Thanks God!

--- 20231103_Friday/20231103.go
+++ 20231103_Friday/20231103.go
@@ -1,9 +1,39 @@
-    package main
+// Leetcode: 1441. Build an Array With Stack Operations.
+// https://leetcode.com/problems/build-an-array-with-stack-operations/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 2 ms, faster than 78.26% of Go online submissions for Build an Array
+// With Stack Operations.
+// Memory Usage: 2.4 MB, less than 47.83% of Go online submissions for Build an
+// Array With Stack Operations.
+// 2023.11.03 Daily Challenge.
+package main
 
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20231103_Friday/20231103.go'")
-    }
+var ans []string
+
+func buildArray(target []int, n int) []string {
+	// Based on the Leetcode's solution:
+	// https://leetcode.com/problems/build-an-array-with-stack-operations/solution/
+	ans = make([]string, 0)
+	i := 0
+	for _, num := range target {
+		for i < num-1 {
+			push()
+			pop()
+			i += 1
+		}
+		push()
+		i += 1
+	}
+	return ans
+
+}
+
+func push() {
+	ans = append(ans, "Push")
+}
+func pop() {
+	ans = append(ans, "Pop")
+}
