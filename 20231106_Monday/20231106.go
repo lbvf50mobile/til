c88c39c43de0b44bda2af26396303cc3a5750d91
Dmitry <lbvf50.mobile@gmail.com>
2023-11-06 22:27:35 +0700

    1737. Leetcode: 1845. Seat Reservation Manager. (ruby tle solution) Thanks God!

---
+++ 20231106_Monday/20231106.go
@@ -0,0 +1,23 @@
+package main
+
+type SeatManager struct {
+}
+
+func Constructor(n int) SeatManager {
+
+}
+
+func (this *SeatManager) Reserve() int {
+
+}
+
+func (this *SeatManager) Unreserve(seatNumber int) {
+
+}
+
+/**
+ * Your SeatManager object will be instantiated and called as such:
+ * obj := Constructor(n);
+ * param_1 := obj.Reserve();
+ * obj.Unreserve(seatNumber);
+ */
---
+++ 20231106_Monday/20231106.rb
@@ -0,0 +1,42 @@
+# Leetcode: 1845. Seat Reservation Manager.
+# https://leetcode.com/problems/seat-reservation-manager/
+# = = = = = = = = = = = = = =
+# Correct.
+# Thanks God, Jesus Christ!
+# = = = = = = = = = = = = = = 
+# TLE.
+# I treat TLE as a normal result when working with Containers::MinHeap in
+# Leetcode. Ruby implementation of MinHeap is tremendously slow.
+class SeatManager
+
+=begin
+    :type n: Integer
+=end
+    def initialize(n)
+      @x = MinHeap.new((1..n).to_a)
+    end
+
+
+=begin
+    :rtype: Integer
+=end
+    def reserve()
+      @x.pop
+    end
+
+
+=begin
+    :type seat_number: Integer
+    :rtype: Void
+=end
+    def unreserve(seat_number)
+      @x.push(seat_number)
+    end
+
+
+end
+
+# Your SeatManager object will be instantiated and called as such:
+# obj = SeatManager.new(n)
+# param_1 = obj.reserve()
+# obj.unreserve(seat_number)
