faa1db68155d432618fd4e1f2cc2c300c59f3752
Dmitry <lbvf50.mobile@gmail.com>
2023-11-08 00:09:27 +0700

    1738. Leetcode: 1921. Eliminate Maximum Number of Monsters. (go solution) Thanks God!

--- 20231107_Tuesday/20231107.go
+++ 20231107_Tuesday/20231107.go
@@ -1,9 +1,34 @@
-    package main
+// Leetcode: 1922. Eliminate Maximum Number of Monsters.
+// https://leetcode.com/problems/eliminate-maximum-number-of-monsters/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 196 ms, faster than 13.33% of Go online submissions for Eliminate
+// Maximum Number of Monsters.
+// Memory Usage: 8.6 MB, less than 93.33% of Go online submissions for Eliminate
+// Maximum Number of Monsters.
+// 2023.11.07 Daily Challenge.
+package main
 
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20231107_Tuesday/20231107.go'")
-    }
+import (
+	"sort"
+)
+
+func eliminateMaximum(dist []int, speed []int) int {
+	// Based on:
+	// https://leetcode.com/problems/eliminate-maximum-number-of-monsters/
+	arrival := make([]float64, len(dist))
+	ans := 0
+	for i, v := range dist {
+		arrival[i] = float64(v) / float64(speed[i])
+	}
+	sort.Float64s(arrival)
+	for i, v := range arrival {
+		if v <= float64(i) {
+			break
+		}
+		ans += 1
+	}
+	return ans
+}
