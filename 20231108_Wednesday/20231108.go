113974bd4d3367a864fdb77ac69c0ac68b46aa54
Dmitry <lbvf50.mobile@gmail.com>
2023-11-08 23:46:36 +0700

    1739. Leetcode: 2849. Determine if a Cell Is Reachable at a Given Time. (go solution) Thanks God!

--- 20231108_Wednesday/20231108.go
+++ 20231108_Wednesday/20231108.go
@@ -1,9 +1,31 @@
-    package main
+// Leetcode: 2849. Determine if a Cell Is Reachable at a Given Time.
+// https://leetcode.com/problems/determine-if-a-cell-is-reachable-at-a-given-time
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 5 ms, faster than 29.85% of Go online submissions for Determine if a
+// Cell Is Reachable at a Given Time.
+// Memory Usage: 2.6 MB, less than 100.00% of Go online submissions for
+// Determine if a Cell Is Reachable at a Given Time.
+// 2023.11.08 Daily Challenge.
+package main
 
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20231108_Wednesday/20231108.go'")
-    }
+func isReachableAtTime(sx int, sy int, fx int, fy int, t int) bool {
+	// Based on:
+	// https://leetcode.com/problems/determine-if-a-cell-is-reachable-at-a-given-time/solution/
+	w := abs(sx - fx)
+	h := abs(sy - fy)
+	if 0 == w && 0 == h && 1 == t {
+		return false
+	}
+	return t >= w && t >= h
+
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
