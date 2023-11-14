c030db00680f2b48b98e972990ef9532ba9599ef
Dmitry <lbvf50.mobile@gmail.com>
2023-11-14 23:35:18 +0700

    1745. Leetcode: 1930. Unique Length-3 Palindromic Subsequences. (go solution) Thanks God!

--- 20231114_Tuesday/20231114.go
+++ 20231114_Tuesday/20231114.go
@@ -1,9 +1,50 @@
-    package main
+// Leetcode: 1930. Unique Length-3 Palindromic Subsequences.
+// https://leetcode.com/problems/unique-length-3-palindromic-subsequences
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 404 ms, faster than 72.22% of Go online submissions for Unique
+// Length-3 Palindromic Subsequences.
+// Memory Usage: 6 MB, less than 100.00% of Go online submissions for Unique
+// Length-3 Palindromic Subsequences.
+// 2023.11.14 Daily Challenge.
+package main
 
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20231114_Tuesday/20231114.go'")
-    }
+func countPalindromicSubsequence(s string) int {
+	// Based on:
+	// https://leetcode.com/problems/unique-length-3-palindromic-subsequences/solution/
+
+	// Indices of chars.
+	// f - first.
+	// l - last.
+	// both arrays have 26 elements.
+	f := []int{-1, -1, -1, -1, -1,
+		-1, -1, -1, -1, -1, -1, -1, -1,
+		-1, -1, -1, -1, -1, -1, -1, -1,
+		-1, -1, -1, -1, -1}
+	l := []int{-1, -1, -1, -1, -1,
+		-1, -1, -1, -1, -1, -1, -1, -1,
+		-1, -1, -1, -1, -1, -1, -1, -1,
+		-1, -1, -1, -1, -1}
+	for i, char := range s {
+		curr := char - 'a'
+		if -1 == f[curr] {
+			f[curr] = i
+		}
+		l[curr] = i
+	}
+	ans := 0
+	for i := 0; i < 26; i += 1 {
+		if -1 == f[i] {
+			continue
+		}
+		between := make(map[byte]bool)
+		for j := f[i] + 1; j < l[i]; j += 1 {
+			char := s[j]
+			between[char] = true
+		}
+		ans += len(between)
+	}
+	return ans
+}
