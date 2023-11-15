66ab91dc033019a04ba005e1e98bc6b2d2601cab
Dmitry <lbvf50.mobile@gmail.com>
2023-11-16 00:15:49 +0700

    1746. Leetcode: 1846. Maximum Element After Decreasing and Rearranging. (go solution) Thanks God!

--- 20231115_Wednesday/20231115.go
+++ 20231115_Wednesday/20231115.go
@@ -1,9 +1,29 @@
-    package main
+// Leetcode: 1846. Maximum Element After Decreasing and Rearranging.
+// https://leetcode.com/problems/maximum-element-after-decreasing-and-rearranging/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 45 ms, faster than 100.00% of Go online submissions for Maximum
+// Element After Decreasing and Rearranging.
+// Memory Usage: 7.6 MB, less than 100.00% of Go online submissions for
+// Maximum Element After Decreasing and Rearranging.
+package main
 
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20231115_Wednesday/20231115.go'")
-    }
+import (
+	"sort"
+)
+
+func maximumElementAfterDecrementingAndRearranging(arr []int) int {
+	// Based on:
+	// https://leetcode.com/problems/maximum-element-after-decreasing-and-rearranging/solution/
+	sort.Ints(arr)
+	ans := 1
+	n := len(arr)
+	for i := 1; i < n; i += 1 {
+		if arr[i] >= ans+1 {
+			ans += 1
+		}
+	}
+	return ans
+}
