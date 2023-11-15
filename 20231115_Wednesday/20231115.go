ba53a7dec4f8f2526ae985c5d249279a345874c7
Dmitry <lbvf50.mobile@gmail.com>
2023-11-15 23:56:06 +0700

    1746. Leetcode: 1846. Maximum Element After Decreasing and Rearranging. (ruby solution) Thanks God!

---
+++ 20231115_Wednesday/20231115.go
@@ -0,0 +1,9 @@
+    package main
+
+    import (
+        "fmt"
+    )
+    
+    func main() {
+        fmt.Println("alias x='go run 20231115_Wednesday/20231115.go'")
+    }
---
+++ 20231115_Wednesday/20231115.rb
@@ -0,0 +1,22 @@
+# Leetcode: 1846. Maximum Element After Decreasing and Rearranging.
+# https://leetcode.com/problems/maximum-element-after-decreasing-and-rearranging/
+# = = = = = = = = = = = = = =
+# Accepted.
+# Thanks God, Jesus Christ!
+# = = = = = = = = = = = = = =
+# Runtime: 124 ms, faster than 100.00% of Ruby online submissions for Maximum
+# Element After Decreasing and Rearranging.
+# Memory Usage: 218.6 MB, less than 100.00% of Ruby online submissions for
+# Maximum Element After Decreasing and Rearranging.
+# @param {Integer[]} arr
+# @return {Integer}
+def maximum_element_after_decrementing_and_rearranging(arr)
+  # Based on:
+  # https://leetcode.com/problems/maximum-element-after-decreasing-and-rearranging/solution/
+  arr.sort!
+  ans = 1
+  (1...arr.size).each do |i|
+    ans += 1 if arr[i] >= ans + 1
+  end
+  return ans
+end
