6af1a09359b4ecbb5aec7cf51752a7a714ef0a20
Dmitry <lbvf50.mobile@gmail.com>
2023-11-17 00:16:20 +0700

    1747. Leetcode: 1980. Find Unique Binary String. (ruby solution) Thanks God!

--- 20231116_Thursday/20231116.rb
+++ 20231116_Thursday/20231116.rb
@@ -1,20 +1,29 @@
 # Leetcode: 1980. Find Unique Binary String.
 # https://leetcode.com/problems/find-unique-binary-string
+# = = = = = = = = = = = = = =
+# Accepted.
+# Thanks God, Jesus Christ!
+# = = = = = = = = = = = = = =
+# Runtime: 51 ms, faster than 100.00% of Ruby online submissions for Find
+# Unique Binary String.
+# Memory Usage: 211.1 MB, less than 100.00% of Ruby online submissions for
+# Find Unique Binary String.
+# 2023.11.16 Daily Challenge.
 # @param {String[]} nums
 # @return {String}
 require "set"
 def find_different_binary_string(nums)
+  # Based on:
+  # https://leetcode.com/problems/find-unique-binary-string/solution/
   ints = Set.new
-  num.each do |x|
+  nums.each do |x|
     ints.add(x.to_i(2))
   end
   n = nums.size
-  n.times do |num|
+  (n+1).times do |num|
     if ! ints.include?(num)
-      ans = num.to_s(2)[2..-1]
-      return "0" * (n - ans.size) + ans
+      return num.to_s(2).rjust(n,?0)
     end
   end
-  
-    
+  raise "This could not be reached."
 end
