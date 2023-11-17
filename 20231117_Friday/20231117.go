622c29e24ac8b14410a6e0df98fdc2fd9a75b1f9
Dmitry <lbvf50.mobile@gmail.com>
2023-11-18 00:01:29 +0700

    1748. Leetcode: 1877. Minimize Maximum Pair Sum in Array. (go solution) Thanks God!

--- 20231117_Friday/20231117.go
+++ 20231117_Friday/20231117.go
@@ -1,9 +1,33 @@
-    package main
+// Leetcode: 1877. Minimize Maximum Pair Sum in Array.
+// https://leetcode.com/problems/minimize-maximum-pair-sum-in-array
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 240 ms, faster than 56.76% of Go online submissions for Minimize
+// Maximum Pair Sum in Array.
+// Memory Usage: 8.7 MB, less than 43.24% of Go online submissions for
+// Minimize Maximum Pair Sum in Array.
+// 2023.11.17 Daily Challenge.
+package main
 
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20231117_Friday/20231117.go'")
-    }
+import (
+	"sort"
+)
+
+func minPairSum(nums []int) int {
+	sort.Ints(nums)
+	i, j := 0, len(nums)-1
+	max := nums[i] + nums[j]
+	var tmp int
+	i += 1
+	j -= 1
+	for i < j {
+		tmp = nums[i] + nums[j]
+		if tmp > max {
+			max = tmp
+		}
+		i, j = i+1, j-1
+	}
+	return max
+}
