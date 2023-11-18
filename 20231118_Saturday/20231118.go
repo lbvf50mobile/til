7adca1c6cc4c7d633cba12478a07fb3d3e33f397
Dmitry <lbvf50.mobile@gmail.com>
2023-11-18 23:49:07 +0700

    1749. Leetcode: 1838. Frequency of the Most Frequent Element. (go solution) Thanks God!

--- 20231118_Saturday/20231118.go
+++ 20231118_Saturday/20231118.go
@@ -1,9 +1,41 @@
-    package main
+// Leetcode: 1838. Frequency of the Most Frequent Element.
+// https://leetcode.com/problems/frequency-of-the-most-frequent-element/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 282 ms, faster than 5.26% of Go online submissions for Frequency
+// of the Most Frequent Element.
+// Memory Usage: 8 MB, less than 97.37% of Go online submissions for Frequency
+// of the Most Frequent Element.
+// Runtime: 184 ms, faster than 76.32% of Go online submissions for Frequency
+// of the Most Frequent Element.
+// Memory Usage: 8.2 MB, less than 92.11% of Go online submissions for
+// Frequency of the Most Frequent Element.
+// 2023.11.18 Daily Challenge.
+package main
 
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20231118_Saturday/20231118.go'")
-    }
+import (
+	"sort"
+)
+
+func maxFrequency(nums []int, k int) int {
+	// Based on:
+	// https://leetcode.com/problems/frequency-of-the-most-frequent-element/solution/
+	sort.Ints(nums)
+	ans, l, curr := 0, 0, 0
+	n := len(nums)
+	for r := 0; r < n; r += 1 {
+		target := nums[r]
+		curr += target
+		for (r-l+1)*target-curr > k {
+			curr -= nums[l]
+			l += 1
+		}
+		tmp := r - l + 1
+		if tmp > ans {
+			ans = tmp
+		}
+	}
+	return ans
+}
