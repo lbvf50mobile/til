5cfd0ce6b6b033ab43d48f88a40a1fc889be4326
Dmitry <lbvf50.mobile@gmail.com>
2023-11-19 22:14:55 +0700

    1750. Leetcode: 1887. Reduction Operations to Make the Array Elements Equal. (ruby solution) Thanks God!

---
+++ 20231119_Sunday/20231119.go
@@ -0,0 +1,9 @@
+    package main
+
+    import (
+        "fmt"
+    )
+    
+    func main() {
+        fmt.Println("alias x='go run 20231119_Sunday/20231119.go'")
+    }
---
+++ 20231119_Sunday/20231119.rb
@@ -0,0 +1,32 @@
+# Leetcode: 1887. Reduction Operations to Make the Array Elements Equal.
+# https://leetcode.com/problems/reduction-operations-to-make-the-array-elements-equal
+# = = = = = = = = = = = = = =
+# Accepted.
+# Thanks God, Jesus Christ!
+# = = = = = = = = = = = = = =
+# Runtime: 339 ms, faster than 100.00% of Ruby online submissions for
+# Reduction Operations to Make the Array Elements Equal.
+# Memory Usage: 229.2 MB, less than 100.00% of Ruby online submissions for
+# Reduction Operations to Make the Array Elements Equal.
+# 2023.11.19 Daily Challenge.
+# @param {Integer[]} nums
+# @return {Integer}
+def reduction_operations(nums)
+  ans = 0
+  prev_max_amount = 0
+  # 1. Create a Counter.
+  counter = nums.tally
+  # 2. Find Minimum in key.
+  min = counter.keys.min
+  # 3. Sort descending and Sum all non-minimum key's values.
+  # Sum in a tricky way. Every time add an ans one more time. Because on every
+  # step amount of next largest equals to it's value plus previous decireased
+  # values.
+  counter.to_a.sort_by(&:first).reverse.each do |i,v|
+    if i != min
+      prev_max_amount += v
+      ans += prev_max_amount 
+    end
+  end
+  return ans
+end
