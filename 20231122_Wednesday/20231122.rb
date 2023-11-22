991e331ec97616de72b465806a4469f7e8fa82b7
Dmitry <lbvf50.mobile@gmail.com>
2023-11-22 23:22:07 +0700

    1753. Leetcode: 1424. Diagonal Traverse II. (ruby tle) Thanks God!

--- 20231122_Wednesday/20231122.rb
+++ 20231122_Wednesday/20231122.rb
@@ -1,5 +1,11 @@
 # Leetcode: 1424. Diagonal Traverse II.
 # https://leetcode.com/problems/diagonal-traverse-ii
+# = = = = = = = = = = = = = =
+# Correct.
+# Thanks God, Jesus Christ!
+# = = = = = = = = = = = = = =
+# TLE.
+# 2023.11.22 Daily Challenge.
 # @param {Integer[][]} nums
 # @return {Integer[]}
 def find_diagonal_order(nums)
@@ -9,9 +15,17 @@ def find_diagonal_order(nums)
   i = 0
   while true
     touched = false
-    j = 0 # Always start from first column.
+    j = 0 # Start from first column.
+
     # Moving up. From current till zero.
-    (0..i).reverse_each do |ii|
+    # Optimization to jump to the existed row.
+    bt = i # Bottom.
+    if bt > (nums.size - 1)
+      diff = bt - (nums.size - 1) 
+      j += diff
+      bt = nums.size - 1
+    end
+    (0..bt).reverse_each do |ii|
       if ii < nums.size && j < nums[ii].size # Column must be in this row.
         ans.push(nums[ii][j])
         touched = true
