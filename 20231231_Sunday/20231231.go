4d28ccbaa77f07c9ad977652a6a58cf117486de8
Dmitry <lbvf50.mobile@gmail.com>
2023-12-31 16:50:38 +0700

    1792. Leetcode: 1624. Largest Substring Between Two Equal Characters. (go solution) Thanks God!

---
+++ 20231231_Sunday/20231231.go
@@ -0,0 +1,30 @@
+// Leetcode: 1624. Largest Substring Between Two Equal Characters.
+// https://leetcode.com/problems/largest-substring-between-two-equal-characters/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 1 ms, faster than 70.59% of Go online submissions for Largest
+// Substring Between Two Equal Characters.
+// Memory Usage: 2.1 MB, less than 47.06% of Go online submissions for Largest
+// Substring Between Two Equal Characters.
+// 2023.12.31 Daily Challenge.
+
+
+package main
+
+func maxLengthBetweenEqualCharacters(s string) int {
+	ans := -1
+	last := make(map[rune]int)
+	for j, c := range s {
+		if i, ok := last[c]; ok {
+			tmp := j - i - 1
+			if tmp > ans {
+				ans = tmp
+			}
+		} else {
+			last[c] = j
+		}
+	}
+	return ans
+}
---
+++ 20231231_Sunday/20231231.rb
@@ -0,0 +1,1 @@
+p "alias x='ruby 20231231_Sunday/20231231.rb'" 
