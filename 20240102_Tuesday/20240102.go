faaf0304f9fb275c2737da490473d7223b62437f
Dmitry <lbvf50.mobile@gmail.com>
2024-01-03 00:10:04 +0700

    1794. Leetcode: 2610. Convert an Array Into a 2D Array With Conditions. (go solution) Thanks God!

--- 20240102_Tuesday/20240102.go
+++ 20240102_Tuesday/20240102.go
@@ -1,23 +1,37 @@
 // Leetcode: 2610. Convert an Array Into a 2D Array With Conditions.
 // https://leetcode.com/problems/convert-an-array-into-a-2d-array-with-conditions/
-// Wrong.
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 4 ms, faster than 68.00% of Go online submissions for Convert an
+// Array Into a 2D Array With Conditions.
+// Memory Usage: 3.4 MB, less than 62.00% of Go online submissions for Convert
+// an Array Into a 2D Array With Conditions.
+// 2024.01.02 Daily Challenge.
 
 package main
 
 func findMatrix(nums []int) [][]int {
-	// Insert in answer when a digit is duplicated.
 	ans := make([][]int, 0)
-	tmp := make([]int, 0)
-	mp := make(map[int]bool)
+	counter := make(map[int]int)
 	for _, v := range nums {
-		if mp[v] {
-			ans = append(ans, tmp)
-			tmp = make([]int, 0)
-			mp = make(map[int]bool)
-		}
-		mp[v] = true
-		tmp = append(tmp, v)
+		counter[v] += 1
 	}
-	ans = append(ans,tmp)
+	did := true
+	for did {
+		did = false
+		tmp := make([]int, 0)
+		for k, v := range counter {
+			if v > 0 {
+				tmp = append(tmp, k)
+				counter[k] -= 1
+				did = true
+			}
+		}
+		if did {
+			ans = append(ans, tmp)
+		}
+	}
 	return ans
 }
