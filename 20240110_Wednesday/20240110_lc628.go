49f406040d6bd250108b83ab3148fe5df6191393
Dmitry <lbvf50.mobile@gmail.com>
2024-01-11 01:51:10 +0700

    1802. Leetcode: 2385. Amount of Time for Binary Tree to Be Infected. (lc628 next go solution) Thanks God!

--- 20240110_Wednesday/20240110_lc628.go
+++ 20240110_Wednesday/20240110_lc628.go
@@ -2,26 +2,42 @@
 // https://leetcode.com/problems/maximum-product-of-three-numbers/
 // https://leetcode.com/discuss/interview-question/4519498/some-simple-but-impt-interview-topics-for-2024
 // = = = = = = = = = = = = = =
-// Accpeted.
+// Accepted.
 // Thanks God, Jesus Christ!
 // = = = = = = = = = = = = = =
-// Runtime: 52 ms, faster than 6.10% of Go online submissions for Maximum
+// Runtime: 24 ms, faster than 96.34% of Go online submissions for Maximum
 // Product of Three Numbers.
-// Memory Usage: 7 MB, less than 8.54% of Go online submissions for Maximum
+// Memory Usage: 6.4 MB, less than 80.49% of Go online submissions for Maximum
 // Product of Three Numbers.
 // 2024.01.10.
 
 package main
 
-import "sort"
-
 func maximumProduct(nums []int) int {
-	sort.Ints(nums)
-	n := len(nums)
+	unMin := -10_000_000 // Unreachable in the tasks scope.
+	unMax := 10_000_000
+	max1, max2, max3 := unMin, unMin, unMin
+	min1, min2 := unMax, unMax
+	for _, v := range nums {
+		// Find values for maximums.
+		if v > max1 {
+			max1, max2, max3 = v, max1, max2
+		} else if v > max2 {
+			max2, max3 = v, max2
+		} else if v > max3 {
+			max3 = v
+		}
+		// Find values for minimums.
+		if v < min1 {
+			min1, min2 = v, min1
+		} else if v < min2 {
+			min2 = v
+		}
+	}
 	// In case of positive.
-	ans := nums[n-1] * nums[n-2] * nums[n-3]
+	ans := max1 * max2 * max3
 	// In case of negative values.
-	tmp := nums[0] * nums[1] * nums[n-1]
+	tmp := min1 * min2 * max1
 	if tmp > ans {
 		ans = tmp
 
