8ae0cfd974f01cd15b927d97b8b00f056dc23ed2
Dmitry <lbvf50.mobile@gmail.com>
2024-01-15 23:28:58 +0700

    1807. Leetcode: 2225. Find Players With Zero or One Losses. (gist) Thanks God!

--- 20240115_Monday/20240115.go
+++ 20240115_Monday/20240115.go
@@ -1,6 +1,6 @@
 // Leetcode: 2225. Find Players With Zero or One Losses.
 // https://leetcode.com/problems/find-players-with-zero-or-one-losses/
-// = = = = = = = = = = = = = = 
+// = = = = = = = = = = = = = =
 // Accepted.
 // Thanks God, Jesus Christ!
 // = = = = = = = = = = = = = =
