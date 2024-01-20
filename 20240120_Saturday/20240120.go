0d80eef744bc86d0f5b42cdd12f6e72641ebc8f1
Dmitry <lbvf50.mobile@gmail.com>
2024-01-20 23:57:28 +0700

    1812. Leetcode: 907. Sum of Subarray Minimums. (gist) Thanks God!

--- 20240120_Saturday/20240120.go
+++ 20240120_Saturday/20240120.go
@@ -9,6 +9,8 @@ type Pr struct {
 }
 
 func sumSubarrayMins(arr []int) int {
+	// Based on:
+	// https://gist.github.com/lbvf50mobile/66b4d8ad9d55c227e1ff8f27cca15168
 	mod := 1000000007
 	left := make([]*Pr,0)
 	leftArr := make([]int,0)
