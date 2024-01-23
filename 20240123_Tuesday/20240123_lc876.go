0160057cd74037c51edfc563ddb91a204cd92a28
Dmitry <lbvf50.mobile@gmail.com>
2024-01-24 03:14:49 +0700

    1815. Leetcode: 876. Middle of the Linked List. (go solution) Thanks God!

---
+++ 20240123_Tuesday/20240123_lc876.go
@@ -0,0 +1,32 @@
+// Leetcode: 876. Middle of the Linked List.
+// https://leetcode.com/problems/middle-of-the-linked-list/
+// = = = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = = = =
+// Runtime: 1 ms, faster than 80.46% of Go online submissions for Middle of
+// the Linked List.
+// Memory Usage: 2 MB, less than 77.87% of Go online submissions for Middle of
+// the Linked List.
+// 2024.01.23.
+
+
+package main
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func middleNode(head *ListNode) *ListNode {
+	mid := head
+	lst := head
+
+	for nil != lst && nil != lst.Next {
+		mid = mid.Next      // One step.
+		lst = lst.Next.Next // Two steps.
+	}
+	return mid
+}
