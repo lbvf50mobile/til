d39403ef0bcac1148d957da743b445b59853b4b5
Dmitry <lbvf50.mobile@gmail.com>
2024-01-31 23:46:30 +0700

    1823. Leetcode: 739. Daily Temperatures. (in action) Thanks God!

---
+++ 20240131_Wednesday/20240131.go
@@ -0,0 +1,10 @@
+// Leetcode: 739. Daily Temperatures.
+// https://leetcode.com/problems/daily-temperatures/
+
+package main
+
+
+func dailyTemperatures(temperatures []int) []int {
+	ans := make([]int, len(temperatures))
+
+}
---
+++ 20240131_Wednesday/20240131.rb
@@ -0,0 +1,1 @@
+p "alias x='ruby 20240131_Wednesday/20240131.rb'" 
