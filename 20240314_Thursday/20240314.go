725484d0c3f65201489ccdce43b25e3041bd9ee3
Dmitry <lbvf50.mobile@gmail.com>
2024-03-14 23:50:02 +0700

    1866. Leetcode: 930. Binary Subarrays With Sum. (go solution) Thanks God!

--- 20240314_Thursday/20240314.go
+++ 20240314_Thursday/20240314.go
@@ -1,23 +1,33 @@
 // Leetcode: 930. Binary Subarrays With Sum.
 // https://leetcode.com/problems/binary-subarrays-with-sum/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 33 ms, faster than 35.48% of Go online submissions for Binary
+// Subarrays With Sum.
+// Memory Usage: 7.9 MB, less than 12.90% of Go online submissions for Binary
+// Subarrays With Sum.
+// 2024.03.14 Daily Challenge.
 
 package main
 
-import "fmt"
-var p = fmt.Println
+// import "fmt"
+// var p = fmt.Println
 
 func numSubarraysWithSum(nums []int, goal int) int {
-	// curr_sum - prev_sum = goal
-	// curr_sum - goal = prav_sum
 	// Take look prev_sum
+	// Hint here.
+	// https://leetcode.com/problems/binary-subarrays-with-sum/solution/
 	h := make(map[int]int)
-	h[0] = 1
 	ans := 0
 	currSum := 0
-	for i, v := range nums {
+	for _, v := range nums {
 		currSum += v
-		prevSum := goal - currSum
-		p(i,currSum, prevSum, h[prevSum])
+		prevSum := currSum - goal // Here!
+		if goal == currSum {
+			ans += 1
+		}
 		ans += h[prevSum]
 		h[currSum] += 1
 	}
