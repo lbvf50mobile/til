d3d56355098af8882353569d96dc06d40f10d590
Dmitry <lbvf50.mobile@gmail.com>
2024-04-17 02:55:54 +0700

    1900. Leetcode:  623. Add One Row to Tree. (go in action1) Thanks God!

--- 20240416_Tuesday/20240416.go
+++ 20240416_Tuesday/20240416.go
@@ -1,6 +1,5 @@
 // Leetcode:  623. Add One Row to Tree.
 // https://leetcode.com/problems/add-one-row-to-tree/
-// In action.
 
 package main
 
@@ -13,23 +12,37 @@ package main
  * }
  */
 func addOneRow(root *TreeNode, val int, depth int) *TreeNode {
-	q1 := make([]*TreeNode, 0)
+	if 1 == depth {
+		return &TreeNode{val, root, nil}
+	}
+	totalDepth := getDepth(root, 0)
+	var q1 []*TreeNode
+	var q2 []*TreeNode
+	q1 = make([]*TreeNode, 0)
 	q1 = append(q1, root)
 	for i := 1; 0 != len(q1); i += 1 {
-		var q2 []*TreeNode
+		q2 = make([]*TreeNode, 0)
 		if i+1 == depth {
 			for _, v := range q1 {
-				if nil != v.Left {
-					tmp := &TreeNode{val, nil, nil}
-					tmp.Left = v.Left
-					v.Left = tmp
-					q2 = append(q2, tmp)
-				}
-				if nil != v.Right {
-					tmp := &TreeNode{val, nil, nil}
-					tmp.Right = v.Right
-					v.Right = tmp // <==== Here.
-					q2 = append(q2, tmp)
+				if i+1 > totalDepth {
+					tmpL := &TreeNode{val, nil, nil}
+					tmpR := &TreeNode{val, nil, nil}
+					v.Left = tmpL
+					v.Right = tmpR
+				} else {
+
+					if nil != v.Left {
+						tmp := &TreeNode{val, nil, nil}
+						tmp.Left = v.Left
+						v.Left = tmp
+						q2 = append(q2, tmp)
+					}
+					if nil != v.Right {
+						tmp := &TreeNode{val, nil, nil}
+						tmp.Right = v.Right
+						v.Right = tmp // <==== Here.
+						q2 = append(q2, tmp)
+					}
 				}
 			}
 		} else {
@@ -47,4 +60,17 @@ func addOneRow(root *TreeNode, val int, depth int) *TreeNode {
 		q2 = nil
 	}
 	return root
+}
+
+func getDepth(x *TreeNode, depth int) int {
+	if nil == x {
+		return depth
+	}
+	a := getDepth(x.Left, depth+1)
+	b := getDepth(x.Right, depth+1)
+	if a > b {
+		return a
+	} else {
+		return b
+	}
 }
