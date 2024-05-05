ef13e075e092cdb3c6b303f52eb55f901d0737ba
Dmitry <lbvf50.mobile@gmail.com>
2024-05-05 15:39:00 +0700

    1918. Leetcode: 237. Delete Node in a Linked List. (second solution) Thanks God!

--- 20240505_Sunday/20240505_0.go
+++ 20240505_Sunday/20240505_0.go
@@ -1,10 +1,10 @@
 // Leetcode: 237. Delete Node in a Linked List.
 // https://leetcode.com/problems/delete-node-in-a-linked-list/
 // = = = = = = = = = = = = = =
-// Accpeted.
+// Accepted.
 // Thanks God, Jesus Christ!
 // = = = = = = = = = = = = = =
-// Runtime: 0 ms, faster than 100.00% of Go online submissions for Delete Node
+// Runtime: 3 ms, faster than 73.24% of Go online submissions for Delete Node
 // in a Linked List.
 // Memory Usage: 2.9 MB, less than 58.80% of Go online submissions for Delete
 // Node in a Linked List.
@@ -13,6 +13,8 @@
 package main
 
 // Just copy next to current.
+// Loop till the penultimate node, copy next's value to current.
+// After the loop, copy the value, and make node's next nil.
 
 /**
  * Definition for singly-linked list.
@@ -22,6 +24,10 @@ package main
  * }
  */
 func deleteNode(node *ListNode) {
+	for nil != node.Next.Next {
+		node.Val = node.Next.Val
+		node = node.Next
+	}
 	node.Val = node.Next.Val
-	node.Next = node.Next.Next
+	node.Next = nil
 }
