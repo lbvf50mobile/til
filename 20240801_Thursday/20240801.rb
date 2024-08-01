77399fb35c4765dbaf37488d572d82ffbadc9d32
Dmitry <lbvf50.mobile@gmail.com>
2024-08-02 00:07:27 +0700

    1929. Leetcode: 2678. Number of Senior Citizens. (ruby, urls, article) Thanks God!

--- 20240801_Thursday/20240801.rb
+++ 20240801_Thursday/20240801.rb
@@ -1,1 +1,15 @@
-p "alias x='ruby 20240801_Thursday/20240801.rb'" 
+# Leetcode: 2678. Number of Senior Citizens.
+# https://leetcode.com/problems/number-of-senior-citizens/?envType=daily-question&envId=2024-08-01
+# = = = = = = = = = = = = = =
+# Accepted.
+# Thanks God, Jesus Christ!
+# = = = = = = = = = = = = = =
+# Runtime: 65 ms, faster than 50.00% of Ruby online submissions for Number of
+# Senior Citizens.
+# Memory Usage: 211.6 MB, less than 33.33% of Ruby online submissions for
+# Number of Senior Citizens.
+# @param {String[]} details
+# @return {Integer}
+def count_seniors(details)
+  details.map{|x| x[11...13].to_i}.count{|x| x > 60}
+end
