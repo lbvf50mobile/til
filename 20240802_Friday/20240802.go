5235157b51f0e6eb8298322f913bddbcb047feba
Dmitry <lbvf50.mobile@gmail.com>
2024-08-02 15:18:56 +0700

    1930. Leetcode: 2134. Minimum Swaps to Group All 1's Together II. (go solution) Thanks God!

--- 20240802_Friday/20240802.go
+++ 20240802_Friday/20240802.go
@@ -1,62 +1,67 @@
 // Leetcode: 2134. Minimum Swaps to Group All 1's Together II.
 // https://leetcode.com/problems/minimum-swaps-to-group-all-1s-together-ii/?envType=daily-question&envId=2024-08-02
-// Greetings first need to read an article perfectly.
-// A swap is defined as taking two distincn positions in an array and swpaain
-// the wallues in them.
-// hey what does it means?
-// a b c d - any two element ans swap them. Great. Simple.
-// Moving next.
-// A circular array is defiend as an array where we condder the frist elemtn
-// and the last elemnt to adjcent.  a b c d => d a b c. It is clear too.
-// Given a binary cilcular array "nums", return the minimum number of swpaps
-// requires to group all 1's present in the rray together at any locaton.
-//
-//	What does in means? I means that need to have enldes sequence of the
-//	11111.
-//	Intuition.
-//
-// 1) Need to count number of onews in the array.
-// 2) Need to count longest sequence of ones. And answer will be
-// total-longest_seq.
-// Incorrect approcah need to have sliding window.
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 72 ms, faster than 100.00% of Go online submissions for Minimum
+// Swaps to Group All 1's Together II.
+// Memory Usage: 8 MB, less than 87.50% of Go online submissions for Minimum
+// Swaps to Group All 1's Together II.
+
+// Sliding Window approach.
+// There is N postions of sliding window.
+// First poistion is calcualted automaticaly.
+// In sliding window need to calculae number of zeros. Minimum number of zeros
+// in an answer.
+// Pre action.
+// 1) calculate a number of ones - it is a size of slinding window.
+// 2) Calculate first sliding window with start at i == 0
+// 3) implent N-1 movement.
+
 package main
 
 func minSwaps(nums []int) int {
 	// 1. Count amount of ones. Total.
-	total := 0
+	// Size of the slinding window.
+	size := 0
 	for _, v := range nums {
-		total += v
+		size += v
 	}
-	// All ones, nothing to do.
-	if total == len(nums) {
-		return 0
-	}
-	// All zeros noting to do too.
-	if total == 0 {
-		return 0
-	}
-	// 2. Find longest sequence of ones.
-	currentStreak := 0
-	maxStreak := 0
-	onesLeft := total
-	i := 0
-	// 2.1 find zero to start.
-	for nums[i] != 1 && i < len(nums) {
-		i += 1
-	}
-	// Now pointer is on an 0, it is abble to start count streaks.
-	// Moving in cirlcles till meat all ones, and onesLeft will be 0.
-	for ; onesLeft > 0; i = (i + 1) % len(nums) {
-		if 0 == nums[i] {
-			currentStreak = 0
 
-		} else {
-			currentStreak += 1
-			onesLeft -= 1
-		}
-		if currentStreak > maxStreak {
-			maxStreak = currentStreak
-		}
+	// No ones nothing to do.
+	// No zeros nothing to do too.
+	if 0 == size || len(nums) == size {
+		return 0
 	}
-	return total - maxStreak
+
+	currSwaps := 0
+	minSwaps := size
+	currOnes := 0
+
+	for i := 0; i < size; i += 1 {
+		currOnes += nums[i]
+	}
+
+	currSwaps = size - currOnes
+	minSwaps = currSwaps
+
+	leavPos, nextPos := 0, size
+
+	// Calculate all other position for the sliding window.
+	for steps := 0; steps < len(nums)-1; steps += 1 {
+		// Revove leaving spot.
+		currOnes -= nums[leavPos]
+		// Add next pos.
+		currOnes += nums[nextPos]
+		currSwaps = size - currOnes
+		if minSwaps > currSwaps {
+			minSwaps = currSwaps
+		}
+
+		leavPos += 1
+		nextPos = (nextPos + 1) % len(nums)
+	}
+
+	return minSwaps
 }
