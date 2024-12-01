415c6d463c6b1ab14796a2d23218f6f8f5aca5a7
Dmitry <lbvf50.mobile@gmail.com>
2024-12-02 00:26:04 +0700

    1933. Leetcode: 1346. 1346. Check If N and Its Double Exist. (article and solution) Thanks God!

--- 20241201_Sunday/20241201.rb
+++ 20241201_Sunday/20241201.rb
@@ -1,22 +1,26 @@
-# Leetcode: 1346. 1346. Check If N and Its Double Exis
+# Leetcode: 1346. Check If N and Its Double Exist.
 # https://leetcode.com/problems/check-if-n-and-its-double-exist/?envType=daily-question&envId=2024-12-01
+# = = = = = = = = = = = = = =
+# Accetped.
+# Thanks God, Jesus Christ!
+# = = = = = = = = = = = = = =
+# Runtime: 3 ms, faster than 43.48% of Ruby online submissions for Check If N
+# and Its Double Exist.
+# Memory Usage: 212.4 MB, less than 7.14% of Ruby online submissions for Check
+# If N and Its Double Exist.
 # @param {Integer[]} arr
 # @return {Boolean}
-# fails [7,1,14,11]
 def check_if_exist(arr)
-  # Having index arr[i], need to find arr[j] == a[i]/2 where j > i.
-  # That means need to have a hath table with a value as a key with maximum j.
-  # The hash will have key and array_value, value as arr_index. In case of
-  # dublicate new hash will be saved because j will be bigger.
-  
+  # Need to read  description carefully!
+  return true if 1 < arr.count(0)
   # Prepare hash.
   hash_vals = {}
   arr.each_with_index do |v,k|
     hash_vals[v] = k
   end
-  # Seek for arr[j]*2 == arr[i] where i < j.
+  # Seek for arr[j]*2 == arr[i] where i != j. <== Here was an error.
   arr.each_with_index do |v,k|
-    return true if 0 == v%2 && hash_vals[v/2] && k < hash_vals[v/2] 
+    return true if 0 == v%2 && hash_vals[v/2] && k != hash_vals[v/2] 
   end
   # found nothing.
   return false
