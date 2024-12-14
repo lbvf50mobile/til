dc4d8184fc87aeaec8166891308a9ae226549eb0
Dmitry <lbvf50.mobile@gmail.com>
2024-12-15 04:16:26 +0700

    1945. Leetcode: 2762. Continuous Subarrays. (solution) Thanks God!

---
+++ 20241214_Saturday/20241214.go
@@ -0,0 +1,9 @@
+    package main
+
+    import (
+        "fmt"
+    )
+    
+    func main() {
+        fmt.Println("alias x='go run 20241214_Saturday/20241214.go'")
+    }
---
+++ 20241214_Saturday/20241214.rb
@@ -0,0 +1,33 @@
+# Leetcode: 2762. Continuous Subarrays
+# https://leetcode.com/problems/continuous-subarrays/
+# = = = = = = = = = = = = = =
+# Accepted.
+# Thanks God, Jesus Christ!
+# = = = = = = = = = = = = = =
+# Runtime: 1216 ms, faster than 100.00% of Ruby online submissions for
+# Continuous Subarrays.
+# Memory Usage: 218.6 MB, less than 100.00% of Ruby online submissions for
+# Continuous Subarrays.
+# 2024.12.14 Daily Challenge.
+# @param {Integer[]} nums
+# @return {Integer}
+def continuous_subarrays(nums)
+  # based on Leetcode's solution
+  f = {}
+  f.default = 0
+  l, r = 0, 0
+  c = 0
+  while r < nums.size
+    f[nums[r]] = f[nums[r]] + 1
+    while (f.max)[0] - (f.min)[0] > 2 
+      f[nums[l]] -= 1
+      if 0 == f[nums[l]]
+        f.delete(nums[l])
+      end
+      l += 1
+    end
+    c += r - l +1
+    r +=1
+  end
+  return c
+end
