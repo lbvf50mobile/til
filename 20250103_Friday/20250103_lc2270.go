bf00daea6f5cca42698a3415b5b896896b9f9835
Dmitry <lbvf50.mobile@gmail.com>
2025-01-04 05:26:07 +0700

    1957. Leetcode: 2270. Number of Ways to Split Array. (second improved solution) Thanks God!

--- 20250103_Friday/20250103_lc2270.go
+++ 20250103_Friday/20250103_lc2270.go
@@ -4,9 +4,8 @@
 // Accepted.
 // Thanks God, Jesus Christ!
 // = = = = = = = = = = = = = =
-// Runtime: 1 ms Beats 65.00%
-// Memory: 11.59 MB Beats 22.50%
-// 2025.01.03 Daily Challenge.
+// Runtime: 2 ms Beats 56.76%
+// Memory: 11.92 MB Beats 21.62%
 // https://gist.github.com/lbvf50mobile/705cf6c829dc7a8ff75bf3b887042f16
 
 package main
@@ -16,29 +15,22 @@ import (
 )
 
 func waysToSplitArray(nums []int) int {
-	ps := make([]int, len(nums)) // Prefix sum.
+	total := 0
 	ans := 0
+	n := len(nums)
 
-	// Fill preifx sum.
-	ps[0] = nums[0]
-	for i := 1; i < len(nums); i += 1 {
-		ps[i] = ps[i-1] + nums[i]
+	for _, v := range nums {
+		total += v
 	}
 
-	// Count valid splits
-	for i := 0; i < len(nums)-1; i += 1 {
-		left, right := getSums(i, ps)
+	right, left := 0, 0
+
+	for i := n - 1; i >= 1; i -= 1 {
+		right += nums[i]
+		left = total - right
 		if left >= right {
 			ans += 1
 		}
 	}
-
 	return ans
-}
-
-func getSums(i int, ps []int) (int, int) {
-	n := len(ps)
-	total := ps[n-1]
-	left, right := ps[i], total-ps[i]
-	return left, right
 }
