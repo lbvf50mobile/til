aed57cc54acf26346af07ca8089b956d22aa2a6e
Dmitry <lbvf50.mobile@gmail.com>
2025-01-16 03:38:38 +0700

    1969. Leetcode: 2429. Minimize XOR. (solution) Thanks God!

--- 20250115_Wednesday/20250115.go
+++ 20250115_Wednesday/20250115.go
@@ -1,12 +1,19 @@
 // Leetcode: 2429. Minimize XOR
 // https://leetcode.com/problems/minimize-xor/
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
+// Runtime: 0 ms Beats 100.00%
+// Memory: 4.05 MB Beats 37.50%
+// 2025.01.15 Daily Challange.
 
 package main
 
 import (
- "fmt"
+ //"fmt"
 )
-var p = fmt.Println
+//var p = fmt.Println
 
 
 func minimizeXor(num1 int, num2 int) int {
@@ -17,7 +24,7 @@ func minimizeXor(num1 int, num2 int) int {
 	if amount2 == amount1 {
 		return num1
 	}
-	p(num1Arr, "num 1 arr to XOR")
+//p(num1Arr, "num 1 arr to XOR")
 	// Fill ones for most significant bits.
 	for i := 31; i >= 0 && amount2 > 0; i -= 1 {
 		if 1 == num1Arr[i] {
@@ -32,7 +39,7 @@ func minimizeXor(num1 int, num2 int) int {
 			amount2 -= 1
 		}
 	}
-	p(xArr, "answer arr")
+//p(xArr, "answer arr")
 	return arr2Int(&xArr)
 }
 
@@ -51,8 +58,9 @@ func bin2Arr(n int, arr *[32]int) int {
 	count := 0
 	for i := 0; i < 32; i += 1 { 
 		if 1 == n%2 {
-			p(n)
+		//p(n)
 			arr[i] = 1 // <= Here was an error! I used tmp.
+			// tmp := *arr <= makes a copy of an array.
 			count += 1
 		}
 		n /= 2
