f853e75f4efd6c6e219b657d201e07a4243e0555
Dmitry <lbvf50.mobile@gmail.com>
2025-06-13 23:29:51 +0700

    1980. Lc2343. Accepted. Thanks God!

--- 20250613_Friday/20250613.go
+++ 20250613_Friday/20250613.go
@@ -1,58 +1,46 @@
 // Leetcode: 2434. Using a Robot to Print the Lexicographically Smallest String
 // https://leetcode.com/problems/using-a-robot-to-print-the-lexicographically-smallest-string/description/?envType=daily-question&envId=2025-06-06
-// Worked code, but incorect.
+// = = = = = = = = = = = = = =
+// Accepted.
+// Thanks God, Jesus Christ!
+// = = = = = = = = = = = = = =
 
 package main
+
+// import "fmt"
+
+// var p = fmt.Println
 
 const size = 26
 
 func robotWithString(s string) string {
-	// Find minumum char in the string.
-	// Put all these chars at the begining.
-	// Copy rest of the string. (But it will be reversed)
-
-	// Define variables.
 	n := len(s)
+	// First need to create a counter.
 	counter := make([]int, size)
-	ans := make([]byte, n)
-
-	// Fill the counter.
-	fillTheCounter(counter, s)
-
-	// Find the minimum char. And it's amount in the input.
-	minChar, amount := findTheMinumumChar(counter)
-
-	// Fill the answer string.
-	i := 0
-	for j := 0; j < amount; j += 1 {
-		ans[j] = minChar
-		i += 1
-	}
-
-	// Rest woul be reversed. Because first put in FILO.
-	for j := n - 1; j >= 0; j -= 1 {
-		tmp := byte(s[j])
-		if tmp != minChar {
-			ans[i] = tmp
-			i += 1
-		}
-	}
-	return string(ans)
-}
-
-func fillTheCounter(counter []int, s string) {
-	for _, v := range s {
-		index := byte(v) - 'a'
+	for _, c := range s {
+		index := int(c) - 'a'
 		counter[index] += 1
 	}
-}
 
-func findTheMinumumChar(counter []int) (byte, int) {
-	// It would be first non zero index in counter.
-	for i, v := range counter {
-		if 0 != v {
-			return byte(i) + 'a', v
+	// Now need to prepare stack and answer.
+	ans := make([]rune, 0, n)
+	stack := make([]rune, 0, n)
+	min := 'a'
+
+	for _, c := range s {
+		// p(i,string(c))
+		stack = append(stack, c)
+		counter[int(c)-'a'] -= 1 // Need to keep counter up do date.
+		// Select a new min if we have too.
+		for ('z' != min) && (counter[int(min)-'a'] <= 0) {
+			min += 1
+		}
+
+		for x := len(stack); 0 < x && min >= stack[x-1]; x = len(stack) {
+			ans = append(ans, stack[x-1])
+			stack = stack[:x-1]
 		}
 	}
-	panic("Just empty string, empty counter.")
+
+	return string(ans)
 }
