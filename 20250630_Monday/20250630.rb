e112e3ed529a091cbd994d0b8cc766a02cdfe7ea
Dmitry <lbvf50.mobile@gmail.com>
2025-07-01 14:03:56 +0700

    1983. Count number of Easy/Medium/Hard task during the June 2025.

--- 20250630_Monday/20250630.rb
+++ 20250630_Monday/20250630.rb
@@ -1,1 +1,18 @@
-p "alias x='ruby 20250630_Monday/20250630.rb'" 
+#!/usr/bin/env ruby
+
+# Just want to count amount of different types of tasks during the june 2025.
+h = {}
+h.default = 0
+STDIN.read.split("\n")
+  .select{|x| /.{1}[0-9]{2}.{1}[EMH]/ === x}
+  .each{|x| 
+    tmp = x.match(/.{1}[0-9]{2}.{1}(?<type>[EMH])/)
+    type = tmp[:type]
+     h[type] += 1 }
+
+puts "Total: #{h.values.sum}"
+puts "Easy: #{h[?E]}"
+puts "Medium: #{h[?M]}"
+puts "Hard: #{h[?H]}"
+
+
