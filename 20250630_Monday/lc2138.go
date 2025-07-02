3d6fe1d333ed43f0658a589220a5e9646d16b777
Dmitry <lbvf50.mobile@gmail.com>
2025-07-02 17:09:11 +0700

    1983. lc2138, lc2200: Accepted. Thanks God!

--- 20250630_Monday/lc2138.go
+++ 20250630_Monday/lc2138.go
@@ -1,8 +1,30 @@
 // Leetcode: 2138. Divide a String Into Groups of Size k
 // https://leetcode.com/problems/divide-a-string-into-groups-of-size-k/description/?envType=daily-question&envId=2025-06-22
+// - - -
+// Accepted.
+// Thanks God, Jesus Christ!
 
 package main
 
 func divideString(s string, k int, fill byte) []string {
-    
+	size := len(s) / k
+	if 0 != len(s)%k {
+		size += 1
+	}
+	ans := make([]string, 0, size)
+	n := len(s)
+	// Two loops: Outer one till i < n;
+	// Inner implements k steps, to fill the group.
+	for i := 0; i < n; {
+		push := ""
+		for j := 0; j < k; i, j = i+1, j+1 {
+			if i < n {
+				push += string(s[i])
+			} else {
+				push += string(fill)
+			}
+		}
+		ans = append(ans, push)
+	}
+	return ans
 }
--- 20250630_Monday/lc2200.go
+++ 20250630_Monday/lc2200.go
@@ -1,1 +1,38 @@
-// Leetcode: 2200
+// Leetcode: 2200. Find All K-Distant Indices in an Array
+// https://leetcode.com/problems/find-all-k-distant-indices-in-an-array/description/?envType=daily-question&envId=2025-06-24
+// - - -
+// Accepted.
+// Thanks God, Jesus Christ!
+
+package main
+
+func findKDistantIndices(nums []int, key int, k int) []int {
+	n := len(nums)
+	// Create an answer, that would be n as maximum.
+	ans := make([]int, 0, n)
+	// Create used to mark used indices.
+	u := make([]bool, n)
+	// Now iterate over all indicies and values.
+	for j, v := range nums {
+		// nums[j] == key
+		if v != key {
+			continue
+		}
+		// Loop to iterate over indices;
+		// |i - j| <= k
+		i := j - k
+		// To catch the fist element.
+		// To avoid stop to early.
+		if i < 0 {
+			i = 0
+		}
+		for ; i < n && i <= j+k; i += 1 {
+			if u[i] {
+				continue
+			}
+			ans = append(ans, i)
+			u[i] = true
+		}
+	}
+	return ans
+}
