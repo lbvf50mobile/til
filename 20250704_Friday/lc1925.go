07ec41b3022ff8729218cc9cfbbfde5a0804fa82
Dmitry <lbvf50.mobile@gmail.com>
2025-07-04 06:59:51 +0700

    1985. lc1925. (one_more) Accepted. Thanks God!

--- 20250704_Friday/lc1925.go
+++ 20250704_Friday/lc1925.go
@@ -8,28 +8,20 @@ package main
 
 func countTriples(n int) int {
 	counter := 0
+	sq := make(map[int]bool)
+	for i := 1; i <= n; i += 1 {
+		tmp := i * i
+		sq[tmp] = true
+	}
 	for i := 1; i <= n; i += 1 {
 		for j := 1; j <= n; j += 1 {
-			if bs(i, j, n) {
+			if 1 == i && i == j {
+				continue
+			}
+			if sq[i*i+j*j] {
 				counter += 1
 			}
 		}
 	}
 	return counter
-}
-
-func bs(i, j, n int) bool {
-	l, r := 1, n
-	for l <= r {
-		m := l + (r-l)/2
-		if m*m == i*i+j*j {
-			return true
-		}
-		if m*m < i*i+j*j {
-			l = m + 1
-		} else {
-			r = m - 1
-		}
-	}
-	return false
 }
