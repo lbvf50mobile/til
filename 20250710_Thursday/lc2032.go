a75fc687563d4be9b752ded7689a92160c05d9f4
Dmitry <lbvf50.mobile@gmail.com>
2025-07-10 12:15:03 +0700

    1987. lc2032: second_solution. Thanks God!

--- 20250710_Thursday/lc2032.go
+++ 20250710_Thursday/lc2032.go
@@ -7,33 +7,21 @@
 package main
 
 func twoOutOfThree(nums1 []int, nums2 []int, nums3 []int) []int {
-	ans := make([]int, 0, max(len(nums1), len(nums2), len(nums3)))
-	m1, m2, m3, used := getIncluded(nums1), getIncluded(nums2), getIncluded(nums3), make(map[int]bool)
-	tmp := gule(nums1, nums2, nums3)
-	for _, v := range tmp {
-		if used[v] {
-			continue
-		}
-		if (m1[v] && m2[v]) || (m2[v] && m3[v]) || (m3[v] && m1[v]) {
-			ans = append(ans, v)
-			used[v] = true
+	freq := make(map[int]int)
+	ans := make([]int, 0, 100)
+	for _, v := range nums1 {
+		freq[v] |= 1
+	}
+	for _, v := range nums2 {
+		freq[v] |= 2
+	}
+	for _, v := range nums3 {
+		freq[v] |= 4
+	}
+	for key, f := range freq {
+		if 0 != f&(f-1) {
+			ans = append(ans, key)
 		}
 	}
 	return ans
-}
-
-func gule(nums ...[]int) []int {
-	tmp := make([]int, 0, len(nums[0])+len(nums[1])+len(nums[2]))
-	tmp = append(tmp, nums[0]...)
-	tmp = append(tmp, nums[1]...)
-	tmp = append(tmp, nums[2]...)
-	return tmp
-}
-
-func getIncluded(x []int) map[int]bool {
-	f := make(map[int]bool)
-	for _, v := range x {
-		f[v] = true
-	}
-	return f
 }
