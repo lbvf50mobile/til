a13de78234b89a6f9e9220aa930d736734f3992b
Dmitry <lbvf50.mobile@gmail.com>
2025-08-16 00:10:35 +0700

    1988. lc1751: Done with DP need to pass delta. Thanks God!

--- 20250710_Thursday/lc1751_dp.go
+++ 20250710_Thursday/lc1751_dp.go
@@ -1,5 +1,8 @@
 // Leetcode: 1751. Maximum Number of Events That Can Be Attended II
 // https://leetcode.com/problems/maximum-number-of-events-that-can-be-attended-ii/description/
+// - - -
+// Accepted.
+// Thanks God, Jesus Christ!
 
 package main
 
@@ -13,6 +16,8 @@ func maxValue(events [][]int, k int) int {
 	// Solution: Push Dp on the matrix where from current postion push to the
 	// next event if atend this competition, and mark next with the same
 	// amount_to_eofroms marked.
+
+	// DP stores delta from other cells.
 
 	// Sort by the begining of an event.
 	sort.Slice(events, func(i, j int) bool { return events[i][0] < events[j][0] })
@@ -32,8 +37,6 @@ func maxValue(events [][]int, k int) int {
 	}
 
 	var doPush func(int, int, int)
-	var doMark func(int, int)
-
 	doPush = func(i int, left int, value int) {
 		if i >= n || 0 > left {
 			return
@@ -41,24 +44,20 @@ func maxValue(events [][]int, k int) int {
 		pushed[i][left] = true                // yes we can push from this cell.
 		dp[i][left] = max(value, dp[i][left]) // Define maximum push for this cell.
 	}
-	doMark = func(i int, left int) {
-		if i >= n || 0 > left {
-			return
-		}
-		pushed[i][left] = true
-	}
 	ans := 0
 	for i := 0; i < n; i += 1 {
 		for j := k - 1; j >= 0; j -= 1 {
 			// No need to use this cell nobody point to it.
-			if !pushed[i][k-1] {
+			if !pushed[i][j] {
 				continue
 			}
+			// DP is only delta if we skip current event, we skip event
+			// weight, not the delta.
 			value := dp[i][j] + events[i][2] // This is final value for this sub. prob.
 			ans = max(ans, value)
-			doMark(i+1, j) // this is jus skip
 			next := sort.Search(n, func(ii int) bool { return events[ii][0] > events[i][1] })
 			doPush(next, j-1, value)
+			doPush(i+1, j, dp[i][j]) // We do not stop the path, we exclude current contentValue!
 		}
 	}
 	return ans
