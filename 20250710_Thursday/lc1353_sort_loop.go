99342a3e01ee7ff92d60e9dc5220a19b29653a4d
Dmitry <lbvf50.mobile@gmail.com>
2025-08-17 13:38:02 +0700

    1988. lc1353: error in tests. Thanks God!

---
+++ 20250710_Thursday/lc1353_sort_loop.go
@@ -0,0 +1,42 @@
+// Leetcode: 1353. Maximum Number of Events That Can Be Attended
+// https://leetcode.com/problems/maximum-number-of-events-that-can-be-attended/description/
+
+// fails
+// [[1,2],[1,2],[3,3],[1,5],[1,5]]
+
+package main
+
+import (
+	"sort"
+)
+
+func maxEvents(events [][]int) int {
+
+	// Idea: Sort evetns, track freeday, and attend in competition as soon as
+	// possible. Iterate over sorted evetns, increae freeday depending on
+	// where is a new event bounds.
+
+	sort.Slice(events, func(i, j int) bool {
+		if events[i][0] == events[j][0] {
+			return events[i][1] < events[j][1]
+		}
+		return events[i][0] < events[j][0]
+	})
+
+	ans := 0
+	free := 1 // Free day.
+	for _, v := range events {
+		start, end := v[0], v[1]
+		if free < start {
+			ans += 1
+			free = start + 1
+		} else if start <= free && free <= end {
+			ans += 1
+			free += 1
+		} else {
+			// This case when end < free
+			//  here just do nothing.
+		}
+	}
+	return ans
+}
