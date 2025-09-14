27f6b5dff2b590f2aec045999b8e5d5aa82ea81a
Dmitry <lbvf50.mobile@gmail.com>
2025-09-15 02:12:22 +0700

    1991. lc966: go solution is accepted. Thanks God!

--- 20250914_Sunday/20250914.go
+++ 20250914_Sunday/20250914.go
@@ -1,9 +1,81 @@
-    package main
+// Leetcode: 966. Vowel Spellchecker
+// https://leetcode.com/problems/vowel-spellchecker/description/?envType=daily-question&envId=2025-09-14
+// - - -
+// Accepted.
+// Thanks God, Jesus Christ!
 
-    import (
-        "fmt"
-    )
-    
-    func main() {
-        fmt.Println("alias x='go run 20250914_Sunday/20250914.go'")
-    }
+package main
+
+import (
+	"strings"
+)
+
+func spellchecker(wordlist []string, queries []string) []string {
+	// Create three maps.
+
+	pure := createPure(wordlist)
+	nocase := createNoCase(wordlist)
+	novow := createNoVowel(wordlist)
+
+	ans := make([]string, len(queries))
+	for i, v := range queries {
+		if v, ok := pure[v]; ok {
+			ans[i] = v
+			continue
+		}
+		nc := strings.ToLower(v)
+		if v, ok := nocase[nc]; ok {
+			ans[i] = v
+			continue
+		}
+		nv := noVowel(v)
+		if v, ok := novow[nv]; ok {
+			ans[i] = v
+			continue
+		}
+		ans[i] = ""
+	}
+	return ans
+}
+
+func createPure(wl []string) map[string]string {
+	ans := make(map[string]string)
+	for _, v := range wl {
+		ans[v] = v
+	}
+	return ans
+}
+func createNoCase(wl []string) map[string]string {
+	tmp := make([]string, len(wl))
+	ans := make(map[string]string)
+	for i, v := range wl {
+		tmp[i] = strings.ToLower(v)
+	}
+	for i, v := range wl {
+		if _, ok := ans[tmp[i]]; !ok {
+			ans[tmp[i]] = v
+		}
+	}
+	return ans
+}
+func createNoVowel(wl []string) map[string]string {
+	tmp := make([]string, len(wl))
+	ans := make(map[string]string)
+	for i, v := range wl {
+		tmp[i] = noVowel(v)
+	}
+	for i, v := range wl {
+		if _, ok := ans[tmp[i]]; !ok {
+			ans[tmp[i]] = v
+		}
+	}
+	return ans
+}
+
+func noVowel(str string) string {
+	str = strings.ToLower(str)
+	for _, char := range []string{"a", "e", "i", "o", "u"} {
+		str = strings.ReplaceAll(str, char, "a")
+	}
+	return str
+}
